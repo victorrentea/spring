cde6fc3202e7d4a06d90076765c3bebcc358be0d
Victor Rentea <victorrentea@gmail.com>
2019-06-19 16:13:01 +0300

    Piscina sapata manual in CURtea ta! Nu o face !

---
+++ spring-one/src/main/java/spring/training/async/CommandSpringApp.java
@@ -0,0 +1,91 @@
+package spring.training.async;
+
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@EnableAsync
+@SpringBootApplication
+public class CommandSpringApp {
+	public static void main(String[] args) {
+		SpringApplication.run(CommandSpringApp.class, args).close(); // Note: .close to stop executors after CLRunner finishes
+	}
+
+	@Bean
+	public ThreadPoolTaskExecutor executor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(1);
+		executor.setMaxPoolSize(1);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("barman-");
+		executor.initialize();
+		executor.setWaitForTasksToCompleteOnShutdown(true);
+		return executor;
+	}
+
+}
+
+@Slf4j
+@Component
+class Drinker implements CommandLineRunner {
+	@Autowired
+	private Barman barman;
+
+
+	
+	// TODO [1] inject and use a ThreadPoolTaskExecutor.submit
+	// TODO [2] make them return a CompletableFuture + @Async + asyncExecutor bean
+	public void run(String... args) throws Exception {
+		log.debug("Submitting my order");
+
+		ExecutorService pool = Executors.newFixedThreadPool(2);
+
+		Future<Ale> futureAle = pool.submit(() -> barman.getOneAle());
+		Future<Whiskey> futureWhiskey = pool.submit(() -> barman.getOneWhiskey());
+		log.debug("A plecat fata cu comanda");
+		Ale ale = futureAle.get();
+		Whiskey whiskey = futureWhiskey.get();
+
+		pool.shutdown();
+//		Whiskey whiskey = barman.getOneWhiskey();
+		log.debug("Got my order! Thank you lad! :  " + Arrays.asList(ale, whiskey));
+	}
+}
+
+@Slf4j
+@Service
+class Barman {
+	public Ale getOneAle() {
+		 log.debug("Pouring Ale...");
+		 ThreadUtils.sleep(1000);
+		 return new Ale();
+	 }
+	
+	 public Whiskey getOneWhiskey() {
+		 log.debug("Pouring Whiskey...");
+		 ThreadUtils.sleep(1000);
+		 return new Whiskey();
+	 }
+}
+
+@Data
+class Ale {
+}
+
+@Data
+class Whiskey {
+}
---
+++ spring-one/src/main/java/spring/training/async/ThreadUtils.java
@@ -0,0 +1,13 @@
+package spring.training.async;
+
+public class ThreadUtils {
+	
+	public static void sleep(long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}		
+	}
+	
+}
