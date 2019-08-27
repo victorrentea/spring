23db23c5ece08145c722cb2042551d461b7a867c
Victor Rentea <victorrentea@gmail.com>
2019-08-27 16:17:51 +0300

    @EventListener iti permite sa reduci cuplarea intre clasele tale, pregatindu-te moral pentru COZI. DAR TOTUL E SYNC INCA!

---
+++ spring-basics/src/main/java/victor/training/spring/event/EventsApp.java
@@ -0,0 +1,71 @@
+package victor.training.spring.event;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+@SpringBootApplication
+public class EventsApp implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(EventsApp.class,args);
+    }
+
+    @Autowired
+    private OrderService orderService;
+    @Override
+    public void run(String... args) throws Exception {
+        orderService.placeOrder(new Order());
+    }
+}
+@RequiredArgsConstructor
+@Service
+@Slf4j
+class OrderService {
+//    private final InvoiceGenerator invoiceGenerator;
+    private final ApplicationEventPublisher publisher;
+    public void placeOrder(Order order) {
+        long newOrderId = 1L;
+        log.debug("Creating order " + newOrderId);
+//        invoiceGenerator.generateInvoice(newOrderId);
+        publisher.publishEvent(new OrderCreatedEvent(newOrderId));
+    }
+}
+class Order{}
+@Data
+class OrderCreatedEvent {
+    private final long orderId;
+}
+//////////////// alt pachet ////////////////
+@Service
+@Slf4j
+class InvoiceGenerator {
+    @EventListener
+    @org.springframework.core.annotation.Order(20)
+    public void handle(OrderCreatedEvent orderCreatedEvent) {
+        generateInvoice(orderCreatedEvent.getOrderId());
+    }
+    public void generateInvoice(long orderId) {
+        log.debug("Generating invoice for order " + orderId);
+    }
+}
+
+@Service
+@Slf4j
+class SendConfirmationEmail {
+    @org.springframework.core.annotation.Order(10)
+    @EventListener
+    public void handle(OrderCreatedEvent orderCreatedEvent) {
+        generateInvoice(orderCreatedEvent.getOrderId());
+    }
+    public void generateInvoice(long orderId) {
+        log.debug("Sending Confirmation email for order " + orderId);
+    }
+}
