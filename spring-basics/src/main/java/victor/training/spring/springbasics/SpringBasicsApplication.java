792da45271154f128ac6919576c1c65aa00506d0
Victor Rentea <victorrentea@gmail.com>
2019-08-27 09:58:47 +0300

    initial from initializer

---
+++ spring-basics/src/main/java/victor/training/spring/springbasics/SpringBasicsApplication.java
@@ -0,0 +1,13 @@
+package victor.training.spring.springbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicsApplication.class, args);
+	}
+
+}
