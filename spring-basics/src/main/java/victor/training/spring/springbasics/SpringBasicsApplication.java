ce9cf46c47e367400f529c64fdac1434bb983f09
Victor Rentea <victorrentea@gmail.com>
2019-08-27 10:09:58 +0300

    Constructor Injection (fara nici un @AUtowired)

--- spring-basics/src/main/java/victor/training/spring/springbasics/SpringBasicsApplication.java
+++ spring-basics/src/main/java/victor/training/spring/springbasics/SpringBasicsApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class SpringBasicsApplication {
@@ -15,9 +16,25 @@ public class SpringBasicsApplication {
 
 @Component // @Service @Repository @Controller @RestController @MessageEndpoint
 class AutoRun implements CommandLineRunner {
+	private final A a;
+	private final B b;
+
+	public AutoRun(A a, B b) {
+		this.a = a;
+		this.b = b;
+	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello Spring!");
+		System.out.println("Hello " + a.m());
+
+	}
+}
+@Service
+class B{}
+@Service
+class A {
+	public String m() {
+		return "Spring";
 	}
 }
