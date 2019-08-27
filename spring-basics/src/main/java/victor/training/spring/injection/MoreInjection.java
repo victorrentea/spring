ab3c5c9bd6d14ff4a55a13c6e84d68991d181885
Victor Rentea <victorrentea@gmail.com>
2019-08-27 11:47:56 +0300

    @Bean ca un fel de factory: creezi de mana instante de obiecte de care apoi Spring are grijE

--- spring-basics/src/main/java/victor/training/spring/injection/MoreInjection.java
+++ spring-basics/src/main/java/victor/training/spring/injection/MoreInjection.java
@@ -1,9 +1,12 @@
 package victor.training.spring.injection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class MoreInjection implements CommandLineRunner {
@@ -15,9 +18,39 @@ public class MoreInjection implements CommandLineRunner {
 	@Autowired
 	//-Dspring.profiles.active=masina.mea
 	private ALogin a;
+	@Qualifier("spaga")
+	@Autowired
+	private WebServiceClient spagaMea;
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println(a);
+		System.out.println("ws:  " + spagaMea);
+	}
+
+	@Bean
+	public WebServiceClient ldap() {
+		return new WebServiceClient("ldap bla bla");
+	}
+
+	@Bean
+	public WebServiceClient spaga() {
+		return new WebServiceClient("spaga bla bla");
+	}
+}
+
+// ldap spaga
+class WebServiceClient {
+	private final String endpoint;
+
+	public WebServiceClient(String endpoint) {
+		this.endpoint = endpoint;
+	}
+
+	@Override
+	public String toString() {
+		return "WebServiceClient{" +
+				"endpoint='" + endpoint + '\'' +
+				'}';
 	}
 }
 
