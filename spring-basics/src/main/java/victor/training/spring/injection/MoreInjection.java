f392c782e6f9fab2c55177f288908d79ff1f36c9
Victor Rentea <victorrentea@gmail.com>
2019-08-27 12:13:11 +0300

    @Value

--- spring-basics/src/main/java/victor/training/spring/injection/MoreInjection.java
+++ spring-basics/src/main/java/victor/training/spring/injection/MoreInjection.java
@@ -7,7 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 //@Configuration // doar intr-o astfel de clasa @Bean iti da fiori: ti-o suprascrie Spring automat pentru a face lifecycle hooks+injection+aspects
@@ -23,23 +25,30 @@ public class MoreInjection implements CommandLineRunner {
 	@Qualifier("spaga")
 	@Autowired
 	private WebServiceClient spagaMea;
+
+	@Value("${x}")
+	List<String> list;
+//	@Value("${mapa}")
+//	Map<String, Integer> map;
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println(a);
 		System.out.println("ws:  " + spagaMea);
+		System.out.println("Lista: "+ list);
+//		System.out.println("Lista: "+ map);
 	}
 
 	@Bean
-	public WebServiceClient ldap() {
+	public WebServiceClient ldap(@Value("${secret:def}") String secret) {
 		System.out.println("creez ldap : oare cine e this ?" + this);
 //		this.securityProvider();
-		return new WebServiceClient("ldap bla bla", securityProvider());
+		return new WebServiceClient("ldap bla bla", securityProvider(), secret);
 	}
 
 	@Bean
-	public WebServiceClient spaga() {
+	public WebServiceClient spaga(@Value("${secret:def}") String secret) {
 		System.out.println("creez spaga");
-		return new WebServiceClient("spaga bla bla", securityProvider());
+		return new WebServiceClient("spaga bla bla", securityProvider(),secret);
 	}
 	@Bean
 	public SecurityProvider securityProvider() {
@@ -58,16 +67,14 @@ public class MoreInjection implements CommandLineRunner {
 
 // ldap spaga
 class WebServiceClient {
-
 	private final String endpoint;
-	private SecurityProvider securityProvider;
+	private final SecurityProvider securityProvider;
+	private final String secret;
 
-	@Value("${secret}")
-	private String secret;
-
-	public WebServiceClient(String endpoint, SecurityProvider securityProvider) {
+	public WebServiceClient(String endpoint, SecurityProvider securityProvider, String secret) {
 		this.endpoint = endpoint;
 		this.securityProvider = securityProvider;
+		this.secret = secret;
 	}
 
 	@Override
