56fcef4c50db87d8d4dbea15383abfc653a74ecf
Victor Rentea <victorrentea@gmail.com>
2019-08-27 12:58:50 +0300

    Sinleton lifecycle play revert

--- spring-one/src/main/java/spring/training/life/SingletonSpringApp.java
+++ spring-one/src/main/java/spring/training/life/SingletonSpringApp.java
@@ -3,6 +3,7 @@ package spring.training.life;
 import java.util.Locale;
 import java.util.Map;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,13 +18,15 @@ import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.context.support.SimpleThreadScope;
 import org.springframework.stereotype.Service;
 
-@EnableCaching
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class SingletonSpringApp implements CommandLineRunner{
 	@Bean
 	public static CustomScopeConfigurer defineThreadScope() {
 		CustomScopeConfigurer configurer = new CustomScopeConfigurer();
-		configurer.addScope("thread", new SimpleThreadScope()); // WARNING: Leaks memory. Prefer 'request' scope or read here: https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/context/support/SimpleThreadScope.html
+		// WARNING: Leaks memory. Prefer 'request' scope or read here: https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/context/support/SimpleThreadScope.html
+		configurer.addScope("thread", new SimpleThreadScope());
 		return configurer;
 	}
 	
@@ -40,86 +43,28 @@ public class SingletonSpringApp implements CommandLineRunner{
 	// TODO [4] thread/request scope. HOW it works?! Leaks: @see SimpleThreadScope javadoc
 	// TODO [5] (after AOP): RequestContext, @Cacheable. on thread?! @ThreadLocal
 	public void run(String... args) throws Exception {
-		new Thread(() -> exporter.export(Locale.ENGLISH)).start();
-		new Thread(() -> exporter.export(Locale.FRENCH)).start();
+		exporter.export(Locale.ENGLISH);
+		// TODO exporter.export(Locale.FRENCH);
 		
 	}
-	
-	@Autowired
-	private Singleton1 s1;
-	@Autowired
-	private Singleton1 s1Prim;
 }
-
-
-// TODO FIXME XXX joacate aici !!
-
-
-@Service
-class Singleton1 {
-	@Autowired
-	private Prototype1 p1;
-	
-	@Autowired
-	private Prototype1 p1Prim;
-	
-	public Singleton1() {
-		System.out.println("+Singleton1");
-	}
-}
-
-@Service
-@Scope("prototype")
-class Prototype1 {
-	
-	@Autowired 
-	private Prototype2 p2;
-	@Autowired 
-	private Prototype2 p2Prim;
-	@Autowired
-	private XX x;
-	public Prototype1() {
-		System.out.println("+Prototype1");
-	}
-}
-@Service
-@Scope("prototype")
-class Prototype2 {
-	public Prototype2() {
-		System.out.println("+Prototype2");
-	}
-}
-@Service
-class XX {
-	public XX() {
-		System.out.println("+XX");
-	}
-	
-}
-
-
-
+@Slf4j
 @Service
 class OrderExporter  {
-	private final static Logger log = LoggerFactory.getLogger(OrderExporter.class);
 	@Autowired
 	private InvoiceExporter invoiceExporter;
 	@Autowired
 	private LabelService labelService;
 
 	public void export(Locale locale) {
-		System.out.println("hash-ul instantei "  +labelService.hashCode());
-		System.out.println("Oare cu cine vorbesc??! " + labelService.getClass());
-		labelService.load(locale);
 		log.debug("Running export in " + locale);
 		log.debug("Origin Country: " + labelService.getCountryName("rO")); 
 		invoiceExporter.exportInvoice();
 	}
 }
-
+@Slf4j
 @Service 
 class InvoiceExporter {
-	private final static Logger log = LoggerFactory.getLogger(InvoiceExporter.class);
 	@Autowired
 	private LabelService labelService;
 	
@@ -128,26 +73,26 @@ class InvoiceExporter {
 	}
 }
 
+@Slf4j
 @Service
-@Scope(value = "thread", proxyMode = ScopedProxyMode.TARGET_CLASS)
 class LabelService {
-	private final static Logger log = LoggerFactory.getLogger(LabelService.class);
 	private CountryRepo countryRepo;
 	
 	public LabelService(CountryRepo countryRepo) {
-		System.out.println("+1 Label Service");
+		System.out.println("+1 Label Service: " + this.hashCode());
 		this.countryRepo = countryRepo;
 	}
 
 	private Map<String, String> countryNames;
-	
-	public void load(Locale locale) {
-		log.debug("Incarc pe instanta de LabelService: " + this.hashCode());
-		countryNames = countryRepo.loadCountryNamesAsMap(locale);
+
+	@PostConstruct
+	public void load() {
+		log.debug("LabelService.load() on: " + this.hashCode());
+		countryNames = countryRepo.loadCountryNamesAsMap(Locale.ENGLISH);
 	}
 	
 	public String getCountryName(String iso2Code) {
-		log.debug("getCountryName() on instance: " + this.hashCode());
+		log.debug("LabelService.getCountryName() on: " + this.hashCode());
 		return countryNames.get(iso2Code.toUpperCase());
 	}
 }
