b957d8517a3d0bb08a69a07f334e3a002d918e1e
Victor Rentea <victorrentea@gmail.com>
2019-08-27 21:17:25 +0300

    lifecycle

--- spring-one/src/main/java/spring/training/life/LifeApp.java
+++ spring-one/src/main/java/spring/training/life/LifeApp.java
@@ -4,24 +4,19 @@ import java.util.Locale;
 import java.util.Map;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.context.support.SimpleThreadScope;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
-public class SingletonSpringApp implements CommandLineRunner{
+public class LifeApp implements CommandLineRunner{
 	@Bean
 	public static CustomScopeConfigurer defineThreadScope() {
 		CustomScopeConfigurer configurer = new CustomScopeConfigurer();
@@ -31,7 +26,7 @@ public class SingletonSpringApp implements CommandLineRunner{
 	}
 	
 	public static void main(String[] args) {
-		SpringApplication.run(SingletonSpringApp.class);
+		SpringApplication.run(LifeApp.class);
 	}
 	
 	@Autowired 
@@ -41,8 +36,8 @@ public class SingletonSpringApp implements CommandLineRunner{
 	// TODO [2] instantiate manually, set dependencies, pass around; no AOP
 	// TODO [3] prototype scope + ObjectFactory or @Lookup. Did you said "Factory"? ...
 	// TODO [4] thread/request scope. HOW it works?! Leaks: @see SimpleThreadScope javadoc
-	// TODO [5] (after AOP): RequestContext, @Cacheable. on thread?! @ThreadLocal
-	public void run(String... args) throws Exception {
+	// TODO [5] (after AOP): RequestContext, @Cacheable on thread?! @ThreadLocal
+	public void run(String... args) {
 		exporter.export(Locale.ENGLISH);
 		// TODO exporter.export(Locale.FRENCH);
 		
