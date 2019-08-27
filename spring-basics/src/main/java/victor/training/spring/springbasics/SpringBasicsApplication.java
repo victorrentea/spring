af947e19505a64f9aa4a7b5e090d4af9df24a1a0
Victor Rentea <victorrentea@gmail.com>
2019-08-27 10:05:27 +0300

    CommandLineRunner ne permita sa rulam cod 'gen main' intr-o componenta Spring

--- spring-basics/src/main/java/victor/training/spring/springbasics/SpringBasicsApplication.java
+++ spring-basics/src/main/java/victor/training/spring/springbasics/SpringBasicsApplication.java
@@ -1,7 +1,9 @@
 package victor.training.spring.springbasics;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SpringBasicsApplication {
@@ -9,5 +11,13 @@ public class SpringBasicsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBasicsApplication.class, args);
 	}
+}
 
+@Component // @Service @Repository @Controller @RestController @MessageEndpoint
+class AutoRun implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello Spring!");
+	}
 }
