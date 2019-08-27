4a7ad27f4388fcd64c9979de733fd111851e4fa3
Victor Rentea <victorrentea@gmail.com>
2019-08-27 14:53:32 +0300

    SIngletong + lifecycle solved

--- spring-basics/src/main/java/victor/training/spring/life/SingletonSpringApp.java
+++ spring-basics/src/main/java/victor/training/spring/life/SingletonSpringApp.java
@@ -3,8 +3,6 @@ package victor.training.spring.life;
 import java.util.Locale;
 import java.util.Map;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.boot.CommandLineRunner;
@@ -12,40 +10,41 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.context.support.SimpleThreadScope;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j // if this doesn't compile:
-// IntelliJ: Settings>Plugins> install "Lombok" plugin + Restart
-// Eclipse: navigate to m2 repo/org/projectlombok/lombok/<latest> => run the .jar as java application (double click) -> "Lombok your Eclipse installation"
-
+@Slf4j
 @EnableCaching
 @SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class SingletonSpringApp implements CommandLineRunner{
 	@Bean
 	public static CustomScopeConfigurer defineThreadScope() {
 		CustomScopeConfigurer configurer = new CustomScopeConfigurer();
-		configurer.addScope("thread", new SimpleThreadScope()); // WARNING: Leaks memory. Prefer 'request' scope or read here: https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/context/support/SimpleThreadScope.html 
+		configurer.addScope("thread", new SimpleThreadScope()); // WARNING: Leaks memory. Prefer 'request' scope or read here: https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/context/support/SimpleThreadScope.html
 		return configurer;
 	}
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(SingletonSpringApp.class);
 	}
-	
-	@Autowired 
+
+	@Autowired
 	private OrderExporter exporter;
-	
+
 	// TODO [1] make singleton; test multi-thread: state is [ | | | ]
 	// TODO [2] instantiate manually, set dependencies, pass around; no AOP
 	// TODO [3] prototype scope + ObjectFactory or @Lookup. Did you said "Factory"? ...
 	// TODO [4] thread/request scope. HOW it works?! Leaks: @see SimpleThreadScope javadoc
 	// TODO [5] (after AOP): RequestContext, @Cacheable. on thread?! @ThreadLocal
 	public void run(String... args) throws Exception {
-		exporter.export(Locale.ENGLISH);
-//		exporter.export(Locale.FRENCH);
+		new Thread(() ->exporter.export(Locale.ENGLISH)).start();
+		new Thread(() ->exporter.export(Locale.FRENCH)).start();
 	}
 }
 
@@ -58,18 +57,19 @@ class OrderExporter  {
 	private LabelService labelService;
 
 	public void export(Locale locale) {
+		log.debug("Oare cu cine vorbesc ? " + labelService.getClass());
+		labelService.load(locale);
 		log.debug("Running export in " + locale);
-		log.debug("Origin Country: " + labelService.getCountryName("rO")); 
+		log.debug("Origin Country: " + labelService.getCountryName("rO"));
 		invoiceExporter.exportInvoice();
 	}
 }
-
 @Slf4j
-@Service 
+@Service
 class InvoiceExporter {
 	@Autowired
 	private LabelService labelService;
-	
+
 	public void exportInvoice() {
 		log.debug("Invoice Country: " + labelService.getCountryName("ES"));
 	}
@@ -77,22 +77,26 @@ class InvoiceExporter {
 
 @Slf4j
 @Service
+@Scope(scopeName = "thread", proxyMode = ScopedProxyMode.TARGET_CLASS)
 class LabelService {
 	@Autowired
 	private CountryRepo countryRepo;
-	
-	public LabelService(CountryRepo countryRepo) {
+
+	public void setCountryRepo(CountryRepo countryRepo) {
 		this.countryRepo = countryRepo;
 	}
 
+	//	public LabelService(CountryRepo countryRepo) {
+//		this.countryRepo = countryRepo;
+//	}
+
 	private Map<String, String> countryNames;
-	
-	@PostConstruct
-	public void load() {
+
+	public void load(Locale locale) {
 		log.debug("load() in instance: " + this.hashCode());
-		countryNames = countryRepo.loadCountryNamesAsMap(Locale.ENGLISH);
+		countryNames = countryRepo.loadCountryNamesAsMap(locale);
 	}
-	
+
 	public String getCountryName(String iso2Code) {
 		log.debug("getCountryName() in instance: " + this.hashCode());
 		return countryNames.get(iso2Code.toUpperCase());
