d245f0f1dd4b55e5b43689a6fb696e89ac79ae5a
Victor Rentea <victorrentea@gmail.com>
2019-09-04 12:32:33 +0300

    configuratii structurate mapate automat dupa . . . . obiecte.

--- spring-one/src/main/java/spring/training/props/ClassWithLoadsOfProps.java
+++ spring-one/src/main/java/spring/training/props/ClassWithLoadsOfProps.java
@@ -2,8 +2,10 @@ package spring.training.props;
 
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -14,16 +16,30 @@ import java.util.Map;
 @Component
 @ToString
 public class ClassWithLoadsOfProps {
-    @Value("${structured.a}")
-    private String a;
-    @Value("${structured.list}")
-    private List<String> list;
-    @Value("#{${structured.map}}")
-    private Map<String,String> map;
+    @Autowired
+    StructuredConfig config;
 
     @PostConstruct
     public void printAll() {
-        System.out.println(this);
+        System.out.println(config);
     }
 
 }
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "structured")
+class StructuredConfig {
+    private String a;
+    private B b;
+    private List<String> list;
+    private Map<String,String> map;
+
+    @Data
+    static class B {
+        private String bb;
+        private String ba;
+    }
+}
+
+
