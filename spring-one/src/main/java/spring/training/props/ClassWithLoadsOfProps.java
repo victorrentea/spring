9b86a8d8d749d4d9cb48826aed32fdb62a32b1fb
Victor Rentea <victorrentea@gmail.com>
2019-09-04 12:28:01 +0300

    injectie de mape, liste si alte bunataturi cu @Value

--- spring-one/src/main/java/spring/training/props/ClassWithLoadsOfProps.java
+++ spring-one/src/main/java/spring/training/props/ClassWithLoadsOfProps.java
@@ -1,16 +1,29 @@
 package spring.training.props;
 
 import lombok.Data;
+import lombok.ToString;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Map;
 
-public class StructuredProps {
+
+@Component
+@ToString
+public class ClassWithLoadsOfProps {
+    @Value("${structured.a}")
     private String a;
+    @Value("${structured.list}")
     private List<String> list;
+    @Value("#{${structured.map}}")
     private Map<String,String> map;
+
+    @PostConstruct
+    public void printAll() {
+        System.out.println(this);
+    }
 
 }
