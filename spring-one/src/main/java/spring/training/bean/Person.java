22ce02be1e52acbc48cf1c219c40fe9547ae1ce6
Victor Rentea <victorrentea@gmail.com>
2019-09-04 11:44:19 +0300

    in @PostConstruct poti face chestii cu ce ti-a fost dat cu @Autowired

--- spring-one/src/main/java/spring/training/bean/Person.java
+++ spring-one/src/main/java/spring/training/bean/Person.java
@@ -1,22 +1,32 @@
 package spring.training.bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 
 public class Person {
     private final String name;
+    @Autowired
+    private Maternitate maternitate;
+
 
     public Person(String name) {
         this.name = name;
+        // this.maternitate e null aici
     }
 
     @PostConstruct
     public void init() {
         System.out.println("new Person " + name);
+        // this.maternitate e INJECTAT deja
     }
 
     public String sayHello() {
         return "Hello! Here is " + name + " from " + OldClass.getInstance().getCurrentCountry();
     }
+}
+@Service
+class Maternitate {
+
 }
