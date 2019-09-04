a1f8e5f2ad22fa26a41ed4301a0f91ef807e769a
Victor Rentea <victorrentea@gmail.com>
2019-09-04 16:57:05 +0300

    @Async care alege ce executor ruleaza (dupa bean name)

--- spring-one/src/main/java/spring/training/async/AsyncApp.java
+++ spring-one/src/main/java/spring/training/async/AsyncApp.java
@@ -24,61 +24,79 @@ import static java.util.concurrent.CompletableFuture.completedFuture;
 @EnableAsync
 @SpringBootApplication
 public class AsyncApp {
-	public static void main(String[] args) {
-		SpringApplication.run(AsyncApp.class, args).close(); // Note: .close to stop executors after CLRunner finishes
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncApp.class, args).close(); // Note: .close to stop executors after CLRunner finishes
+    }
 
-	@Bean
-	public ThreadPoolTaskExecutor executor(@Value("${barman.thread.count}") int threadCount) {
-		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(threadCount);
-		executor.setMaxPoolSize(threadCount);
-		executor.setQueueCapacity(500);
-		executor.setThreadNamePrefix("barman-");
-		executor.initialize();
-		executor.setWaitForTasksToCompleteOnShutdown(true);
-		return executor;
-	}
+    @Bean
+    public ThreadPoolTaskExecutor executor(@Value("${barman.thread.count}") int threadCount) {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(threadCount);
+        executor.setMaxPoolSize(threadCount);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("bar-");
+        executor.initialize();
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        return executor;
+    }
+    @Bean
+    public ThreadPoolTaskExecutor customExecutor(@Value("${poli.thread.count:100}") int threadCount) {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(threadCount);
+        executor.setMaxPoolSize(threadCount);
+        executor.setQueueCapacity(500);
+        //pub.ro
+        executor.setThreadNamePrefix("poli-");
+        executor.initialize();
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        return executor;
+    }
 
 }
 
 @Slf4j
 @Component
 class Drinker implements CommandLineRunner {
-	@Autowired
-	private Barman barman;
+    @Autowired
+    private Barman barman;
 
-	// TODO [1] inject and use a ThreadPoolTaskExecutor.submit
-	// TODO [2] make them return a CompletableFuture + @Async + asyncExecutor bean
-	public void run(String... args) throws Exception {
-		Thread.sleep(3000);
-		log.debug("Submitting my order");
+    // TODO [1] inject and use a ThreadPoolTaskExecutor.submit
+    // TODO [2] make them return a CompletableFuture + @Async + asyncExecutor bean
+    public void run(String... args) throws Exception {
+        Thread.sleep(3000);
+        log.debug("Submitting my order");
 
-		Future<Ale> futureAle = barman.getOneAle();
-		Future<Whiskey> futureWhiskey = barman.getOneWhiskey();
-		log.debug("A plecat fata cu comenzile (2)");
-		Ale ale = futureAle.get();
-		Whiskey whiskey = futureWhiskey.get();
-		log.debug("Got my order! Thank you lad! " + Arrays.asList(ale, whiskey));
-	}
+        Future<Ale> futureAle = barman.getOneAle();
+        Future<Ale> futureAle2 = barman.getOneAle();
+        Future<Ale> futureAle3 = barman.getOneAle();
+        Future<Ale> futureAle4 = barman.getOneAle();
+        Future<Ale> futureAle5 = barman.getOneAle();
+        Future<Ale> futureAle6 = barman.getOneAle();
+        Future<Ale> futureAle7 = barman.getOneAle();
+        Future<Whiskey> futureWhiskey = barman.getOneWhiskey();
+        log.debug("A plecat fata cu comenzile (2)");
+        Ale ale = futureAle.get();
+        Whiskey whiskey = futureWhiskey.get();
+        log.debug("Got my order! Thank you lad! " + Arrays.asList(ale, whiskey));
+    }
 }
 
 @Slf4j
 @Service
 class Barman {
-	 @Async
-	public Future<Ale> getOneAle() {
-		 log.debug("Pouring Ale...");
-		 ThreadUtils.sleep(1000);
-		 return completedFuture(new Ale());
-	 }
+    @Async("customExecutor") // pe ce executor vreau sa ruleze.
+    public Future<Ale> getOneAle() {
+        log.debug("Pouring Ale...");
+        ThreadUtils.sleep(1000);
+        return completedFuture(new Ale());
+    }
 
-	 @Async
-	 public Future<Whiskey> getOneWhiskey() {
-		 log.debug("Pouring Whiskey...");
-		 ThreadUtils.sleep(1000);
-		 return completedFuture(new Whiskey());
-	 }
+    @Async
+    public Future<Whiskey> getOneWhiskey() {
+        log.debug("Pouring Whiskey...");
+        ThreadUtils.sleep(1000);
+        return completedFuture(new Whiskey());
+    }
 }
 
 @Data
