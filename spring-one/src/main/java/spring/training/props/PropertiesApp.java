08ca10313c235e08daa08cea30d081b9a60414d1
Victor Rentea <victorrentea@gmail.com>
2019-09-04 12:22:11 +0300

    @Value ia din fisier sau din -D jvm options. Plus alte 18 locuri: https://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-external-config.html

--- spring-one/src/main/java/spring/training/props/PropertiesApp.java
+++ spring-one/src/main/java/spring/training/props/PropertiesApp.java
@@ -1,12 +1,14 @@
 package spring.training.props;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Locale;
 
@@ -16,24 +18,32 @@ public class PropertiesApp implements CommandLineRunner {
         SpringApplication.run(PropertiesApp.class);
     }
 
+    @Value("${secret.mare}")
+    private String secret;
     @Bean
     public ManuallyConfigurable manuallyConfigurable() {
-        return new ManuallyConfigurable("12345678");
+        return new ManuallyConfigurable(secret);
     }
 
     @Override
     public void run(String... args) {
+        System.out.println("Log.info pass= " + secret);
         System.out.println("Structured Props: ");
     }
+}
+@Service
+class A {
+    @Autowired
+    ManuallyConfigurable configurable;
 }
 
 
 // "Can't touch this!"
 class ManuallyConfigurable {
-    private final String secret;
-    public ManuallyConfigurable(String secret) {
-        this.secret = secret;
-        System.out.println("Using secret: " + secret);
+    private final String secretPwd;
+    public ManuallyConfigurable(String secretPwd) {
+        this.secretPwd = secretPwd;
+        System.out.println("Using secret: " + secretPwd);
     }
     public void logic() {
         // using secret
