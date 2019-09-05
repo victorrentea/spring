6b77383ca0881f2cb4b95240eacf697b614d23d9
Victor Rentea <victorrentea@gmail.com>
2019-09-06 01:24:12 +0300

    Scheduled Stub

---
+++ spring-one/src/main/java/spring/training/scheduled/ScheduledApp.java
@@ -0,0 +1,24 @@
+package spring.training.scheduled;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class ScheduledApp implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduledApp.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Thread.sleep(5000);
+    }
+
+    // TODO should run every 5 seconds / configurable / cron
+    public void lookIntoFolder() {
+        log.debug("Looking into folder");
+    }
+}
