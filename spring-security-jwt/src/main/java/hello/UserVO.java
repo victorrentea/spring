569725fdabe3afbf073944f2d22f91f96c508e0c
Victor Rentea <victorrentea@gmail.com>
2020-01-27 21:24:39 +0200

    Add security - JWT

---
+++ spring-security-jwt/src/main/java/hello/UserVO.java
@@ -0,0 +1,40 @@
+package hello;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@Data
+public class UserVO implements UserDetails {
+    private final String username, fullName, role;
+    private boolean enabled = true;
+
+    @Override
+    public Collection<GrantedAuthority> getAuthorities() {
+        return Arrays.asList(() -> role);
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+}
---
+++ spring-security-jwt/src/main/java/hello/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                    .authenticationProvider(preAuthenticatedProvider())
+                    .addFilterBefore(jwtFilter(), BasicAuthenticationFilter.class)
+                    .logout().permitAll();
+
+    }
+
+    @Bean
+    public AuthenticationProvider preAuthenticatedProvider() {
+        PreAuthenticatedAuthenticationProvider provider = new PreAuthenticatedAuthenticationProvider();
+        provider.setPreAuthenticatedUserDetailsService(preauthUserDetailsService());
+        return provider;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public JwtAuthorizationHeaderFilter jwtFilter() throws Exception {
+        return new JwtAuthorizationHeaderFilter(authenticationManagerBean());
+    }
+
+    @Bean
+    protected DatabaseUserDetailsService preauthUserDetailsService() {
+        return new DatabaseUserDetailsService();
+    }
+
+}
