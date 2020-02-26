763a304411134427b75e4d58de54ff00652ce698
Victor Rentea <victorrentea@gmail.com>
2020-02-26 11:16:44 +0200

    Instantiez de mana obiectele care cara in ele STARE. Ori de cate ori o clasa are nevoie sa tina campuri cu date, NU poate fi singleton in Spring. O idee: NEW. Dar Spring ma loveste cand trebuie sa ii ofer EU dependintele acelei clase pe care o fac NEW.

--- spring-demo/src/main/java/victor/training/springdemo/life/SingletonSpringApp.java
+++ spring-demo/src/main/java/victor/training/springdemo/life/SingletonSpringApp.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.SimpleThreadScope;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.util.Locale;
 import java.util.Map;
 
@@ -53,29 +52,28 @@ public class SingletonSpringApp implements CommandLineRunner {
 @RequiredArgsConstructor
 class OrderExporter  {
 	private final InvoiceExporter invoiceExporter;
-	private final LabelService labelService;
+	private final CountryRepo countryRepo;
 
 	public void export(Locale locale) {
+		LabelService labelService = new LabelService(countryRepo);
 		labelService.load(locale);
 		log.debug("Running export in " + locale);
 		log.debug("Origin Country: " + labelService.getCountryName("rO")); 
-		invoiceExporter.exportInvoice();
+		invoiceExporter.exportInvoice(labelService);
 	}
 }
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
 class InvoiceExporter {
-	private final LabelService labelService;
+//	private final LabelService labelService;
 
-	public void exportInvoice() {
+	public void exportInvoice(LabelService labelService) {
 		log.debug("Invoice Country: " + labelService.getCountryName("ES"));
 	}
 }
 
 @Slf4j
-@Service
 class LabelService {
 	private CountryRepo countryRepo;
 	public LabelService(CountryRepo countryRepo) {
