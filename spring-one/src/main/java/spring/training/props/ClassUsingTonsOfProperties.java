69f007879593584b5c67de0769bcb80de461f847
Victor Rentea <victorrentea@gmail.com>
2020-02-27 11:29:05 +0200

    Configuration properties

--- spring-one/src/main/java/spring/training/props/ClassUsingTonsOfProperties.java
+++ spring-one/src/main/java/spring/training/props/ClassUsingTonsOfProperties.java
@@ -1,16 +1,33 @@
 package spring.training.props;
 
 import lombok.Data;
+import lombok.ToString;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Map;
 
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "structured")
 public class ClassUsingTonsOfProperties {
     private String a;
     private List<String> list;
     private Map<String,String> map;
 
+    private B b;
+
+    @PostConstruct
+    public void show() {
+        System.out.println("Props: " + this);
+    }
+}
+@Data
+class B {
+    private String ba;
+    private String bb;
 }
--- spring-one/src/main/java/spring/training/props/PropertiesApp.java
+++ spring-one/src/main/java/spring/training/props/PropertiesApp.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 import java.util.Locale;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class PropertiesApp implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(PropertiesApp.class);
