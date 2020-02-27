5fc8138ef58248a36d9709adb6f9532ba6a1dc04
Victor Rentea <victorrentea@gmail.com>
2020-02-27 13:14:35 +0200

    Secure Service TODO list (pick me)

---
+++ spring-web/src/main/java/victor/spring/web/SecureService.java
@@ -0,0 +1,13 @@
+package victor.spring.web;
+
+public class SecureService {
+    // TODO [opt] propagate identity through async calls
+
+    // TODO allow sendSMS only for role ADMIN or CRASH
+
+    // TODO allow sendSMS only for role ADMIN or SKIP
+
+    // TODO set criteria.includeConfidential = false if != ADMIN
+
+    // TODO allow method call only if user.jurisdictions.contains(countryCode)
+}
