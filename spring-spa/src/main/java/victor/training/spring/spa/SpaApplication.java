f0d812d6f4ddb3eeb8c0eca41ae7659c6caeedf8
Victor Rentea <victorrentea@gmail.com>
2020-04-27 14:48:37 +0300

    Enable swagger

--- spring-spa/src/main/java/victor/training/spring/spa/SpaApplication.java
+++ spring-spa/src/main/java/victor/training/spring/spa/SpaApplication.java
@@ -2,8 +2,10 @@ package victor.training.spring.spa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpaApplication {
 
 	public static void main(String[] args) {
