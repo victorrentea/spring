a78b1303ed757537b465194f8d1eca81411f41ea
Victor Rentea <victorrentea@gmail.com>
2020-04-27 10:06:24 +0300

    Lombok

--- spring-first/src/main/java/com/example/springfirst/SpringFirstApplication.java
+++ spring-first/src/main/java/com/example/springfirst/SpringFirstApplication.java
@@ -1,5 +1,12 @@
 package com.example.springfirst;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,12 +15,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-import javax.sql.DataSource;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+@Slf4j
 @SpringBootApplication
 public class SpringFirstApplication implements CommandLineRunner {
+//	private static final Logger log = LoggerFactory.getLogger(SpringFirstApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(SpringFirstApplication.class, args);
 	}
@@ -25,7 +33,7 @@ public class SpringFirstApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		System.out.println("Start");
 		a.m();
-
+		log.debug("logu magic");
 	}
 }
 @Facade
@@ -34,12 +42,60 @@ class A {
 	B b;
 	public void m() {
 	    b.met();
+		EntitateHibernate e = new EntitateHibernate();
+		e.setName("name");
+		e.setPhone("8989989");
+//	    new EntitateHibernate().setId(1l);
+//		new EntitateHibernate().setStatus(EntitateHibernate.Status.DELETED);
 	}
 }
+
+@Data
+class QueryResult {
+	private final long id;
+	private final String name;
+}
+
+//@Data// niciodata pe entitati de dom
+class EntitateHibernate{
+	enum Status {
+		ACTIVE,DRAFT,DELETED
+	}
+	@Getter
+	private Long id;
+	@Getter
+	@Setter
+	private String name;
+	@Getter
+	@Setter
+	private String phone;
+	@Getter
+	private Status status;
+	public void delete() {
+		//verifica starea precedenta
+		if (status != Status.ACTIVE) {
+			throw new IllegalStateException();
+		}
+		status = Status.DELETED;
+	}
+
+}
+
+
+
 @Service
+@RequiredArgsConstructor
 class B {
-	@Autowired
-	C c;
+	private final C c;
+	private final D d;
+	private final D d2;
+	private final D d3;
+	private final D d4;
+	private final D d5;
+	private final D d6;
+
+
+
 	public void met() {
 		c.met();
 	}
@@ -47,6 +103,12 @@ class B {
 
 @Repository // general purpose
 class C {
+	public void met() {
+		System.out.println("Hello Spring World");
+	}
+}
+@Repository // general purpose
+class D {
 	public void met() {
 		System.out.println("Hello Spring World");
 	}
