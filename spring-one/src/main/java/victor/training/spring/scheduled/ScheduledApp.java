c33d85b29a9deef74a308d4198a750373ce78c66
Victor Rentea <victorrentea@gmail.com>
2020-04-28 13:28:58 +0300

    Scheduled:2 threaduri care ruleaza doua joburi separat, fara sa se blocheze unul pe altul

--- spring-one/src/main/java/victor/training/spring/scheduled/ScheduledApp.java
+++ spring-one/src/main/java/victor/training/spring/scheduled/ScheduledApp.java
@@ -22,17 +22,22 @@ public class ScheduledApp  {
         SpringApplication.run(ScheduledApp.class);
     }
 
-    // TODO 1 Should run every 5 seconds / configurable / cron "*/5 * * * * *"
+    // TODO 1 Should run every 5 seconds / configurable / cron "*/5 * * * * *" - diff vs rate
     // TODO 3 Should run on a separate 1-thread pool
-    @Scheduled(fixedRate = 5000)
+//    @Scheduled(fixedRate = 5000)
+//    @Scheduled(fixedRateString = "${folder.poller.rate.millis}")
+    @Scheduled(cron = "${folder.poller.cron}")
     public void lookIntoFolder() {
         log.debug("Looking into folder");
-        ThreadUtils.sleep(7000);
+        ThreadUtils.sleep(4000);
         log.debug("DONE");
     }
 
+    /// over the hills and far away
+
     // TODO 2 define another task at each second. This should not block.
     // TODO explore application.properties
+    @Scheduled(fixedRate = 1000)
     public void pollFast() {
         log.debug("FAST each second");
     }
