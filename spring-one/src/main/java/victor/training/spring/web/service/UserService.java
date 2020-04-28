3ed77908bb2d309cc5af3a4af0eee904f3987f2b
Victor Rentea <victorrentea@gmail.com>
2020-04-28 12:27:54 +0300

    Cache cu redis

--- spring-one/src/main/java/victor/training/spring/web/service/UserService.java
+++ spring-one/src/main/java/victor/training/spring/web/service/UserService.java
@@ -1,25 +1,46 @@
 package victor.training.spring.web.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import victor.training.spring.web.domain.User;
 import victor.training.spring.web.repo.UserRepo;
 
+import javax.annotation.PostConstruct;
+
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements CommandLineRunner {
     private final UserRepo userRepo;
 
     @Cacheable("user-count")
     public long countUsers() {
         return userRepo.count();
-        //count -> 2 ; create ; count ->
     }
+
+    //count -> 2 ; create ; count -> tot 2 (gresit) -> solutie: adauga cacheEvict
     @CacheEvict("user-count")
     public void createUser() {
         userRepo.save(new User());
     }
+
+    @Override
+    @CacheEvict("user-count")
+    public void run(String... args) throws Exception {
+        System.out.println("MERGE!!! CLR poate fi interceptat/proxiat");
+    }
+
+    // pe doua noduri => tot gresit sa ai cache-uri izolate (de ex in memory)
+    // Scenariu:
+    // N1: get (=2)
+    // N2: get (=2)
+    // N1: create
+    // N1: get (=3)
+    // N2: get (=2) GRESIT
 }
 
