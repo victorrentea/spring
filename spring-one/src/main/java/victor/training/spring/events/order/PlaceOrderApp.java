ade28c21f78cc7ae52768a7507f4d08df39c17a6
Victor Rentea <victorrentea@gmail.com>
2020-04-29 10:46:37 +0300

    Arunc event in loc sa chem direct functia. Loosly coupled.

--- spring-one/src/main/java/victor/training/spring/events/order/PlaceOrderApp.java
+++ spring-one/src/main/java/victor/training/spring/events/order/PlaceOrderApp.java
@@ -1,9 +1,12 @@
 package victor.training.spring.events.order;
 
+import lombok.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
@@ -12,10 +15,10 @@ import lombok.extern.slf4j.Slf4j;
 //@EnableBinding({Queues.class})
 @SpringBootApplication
 public class PlaceOrderApp implements CommandLineRunner {
-	public static void main(String[] args) {
-		SpringApplication.run(PlaceOrderApp.class, args);
-	}
-	
+    public static void main(String[] args) {
+        SpringApplication.run(PlaceOrderApp.class, args);
+    }
+
 //	@Bean
 //    public ApplicationEventMulticaster applicationEventMulticaster() {
 //        SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
@@ -24,53 +27,60 @@ public class PlaceOrderApp implements CommandLineRunner {
 //    }
 
 
-	// TODO [1] Decouple using @EventListener and ApplicationEventPublisher
-	// TODO [2] also generate invoice
-	// TODO [2] control the @Order
-	// TODO [3] event chaining
-	// TODO [4] handle asynchronously: exceptions, Tx, Thread Scope, debate
-	// TODO [5] queues: retries, chaining, topics
-	// TODO [opt] Transaction-scoped events
+    // TODO [1] Decouple using @EventListener and ApplicationEventPublisher
+    // TODO [2] also generate invoice
+    // TODO [2] control the @Order
+    // TODO [3] event chaining
+    // TODO [4] handle asynchronously: exceptions, Tx, Thread Scope, debate
+    // TODO [5] queues: retries, chaining, topics
+    // TODO [opt] Transaction-scoped events
 
-	@Autowired
-	private StockManagementService stockManagementService;
 
-	@Autowired
-	private InvoiceService invoiceService;
+    @Autowired
+    private InvoiceService invoiceService;
 
-	public void run(String... args) {
-		placeOrder();
-	}
+    @Autowired
+    ApplicationEventPublisher eventPublisher;
 
-	private void placeOrder() {
-		log.debug(">> PERSIST new Order");
-		long orderId = 13L;
-		stockManagementService.process(orderId);
-		invoiceService.sendInvoice(orderId);
-	}
+    public void run(String... args) {
+        placeOrder();
+    }
+
+    private void placeOrder() {
+        log.debug(">> PERSIST new Order");
+        long orderId = 13L;
+        eventPublisher.publishEvent(new OrderPlacedEvent(orderId));
+        invoiceService.sendInvoice(orderId);
+    }
+}
+
+@Value
+class OrderPlacedEvent {
+    long orderId;
 }
 
 @Slf4j
 @Service
 class StockManagementService {
-	private int stock = 3; // silly implem :D
+    private int stock = 3; // silly implem :D
 
-	public void process(long orderId) {
-		log.info("Checking stock for products in order " + orderId);
-		if (stock == 0) {
-			throw new IllegalStateException("Out of stock");
-		}
-		stock --;
-		log.info(">> PERSIST new STOCK!!");
-	}
+	@EventListener
+    public void process(OrderPlacedEvent event) {
+        log.info("Checking stock for products in order " + event.getOrderId());
+        if (stock == 0) {
+            throw new IllegalStateException("Out of stock");
+        }
+        stock--;
+        log.info(">> PERSIST new STOCK!!");
+    }
 }
 
 @Slf4j
 @Service
 class InvoiceService {
-	public void sendInvoice(long orderId) {
-		log.info("Generating invoice for order " + orderId);
-		// TODO what if (random() < .3) throw new RuntimeException("Invoice Generation Failed");
-		log.info(">> PERSIST Invoice!!");
-	}
+    public void sendInvoice(long orderId) {
+        log.info("Generating invoice for order " + orderId);
+        // TODO what if (random() < .3) throw new RuntimeException("Invoice Generation Failed");
+        log.info(">> PERSIST Invoice!!");
+    }
 }
