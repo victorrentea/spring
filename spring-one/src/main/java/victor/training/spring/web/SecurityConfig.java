6ced07c0787a034745098689e8b587943b268d5a
Victor Rentea <victorrentea@gmail.com>
2020-04-29 22:40:09 +0300

    Prepare Spring Security

--- spring-one/src/main/java/victor/training/spring/web/SecurityConfig.java
+++ spring-one/src/main/java/victor/training/spring/web/SecurityConfig.java
@@ -1,19 +1,32 @@
 package victor.training.spring.web;
 
+import org.springframework.context.annotation.Bean;
+
 //@EnableWebSecurity
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig  /*extends WebSecurityConfigurerAdapter*/ {
 
-    // TODO [SEC] Start with ROLE-based authorization
+    // TODO [SEC] Start with ROLE-based authorization on URL-patterns
 
-    //
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
+//       http
+//            .csrf().disable()
+//            ;
 //    }
-//
-//    //Define a UserDetailsService
+
+    // *** Dummy users 100% in-mem
+//    @Bean
+//    public UserDetailsService userDetailsService() {
 //        UserDetails userDetails = User.withDefaultPasswordEncoder()...
-//        return new InMemoryUserDetailsManager(userDetails);
-//}
+//        return new InMemoryUserDetailsManager(userDetails, adminDetails);
+//    }
+
+    // ... then, switch to loading user data from DB:
+    // *** Also loading data from DB
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        return new DatabaseUserDetailsService();
+//    }
 
 }
--- spring-one/src/main/java/victor/training/spring/web/security/JwtDatabaseUserDetailsService.java
+++ spring-one/src/main/java/victor/training/spring/web/security/JwtDatabaseUserDetailsService.java
@@ -1,30 +1,38 @@
-//package victor.training.spring.web.security;
+package victor.training.spring.web.security;//package victor.training.spring.web.security;
 //
 //
 //import lombok.extern.slf4j.Slf4j;
 //import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
 //import org.springframework.security.core.userdetails.UserDetails;
 //import org.springframework.security.core.userdetails.UserDetailsService;
 //import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 //import victor.training.spring.web.domain.User;
 //import victor.training.spring.web.repo.UserRepo;
 //
+//import java.util.Optional;
+//
 //@Slf4j
-//public class DatabaseUserDetailsService implements UserDetailsService {
+//public class JwtDatabaseUserDetailsService implements AuthenticationUserDetailsService<PreAuthenticatedAuthenticationToken> {
 //    @Autowired
 //    private UserRepo userRepository;
 //
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//    @Override
+//    public UserDetails loadUserDetails(PreAuthenticatedAuthenticationToken token) throws UsernameNotFoundException {
+//        JwtPrincipal jwtPrincipal = (JwtPrincipal) token.getPrincipal();
+//        String username = jwtPrincipal.getUsername();
+//
 //        log.debug("Lookup username {} in database", username);
 //        User user = userRepository.getForLogin(username)
 //                .orElseThrow(() -> new UsernameNotFoundException("User '" + username + "' not in database"));
 //
-//        log.debug("Successful login");
-//        // WARNING: the implem of SecurtyUser class is hacked to expect the password of the user == its username
+//        log.debug("Fetched from database");
 //        return new SecurityUser(user.getUsername(),
 //                user.getProfile().permissions,
 //                user.getManagedTeacherIds());
 //    }
+//
 //
 //
 //}
