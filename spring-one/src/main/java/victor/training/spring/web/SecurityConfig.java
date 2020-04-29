8e180fe13bee37f6c2b2a27912c6f306c793ac9d
Victor Rentea <victorrentea@gmail.com>
2020-04-29 13:55:16 +0300

    Commit for JWT homework

--- spring-one/src/main/java/victor/training/spring/web/SecurityConfig.java
+++ spring-one/src/main/java/victor/training/spring/web/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
 //        .httpBasic()
+
 //                .and()
         .authenticationProvider(preAuthenticatedProvider())
                 .addFilterBefore(jwtFilter(), BasicAuthenticationFilter.class)
