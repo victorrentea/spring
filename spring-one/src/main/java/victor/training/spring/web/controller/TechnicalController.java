e144d77f8d8fa907366bd8c66c49de2d61acd64c
Victor Rentea <victorrentea@gmail.com>
2020-04-29 22:50:32 +0300

    cleanup

--- spring-one/src/main/java/victor/training/spring/web/controller/TechnicalController.java
+++ spring-one/src/main/java/victor/training/spring/web/controller/TechnicalController.java
@@ -5,28 +5,36 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import victor.training.spring.props.WelcomeInfo;
+import victor.training.spring.web.service.UserService;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 public class TechnicalController {
+	@Autowired
+	private UserService userService;
+
 	@GetMapping("rest/user/current")
 	public String getCurrentUsername() {
 		// TODO implement me
 		return "TODO:user";
+		// try getting the user from an Async task:
+		//	return userService.getCurrentUsername().get(); // this only works due to the @PostConstruct below
+	}
+
+	@PostConstruct
+	public void enableSecurityContextPropagationOverAsync() {
+//		SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
 	}
 
 //	@Autowired  // TODO Import the other Spring Boot Application
-	WelcomeInfo welcomeInfo;
+	private WelcomeInfo welcomeInfo;
 
 	// TODO [SEC] allow unsecured access
 	@GetMapping("unsecured/welcome-info")
 	public WelcomeInfo showWelcomeInfo(){
 		return welcomeInfo;
-	}
-
-	@GetMapping("beer")
-	public void haveABeer() {
-		// TODO [opt] propagate identity through async calls
-		// https://www.baeldung.com/spring-security-async-principal-propagation
 	}
 
 	@GetMapping("ping")
