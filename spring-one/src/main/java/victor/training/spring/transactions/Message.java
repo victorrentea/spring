77ca26ae994744ac17b25f6cfa70bbe47b6bc07d
Victor Rentea <victorrentea@gmail.com>
2020-04-29 10:15:04 +0300

    crapa tranzactia mica

--- spring-one/src/main/java/victor/training/spring/transactions/Message.java
+++ spring-one/src/main/java/victor/training/spring/transactions/Message.java
@@ -1,10 +1,13 @@
 package victor.training.spring.transactions;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class Message {
     @Id
     @GeneratedValue
--- spring-one/src/main/java/victor/training/spring/transactions/Playground.java
+++ spring-one/src/main/java/victor/training/spring/transactions/Playground.java
@@ -1,28 +1,51 @@
 package victor.training.spring.transactions;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.lang.Nullable;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManager;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class Playground {
     private final MessageRepo repo;
-    private final EntityManager em;
-    private final JdbcTemplate jdbc;
     private final AnotherClass other;
+//    TransactionTemplate txTemplate;
+
+    @Autowired
+    public void setupTxTemplate(PlatformTransactionManager transactionManager) {
+//        txTemplate = new TransactionTemplate(transactionManager);
+//        txTemplate.execute(status -> {
+//            em.
+//            return 1;
+//        });
+    }
 
     @Transactional
     public void transactionOne() {
-        jdbc.update("insert into MESSAGE(id, message) values ( 100,'ALO' )");
-        repo.save(new Message("jpa"));
+        repo.save(new Message("initial"));
+        log.debug("Final metoda");
+//        if (true) {
+//            throw new IllegalArgumentException();
+//        }
     }
     @Transactional
     public void transactionTwo() {
+        Message message = repo.findById(1L).get();
+        Message message2 = other.method();
+
+        System.out.println(message == message2);
+        message.setMessage("changed");
+        try {
+            other.riscanta();
+        } catch (Exception e) {
+            e.printStackTrace(); // shawarma
+        }
+        log.debug("Dupa");
         // TODO Repo API
         // TODO @NonNullApi
     }
@@ -31,8 +54,15 @@ public class Playground {
 @RequiredArgsConstructor
 class AnotherClass {
     private final MessageRepo repo;
-    
-    public void method() {
-
+    @Transactional
+    public Message method() {
+        return repo.findById(1L).get();
+        // daca ceri din nou aceeasi entitate dupa ID din baza din aceeasi tranzactie,
+        // JPA iti va da ACEEASI instanta pe care o avea deja in mem (fara sa faca un nou query)
+    }
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void riscanta() {
+        repo.findById(1L).get().setMessage("inainte de crapare");
+        throw new RuntimeException();
     }
 }
