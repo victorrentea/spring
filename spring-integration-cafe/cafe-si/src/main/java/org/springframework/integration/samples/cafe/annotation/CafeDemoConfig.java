0a6012f8d50db83cbebbf8d4a6609c6a296695d4
Victor Rentea <victorrentea@gmail.com>
2020-04-30 11:43:57 +0300

    Routare care sparge fluxul in doua fluxuri

--- spring-integration-cafe/cafe-si/src/main/java/org/springframework/integration/samples/cafe/annotation/CafeDemoConfig.java
+++ spring-integration-cafe/cafe-si/src/main/java/org/springframework/integration/samples/cafe/annotation/CafeDemoConfig.java
@@ -12,6 +12,7 @@ import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.Pollers;
 import org.springframework.integration.handler.LoggingHandler;
+import org.springframework.integration.samples.cafe.OrderItem;
 import org.springframework.messaging.MessageChannel;
 @EnableIntegration
 @IntegrationComponentScan("org.springframework.integration.samples.cafe") // sa scaneze si interfete @MessageGateway pt care sa creeze apoi proxyuri
@@ -24,8 +25,19 @@ public class CafeDemoConfig {
             return IntegrationFlows.from("orders")
 //                    .split("orderSplitter", "split")
                     .split("payload.items") // SpEL   -> payload este un atribut al GenericMessage (vezi log)
-                    .log(LoggingHandler.Level.WARN)
+                    .route((OrderItem orderItem) -> orderItem.isIced()? "coldDrinks" : "hotDrinks")
                     .get();
     }
-
+    @Bean
+    public IntegrationFlow logColdDrinks() {
+        return IntegrationFlows.from("coldDrinks")
+                .log(LoggingHandler.Level.WARN, "cold")
+                .get();
+    }
+    @Bean
+    public IntegrationFlow logHotDrinks() {
+        return IntegrationFlows.from("hotDrinks")
+                .log(LoggingHandler.Level.WARN, "hot")
+                .get();
+    }
 }
