56f96dfc2ac40edc2f219454f554a6b0af679459
Victor Rentea <victorrentea@gmail.com>
2020-05-11 11:38:57 +0300

    Mod folosire corect: doar pe local preferi o varianta hackarita

--- spring-one/src/main/java/victor/training/spring/life/Profiles.java
+++ spring-one/src/main/java/victor/training/spring/life/Profiles.java
@@ -3,6 +3,8 @@ package victor.training.spring.life;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,14 +22,17 @@ interface I {
     void m();
 }
 @Component("varza")
-class IA implements I {
+class IA implements I { // hackareala pentru confort
     public void m() {
-        System.out.println("A");
+        System.out.println("A de pe disk ca e confortabil pentru dev");
     }
 }
-//@Component
-//class IB implements I {
-//    public void m() {
-//        System.out.println("B");
-//    }
-//}
+// only available without -Dspring.profiles.active=local
+@Primary
+@Profile("! local")
+@Component
+class IB implements I { //asta e pe bune
+    public void m() {
+        System.out.println("B de pe alt FTP ca asa tre in prod");
+    }
+}
