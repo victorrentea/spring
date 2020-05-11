91d90f0b706f639958b5e28eeab44a6e342eda22
Victor Rentea <victorrentea@gmail.com>
2020-05-11 13:23:48 +0300

    Tenant - idee2 (WIP)

--- spring-one/src/main/java/victor/training/spring/bean/TenantProblem.java
+++ spring-one/src/main/java/victor/training/spring/bean/TenantProblem.java
@@ -1,12 +1,16 @@
 package victor.training.spring.bean;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
+import java.util.List;
 
 @Configuration
 public class TenantProblem  implements CommandLineRunner {
@@ -17,37 +21,93 @@ public class TenantProblem  implements CommandLineRunner {
         resolver.resolve("RO").m();
         resolver.resolve("HU").m();
     }
+
+//    @Bean("CORE-accountCoreLevel3Service")
+//    public IService n() {
+//        return new Level3Service();
+//    }
 }
 @Component
 class ServiceResolver {
-    private final Map<String, IService> allImplems;
-    private final DefaultService defaultService;
-    private ServiceResolver(Map<String, IService> allImplems, DefaultService defaultService) {
+    private final List<IService> allImplems;
+
+    public ServiceResolver(List<IService> allImplems) {
         this.allImplems = allImplems;
-        this.defaultService = defaultService;
     }
+
     public IService resolve(String countryIso) {
-        return allImplems.getOrDefault(countryIso, defaultService);
+        return allImplems.stream().filter(i -> i.accepts(countryIso, DB.USER)).findFirst().get();
     }
+}
+enum DB {
+    USER, PROFILE
 }
 
 interface IService {
-//    boolean accepts(String... params);
+    boolean accepts(String countryIso, DB db);
     void m();
 }
+@Service
+class ROServiceDebit implements IService {
+    @Override
+    public boolean accepts(String countryIso, DB db) {
+        return "RO".equals(countryIso);// && db...;
+    }
+
+    @Override
+    public void m() {
+        System.out.println("RO implem");
+    }
+}
+@Service
+class ROServiceCurent implements IService {
+    @Override
+    public boolean accepts(String countryIso, DB db) {
+        return "RO".equals(countryIso);// && db...;
+    }
+
+    @Override
+    public void m() {
+        System.out.println("RO implem");
+    }
+}
+//@Debit
+@Service
+class HUServiceDebit implements IService {
+    @Override
+    public boolean accepts(String countryIso, DB db) {
+        return "RO".equals(countryIso);// && db...;
+    }
+
+    @Override
+    public void m() {
+        System.out.println("RO implem");
+    }
+}
+//@Curent
+@Service
+class HUServiceCurent implements IService {
+    @Override
+    public boolean accepts(String countryIso, DB db) {
+        return "RO".equals(countryIso);// && db...;
+    }
+
+    @Override
+    public void m() {
+        System.out.println("RO implem");
+    }
+}
+
 
 @Service
 class DefaultService implements IService {
     @Override
-    public void m() {
-        System.out.println("Default implem");
+    public boolean accepts(String countryIso, DB db) {
+        return true;
     }
-}
 
-@Service("RO")
-class ROService implements IService {
     @Override
     public void m() {
-        System.out.println("RO implem");
+        System.out.println("Default implem");
     }
 }
