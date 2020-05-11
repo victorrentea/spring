f55a0d607954dbc364ee0334808f43d2285ad601
Victor Rentea <victorrentea@gmail.com>
2020-05-11 11:51:15 +0300

    Chiar si fara Qualifier, daca numele campului sau param in care injectezi este == cu numele beanului, ai noroc cu caru

--- spring-one/src/main/java/victor/training/spring/life/Profiles.java
+++ spring-one/src/main/java/victor/training/spring/life/Profiles.java
@@ -8,13 +8,18 @@ import java.util.List;
 @Component
 public class Profiles implements CommandLineRunner {
     private final List<I> toate;
+    private final I safe;
 
-    public Profiles(List<I> toate) {
+
+    public Profiles(List<I> toate, I safe) {
         this.toate = toate;
+        this.safe = safe;
     }
 
     public void run(String... args) throws Exception {
         toate.forEach(I::m);
+        System.out.println("----");
+        safe.m();
     }
 }
 interface I {
@@ -30,7 +35,7 @@ class IA implements I { // hackareala pentru confort
 //@Primary
 //@Profile("! local")
 //@Profile("prod") // NICIODATA nu def un profil doar pt PROD.
-@Component
+@Component("safe")
 class IB implements I { //asta e pe bune
     public void m() {
         System.out.println("B de pe alt FTP ca asa tre in prod");
