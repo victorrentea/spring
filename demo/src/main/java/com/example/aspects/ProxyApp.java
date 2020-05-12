bdb6544dca5d3caf64e744950e002fd6f457afc1
Victor Rentea <victorrentea@gmail.com>
2020-05-12 10:59:47 +0300

    init AOP

---
+++ demo/src/main/java/com/example/aspects/ProxyApp.java
@@ -0,0 +1,51 @@
+package com.example.aspects;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import java.io.File;
+
+@EnableAspectJAutoProxy(exposeProxy = true)
+@EnableCaching
+@SpringBootApplication
+@Slf4j
+public class ProxyApp implements CommandLineRunner {
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyApp.class, args);
+	}
+
+	// [1] Implement decorator with Spring
+	// [2] InterfaceProxy.proxy (no Spring)
+	// [3] ClassProxy.proxy (no Spring)
+	// [4] Spring Cache support [opt: redis]
+	// [5] Spring aspect, @Facade, @Logged
+	// [6] Tips: self proxy, debugging, final
+	// [7] OPT: Manual proxying using BeanPostProcessor
+
+	// Holy Domain Logic.
+	// Very precious things that I want to keep agnostic to technical details
+	@Autowired
+	private ExpensiveOps ops;
+
+	public void run(String... args) {
+		log.debug("\n");
+ 		log.debug("---- CPU Intensive ~ memoization?");
+		log.debug("10000169 is prime ? ");
+		log.debug("Got: " + ops.isPrime(10000169) + "\n");
+		log.debug("10000169 is prime ? ");
+		log.debug("Got: " + ops.isPrime(10000169) + "\n");
+		
+		log.debug("---- I/O Intensive ~ \"There are only two things hard in programming...\"");
+		log.debug("Folder . MD5: ");
+		log.debug("Got: " + ops.hashAllFiles(new File(".")) + "\n");
+		log.debug("Got: " + ops.hashAllFiles(new File(".")) + "\n");
+		log.debug("Folder . MD5: ");
+		log.debug("Got: " + ops.hashAllFiles(new File(".")) + "\n");
+	}
+}
+
