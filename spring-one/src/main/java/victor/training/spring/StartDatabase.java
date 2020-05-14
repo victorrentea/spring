67a5400aa968a84ced7f1b74013e08cec8e5ee4d
Victor Rentea <victorrentea@gmail.com>
2020-05-14 08:27:20 +0300

    fix DB start program

--- spring-one/src/main/java/victor/training/spring/StartDatabase.java
+++ spring-one/src/main/java/victor/training/spring/StartDatabase.java
@@ -1,40 +1,14 @@
 package victor.training.spring;
 
-import org.h2.tools.Server;
-import org.springframework.util.FileSystemUtils;
-
-import java.io.File;
 import java.sql.SQLException;
 
 public class StartDatabase {
 	public static void main(String[] args) throws SQLException {
-		deletePreviousDBContents();
-
 		System.out.println("Started DB...");
+		System.out.println("Connecting to 'jdbc:h2:tcp://localhost:9092/~/test' will auto-create a database file 'test.mv.db' in user home (~)...");
 
 		// Allow auto-creating new databases on disk at first connection
 		org.h2.tools.Server.createTcpServer("-ifNotExists").start();
 	}
 
-	private static void deletePreviousDBContents() {
-		File userHomeFolder = new File(System.getProperty("user.home"));
-		if (!userHomeFolder.isDirectory()) {
-			throw new IllegalArgumentException("Could not locate userHome");
-		}
-		System.out.println("Assuming user home folder is: " + userHomeFolder.getAbsolutePath());
-		File databasePath = new File(userHomeFolder, "source/db");
-		if (databasePath.isDirectory()) {
-			System.out.println("Deleting previous db contents...");
-			boolean ok = FileSystemUtils.deleteRecursively(databasePath);
-			if (!ok) {
-				System.err.println("Could not delete folder " + databasePath.getAbsolutePath());
-			} else {
-				System.out.println("SUCCESS");
-			}
-		} else {
-			System.out.println("Nothing found at db path: " + databasePath.getAbsolutePath());
-		}
-		System.out.println("Check out folder: ~/source/db/database/db");
-
-	}
 }
