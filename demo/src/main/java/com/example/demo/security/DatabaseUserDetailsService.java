837c7d012a6720d7478d94bd171fc5045cd4066f
Victor Rentea <victorrentea@gmail.com>
2020-05-15 10:33:27 +0300

    verifici si iei userii din baza ta

--- demo/src/main/java/com/example/demo/security/DatabaseUserDetailsService.java
+++ demo/src/main/java/com/example/demo/security/DatabaseUserDetailsService.java
@@ -1,17 +1,27 @@
 package com.example.demo.security;
 
+import com.example.demo.entity.User;
+import com.example.demo.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 @Component
 public class DatabaseUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private UserRepo userRepo;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new DemoPrincipal(username, "FULL NAME", Arrays.asList("USER"));
+        Optional<User> userOpt = userRepo.findById(username);
+        User user = userOpt.orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return new DemoPrincipal(username, user.getName(), Arrays.asList(user.getRole().name()));
     }
 }
