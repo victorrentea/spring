95027708c7f2bf5dd7a0bf8e0c219e3558a11d51
Victor Rentea <victorrentea@gmail.com>
2020-05-15 09:10:02 +0300

    Added external H2 DB

---
+++ demo/src/main/java/com/example/demo/StartDatabase.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import java.sql.SQLException;
+
+public class StartDatabase {
+	public static void main(String[] args) throws SQLException {
+		System.out.println("Started DB...");
+		System.out.println("Connecting to 'jdbc:h2:tcp://localhost:9092/~/test' will auto-create a database file 'test.mv.db' in user home (UNIX:~, WIN:c:\\users\\<youruser>)...");
+
+		// Allow auto-creating new databases on disk at first connection
+		org.h2.tools.Server.createTcpServer("-ifNotExists").start();
+	}
+
+}
