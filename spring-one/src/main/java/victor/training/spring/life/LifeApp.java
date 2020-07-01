a53ed63bd64c9b64532f6d62332d59e384c18b41
Victor Rentea <victorrentea@gmail.com>
2020-07-01 16:35:22 +0300

    Pare ca merge, sa chemi load() manual **inainte** de get()

--- spring-one/src/main/java/victor/training/spring/life/LifeApp.java
+++ spring-one/src/main/java/victor/training/spring/life/LifeApp.java
@@ -3,6 +3,7 @@ package victor.training.spring.life;
 import java.util.Locale;
 import java.util.Map;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.CustomScopeConfigurer;
@@ -16,10 +17,8 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.support.TransactionTemplate;
 
-import javax.annotation.PostConstruct;
-
 @SpringBootApplication
-public class LifeApp implements CommandLineRunner{
+public class LifeApp implements CommandLineRunner {
 	@Bean
 	public static CustomScopeConfigurer defineThreadScope() {
 		CustomScopeConfigurer configurer = new CustomScopeConfigurer();
@@ -29,7 +28,7 @@ public class LifeApp implements CommandLineRunner{
 	}
 	
 	public static void main(String[] args) {
-		SpringApplication.run(LifeApp.class);
+		SpringApplication.run(LifeApp.class, args);
 	}
 
 
@@ -40,7 +39,7 @@ public class LifeApp implements CommandLineRunner{
 		txTemplate = new TransactionTemplate(transactionManager);
 		txTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
 
-		// in codul tau:
+		// in codul tau ,din oricat threaduri:
 		txTemplate.execute(status -> {
 			//INSERT in tx noua
 			return null;
@@ -57,32 +56,39 @@ public class LifeApp implements CommandLineRunner{
 	// TODO [3] prototype scope + ObjectFactory or @Lookup. Did you said "Factory"? ...
 	// TODO [4] thread/request scope. HOW it works?! Leaks: @see SimpleThreadScope javadoc
 
+
+
+
+//	@Transactional merge pus aici
 	public void run(String... args) {
+		//tipic noi facem persist de date dummy
+//		em.persist()
+
 		exporter.export(Locale.ENGLISH);
-		// TODO exporter.export(Locale.FRENCH);
+		exporter.export(Locale.FRENCH);
 		
 	}
 }
 @Slf4j
 @Service
+@RequiredArgsConstructor
 class OrderExporter  {
-	@Autowired
-	private InvoiceExporter invoiceExporter;
-	@Autowired
-	private LabelService labelService;
+	private final InvoiceExporter invoiceExporter;
+	private final LabelService labelService;
 
 	public void export(Locale locale) {
 		log.debug("Running export in " + locale);
-		log.debug("Origin Country: " + labelService.getCountryName("rO")); 
+		labelService.load(locale);
+		log.debug("Origin Country: " + labelService.getCountryName("rO"));
 		invoiceExporter.exportInvoice();
 	}
 }
 @Slf4j
-@Service 
+@Service
+@RequiredArgsConstructor
 class InvoiceExporter {
-	@Autowired
-	private LabelService labelService;
-	
+	private final LabelService labelService;
+
 	public void exportInvoice() {
 		log.debug("Invoice Country: " + labelService.getCountryName("ES"));
 	}
@@ -100,10 +106,9 @@ class LabelService {
 
 	private Map<String, String> countryNames;
 
-	@PostConstruct
-	public void load() {
+	public void load(Locale locale) {
 		log.debug("LabelService.load() on instance " + this.hashCode());
-		countryNames = countryRepo.loadCountryNamesAsMap(Locale.ENGLISH);
+		countryNames = countryRepo.loadCountryNamesAsMap(locale);
 	}
 	
 	public String getCountryName(String iso2Code) {
