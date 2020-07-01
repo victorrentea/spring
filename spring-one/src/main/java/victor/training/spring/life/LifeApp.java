c82a99c175095931d114095ed2fd2c5f8bad73a4
Victor Rentea <victorrentea@gmail.com>
2020-07-01 17:10:27 +0300

    Merge dar e prea generic APplicaitonContext injectata

--- spring-one/src/main/java/victor/training/spring/life/LifeApp.java
+++ spring-one/src/main/java/victor/training/spring/life/LifeApp.java
@@ -1,121 +1,171 @@
 package victor.training.spring.life;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.config.CustomScopeConfigurer;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationContextInitializedEvent;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.event.EventListener;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.SimpleThreadScope;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class LifeApp implements CommandLineRunner {
-	@Bean
-	public static CustomScopeConfigurer defineThreadScope() {
-		CustomScopeConfigurer configurer = new CustomScopeConfigurer();
-		// WARNING: Leaks memory. Prefer 'request' scope or read here: https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/context/support/SimpleThreadScope.html
-		configurer.addScope("thread", new SimpleThreadScope());
-		return configurer;
-	}
-	
-	public static void main(String[] args) {
-		SpringApplication.run(LifeApp.class, args);
-	}
+   @Bean
+   public static CustomScopeConfigurer defineThreadScope() {
+      CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+      // WARNING: Leaks memory. Prefer 'request' scope or read here: https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/context/support/SimpleThreadScope.html
+      configurer.addScope("thread", new SimpleThreadScope());
+      return configurer;
+   }
+
+   public static void main(String[] args) {
+      SpringApplication.run(LifeApp.class, args);
+   }
 
 
-	private TransactionTemplate txTemplate;
+   private TransactionTemplate txTemplate;
 
-	@Autowired
-	public void initTxTemplate(PlatformTransactionManager transactionManager) {
-		txTemplate = new TransactionTemplate(transactionManager);
-		txTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+   @Autowired
+   public void initTxTemplate(PlatformTransactionManager transactionManager) {
+      txTemplate = new TransactionTemplate(transactionManager);
+      txTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
 
-		// in codul tau ,din oricat threaduri:
-		txTemplate.execute(status -> {
-			//INSERT in tx noua
-			return null;
-		});
-	}
-
-
-
-	@Autowired 
-	private OrderExporter exporter;
-	
-	// TODO [1] make singleton; multi-thread + mutable state = BAD
-	// TODO [2] instantiate manually, set dependencies, pass around; no AOP
-	// TODO [3] prototype scope + ObjectFactory or @Lookup. Did you said "Factory"? ...
-	// TODO [4] thread/request scope. HOW it works?! Leaks: @see SimpleThreadScope javadoc
+      // in codul tau ,din oricat threaduri:
+      txTemplate.execute(status -> {
+         //INSERT in tx noua
+         return null;
+      });
+   }
 
 
+   @Autowired
+   private OrderExporter exporter;
+
+   // TODO [1] make singleton; multi-thread + mutable state = BAD
+   // TODO [2] instantiate manually, set dependencies, pass around; no AOP
+   // TODO [3] prototype scope + ObjectFactory or @Lookup. Did you said "Factory"? ...
+   // TODO [4] thread/request scope. HOW it works?! Leaks: @see SimpleThreadScope javadoc
 
 
-//	@Transactional merge pus aici
-	public void run(String... args) {
-		//tipic noi facem persist de date dummy
+   //	@Transactional merge pus aici
+   public void run(String... args) {
+      //tipic noi facem persist de date dummy
 //		em.persist()
+      configureBeans();
 
-		new Thread(() -> exporter.export(Locale.ENGLISH)).start();
-		new Thread(() -> exporter.export(Locale.FRENCH)).start();
-		
-	}
+      new Thread(() -> exporter.export(Locale.ENGLISH)).start();
+      new Thread(() -> exporter.export(Locale.FRENCH)).start();
+
+   }
+
+   @Bean
+   public RestTemplate restTemplate() {
+      return new RestTemplate();
+   }
+//	@Bean
+//	public WSClient fbClient(@Autowired RestTemplate restTemplate) {
+//		return new WSClient("fb.com",9989, restTemplate);
+//	}
+//	@Bean
+//	public WSClient linkedInClient(@Autowired RestTemplate restTemplate) {
+//		return new WSClient("linkedin.com",9989, restTemplate);
+//	}
+
+   @Autowired
+   ConfigurableListableBeanFactory beanFactory;
+
+//   @PostConstruct
+   public void configureBeans() {
+      for (int i = 0; i < 3; i++) {
+         beanFactory.registerSingleton("wsClientBean"+i,
+             new WSClient("host" + i, i, restTemplate()));
+      }
+   }
+
 }
+
+
+@Data
+class WSClient {
+   private final String host;
+   private final int port;
+   private final RestTemplate restTemplate;
+}
+
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
-class OrderExporter  {
-	private final InvoiceExporter invoiceExporter;
-	private final LabelService labelService;
+class OrderExporter {
+   private final InvoiceExporter invoiceExporter;
+   private final ApplicationContext spring;
 
-	public void export(Locale locale) {
-		log.debug("Running export in " + locale);
-
-		log.debug("Origin Country: " + labelService.getCountryName(locale, "rO"));
-		invoiceExporter.exportInvoice(locale);
-	}
+   public void export(Locale locale) {
+      log.debug("Running export in " + locale);
+      LabelService labelService = spring.getBean(LabelService.class);
+      labelService.load(locale);
+      log.debug("Origin Country: " + labelService.getCountryName("rO"));
+      invoiceExporter.exportInvoice(labelService);
+   }
 }
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 class InvoiceExporter {
-	private final LabelService labelService;
+//	private final LabelService labelService;
 
-	public void exportInvoice(Locale locale) {
-		log.debug("Invoice Country: " + labelService.getCountryName(locale, "ES"));
-	}
+   public void exportInvoice(LabelService labelService) {
+      log.debug("Invoice Country: " + labelService.getCountryName("ES"));
+   }
 }
 
 @Slf4j
 @Service
 @Scope("prototype")
 class LabelService {
-	private final CountryRepo countryRepo;
-	
-	public LabelService(CountryRepo countryRepo) {
-		System.out.println("+1 Label Service: " + this.hashCode());
-		this.countryRepo = countryRepo;
-	}
+   private final CountryRepo countryRepo;
 
-	private Map<String, String> countryNames;
+   public LabelService(CountryRepo countryRepo) {
+      System.out.println("+1 Label Service: " + this.hashCode());
+      this.countryRepo = countryRepo;
+   }
 
-	private void load(Locale locale) {
-		log.debug("LabelService.load() on instance " + this.hashCode());
-		countryNames = countryRepo.loadCountryNamesAsMap(locale);
-	}
-	
-	public synchronized String getCountryName(Locale locale, String iso2Code) {
-		load(locale);
-		log.debug("LabelService.getCountryName() on instance " + this.hashCode());
-		return countryNames.get(iso2Code.toUpperCase());
-	}
+   private Map<String, String> countryNames;
+
+   public void load(Locale locale) {
+      log.debug("LabelService.load() on instance " + this.hashCode());
+      countryNames = countryRepo.loadCountryNamesAsMap(locale);
+   }
+
+   public String getCountryName(String iso2Code) {
+      log.debug("LabelService.getCountryName() on instance " + this.hashCode());
+      return countryNames.get(iso2Code.toUpperCase());
+   }
 }
