0edfa226f234d02f6eeabe6938e6a5a27e0794f6
Victor Rentea <victorrentea@gmail.com>
2020-07-02 14:49:26 +0300

    @Bean use-cases

--- spring-one/src/main/java/victor/training/spring/bean/BeanApp.java
+++ spring-one/src/main/java/victor/training/spring/bean/BeanApp.java
@@ -2,50 +2,91 @@ package victor.training.spring.bean;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.time.LocalDate;
 
 @SpringBootApplication
-public class BeanApp {
+public class BeanApp implements CommandLineRunner {
+
+   @Autowired
+   private ApplicationContext spring;
+   @Override
+   public void run(String... args) throws Exception {
+      System.out.println("concert: " + spring.getBean("inCort"));
+      System.out.println("concert: " + spring.getBean("inCort"));
+   }
+
    public static void main(String[] args) {
       SpringApplication.run(BeanApp.class, args);
+   }
+
+   @Bean
+   public OldClass oldClass() {
+      return OldClass.getInstance();
+   }
+
+   @Bean
+   public Concert driveIn() {
+      System.out.println("Se anunta concertul driveIn");
+      return new Concert("Drive-In-Carla", carlaX());
+   }
+
+   @Bean
+   @Scope("prototype")
+   public Concert inCort() {
+      System.out.println("Se anunta concertul inCort");
+      return new Concert("Carla-n-Cort", carlaX());
    }
 
    @Bean
    public Artist carlaX() { // numele beanului -> numele metodei
       return new Artist("Carla's Dreams");
    }
+}
+//
+//class Hack extends BeanApp {
+//   @Override
+//   public Artist carlaX() {
+//      if (daca nu-l a, inca) {
+//         v = super.carlaX();
+//processautowired(v)
+//processpostconstruct(v)
+//return v
+//      } else {
+//         return din cache, singletonul unic.
+//      }
+//   }
+//}
 
-   @Autowired
-   Artist car;
-
-   @Bean
-   public Concert driveIn() {
-      return new Concert("Drive-In-Carla", car);
+@Service
+class CasaDeDiscuri {
+   public CasaDeDiscuri() {
+      System.out.println("New casa de discuris");
    }
-
-   @Bean
-   public Concert inCort() {
-      return new Concert("Carla-n-Cort", car);
-   }
-
-
 }
 
 @Data
 class Artist {
    private final String name;
+   @Autowired
+   private CasaDeDiscuri casaDeDiscuri;
+
+   public Artist(String name) {
+      this.name = name;
+      System.out.println("S-a nascut o stea cu : " + casaDeDiscuri);
+
+   }
    @PostConstruct
    public void init() {
-      System.out.println("S-a nascut o stea");
+      System.out.println("Steaua are : " + casaDeDiscuri);
    }
 }
 
@@ -55,9 +96,14 @@ class Artist {
 //   private String name;
 //}
 
-@Data
 class Concert {
    private final String name;
    private final Artist artist;
+   private LocalDate date;
+
+   Concert(String name, Artist artist) {
+      this.name = name;
+      this.artist = artist;
+   }
 }
 
