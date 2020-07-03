b00a6f4caa2a8264ca525ba0d7536396d0cff738
Victor Rentea <victorrentea@gmail.com>
2020-07-03 15:36:27 +0300

    Cache+Evict on ONE MACHINE

--- spring-one/src/main/java/victor/training/spring/web/SpaApplication.java
+++ spring-one/src/main/java/victor/training/spring/web/SpaApplication.java
@@ -2,8 +2,10 @@ package victor.training.spring.web;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableCaching
 @EnableSwagger2
 @SpringBootApplication
 public class SpaApplication {
