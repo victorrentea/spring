a66d3ce0849b9c11c7f900b07a2fca0cb53ff112
Victor Rentea <victorrentea@gmail.com>
2020-07-03 16:08:19 +0300

    @Import

---
+++ spring-one/src/main/java/victor/training/spring/web/DBConfig.java
@@ -0,0 +1,9 @@
+package victor.training.spring.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DBConfig {
+//   @Bean
+}
--- spring-one/src/main/java/victor/training/spring/web/SpaApplication.java
+++ spring-one/src/main/java/victor/training/spring/web/SpaApplication.java
@@ -3,11 +3,13 @@ package victor.training.spring.web;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Import;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableCaching
 @EnableSwagger2
 @SpringBootApplication
+@Import(DBConfig.class)
 public class SpaApplication {
 
 	public static void main(String[] args) {
