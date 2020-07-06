5f29aa1afe6e74c57cbc17ce75b18d1c31d118c1
Victor Rentea <victorrentea@gmail.com>
2020-07-06 12:13:18 +0300

    Client calling Service with hard-coded URL

--- spring-microservices/boot-client/src/main/java/victor/training/spring/ms/bootclient/ReservationRest.java
+++ spring-microservices/boot-client/src/main/java/victor/training/spring/ms/bootclient/ReservationRest.java
@@ -1,12 +1,18 @@
 package victor.training.spring.ms.bootclient;
 
 import lombok.Data;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @RestController
 public class ReservationRest {
@@ -14,11 +20,16 @@ public class ReservationRest {
    public List<String> getNames() {
       RestTemplate rest = new RestTemplate();
       // cum pot sa demarshallizez o List<ReservationDto> din JSON-ul de raspuns?
-//      List<ReservationDto>.class
 
+      ResponseEntity<List<ReservationDto>> response = rest.exchange("http://localhost:8080/reservations",
+          HttpMethod.GET, null,
+          new ParameterizedTypeReference<List<ReservationDto>>() {
+          });
 
-//      rest.getForObject("http://localhost:8080/resevations", ReservationDto.class);
-      return Collections.emptyList();
+      return response.getBody()
+          .stream()
+          .map(ReservationDto::getName)
+          .collect(toList());
    }
 }
 
