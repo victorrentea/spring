55133a8b87d4bd69f52f1127f8e2ee84785dad89
Victor Rentea <victorrentea@gmail.com>
2020-07-06 09:53:43 +0300

    Repository JPA

--- spring-microservices/boot-service/src/main/java/victor/training/spring/ms/bootservice/BootServiceApplication.java
+++ spring-microservices/boot-service/src/main/java/victor/training/spring/ms/bootservice/BootServiceApplication.java
@@ -1,17 +1,35 @@
 package victor.training.spring.ms.bootservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @SpringBootApplication
-public class BootServiceApplication {
+public class BootServiceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootServiceApplication.class, args);
 	}
+
+	@Autowired
+	private ReservationRepo reservationRepo;
+
+//	@Transactional
+	@Override
+	public void run(String... args) throws Exception {
+		reservationRepo.save(new Reservation("John"));
+	}
+}
+
+interface ReservationRepo extends JpaRepository<Reservation, Long> {
 
 }
 
@@ -19,11 +37,12 @@ public class BootServiceApplication {
 @Entity
 class Reservation {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	protected Reservation() {}
 
-	public Reservation(Long id) {
-		this.id = id;
+	public Reservation(String name) {
+		this.name = name;
 	}
 }
