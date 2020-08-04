0caddead67b83f7cc7d11ebbde52b17748abac92
Victor Rentea <victorrentea@gmail.com>
2020-08-04 16:28:32 +0300

    @Async sa protejezi o resursa critica care nusuporta mult multithreading

--- spring-one/src/main/java/victor/training/spring/web/controller/AsyncController.java
+++ spring-one/src/main/java/victor/training/spring/web/controller/AsyncController.java
@@ -2,7 +2,9 @@ package victor.training.spring.web.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +22,14 @@ public class AsyncController {
    private final JobCareDureazaMult job1;
    private final JobCareDureazaMult2 job2;
 
+   @Bean
+   public ThreadPoolTaskExecutor poolPentruAiaFragili() {
+      ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
+      pool.setMaxPoolSize(1);
+      pool.setCorePoolSize(1);
+      return pool;
+   }
+
    static ExecutorService pool = Executors.newFixedThreadPool(2);
 
    @GetMapping("job")
@@ -28,6 +38,8 @@ public class AsyncController {
       log.info("Oare cui trimit eu apelul meu de functie? " + job1.getClass());
       CompletableFuture<String> job1Result = job1.m();
       CompletableFuture<String> job2Result = job2.m();
+
+
 
       CompletableFuture<String> finalResult = job1Result.thenCombineAsync(job2Result, (j1, j2) -> "Rezultate: " + j1 + " si " + j2);
       // finalResult devine gata dupa ce ambele promiserui sunt gata
@@ -42,14 +54,20 @@ public class AsyncController {
 //      log.debug("Gata Ambele");
 //      return "Rezultate: " + status1 + " si " + status2;
    }
+
+   @GetMapping("extern")
+   public String callExtern() throws ExecutionException, InterruptedException {
+      return job1.m().get();
+
+   }
 }
 @Component
 @Slf4j
 class JobCareDureazaMult {
-   @Async
+   @Async("poolPentruAiaFragili") //thread poolul de aici cat de mare tre sa fie 300 - DDOS
    public CompletableFuture<String> m() {
       log.debug("Start");
-      ThreadUtils.sleep(3000);
+      ThreadUtils.sleep(3000); // REST Call catre api-ul echipe adverse
       log.debug("End");
       return completedFuture("SUCCESS");
    }
