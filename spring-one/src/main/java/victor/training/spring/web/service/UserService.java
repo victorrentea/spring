e43c8631f74fb5fbb8e264d047c5e74145e90579
Victor Rentea <victorrentea@gmail.com>
2020-08-04 10:11:44 +0300

    Schite la intrebari + cache evict merge pe o singura masina

--- spring-one/src/main/java/victor/training/spring/web/service/UserService.java
+++ spring-one/src/main/java/victor/training/spring/web/service/UserService.java
@@ -2,10 +2,15 @@ package victor.training.spring.web.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import victor.training.spring.web.domain.User;
 import victor.training.spring.web.repo.UserRepo;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -13,6 +18,22 @@ import victor.training.spring.web.repo.UserRepo;
 public class UserService  {
     private final UserRepo userRepo;
 
+//    static Map<String, String> cachePtM = new HashMap<>();
+//    static Map<List<Object>, String> cachePtM2 = new HashMap<>();
+//    static Map<Void, Long> usersCount = new HashMap<>();
+//
+//    @Cacheable("m")
+//    public String m(String s) {
+//
+//    }
+//    @Cacheable("m2")
+//    public String m2(String s, String p, Integer x) {
+//        m(s);
+//    }
+//    @CacheEvict("m2")
+//    public String evictM(String s, String p, Integer x) {
+//
+//    }
 
     @Cacheable("usersCount")
     public long countUsers() {
@@ -25,6 +46,7 @@ public class UserService  {
     // TODO 3 Avoid inconsistencies on multiple machines - use Redis as centralized cache
     // Scenario: N1.get(=2), N2.get(=2), N1.create, N1.get(=3), N2.get(=2) STALE!!!
     // TODO 4 At restart, clean the Redis cache (with CLR)
+    @CacheEvict(cacheNames = "usersCount", allEntries = true)
     public void createUser() {
         userRepo.save(new User());
     }
