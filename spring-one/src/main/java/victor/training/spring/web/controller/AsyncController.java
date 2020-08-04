8fd3b9ca23b394cc5c9ac0bec51ad2a108bfec5a
Victor Rentea <victorrentea@gmail.com>
2020-08-04 22:37:34 +0300

    yolo

--- spring-one/src/main/java/victor/training/spring/web/controller/AsyncController.java
+++ spring-one/src/main/java/victor/training/spring/web/controller/AsyncController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
