7552ffca0928f566ded0a045b52368b403479f71
Victor Rentea <victorrentea@gmail.com>
2020-08-05 11:02:35 +0300

    pt Andreea

--- spring-one/src/main/java/victor/training/spring/web/controller/TrainingController.java
+++ spring-one/src/main/java/victor/training/spring/web/controller/TrainingController.java
@@ -1,17 +1,17 @@
 package victor.training.spring.web.controller;
 
 import java.text.ParseException;
+import java.util.EnumMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import victor.training.spring.web.controller.dto.TrainingDto;
 import victor.training.spring.web.service.TrainingService;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static java.util.Arrays.asList;
 
 @RestController
 @RequestMapping("/rest/trainings")
@@ -55,3 +55,47 @@ public class TrainingController {
 		trainingService.createTraining(dto);
 	}
 }
+
+//
+//enum TriggerType {
+//	TIMESTAMP(new TimestampTypeStrategy()),//(true, false, 10, SECONDS),
+//	LOCATION,
+//	LANGUAGE
+//
+//	;
+//
+//	public final TriggerTypeStrategy strategy;
+//
+//	TriggerType(TriggerTypeStrategy strategyClass) {
+//
+//		this.strategy = strategyClass;
+//	}
+//
+//	public List<String> getEntityList() {
+//		return TriggerEntityList.TRIGGER_TYPE_LIST.get(this);
+//	}
+//}
+//
+//
+//interface TriggerTypeStrategy {
+//	List<String> getEntityList();
+//	List<String> getEntityList2();
+//	String getEntityList3();
+//
+//}
+//class TimestampTypeStrategy implements TriggerTypeStrategy{
+//
+//}
+//
+//class TriggerEntityList {
+//	public static final EnumMap<TriggerType, List<String>> TRIGGER_TYPE_LIST = new EnumMap<>(TriggerType.class);
+//	static {
+//		TRIGGER_TYPE_LIST.put(TriggerType.TIMESTAMP,  asList("a","b"));
+//		for (TriggerType value : TriggerType.values()) {
+//			if (!TRIGGER_TYPE_LIST.containsKey(value)) {
+//				throw new IllegalArgumentException();
+//			}
+//		}
+//	}
+//
+//}
