9ba93f83d6e38934ee4728d68b1a1360d8e99afc
Victor Rentea <victorrentea@gmail.com>
2020-08-05 09:29:21 +0300

    primul nostru login. Sa fie intr-un ceas bun

--- spring-one/src/main/java/victor/training/spring/web/SecurityConfig.java
+++ spring-one/src/main/java/victor/training/spring/web/SecurityConfig.java
@@ -1,26 +1,42 @@
 package victor.training.spring.web;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.stereotype.Service;
 
-//@EnableWebSecurity
+@Service
+@EnableWebSecurity
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig  /*extends WebSecurityConfigurerAdapter*/ {
+public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
     // TODO [SEC] Start with ROLE-based authorization on URL-patterns
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//       http
-//            .csrf().disable()
-//            ;
-//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       http
+            .csrf().disable()
+            .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin().permitAll()
+            ;
+    }
+    // authentication: certifica ca esti cine spui ca esti
+    // autorizarea: dat fiind ca esti userul X, ai voie sa faci operatia Y
 
     // *** Dummy users 100% in-mem
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails userDetails = User.withDefaultPasswordEncoder()...
-//        return new InMemoryUserDetailsManager(userDetails, adminDetails);
-//    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder().username("user").password("user").roles("USER").build();
+        UserDetails adminDetails = User.withDefaultPasswordEncoder().username("admin").password("admin").roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(userDetails, adminDetails);
+    }
 
     // ... then, switch to loading user data from DB:
     // *** Also loading data from DB
