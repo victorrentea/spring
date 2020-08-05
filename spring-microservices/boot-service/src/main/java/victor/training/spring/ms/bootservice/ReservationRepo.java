8c4fa8ae0517bcb746595dc827e26ee4a8f841be
Victor Rentea <victorrentea@gmail.com>
2020-08-06 02:42:33 +0300

    compilation cleanup + restructure JWT app

--- spring-microservices/boot-service/src/main/java/victor/training/spring/ms/bootservice/ReservationRepo.java
+++ spring-microservices/boot-service/src/main/java/victor/training/spring/ms/bootservice/ReservationRepo.java
@@ -1,11 +1,6 @@
 package victor.training.spring.ms.bootservice;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 public interface ReservationRepo extends JpaRepository<Reservation, Long> {
 }
