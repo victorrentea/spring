147a611d6daeb121133797ac3b9f764b5ef124ac
Victor Rentea <victorrentea@gmail.com>
2020-10-07 17:49:50 +0300

    @Scheduled calling @Async = un bug in asteptare

--- spring-one/src/main/java/victor/training/spring/scheduled/ScheduledApp.java
+++ spring-one/src/main/java/victor/training/spring/scheduled/ScheduledApp.java
@@ -1,6 +1,7 @@
 package victor.training.spring.scheduled;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import victor.training.spring.ThreadUtils;
 
@@ -17,8 +19,12 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 @SpringBootApplication
 @Slf4j
+@EnableAsync
 @EnableScheduling
 public class ScheduledApp  {
+    @Autowired
+    private AltaClasa altaClasa;
+
     public static void main(String[] args) {
         SpringApplication.run(ScheduledApp.class);
     }
@@ -35,10 +41,9 @@ public class ScheduledApp  {
 
     // idee enterprise: quartz (+2PC prin JDBC XA), care tine evidenta cand si cat si daca, etc.
     public void lookIntoFolder() {
-        log.debug("Looking into folder");
-        ThreadUtils.sleep(7000);
-        log.debug("DONE");
+        altaClasa.altaMetoda();
     }
+
 
     // TODO 2 define another task at each second. This should not block.
     // TODO explore application.properties (thread #)
@@ -46,4 +51,17 @@ public class ScheduledApp  {
 //    public void pollFast() {
 //        log.debug("FAST each second");
 //    }
+}
+
+@Component
+@Slf4j
+class AltaClasa {
+
+    @Async
+    public void altaMetoda() {
+        log.debug("Looking into folder");
+        ThreadUtils.sleep(7000);
+        log.debug("DONE");
+
+    }
 }
--- spring-one/src/main/java/victor/training/spring/transactions/HashIncredibil.java
+++ spring-one/src/main/java/victor/training/spring/transactions/HashIncredibil.java
@@ -3,8 +3,8 @@ package victor.training.spring.transactions;
 public class HashIncredibil {
    public static void main(String[] args) {
 
-      System.out.println("HVB22Q".hashCode());
-      System.out.println("HVB232".hashCode());
+      System.out.println("2Q".hashCode());
+      System.out.println("32".hashCode());
    }
 }
 
