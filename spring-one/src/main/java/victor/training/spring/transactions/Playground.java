16b972d9d3739d1a129788a30e9a32037cadd8a2
Victor Rentea <victorrentea@gmail.com>
2020-10-07 16:26:18 +0300

    exceptiile runtime by default cauzeaza tranzactia curenta sa faca ROLLBACK

--- spring-one/src/main/java/victor/training/spring/transactions/Playground.java
+++ spring-one/src/main/java/victor/training/spring/transactions/Playground.java
@@ -16,10 +16,10 @@ public class Playground {
     private final AnotherClass other;
 
     @Transactional
-    public void transactionOne() throws Exception {
+    public void transactionOne() {
         jdbc.update("insert into TEACHER(ID, NAME) VALUES ( 99, 'Profu de Mate' )");
         jdbc.update("insert into MESSAGE(id, message) values ( 100,'null' )");
-        throw new Exception("Intentioanta");
+        throw new IllegalArgumentException("Intentioanta");
     }
     @Transactional
     public void transactionTwo() {
