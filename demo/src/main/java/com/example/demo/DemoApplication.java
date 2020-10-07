06c5431813f7e03ff993cf639e23bad111fe31f5
Victor Rentea <victorrentea@gmail.com>
2020-10-07 12:34:03 +0300

    properties file (next: yaml)

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
-	@Value("${param}")
+	@Value("${param.val}")
 	private String param;
 
 	public static void main(String[] args) {
