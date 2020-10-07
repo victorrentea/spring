e03a6c6f5ecbb9501c609112e51fe69ea3c95997
Victor Rentea <victorrentea@gmail.com>
2020-10-07 17:32:38 +0300

    Un @Scheduled nu va fi startat din nou pana cand el nu a terminat executia sa.
    Alte @Scheduled insa pot executa in paralel daca scrii ce e aici in application.properties

--- spring-one/src/main/java/victor/training/spring/scheduled/ScheduledApp.java
+++ spring-one/src/main/java/victor/training/spring/scheduled/ScheduledApp.java
@@ -16,6 +16,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 @SpringBootApplication
 @Slf4j
+@EnableScheduling
 public class ScheduledApp  {
     public static void main(String[] args) {
         SpringApplication.run(ScheduledApp.class);
@@ -24,6 +25,7 @@ public class ScheduledApp  {
     // TODO 1 Should run every 5 seconds / configurable / cron "*/5 * * * * *"
     // TODO 3 Play with delays. cron vs fixedRate? Overlapping executions?
     // TODO 4 Should run on a separate 1-thread pool
+    @Scheduled(fixedRate = 5000)
     public void lookIntoFolder() {
         log.debug("Looking into folder");
         ThreadUtils.sleep(7000);
@@ -32,7 +34,8 @@ public class ScheduledApp  {
 
     // TODO 2 define another task at each second. This should not block.
     // TODO explore application.properties (thread #)
-    public void pollFast() {
-        log.debug("FAST each second");
-    }
+//    @Scheduled(fixedRate = 1000)
+//    public void pollFast() {
+//        log.debug("FAST each second");
+//    }
 }
