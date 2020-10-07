493dd9a12964d85e5b1558b8b9c6b4cfb058cea1
Victor Rentea <victorrentea@gmail.com>
2020-10-07 10:14:30 +0300

    Spring nu mai poate chema metoda load asa cum cerea @PostConstruct pt ca metoda are parametrii

--- spring-one/src/main/java/victor/training/spring/life/LifeApp.java
+++ spring-one/src/main/java/victor/training/spring/life/LifeApp.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Locale;
 import java.util.Map;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.CustomScopeConfigurer;
@@ -45,30 +46,28 @@ public class LifeApp implements CommandLineRunner{
 		System.out.println("Am primit cadou de la spring " + altaClasa);
 		System.out.println("Param de command: " + Arrays.toString(args));
 		exporter.export(Locale.ENGLISH);
-		// TODO exporter.export(Locale.FRENCH);
-		
+		exporter.export(Locale.FRENCH);
 	}
 }
 @Slf4j
 @Service
+@RequiredArgsConstructor
 class OrderExporter  {
-	@Autowired
-	private InvoiceExporter invoiceExporter;
-	@Autowired
-	private LabelService labelService;
+	private final InvoiceExporter invoiceExporter;
+	private final LabelService labelService;
 
 	public void export(Locale locale) {
 		log.debug("Running export in " + locale);
-		log.debug("Origin Country: " + labelService.getCountryName("rO")); 
+		log.debug("Origin Country: " + labelService.getCountryName("rO"));
 		invoiceExporter.exportInvoice();
 	}
 }
 @Slf4j
-@Service 
+@Service
+@RequiredArgsConstructor
 class InvoiceExporter {
-	@Autowired
-	private LabelService labelService;
-	
+	private final LabelService labelService;
+
 	public void exportInvoice() {
 		log.debug("Invoice Country: " + labelService.getCountryName("ES"));
 	}
@@ -87,9 +86,9 @@ class LabelService {
 	private Map<String, String> countryNames;
 
 	@PostConstruct
-	public void load() {
+	public void load(Locale locale) {
 		log.debug("LabelService.load() on instance " + this.hashCode());
-		countryNames = countryRepo.loadCountryNamesAsMap(Locale.ENGLISH);
+		countryNames = countryRepo.loadCountryNamesAsMap(locale);
 	}
 	
 	public String getCountryName(String iso2Code) {
