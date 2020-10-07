bd641b3a2ee1e682e1c51ec5a1b29f9ecd7a8891
Victor Rentea <victorrentea@gmail.com>
2020-10-07 11:00:26 +0300

    revert to prototype

--- spring-one/src/main/java/victor/training/spring/life/LifeApp.java
+++ spring-one/src/main/java/victor/training/spring/life/LifeApp.java
@@ -6,15 +6,15 @@ import java.util.Map;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.context.support.SimpleThreadScope;
 import org.springframework.stereotype.Service;
 import victor.training.spring.life.subpachet.AltaClasa;
@@ -28,16 +28,16 @@ public class LifeApp implements CommandLineRunner{
 		configurer.addScope("thread", new SimpleThreadScope());
 		return configurer;
 	}
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(LifeApp.class, args);
 	}
-	
-	@Autowired 
+
+	@Autowired
 	private OrderExporter exporter;
 	@Autowired
 	private AltaClasa altaClasa;
-	
+
 	// TODO [1] make singleton; multi-thread + mutable state = BAD
 	// TODO [2] instantiate manually, set dependencies, pass around; no AOP
 	// TODO [3] prototype scope + ObjectFactory or @Lookup. Did you said "Factory"? ...
@@ -53,27 +53,17 @@ public class LifeApp implements CommandLineRunner{
 @Slf4j
 @Service
 @RequiredArgsConstructor
-abstract class OrderExporter  {
+class OrderExporter  {
 	private final InvoiceExporter invoiceExporter;
-//	private final LabelService labelService;
-
-//	OrderExporter(InvoiceExporter invoiceExporter, LabelService labelService) {
-//		this.invoiceExporter = invoiceExporter;
-//		this.labelService = labelService;
-//	}
-
+	private final ObjectFactory<LabelService> labelServiceFactory;
 
 	public void export(Locale locale) {
-//		log.debug("Oare pe ce obiect chem eu metodele mele ? " + labelService.getClass());
 		log.debug("Running export in " + locale);
-		LabelService labelService = getNewLabelService();
+		LabelService labelService = labelServiceFactory.getObject();
 		labelService.load(locale);
 		log.debug("Origin Country: " + labelService.getCountryName("rO"));
 		invoiceExporter.exportInvoice(labelService);
 	}
-
-	@Lookup
-	public abstract LabelService getNewLabelService();
 }
 @Slf4j
 @Service
@@ -89,7 +79,6 @@ class InvoiceExporter {
 @Slf4j
 @Service
 @Scope("prototype")
-//@Scope(scopeName = "thread", proxyMode = ScopedProxyMode.TARGET_CLASS)
 class LabelService {
 	private final CountryRepo countryRepo;
 
