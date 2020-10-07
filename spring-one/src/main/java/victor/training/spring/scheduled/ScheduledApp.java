c9511e4e6c77d89d132ee1f23475bbccd3350319
Victor Rentea <victorrentea@gmail.com>
2020-10-07 17:43:37 +0300

    @Scheduled DONE

--- spring-one/src/main/java/victor/training/spring/scheduled/ScheduledApp.java
+++ spring-one/src/main/java/victor/training/spring/scheduled/ScheduledApp.java
@@ -10,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.bind.annotation.GetMapping;
 import victor.training.spring.ThreadUtils;
 
 import java.util.concurrent.ThreadPoolExecutor;
@@ -25,7 +26,14 @@ public class ScheduledApp  {
     // TODO 1 Should run every 5 seconds / configurable / cron "*/5 * * * * *"
     // TODO 3 Play with delays. cron vs fixedRate? Overlapping executions?
     // TODO 4 Should run on a separate 1-thread pool
-    @Scheduled(fixedRate = 5000)
+//    @Scheduled(fixedRate = 5000)
+//    @Scheduled(fixedDelay = 5000)
+    @Scheduled(cron = "${folder.poller.cron}")
+
+    // idee creatza:
+//    @GetMapping("/check-folder") // + crontab curl http://locahost:8080/check-folder
+
+    // idee enterprise: quartz (+2PC prin JDBC XA), care tine evidenta cand si cat si daca, etc.
     public void lookIntoFolder() {
         log.debug("Looking into folder");
         ThreadUtils.sleep(7000);
