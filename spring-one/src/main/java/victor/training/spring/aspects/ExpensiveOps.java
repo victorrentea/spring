0ca7edfa86f8949345b91557eb6bdaa014c90f21
Victor Rentea <victorrentea@gmail.com>
2020-10-07 14:32:49 +0300

    RAU: metoda finala e silently ignored de proxy-uri. RAU. RAU. as fi preferat sa crape la deploy.

--- spring-one/src/main/java/victor/training/spring/aspects/ExpensiveOps.java
+++ spring-one/src/main/java/victor/training/spring/aspects/ExpensiveOps.java
@@ -18,13 +18,13 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
-public final class ExpensiveOps {
+public class ExpensiveOps {
 	private final static Logger log = LoggerFactory.getLogger(ExpensiveOps.class);
 	
 	private static final BigDecimal TWO = new BigDecimal("2");
-	
+
 	@Cacheable("primes")
-	public Boolean isPrime(int n) {
+	public final Boolean isPrime(int n) {
 		log.debug("Computing isPrime({})...", n);
 		new RuntimeException().printStackTrace();
 		BigDecimal number = new BigDecimal(n);
