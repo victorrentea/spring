c0b4b449d5c9db936b8b7cdd3f8d3ed201c11bf2
Victor Rentea <victorrentea@gmail.com>
2020-10-07 12:29:39 +0300

    properties source

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	@Value("${param}")
+	private String param;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	public void run(String... args) throws Exception {
+		System.out.println(param);
+	}
 }
--- spring-one/src/main/java/victor/training/spring/props/PropertiesApp.java
+++ spring-one/src/main/java/victor/training/spring/props/PropertiesApp.java
@@ -2,25 +2,43 @@ package victor.training.spring.props;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication
-public class PropertiesApp {
+public class PropertiesApp implements  CommandLineRunner{
     public static void main(String[] args) {
         SpringApplication.run(PropertiesApp.class);
     }
 
+    @Autowired
+    EmailSender emailSender;
+    @Override
+    public void run(String... args) throws Exception {
+        emailSender.sendEmail();
+    }
 }
 
+@Service
+class EmailSender {
+    @Value("${email.reply-to}") // nu faceti asta:
+    private String replyTo;
 
+    public void sendEmail() {
+        //
+        System.out.println("Add reply-to: " + replyTo);
+    }
+}
 
 
