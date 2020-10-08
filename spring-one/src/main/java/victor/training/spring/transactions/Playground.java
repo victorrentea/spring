8f2ca31fa49de692be622f089e931bd5a1439d06
Victor Rentea <victorrentea@gmail.com>
2020-10-08 09:29:19 +0300

    daca o metoda nu are @Transactional pe ea/clasa, atunci exceptia aruncata nu distruge tranzactia

--- spring-one/src/main/java/victor/training/spring/transactions/Playground.java
+++ spring-one/src/main/java/victor/training/spring/transactions/Playground.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionTemplate;
@@ -26,6 +27,7 @@ public class Playground {
     @Autowired
     public void initTxTemplate(PlatformTransactionManager transactionManager) {
         transactionTemplate = new TransactionTemplate(transactionManager);
+        transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
     }
 
 //    @Transactional
@@ -33,12 +35,12 @@ public class Playground {
         transactionTemplate.execute(status -> {
 
             jdbc.update("insert into TEACHER(ID, NAME) VALUES ( 99, 'Profu de Mate' )");
-    //        try {
+            try {
                 other.method();
-    //        } catch (Exception e) {
+            } catch (Exception e) {
                 // shaworma - posibil viitor career path daca faci asta des.
     //            jdbc.update("insert into MESSAGE(id, message) values ( 100,'Error: "+e.getMessage()+"' )");
-    //        }
+            }
             return null;
         });
     }
@@ -56,8 +58,8 @@ class AnotherClass {
     private final MessageRepo repo;
     private final JdbcTemplate jdbc;
 
-    @Async("myServiceExecutor")
-    @Transactional/*(propagation = Propagation.REQUIRES_NEW)*/
+//    @Async("myServiceExecutor")
+//    @Transactional/*(propagation = Propagation.REQUIRES_NEW)*/
     public void method() {
         jdbc.update("insert into TEACHER(ID, NAME) VALUES ( 101, 'Profu de Mate' )");
         throw new IllegalArgumentException("Intentioanta");
