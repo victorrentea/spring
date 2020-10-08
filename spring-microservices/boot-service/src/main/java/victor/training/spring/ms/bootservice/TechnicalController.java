4d21905fbd87e9aed38a663f679d5827b0d258aa
Victor Rentea <victorrentea@gmail.com>
2020-10-08 10:25:17 +0300

    actuator care permite coborarea logului pe "debug" la runtime

--- spring-microservices/boot-service/src/main/java/victor/training/spring/ms/bootservice/TechnicalController.java
+++ spring-microservices/boot-service/src/main/java/victor/training/spring/ms/bootservice/TechnicalController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.PostConstruct;
 
 @RestController
+@Slf4j
 public class TechnicalController {
    @Value("${hello.message}")
    private String helloMessage;
@@ -22,7 +23,8 @@ public class TechnicalController {
 
    @GetMapping("hello")
    public String hello() {
-      if (true) throw new IllegalArgumentException();
+      log.debug("Stuff");
+//      if (true) throw new IllegalArgumentException("inginer");
       return helloMessage;
    }
 }
