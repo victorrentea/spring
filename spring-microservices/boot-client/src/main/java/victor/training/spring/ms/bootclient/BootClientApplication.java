ba4b1e7565e14301c26a146e76a931f7e7177fca
Victor Rentea <victorrentea@gmail.com>
2020-10-08 11:48:48 +0300

    PROST. RAU. GRESIT. Aparent bine. Dar gresit. REGULA: NU TRIMITI MODIFICARI PESTE REST.
    Ci le vom trimite ca MESAJE PE COZI cu guranteed delivery, tranzactate si persistate.

--- spring-microservices/boot-client/src/main/java/victor/training/spring/ms/bootclient/BootClientApplication.java
+++ spring-microservices/boot-client/src/main/java/victor/training/spring/ms/bootclient/BootClientApplication.java
@@ -8,11 +8,15 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.http.client.ClientHttpRequestInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -26,6 +30,7 @@ public class BootClientApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
--- spring-microservices/boot-client/src/main/java/victor/training/spring/ms/bootclient/ReservationController.java
+++ spring-microservices/boot-client/src/main/java/victor/training/spring/ms/bootclient/ReservationController.java
@@ -3,21 +3,16 @@ package victor.training.spring.ms.bootclient;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.http.client.ClientHttpRequestInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,7 +25,8 @@ public class ReservationController {
    @GetMapping("reservation-names")
    public String getReservationNames() {
 
-      ReservationsListDto list = rest.getForObject("http://localhost:8080/reservations", ReservationsListDto.class);
+
+      ReservationsListDto list = rest.getForObject("http://boot-service/reservations", ReservationsListDto.class);
 
       return list.getReservations().stream().map(ReservationDto::getName).collect(Collectors.joining(","));
 
@@ -45,16 +41,20 @@ public class ReservationController {
 //   @Autowired
 //   private Source channel;
 
+
+   // "CEA MAI GRAVA BOALA DIN PROIECTE CU MICORSERVICII ESTE FOLOSIREA EXCLUSIVA A REST PENTRU COMUNICATIA INTRE ELE"
+
+
+
    @PostMapping("reservation")
    public String createReservation(@RequestParam String name) {
       log.info("Rulez");
-//      ReservationDto dto = new ReservationDto();
-//      dto.setName(name);
-//      rest.postForObject("http://boot-service/reservations", dto, Void.class);
+      ReservationDto dto = new ReservationDto();
+      dto.setName(name);
+      rest.postForObject("http://boot-service/reservations", dto, Void.class);
 
 //      Message<String> message = MessageBuilder.withPayload(name).build();
 //      channel.output().send(message);
-
       System.out.println("Message Sent");
       log.info("End");
       return "Created reservation id: ";
