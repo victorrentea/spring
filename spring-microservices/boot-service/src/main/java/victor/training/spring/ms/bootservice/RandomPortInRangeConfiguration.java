dbf89d3bbaa4440ad79ac465faf8275d88ef8be6
Victor Rentea <victorrentea@gmail.com>
2020-10-08 12:06:12 +0300

    Pornind de mai multe ori boot-service, boot-client va trimite requesturi la ambele. (load balancing client side)

--- spring-microservices/boot-service/src/main/java/victor/training/spring/ms/bootservice/RandomPortInRangeConfiguration.java
+++ spring-microservices/boot-service/src/main/java/victor/training/spring/ms/bootservice/RandomPortInRangeConfiguration.java
@@ -1,25 +1,25 @@
-//package victor.training.spring.ms.bootservice;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-//import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.util.SocketUtils;
-//
-//// setting server.port=0 doesn't play well with Eureka
-//@Configuration
-//public class RandomPortInRangeConfiguration implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
-//
-//    @Value("${port.number.min:8080}")
-//    private Integer minPort;
-//
-//    @Value("${port.number.max:8090}")
-//    private Integer maxPort;
-//
-//    @Override
-//    public void customize(ConfigurableServletWebServerFactory factory) {
-//        int port = SocketUtils.findAvailableTcpPort(minPort, maxPort);
-//        factory.setPort(port);
-//        System.getProperties().put("server.port", port);
-//    }
-//}
+package victor.training.spring.ms.bootservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.SocketUtils;
+
+// setting server.port=0 doesn't play well with Eureka
+@Configuration
+public class RandomPortInRangeConfiguration implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+
+    @Value("${port.number.min:8080}")
+    private Integer minPort;
+
+    @Value("${port.number.max:8090}")
+    private Integer maxPort;
+
+    @Override
+    public void customize(ConfigurableServletWebServerFactory factory) {
+        int port = SocketUtils.findAvailableTcpPort(minPort, maxPort);
+        factory.setPort(port);
+        System.getProperties().put("server.port", port);
+    }
+}
