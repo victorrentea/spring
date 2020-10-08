b4456bd50b5ad271d30b1b383a76b6743c77a5ad
Victor Rentea <victorrentea@gmail.com>
2020-10-08 15:56:18 +0300

    URL-based restrictions.

--- spring-one/src/main/java/victor/training/spring/web/SecurityConfig.java
+++ spring-one/src/main/java/victor/training/spring/web/SecurityConfig.java
@@ -1,6 +1,7 @@
 package victor.training.spring.web;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,29 +14,33 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    // TODO [SEC] Start with ROLE-based authorization on URL-patterns
+   // TODO [SEC] Start with ROLE-based authorization on URL-patterns
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-       http
-            .csrf().disable()
-            .authorizeRequests()
-               .anyRequest().authenticated()
-            .and()
-            .formLogin().permitAll()
-       // unsecured/welcome-info la toti
-       // admin/restart la admin
-       // DELETE pe rest/trainings/{id} doar admin
-       // in rest pt celelalte sa fii autentificat cu orice user, sa fii trecut de login doar.
-            ;
-    }
-    // *** Dummy users 100% in-mem
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails userDetails = User.withDefaultPasswordEncoder().username("user").password("parola").roles("USER").build();
-        UserDetails adminDetails = User.withDefaultPasswordEncoder().username("admin").password("parola").roles("ADMIN").build();
-        return new InMemoryUserDetailsManager(userDetails, adminDetails);
-    }
+   @Override
+   protected void configure(HttpSecurity http) throws Exception {
+      http
+          .csrf().disable()
+          .authorizeRequests()
+             .mvcMatchers("unsecured/welcome-info").anonymous()
+             .mvcMatchers("admin/*").hasRole("ADMIN")
+             .mvcMatchers(HttpMethod.DELETE, "rest/trainings/{id}").hasRole("ADMIN")
+             .anyRequest().authenticated()
+          .and()
+          .formLogin().permitAll()
+      // unsecured/welcome-info la toti
+      // admin/restart la admin
+      // DELETE pe rest/trainings/{id} doar admin
+      // OK in rest pt celelalte sa fii autentificat cu orice user, sa fii trecut de login doar.
+      ;
+   }
+
+   // *** Dummy users 100% in-mem
+   @Bean
+   public UserDetailsService userDetailsService() {
+      UserDetails userDetails = User.withDefaultPasswordEncoder().username("user").password("parola").roles("USER").build();
+      UserDetails adminDetails = User.withDefaultPasswordEncoder().username("admin").password("parola").roles("ADMIN").build();
+      return new InMemoryUserDetailsManager(userDetails, adminDetails);
+   }
 /*
 
      ... then, switch to loading user data from DB:
