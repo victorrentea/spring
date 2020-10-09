8d905d00769d250729b4ec79427bdc049981ba0a
Victor Rentea <victorrentea@gmail.com>
2020-10-09 11:11:33 +0300

    @Transactional @Test  curata orice ai inserat in baza din testul tau

--- spring-one/src/main/java/victor/training/spring/web/controller/dto/ProductSearchCriteria.java
+++ spring-one/src/main/java/victor/training/spring/web/controller/dto/ProductSearchCriteria.java
@@ -10,4 +10,9 @@ public class ProductSearchCriteria { // smells like JSON
     public String name;
     public ProductCategory category;
     public Long supplierId;
+    // name: [    ]
+    // category: [    |v]
+    // supplier: [    |v]
+    // [search]
+
 }
--- spring-one/src/test/java/victor/training/spring/web/spring/repo/ProductRepoSearchTest.java
+++ spring-one/src/test/java/victor/training/spring/web/spring/repo/ProductRepoSearchTest.java
@@ -2,19 +2,22 @@ package victor.training.spring.web.spring.repo;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 import victor.training.spring.web.controller.dto.ProductSearchCriteria;
 import victor.training.spring.web.domain.Product;
 import victor.training.spring.web.repo.ProductRepo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("db-mem")
+@ActiveProfiles({"db-mem","mock-user-service"})
+@Transactional
 public class ProductRepoSearchTest {
     @Autowired
     private ProductRepo repo;
@@ -23,6 +26,12 @@ public class ProductRepoSearchTest {
 
     @Test
     public void noCriteria() {
+        repo.save(new Product());
+        Assert.assertEquals(1, repo.search(criteria).size());
+        Assertions.assertThat(repo.search(criteria)).hasSize(1);
+    }
+    @Test
+    public void noCriteria2() {
         repo.save(new Product());
         Assert.assertEquals(1, repo.search(criteria).size());
         Assertions.assertThat(repo.search(criteria)).hasSize(1);
