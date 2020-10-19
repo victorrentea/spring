cc50a4509b8121132a4d7901cc2fa2e025532ef1
Victor Rentea <victorrentea@gmail.com>
2020-10-20 01:17:22 +0300

    CSRF attack

--- spring-one/src/main/java/victor/training/spring/web/controller/VictimController.java
+++ spring-one/src/main/java/victor/training/spring/web/controller/VictimController.java
@@ -1,21 +1,31 @@
 package victor.training.spring.web.controller;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
-@RestController
+@Controller
 @RequestMapping("victim")
 public class VictimController {
    @PostMapping("transfer-rest")
    //@CrossOrigin(origins = "*")  or  implement WebMvcConfigurer with @Configuration + cors
-   public void transfer(@RequestBody MoneyTransferRequest request) {
-      log.warn("Transferring {} to {}, IBAN = {}", request.amount, request.recipientFullName, request.recipientIban);
+   public void transferRest(@RequestBody MoneyTransferRequest request) {
+      log.warn("Transferring {} to {}, IBAN = {}", request.getAmount(), request.getRecipientFullName(), request.getRecipientIban());
+   }
+
+   @PostMapping("transfer-form")
+   @ResponseBody
+   public String transferForm(@ModelAttribute MoneyTransferRequest request) {
+      log.warn("Transferring {} to {}, IBAN = {}", request.getAmount(), request.getRecipientFullName(), request.getRecipientIban());
+      return "Done";
    }
 }
 
+@Data
 class MoneyTransferRequest {
-   public String recipientFullName;
-   public String recipientIban;
-   public int amount;
+   private String recipientFullName;
+   private String recipientIban;
+   private int amount;
 }
