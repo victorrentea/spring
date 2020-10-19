a947019984d6399aefb111ae77c7056a2eb04566
Victor Rentea <victorrentea@gmail.com>
2020-10-19 10:29:01 +0300

    DI intro

--- spring-one/src/main/java/victor/training/spring/life/Inainte.java
+++ spring-one/src/main/java/victor/training/spring/life/Inainte.java
@@ -1,12 +1,55 @@
 package victor.training.spring.life;
 
-public class Inainte {
-   A a;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.web.bind.annotation.RestController;
 
-   public void setA(A a) {
-      // IoC - Inversion of Control = tu nu-ti IEI singur ce ai nevoie, ci ti se da, in fct de ce declari ca vrei
+import javax.annotation.PostConstruct;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Component
+@Retention(RetentionPolicy.RUNTIME)
+@interface Facade {
+
+}
+
+//@Service
+//@Repository
+//@Controller
+//@RestController
+//@Configuration //@ServiceActivator
+@Facade
+class B {
+   private final A a;
+
+   public B(A a) {
       this.a = a;
    }
+
+   // singurul motiv real pt method injection 0.1%
+
+   private TransactionTemplate txTemplate;
+
+   @Autowired
+   public void setTransactionManager(PlatformTransactionManager transactionManager) {
+      // IoC - Inversion of Control = tu nu-ti IEI singur ce ai nevoie, ci ti se da, in fct de ce declari ca vrei
+      this.txTemplate = new TransactionTemplate(transactionManager);
+      txTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+   }
+
+   @PostConstruct
+   public void initialize() { // ruleaza DUPA constructor si DUPA ce au fost injectate toate dependintele
+      System.out.println(method());
+   }
+
 
    // tu vrei sa testezi asta:
    public String method() {
@@ -17,23 +60,12 @@ public class Inainte {
 }
 
 
-class ServiceLocator {
-   //aka ServiceRegistry
-//   public static A getObject(Class<?> aClass) {
-//      if din teste mi-a hackuit clasa A
-//          return hackA;
-//      else{
-//         return new A();
-//      }
-//   }
-   public void setFakeImplem(Class<?> c, Object o) {
-
-   }
+@Component
+class C {
 }
-
+@Component
 class A {
-
    public String met() { // vrei sa o mockuiesti
-      return null;
+      return "string";
    }
 }
