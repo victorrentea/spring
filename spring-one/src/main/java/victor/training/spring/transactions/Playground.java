9365e76664bf60c61078e29428b58daec9ac7e2c
Victor Rentea <victorrentea@gmail.com>
2020-10-19 17:32:55 +0300

    Events de handling pentru transaction succes/rollback/either. cleanup sau mailuri de ACK

--- spring-one/src/main/java/victor/training/spring/transactions/Playground.java
+++ spring-one/src/main/java/victor/training/spring/transactions/Playground.java
@@ -2,18 +2,39 @@ package victor.training.spring.transactions;
 
 import lombok.RequiredArgsConstructor;
 import org.hibernate.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import javax.persistence.EntityManager;
 import java.io.IOException;
 
+
+class DeleteTemporaryFiles {
+
+}
+class NotifyTransactionCompleted {
+   private final String email;
+
+   NotifyTransactionCompleted(String email) {
+      this.email = email;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+}
+
 @Service
 @RequiredArgsConstructor
 public class Playground {
+   private static final Logger log = LoggerFactory.getLogger(Playground.class);
     @Autowired
     private final AnotherClass other;
 
@@ -24,19 +45,34 @@ public class Playground {
         return entityManager.unwrap(Session.class);
     }
 
-    @Transactional
-    public void transactionOne() throws IOException {
-        try {
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+   public void notifyTxOK(NotifyTransactionCompleted notifyTransactionCompleted) {
+      log.info("Notifying success to : " + notifyTransactionCompleted.getEmail());
+   }
+   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMPLETION)
+   public void deleteTempFiles(DeleteTemporaryFiles event) {
+      log.info("Delete temp files : ");
+   }
+
+   @Transactional
+   public void transactionOne() throws IOException {
+      try {
 //        getSession().save(new Message("asdsadsadkasjdksajdksajdksajdksajdksajdksajdksajdksadjksadksajdksajdksajdsadksajdasdsadsadkasjdksajdksajdksajdksajdksajdksajdksajdksadjksadksajdksajdksajdsadksajdasdsadsadkasjdksajdksajdksajdksajdksajdksajdksajdksadjksadksajdksajdksajdsadksajdasdsadsadkasjdksajdksajdksajdksajdksajdksajdksajdksadjksadksajdksajdksajdsadksajdasdsadsadkasjdksajdksajdksajdksajdksajdksajdksajdksadjksadksajdksajdksajdsadksajdasdsadsadkasjdksajdksajdksajdksajdksajdksajdksajdksadjksadksajdksajdksajdsadksajd"));
-            boolean naspa = true;
-            getSession().save(new Message("JOB START"));
-           other.oMetodaChemataCandva(naspa);
-            getSession().save(new Message("JOB DONE"));
-        } catch (Exception e) {
-            other.persistFailureInDB(e.getMessage());
-            //throw e; NIMIC! VideoChat scrie pe tine
-        }
-    }
+         boolean naspa = true;
+         publisher.publishEvent(new NotifyTransactionCompleted("a@b.com"));
+         publisher.publishEvent(new DeleteTemporaryFiles(/*"file.getPath"*/));
+         log.info("Aici incep de fapt treaba");
+         getSession().save(new Message("JOB START"));
+         other.oMetodaChemataCandva(naspa);
+         getSession().save(new Message("JOB DONE"));
+      } catch (Exception e) {
+         other.persistFailureInDB(e.getMessage());
+         //throw e; NIMIC! VideoChat scrie pe tine
+      }
+   }
     @Transactional
     public void transactionTwo() {
         // TODO Repo API
@@ -46,12 +82,13 @@ public class Playground {
 @Service
 @RequiredArgsConstructor
 class AnotherClass {
+   private static final Logger log = LoggerFactory.getLogger(AnotherClass.class);
     private final MessageRepo repo;
 
 
     @Transactional
    public void oMetodaChemataCandva(boolean naspa) {
-        if (naspa) throw new RuntimeException();
+        if (false) throw new RuntimeException();
     }
 
     @Autowired
@@ -71,7 +108,7 @@ class AnotherClass {
 //        }
         Message message = new Message("inserata din alta clasa: " + messageStr);
         getSession().save(message);
-        System.out.println("Is the current tx dead ?" + getSession().getTransaction().getRollbackOnly());
-        System.out.println(message.getId());
+        log.info("Is the current tx dead ?" + getSession().getTransaction().getRollbackOnly());
+        log.info(message.getId() + " id");
     }
 }
