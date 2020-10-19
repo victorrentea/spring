045cefd57d5f6a9f55b04ab080d1acdaf64716e2
Victor Rentea <victorrentea@gmail.com>
2020-10-20 01:25:54 +0300

    wip

--- spring-one/src/main/java/victor/training/spring/web/controller/VictimController.java
+++ spring-one/src/main/java/victor/training/spring/web/controller/VictimController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping("victim")
 public class VictimController {
    @PostMapping("transfer-rest")
@@ -16,7 +16,6 @@ public class VictimController {
    }
 
    @PostMapping("transfer-form")
-   @ResponseBody
    public String transferForm(@ModelAttribute MoneyTransferRequest request) {
       log.warn("Transferring {} to {}, IBAN = {}", request.getAmount(), request.getRecipientFullName(), request.getRecipientIban());
       return "Done";
