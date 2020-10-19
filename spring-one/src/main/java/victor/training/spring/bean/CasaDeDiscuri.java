954e494a42c575291a796c63cba601088001815c
Victor Rentea <victorrentea@gmail.com>
2020-10-19 11:27:34 +0300

    Nu vrem sa luam din ApplicationContext programatic pentru ca e fragil la runtime poate sa crape

--- spring-one/src/main/java/victor/training/spring/bean/CasaDeDiscuri.java
+++ spring-one/src/main/java/victor/training/spring/bean/CasaDeDiscuri.java
@@ -1,9 +1,14 @@
 package victor.training.spring.bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class CasaDeDiscuri {
@@ -12,21 +17,32 @@ public class CasaDeDiscuri {
    }
    @Value("${welcome.welcomeMessage}")
    private String welcome;
+   @Lazy
    @Bean
    public Cantaret smiley(@Value("${welcome.welcomeMessage}") String welcome) {
       System.out.println("Se naste o stea " + welcome);
       return new Cantaret("Smiley");
    }
+   @Lazy
    @Bean
    public Cantec ceMaiFaciStraine(Cantaret smiley,  @Value("${welcome.welcomeMessage}") String welcome) {
       System.out.println("ce mai faci " + smiley);
       return new Cantec(smiley, "Ce mai faci, straine? + " + welcome);
    }
-
+   @Lazy
    @Bean
    public Cantec adeline() {
       System.out.println("adeline");
       return new Cantec(smiley(null), "Adeline");
+   }
+
+   @Autowired
+   private ApplicationContext spring;
+
+   public void test() {
+      System.out.println("Start");
+      System.out.println(spring.getBean(Cantaret.class));
+      System.out.println(spring.getBean(Cantec.class));
    }
 }
 
