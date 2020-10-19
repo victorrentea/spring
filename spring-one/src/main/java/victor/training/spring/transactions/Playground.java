0388b13beda21ec84a3bb8cb280cb8b47dae5bd5
Victor Rentea <victorrentea@gmail.com>
2020-10-19 17:24:21 +0300

    daca chemi o alta metoda @Transactional si arunca exceptie iti invalideaza tranzactia, chiar daca tu prinzi si inghiti exceptia aceea.

--- spring-one/src/main/java/victor/training/spring/transactions/Playground.java
+++ spring-one/src/main/java/victor/training/spring/transactions/Playground.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,11 +29,12 @@ public class Playground {
         try {
 //        getSession().save(new Message("asdsadsadkasjdksajdksajdksajdksajdksajdksajdksajdksadjksadksajdksajdksajdsadksajdasdsadsadkasjdksajdksajdksajdksajdksajdksajdksajdksadjksadksajdksajdksajdsadksajdasdsadsadkasjdksajdksajdksajdksajdksajdksajdksajdksadjksadksajdksajdksajdsadksajdasdsadsadkasjdksajdksajdksajdksajdksajdksajdksajdksadjksadksajdksajdksajdsadksajdasdsadsadkasjdksajdksajdksajdksajdksajdksajdksajdksadjksadksajdksajdksajdsadksajdasdsadsadkasjdksajdksajdksajdksajdksajdksajdksajdksadjksadksajdksajdksajdsadksajd"));
             boolean naspa = true;
-            if (naspa) throw new RuntimeException();
+            getSession().save(new Message("JOB START"));
+           other.oMetodaChemataCandva(naspa);
             getSession().save(new Message("JOB DONE"));
         } catch (Exception e) {
             other.persistFailureInDB(e.getMessage());
-            throw e;
+            //throw e; NIMIC! VideoChat scrie pe tine
         }
     }
     @Transactional
@@ -46,6 +48,12 @@ public class Playground {
 class AnotherClass {
     private final MessageRepo repo;
 
+
+    @Transactional
+   public void oMetodaChemataCandva(boolean naspa) {
+        if (naspa) throw new RuntimeException();
+    }
+
     @Autowired
     private EntityManager entityManager;
 
@@ -53,10 +61,17 @@ class AnotherClass {
         return entityManager.unwrap(Session.class);
     }
 
+//    PlatformTransactionManager t;
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void persistFailureInDB(String messageStr) {
+//        if (!t.getTransaction().isRollbackOnly()) {
+//            run in new Tx
+//        } else {
+//            run in this TX
+//        }
         Message message = new Message("inserata din alta clasa: " + messageStr);
         getSession().save(message);
+        System.out.println("Is the current tx dead ?" + getSession().getTransaction().getRollbackOnly());
         System.out.println(message.getId());
     }
 }
