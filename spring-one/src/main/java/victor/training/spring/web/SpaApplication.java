9e86d97ebc7d8825ddfc206a88603bbcfd945de7
Victor Rentea <victorrentea@gmail.com>
2020-10-20 11:01:05 +0300

    enable spring securit

--- spring-one/src/main/java/victor/training/spring/web/SpaApplication.java
+++ spring-one/src/main/java/victor/training/spring/web/SpaApplication.java
@@ -2,12 +2,15 @@ package victor.training.spring.web;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import victor.training.spring.async.AsyncApp;
 
 @EnableSwagger2
 @SpringBootApplication
+@Import(AsyncApp.class)
 public class SpaApplication {
 
 	public static void main(String[] args) {
