eec45b3b88960f610e3bdeaabd5bf4643e4fc50f
Victor Rentea <victorrentea@gmail.com>
2020-11-10 16:45:20 +0200

    cu qualifier poti identifica beanul dorit dupa nume

--- spring-one/src/main/java/victor/training/spring/bean/CasaDeDiscuriApp.java
+++ spring-one/src/main/java/victor/training/spring/bean/CasaDeDiscuriApp.java
@@ -1,8 +1,10 @@
 package victor.training.spring.bean;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -12,29 +14,34 @@ public class CasaDeDiscuriApp {
    public static void main(String[] args) {
        SpringApplication.run(CasaDeDiscuriApp.class, args);
    }
-   @Autowired
-   private Singer singer;
-
-   @PostConstruct
-   public void init() {
-      System.out.println("RUN AT INIT: " + singer);
+//   <bean id=carla class=Singer>
+//   <bean id=delia class=Singer>
+//   <bean id=smiley class=Singer>
+   @Bean
+   public Singer carla() {
+      return new Singer("Carla's Dream");
    }
+   @Bean
+   public Singer delia() {
+      return new Singer("Delia");
+   }
+  @Bean
+  public Song laTara() {
+     return new Song("Vreau la Tara", delia());
+  }
+
 
 }
 
 @Component
 class A {
-   private final Singer singer;
-
-//   @Autowired nu mai e nevoie din Spring 4.3
-   public A(Singer singer) {
-      this.singer = singer;
-
-   }
+   // o vreau pe Delia
+   @Autowired
+   @Qualifier("delia")
+   private Singer ceNumeVreauEu;
 
    @PostConstruct
    public void init() {
-      System.out.println("RUN AT INIT2: "+ singer.getAlta());
+      System.out.println(ceNumeVreauEu);
    }
-
 }
--- spring-one/src/main/java/victor/training/spring/bean/Singer.java
+++ spring-one/src/main/java/victor/training/spring/bean/Singer.java
@@ -1,55 +1,43 @@
 package victor.training.spring.bean;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import lombok.Data;
 
-import javax.annotation.PostConstruct;
-
-@Component
+@Data
 public class Singer {
-   @Autowired
-   private Alta alta;
-
-   public Alta getAlta() {
-      return alta;
-   }
-
-   @PostConstruct
-   public void init() {
-      System.out.println("RUN AT INIT singer");
-   }
+   private final String name;
 }
 
-@Component
-class Alta {
-
+@Data
+class Song {
+   private final String name;
+   private final Singer singer;
 }
 
 
 
-@Component
-class Ying {
-   final Yang yang;
-
-   public Ying(Yang yang) {
-      this.yang = yang;
-   }
-
-   @PostConstruct
-   public void init() {
-      System.out.println("RUN AT yang: " + yang.ying);
-   }
-}
-@Component
-class Yang {
-   final Ying ying;
-
-   public Yang(Ying ying) {
-      this.ying = ying;
-   }
-
-   @PostConstruct
-   public void init() {
-      System.out.println("RUN AT ying: "+ ying.yang);
-   }
-}
+//@Component
+//class Ying {
+//   final Yang yang;
+//
+//   public Ying(Yang yang) {
+//      this.yang = yang;
+//   }
+//
+//   @PostConstruct
+//   public void init() {
+//      System.out.println("RUN AT yang: " + yang.ying);
+//   }
+//}
+//@Component
+//class Yang {
+//   final Ying ying;
+//
+//   public Yang(Ying ying) {
+//      this.ying = ying;
+//   }
+//
+//   @PostConstruct
+//   public void init() {
+//      System.out.println("RUN AT ying: "+ ying.yang);
+//   }
+//}
