3571c2d22ebf6203f6c73bf5ab91b217a2633fbe
Victor Rentea <victorrentea@gmail.com>
2020-11-10 09:10:47 +0200

    MyPermissionEvaluator.java

--- spring-one/src/main/java/victor/training/spring/web/controller/VictimController.java
+++ spring-one/src/main/java/victor/training/spring/web/controller/VictimController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+
 @Slf4j
 @RestController
 @RequestMapping("victim")
