69a7cc6b01160f7cd5a5f7977559a0f4f83f9359
Victor Rentea <victorrentea@gmail.com>
2020-11-10 09:56:39 +0200

    spring boot

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.io.File;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Value("${my.prop}")
+	private Integer myProp;
+	@Value("${alta.prop}")
+	private Integer alta;
+	@Autowired
+	private Authenticator authenticator;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	public void run(String... args) throws Exception {
+		System.out.println("Folosesc " + dataSource);
+		System.out.println("My prop: " + myProp);
+		System.out.println(authenticator);
+	}
+}
+
+
+interface Authenticator {
+
+}
+@Profile("prod")
+@Component
+class SSOAuthenticator implements  Authenticator{
+	@Value("${in.folder}")
+	private File inFolder;
+}
+@Profile("!prod")
+@Component
+class DummyAuthenticator implements  Authenticator{
+
+}
