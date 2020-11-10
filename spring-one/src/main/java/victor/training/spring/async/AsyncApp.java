e91db99bbd0629f32c1e177fb613707b442f1208
Victor Rentea <victorrentea@gmail.com>
2020-11-10 16:00:58 +0200

    CompletableFutures pot fi legate intre ele pentru a nu bloca threadurile aplicatiei tale.

--- spring-one/src/main/java/victor/training/spring/async/AsyncApp.java
+++ spring-one/src/main/java/victor/training/spring/async/AsyncApp.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import victor.training.spring.ThreadUtils;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @EnableAsync
@@ -62,17 +63,23 @@ class Drinker implements CommandLineRunner {
 	// TODO [2] make them return a CompletableFuture + @Async + asyncExecutor bean
 	// TODO [3] Enable messaging...
 	public void run(String... args) throws Exception {
-		Thread.sleep(3000);
 		log.debug("Submitting my order " + barman.getClass());
 		CompletableFuture<Beer> futureBeer = barman.getOneBeerStupida();
 		CompletableFuture<Vodka> futureVodka = barman.getOneVodkaCPU();
 
-		log.debug("Astept sa vina beuturile. bat darabana");
-		Beer beer = futureBeer.get(); // 1sec
-		Vodka vodka = futureVodka.get(); // 1sec
+		CompletableFuture<Comanda> futureComanda = futureBeer.thenCombine(futureVodka, (b, v) -> new Comanda(Arrays.asList(b, v)));
 
-		log.debug("Got my order! Thank you lad! " + Arrays.asList(beer, vodka));
+		futureComanda.thenAccept(comanda ->  log.debug("Got my order! Thank you lad! " + comanda));
 		// ziua in care incepi sa scrii multithreading code este si ziua in care iti faci viata multithreading: pe un thread fixezi bugurile produse, pe altul: bestjobs, ejobs, monster, linkein
+		System.out.println("A plecat main");
+	}
+}
+
+class Comanda {
+	private final List<Object> bauturi;
+
+	Comanda(List<Object> bauturi) {
+		this.bauturi = bauturi;
 	}
 }
 
