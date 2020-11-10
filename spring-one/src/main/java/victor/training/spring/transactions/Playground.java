372bf2c6647884074c7fe12af110870ef00d395c
Victor Rentea <victorrentea@gmail.com>
2020-11-10 12:51:44 +0200

    exercitiu: 2 inserturi comise separat 2 update-uri comise impreua

--- spring-one/src/main/java/victor/training/spring/transactions/Playground.java
+++ spring-one/src/main/java/victor/training/spring/transactions/Playground.java
@@ -35,19 +35,42 @@ public class Playground {
 
         repo.save(new Message("jpa")); // Spring Data JPA 60%
     }
-    @Transactional
+//    @Transactional
     public void transactionTwo() {
 //        jdbc.update("update MESSAGE set message = 'DOI' where ID = 100");
 //
+        repo.save(new Message("jpa2")); // Spring Data JPA 60%
+
         Message message = repo.findById(1L).get();
         message.setMessage("DOI");
         System.out.println("deasupra");
+
+//        other.method();
+
+        repo.save(new Message("jpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpajpa")); // Spring Data JPA 60%
+
 
         // TODO cine scrie si cand in baza modificarile pe entitatea incarcata in cadrul unei tranzactii - proxy-ul scrie modificarile in db
 
         // TODO Repo API
         // TODO @NonNullApi
     }
+
+    // cum inseram in aceeasi Tranzactie doua mesaje:
+//    @Transactional
+    public void test1() {
+        repo.save(new Message("1"));
+        repo.save(new Message("2"));
+    }
+    // cum modific cele doua mesaje, stiind ca au id-urile 1 si 2 in aceeasi tranzactie
+    @Transactional
+    public void test2() {
+        Message message1 = repo.findById(1L).get();
+        message1.setMessage("1`");
+        Message message2 = repo.findById(2L).get();
+        message2.setMessage("2`");
+    }
+
 }
 @Service
 @RequiredArgsConstructor
