46b3f6c879e0fd5e0e0d27945460b4a43899a262
Victor Rentea <victorrentea@gmail.com>
2020-11-10 14:35:50 +0200

    Checked exceptions (alea de tre sa lefaci trhrows) lasa Tx sa COMMIT. WTH !?!!

--- spring-one/src/main/java/victor/training/spring/transactions/TranzactiiSiExceptii.java
+++ spring-one/src/main/java/victor/training/spring/transactions/TranzactiiSiExceptii.java
@@ -4,14 +4,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+
 @Service
 @RequiredArgsConstructor // constructor generat doar in bytecode (invizibil in cod)
 public class TranzactiiSiExceptii {
    private final MessageRepo repo;
 
    @Transactional
-   public void method() {
+   public void method() throws IOException {
       repo.save(new Message("PRIMUL"));
-      throw new IllegalArgumentException();
+//      throw new IllegalArgumentException(); // runtime ex distruge tranzactia curenta
+      throw new IOException(); // runtime ex distruge tranzactia curenta
    }
 }
