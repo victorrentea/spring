8120047437bf5a405066996b1146ff2b70dbe5bf
Victor Rentea <victorrentea@gmail.com>
2020-11-10 14:59:47 +0200

    REQUIRES NEW cauzeaza 2 tranzactii sa apara. Cea interioara la noi acum primeste ROLLBACK dar cea exterioara se comite,

--- spring-one/src/main/java/victor/training/spring/transactions/TranzactiiSiExceptii.java
+++ spring-one/src/main/java/victor/training/spring/transactions/TranzactiiSiExceptii.java
@@ -1,7 +1,9 @@
 package victor.training.spring.transactions;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
@@ -10,11 +12,37 @@ import java.io.IOException;
 @RequiredArgsConstructor // constructor generat doar in bytecode (invizibil in cod)
 public class TranzactiiSiExceptii {
    private final MessageRepo repo;
+   private final Alta alta;
 
    @Transactional
    public void method() throws IOException {
       repo.save(new Message("PRIMUL"));
 //      throw new IllegalArgumentException(); // runtime ex distruge tranzactia curenta
-      throw new IOException(); // runtime ex distruge tranzactia curenta
+//      throw new IOException(); // runtime ex distruge tranzactia curenta
+      try {
+         alta.altaMetoda();
+      } catch (Exception e) {
+         // va rog eu mult. Nu faceti NICIODATA ASA CEVA: 
+         e.printStackTrace(); // inghit exceptia
+         // Shaworma-driven error handling
+//         throw new RuntimeException(e);
+      }
+   }
+
+}
+
+@Component
+@RequiredArgsConstructor
+class Alta {
+   private final MessageRepo repo;
+
+   @Transactional(propagation = Propagation.REQUIRES_NEW)
+   public void altaMetoda() throws IOException {
+      repo.save(new Message("A DOUA"));
+      throw new IllegalArgumentException();
    }
 }
+
+
+
+
