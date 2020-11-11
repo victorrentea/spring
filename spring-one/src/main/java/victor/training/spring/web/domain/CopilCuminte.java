60bc22048f054bcb920aee13338c9cd45dd5dd5e
Victor Rentea <victorrentea@gmail.com>
2020-11-11 17:48:17 +0200

    CORS ar trebui sa permita accesul doar aplurilor AJAX care vin de pe domenii controlate

--- spring-one/src/main/java/victor/training/spring/web/domain/CopilCuminte.java
+++ spring-one/src/main/java/victor/training/spring/web/domain/CopilCuminte.java
@@ -3,10 +3,13 @@ package victor.training.spring.web.domain;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 @Entity
 public class CopilCuminte {
+   @Id
+   private Long id;
    @Setter
    @ManyToOne
    private EntitateCuPretentii parinte;
