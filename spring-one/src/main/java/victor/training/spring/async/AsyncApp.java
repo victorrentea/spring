3b84e784a4a216c5eae00d0c96075a4d23c769c4
Victor Rentea <victorrentea@gmail.com>
2021-01-25 12:32:13 +0200

    @Async

--- spring-one/src/main/java/victor/training/spring/async/AsyncApp.java
+++ spring-one/src/main/java/victor/training/spring/async/AsyncApp.java
@@ -3,12 +3,12 @@ package victor.training.spring.async;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
@@ -16,6 +16,8 @@ import org.springframework.stereotype.Service;
 import victor.training.spring.ThreadUtils;
 
 import java.util.concurrent.*;
+
+import static java.util.concurrent.CompletableFuture.completedFuture;
 
 @EnableAsync
 @SpringBootApplication
@@ -50,34 +52,34 @@ class Drinker implements CommandLineRunner {
 	// TODO [2] make them return a CompletableFuture + @Async + asyncExecutor bean
 	// TODO [3] Enable messaging...
 	public void run(String... args) throws Exception {
-		log.info("Enter the bar");
+		log.info("Enter the bar: " + barman.getClass());
 
 //		ExecutorService pool = Executors.newFixedThreadPool(2);
 
-		Future<Beer> futureBeer = pool.submit(barman::getOneBeer);
-
-		Future<Vodka> futureVodka = pool.submit(barman::getOneVodka);
+		Future<Beer> futureBeer = barman.getOneBeer();
+		Future<Vodka> futureVodka = barman.getOneVodka();
 
 		Beer beer = futureBeer.get(); // how much time it take to complete ? - 1
 		Vodka vodka = futureVodka.get();// how much time it take to complete ? - 0
 
 		log.info("Drinking {} and {}", beer, vodka );
 	}
-
 }
 
 @Slf4j
 @Service
 class Barman {
-	public Beer getOneBeer() {
+	@Async
+	public CompletableFuture<Beer> getOneBeer() {
 		 log.debug("Pouring Beer...");
 		 ThreadUtils.sleep(1000); /// externa sytems (API, device, something remote that takes time to reply).
-		 return new Beer();
+		 return completedFuture(new Beer());
 	 }
-	public Vodka getOneVodka() {
+	 @Async
+	public CompletableFuture<Vodka> getOneVodka() {
 		 log.debug("Pouring Vodka...");
 		 ThreadUtils.sleep(1000);
-		return new Vodka();
+		return completedFuture(new Vodka());
 	 }
 }
 
