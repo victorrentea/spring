638dde7756b31e521ef0f1ed082a5505b1a7c737
Victor Rentea <victorrentea@gmail.com>
2021-01-25 09:50:30 +0200

    isolating transactionions: working with two nested transacations

--- spring-one/src/main/java/victor/training/spring/transactions/Playground.java
+++ spring-one/src/main/java/victor/training/spring/transactions/Playground.java
@@ -4,41 +4,44 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class Playground {
-    private final JdbcTemplate jdbc;
-    private final  SimpleExamplesMapper mapper;
+    private final SimpleExamplesMapper mapper;
+    private final AnotherClass another;
 
     @Transactional
     public void transactionOne() {
-//        jdbc.update("INSERT INTO MESSAGE(ID, MESSAGE) VALUES" +
-//                    " ( 1 , ? )",
-//            "ONE");
-
         mapper.insert(new Message(1L, "ONE"));
 
         Message message = mapper.select(1);
         System.out.println(message);
-//        jdbc.update("INSERT INTO MESSAGE( MESSAGE) VALUES" +
-//                    " (  ? )",
-//            "TWO");
+
+        try {
+            another.insert();
+        } catch (Exception e) {
+            //shaworma
+            e.printStackTrace();
+        }
+
     }
+
     @Transactional
     public void transactionTwo() {
     }
 }
 
-
-// iBatis/ myBatis is just an Mapper Entity - Tables
-// Hibernate is so much more than that.
-
-
 @Service
 @RequiredArgsConstructor
 class AnotherClass {
-    private final MessageRepo repo;
+    private final  SimpleExamplesMapper mapper;
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void insert() {
+        mapper.insert(new Message(null, "ONE"));
+    }
 }
