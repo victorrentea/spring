990dc657a552a6cb3bdd199505334702c6e8c26c
Victor Rentea <victorrentea@gmail.com>
2021-02-16 15:56:13 +0200

    uploadfile

--- spring-one/src/main/java/victor/training/spring/web/controller/TechnicalController.java
+++ spring-one/src/main/java/victor/training/spring/web/controller/TechnicalController.java
@@ -1,12 +1,24 @@
 package victor.training.spring.web.controller;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import victor.training.spring.props.WelcomeInfo;
 import victor.training.spring.web.service.UserService;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.channels.Channel;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @RestController
 public class TechnicalController {
@@ -45,5 +57,38 @@ public class TechnicalController {
 	@GetMapping("admin/launch")
 	public String restart() {
 		return "What does this red button do?     ... [Missile Launched]";
+	}
+
+
+	@Scheduled(fixedRate = 10000)
+	public void method() {
+	    // ma uit in /tmp daca mai e macar 10G de spatiu. daca nu -> mail
+	}
+
+	@PostMapping
+	public void uploadFile(HttpServletRequest request) throws IOException {
+
+		File tmpFile = Files.createTempFile("data", ".dat").toFile(); //    eg /tmp/data-001.dat  - Doamne ajuta sa ai quota acolo destula
+
+		try (OutputStream outputStream = new FileOutputStream(tmpFile)) {
+			IOUtils.copy(request.getInputStream(), outputStream);
+
+
+			// si acum am un fisier de 1G pe disc. Ce fac cu el ?
+
+			// 1) uploadezi in DB
+			// 2) parsezi ca chinezu batran si le inseri rand cu rand
+			// 3) trimiti la altul sFTP
+		}
+
+		// MAI FITZOS: NIO: Java NIO iti permite sa copiezi din bufferul placii de retea direct in HDD fara sa treci prin RAM?!
+		// Direct Memory Access (DMA) controller.
+
+//FileChannel
+		boolean deleted = tmpFile.delete();
+		if (!deleted) {
+			// TODO mail SMS like pe FB
+		}
+
 	}
 }
