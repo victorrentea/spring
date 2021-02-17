85c6dd111414edd9c323a2cf059a3963aa16194d
Victor Rentea <victorrentea@gmail.com>
2021-02-17 11:06:03 +0200

    start spring security

--- spring-one/src/main/java/victor/training/spring/web/SpaApplication.java
+++ spring-one/src/main/java/victor/training/spring/web/SpaApplication.java
@@ -7,12 +7,26 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+
 @EnableCaching
 @EnableSwagger2
 @SpringBootApplication
 public class SpaApplication {
 
 	public static void main(String[] args) {
+
+		// 2-SSL : Mutual Authentication
+
+//		SSLContext sslContext;
+//		SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+//		sslSocketFactory.createSocket()
+
+		//
+
+
+
 		new SpringApplicationBuilder(SpaApplication.class)
 				.profiles("spa")
 				.run(args);
