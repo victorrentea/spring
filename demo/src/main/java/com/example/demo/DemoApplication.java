6b842c9738c7c1844e3dd74ac2dde4b8d983096a
Victor Rentea <victorrentea@gmail.com>
2021-02-24 13:06:59 +0200

    cel mai fitza mod sa incarci propr multe intr-un mod structurat. inclusiv mape

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,17 @@
 package com.example.demo;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URL;
+import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication
@@ -17,20 +23,39 @@ public class DemoApplication {
 
 }
 
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "welcome")
+class WelcomeConfig {
+	private String welcomeMessage;
+//	private int hostPort;
+	private List<String> supportUrls;
+	private Map<String, String> localContactPhone;
+	private HelpInfo help;
+	@Data
+	static class HelpInfo {
+		private URL helpUrl;
+		private String iconUri;
+	}
+}
+
+
 @RestController
+@RequiredArgsConstructor
 class PrimuRest {
+	private final WelcomeConfig config;
 
 	@Value("${welcome.welcomeMessage}")
 	private String message;
 
-//	@Value("${welcome.localContactPhone}")
-//	Map<String, String> phones;
-
 	@GetMapping("hello")
 	public String method() {
-	    return message;
+	    return config.toString();
 	}
 
 	// @ConfigurationProperties
 	// de unde vin proprietatile
 }
+
+
