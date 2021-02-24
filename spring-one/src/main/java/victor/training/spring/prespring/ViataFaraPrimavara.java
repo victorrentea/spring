cdf72e4bce026c37179e58c8e4f554d2060cbc90
Victor Rentea <victorrentea@gmail.com>
2021-02-24 10:51:13 +0200

    Uite cum scaneaza Spring subpachetele

--- spring-one/src/main/java/victor/training/spring/prespring/ViataFaraPrimavara.java
+++ spring-one/src/main/java/victor/training/spring/prespring/ViataFaraPrimavara.java
@@ -1,49 +1,14 @@
 package victor.training.spring.prespring;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class ViataFaraPrimavara {
 
    public static void main(String[] args) {
        SpringApplication.run(ViataFaraPrimavara.class, args);
-      B mockB = new B();
-       new A(mockB, c);
    }
 
 }
-@Service class C {}
 
-@Service
-//@RequiredArgsConstructor // dovada ca limbajul Java are 25 de ani vechime
-class A implements CommandLineRunner {
-   private final B b; // 50-100 de linii de XML iti injectai o instanta de B manageuita de Application Server (EJB container).
-
-   public A(B b) {
-      this.b = b;
-   }
-
-   public void method() {
-      // logica
-      b.isPrime(3);
-      b.isPrime(10_000_169);
-      // logica
-   }
-
-   @Override
-   public void run(String... args) throws Exception {
-      System.out.println("Perfect pt teste si joaca : " + b);
-   }
-}
-
-@Service
-class B {
-   public boolean isPrime(int n) {
-      // ASDSAD
-      // ASDSAD
-      return true;
-   }
-}
---
+++ spring-one/src/main/java/victor/training/spring/prespring/sub/B.java
@@ -0,0 +1,12 @@
+package victor.training.spring.prespring.sub;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class B {
+   public boolean isPrime(int n) {
+      // ASDSAD
+      // ASDSAD
+      return true;
+   }
+}
