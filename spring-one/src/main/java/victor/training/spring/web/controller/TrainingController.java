b888f446c9bc4af2ee29cb15b74310a8a488712e
Victor Rentea <victorrentea@gmail.com>
2021-02-26 11:15:24 +0200

    session-scoped lifecycle

--- spring-one/src/main/java/victor/training/spring/web/controller/TrainingController.java
+++ spring-one/src/main/java/victor/training/spring/web/controller/TrainingController.java
@@ -2,13 +2,21 @@ package victor.training.spring.web.controller;
 
 import java.text.ParseException;
 import java.util.List;
+import java.util.Locale;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 import victor.training.spring.web.controller.dto.TrainingDto;
 import victor.training.spring.web.service.TrainingService;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("rest/trainings")
@@ -49,8 +57,51 @@ public class TrainingController {
 		trainingService.deleteById(id);
 	}
 
+	@Autowired
+	CurrentSession currentSession;
+
 	@PostMapping
-	public void createTraining(@RequestBody TrainingDto dto) throws ParseException {
+	public void createTraining(@RequestBody TrainingDto dto, HttpSession session) throws ParseException {
 		trainingService.createTraining(dto);
+		currentSession.setLastCreate(dto);
+//		session.setAttribute("last-create", dto);
+	}
+
+	@GetMapping("last")
+	public TrainingDto getLastCreateData(HttpSession session) {
+		return ciudat.biz(session);
+	}
+	@Autowired
+	CiudatFrate ciudat;
+
+}
+
+@RequiredArgsConstructor
+@Component
+class CiudatFrate {
+	private final CurrentSession currentSession;
+
+	public TrainingDto biz(HttpSession session) {
+		System.out.println("De fapt curretSession nu e chiar o instanta reala ci e un " + currentSession.getClass());
+//		return (TrainingDto) session.getAttribute("last-create");
+		return currentSession.getLastCreate();
 	}
 }
+
+
+@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
+@Data
+@Component
+class CurrentSession {
+	private TrainingDto lastCreate;
+}
+
+//@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+//@Data
+//@Component
+//class CurrentRequest {
+//	private long userId;
+//	private Locale browserLocale;
+//	private long tenantId;
+//
+//}
--- spring-one/src/main/java/victor/training/spring/web/controller/dto/TrainingDto.java
+++ spring-one/src/main/java/victor/training/spring/web/controller/dto/TrainingDto.java
@@ -1,6 +1,8 @@
 package victor.training.spring.web.controller.dto;
 
-public class TrainingDto {
+import java.io.Serializable;
+
+public class TrainingDto implements Serializable {
 	public Long id;
 	public String name; //
 	public Long teacherId; //
