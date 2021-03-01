4154d6066d517e0bc706c358f40c6c608c2afe4f
Victor Rentea <victorrentea@gmail.com>
2021-03-01 17:39:24 +0200

    Cache distribuit cu redis. ca sa-ti mearga, tre doar sa instalezi Docker Desktop + rulezi run-redis.bat + pornesti 2 instante de app.

--- spring-one/src/main/java/victor/training/spring/web/service/UserService.java
+++ spring-one/src/main/java/victor/training/spring/web/service/UserService.java
@@ -2,6 +2,7 @@ package victor.training.spring.web.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import victor.training.spring.web.domain.User;
@@ -17,6 +18,10 @@ public class UserService  {
     public long countUsers() {
         return userRepo.count();
     }
+//    @Cacheable("user-data")
+//    public Profile loadUserProfile(String username) {
+//        return userRepo.count();
+//    }
 
     // TODO 1 Avoid extra query - get from cache
     // TODO 2 Avoid inconsistencies - evict cache
@@ -24,6 +29,7 @@ public class UserService  {
     // TODO 3 Avoid inconsistencies on multiple machines - use Redis as centralized cache
     // Scenario: N1.get(=2), N2.get(=2), N1.create, N1.get(=3), N2.get(=2) STALE!!!
     // TODO 4 At restart, clean the Redis cache (with CLR)
+    @CacheEvict("users-counts")
     public void createUser() {
         userRepo.save(new User());
     }
