4515955899269fce0499426a7a8d585e86b24cab
Victor Rentea <victorrentea@gmail.com>
2021-03-01 16:11:11 +0200

    Cleanup

--- spring-one/src/main/java/victor/training/spring/props/WelcomeInfo.java
+++ spring-one/src/main/java/victor/training/spring/props/WelcomeInfo.java
@@ -6,24 +6,25 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
 import java.net.URI;
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
 @Slf4j
-@Data // getters & setters mandatory!
+@Data // getters & setters are mandatory!
 @Component
 public class WelcomeInfo {
     private String welcomeMessage;
-    private List<String> supportUrls;
+    private List<URL> supportUrls;
     private Map<String,String> localContactPhone; // per country
     private HelpInfo help;
 
     @Component
     public static class HelpInfo {
-        private URL helpUrl;
-        private URI iconUri;
+        private Integer appId;
+        private File file;
     }
 
     @PostConstruct
