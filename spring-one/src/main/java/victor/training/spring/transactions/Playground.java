8513532ff28d5f38676518ff74fb67fcf6ec5fcd
Victor Rentea <victorrentea@gmail.com>
2021-03-02 10:40:39 +0200

    Auto-flush al modificarilor pe entitatile obtinute in cadrul unei @Transactional.. se face automat la finalul Tx, inainte de COMMIT

--- spring-one/src/main/java/victor/training/spring/transactions/Playground.java
+++ spring-one/src/main/java/victor/training/spring/transactions/Playground.java
@@ -1,6 +1,7 @@
 package victor.training.spring.transactions;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,35 +11,48 @@ import java.io.IOException;
 @Service
 @RequiredArgsConstructor
 public class Playground {
-    private final EntityManager entityManager;
-    private final MessageRepo repo;
+   private final EntityManager entityManager;
+   private final MessageRepo repo;
+   private final JdbcTemplate jdbc;
+   private final AnotherClass other;
 
-    private final AnotherClass other;
+   @Transactional
+   public void transactionOne() throws IOException {
+      repo.save(new Message("jpa"));
+      try {
+         jdbc.update("INSERT INTO MESSAGE(ID, MESSAGE) VALUES ( -1,'ALO' )");
+//         this.riskyStep();// does not cause a rollback since no RuntimeException
+         // goes through any Transactional proxy
+//         System.out.println(other.getClass());
+//         other.riskyStep();
+      } catch (Exception e) {
+         repo.save(new Message("EROARE: " + e.getMessage()));
+      }
+      System.out.println("Ies din metoda");
+   }
+   @Transactional
+   public void riskyStep() throws IOException {
+      throw new RuntimeException("");
+   }
+   @Transactional
+   public void transactionTwo() {
+      Message message = repo.findById(1L).get();
 
-    @Transactional
-    public void transactionOne() throws IOException {
-        repo.save(new Message("jpa"));
-        try {
-            riskyStep();
-        } catch (Exception e) {
-            repo.save(new Message("EROARE: " + e.getMessage()));
-        }
-    }
-
-    private void riskyStep() throws IOException {
-        throw new RuntimeException("");
-    }
-
-    @Transactional
-    public void transactionTwo() {
-        // TODO Repo API
-        // TODO @NonNullApi
-    }
+      message.setMessage("nou mesaj");
+      System.out.println("Ies din met");
+      // TODO Repo API
+      // TODO @NonNullApi
+   }
 }
 
-
 @Service
-@RequiredArgsConstructor // generates constructor for all final fields, used by Spring to inject dependencies
+@RequiredArgsConstructor
+@Transactional
+    // generates constructor for all final fields, used by Spring to inject dependencies
 class AnotherClass {
-    private final MessageRepo repo;
+   private final MessageRepo repo;
+
+   public void riskyStep() throws IOException {
+      throw new RuntimeException("");
+   }
 }
