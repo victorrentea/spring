8bbf19f0d65ca2f81e86a56bad4c47b4a8a7e35a
Victor Rentea <victorrentea@gmail.com>
2021-03-03 01:47:07 +0200

    mybatis XML working

--- spring-one/src/main/java/victor/training/spring/transactions/MyBatisMapper.java
+++ spring-one/src/main/java/victor/training/spring/transactions/MyBatisMapper.java
@@ -8,8 +8,8 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface MyBatisMapper {
 
-	@Select("SELECT id, message from MESSAGE where id = #{id}")
-	Message search(@Param("id") int id);
+//	@Select("SELECT id, message from MESSAGE where id = #{id}")
+	Message search(int id);
 
 	@Insert("INSERT INTO MESSAGE(id, message) values (#{id},#{message})")
 	void insert(Message message);
