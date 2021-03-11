a3cd12da081f189fe196fc772686969e5ed1e098
Victor Rentea <victorrentea@gmail.com>
2021-03-11 22:19:22 +0200

    TODO @Valid

--- spring-one/src/main/java/victor/training/spring/web/controller/TrainingController.java
+++ spring-one/src/main/java/victor/training/spring/web/controller/TrainingController.java
@@ -22,21 +22,22 @@ public class TrainingController {
 		return trainingService.getTrainingById(id);
 	}
 
+	// TODO @Valid
+	public void createTraining(TrainingDto dto) throws ParseException {
+		trainingService.createTraining(dto);
+	}
+
 	public void updateTraining(Long id, TrainingDto dto) throws ParseException {
 		trainingService.updateTraining(id, dto);
 	}
-
 	// TODO Allow only for role 'ADMIN'... or POWER or SUPER
 	// TODO Allow for authority 'training.edit'
 	// TODO Requirement: The current user manages the the teacher of that training (User.getManagedTeacherIds)
 	// TODO @accessController.canDeleteTraining(#id)
 	// TODO PermissionEvaluator
+
 	public void deleteTrainingById(Long id) {
 		trainingService.deleteById(id);
-	}
-
-	public void createTraining(TrainingDto dto) throws ParseException {
-		trainingService.createTraining(dto);
 	}
 
 	public List<TrainingDto> search(TrainingSearchCriteria criteria) {
