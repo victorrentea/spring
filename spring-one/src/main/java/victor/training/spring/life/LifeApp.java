ff26f95f2fb919f3ddc2d9b789e0e6c9cbe10e91
Victor Rentea <victorrentea@gmail.com>
2021-03-25 11:42:08 +0200

    Explicit factory for prototype

--- spring-one/src/main/java/victor/training/spring/life/LifeApp.java
+++ spring-one/src/main/java/victor/training/spring/life/LifeApp.java
@@ -14,83 +14,102 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.support.SimpleThreadScope;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @SpringBootApplication
-public class LifeApp implements CommandLineRunner{
-	@Bean
-	public static CustomScopeConfigurer defineThreadScope() {
-		CustomScopeConfigurer configurer = new CustomScopeConfigurer();
-		// WARNING: Leaks memory. Prefer 'request' scope or read here: https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/context/support/SimpleThreadScope.html
-		configurer.addScope("thread", new SimpleThreadScope());
-		return configurer;
-	}
-	
-	public static void main(String[] args) {
-		SpringApplication.run(LifeApp.class);
-	}
-	
-	@Autowired 
-	private OrderExporter exporter;
-	
-	// TODO [1] make singleton; multi-thread + mutable state = BAD
-	// TODO [2] instantiate manually, set dependencies, pass around; no AOP
-	// TODO [3] prototype scope + ObjectFactory or @Lookup. Did you said "Factory"? ...
-	// TODO [4] thread/request scope. HOW it works?! Leaks: @see SimpleThreadScope javadoc
+public class LifeApp implements CommandLineRunner {
+   @Bean
+   public static CustomScopeConfigurer defineThreadScope() {
+      CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+      // WARNING: Leaks memory. Prefer 'request' scope or read here: https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/context/support/SimpleThreadScope.html
+      configurer.addScope("thread", new SimpleThreadScope());
+      return configurer;
+   }
 
-	public void run(String... args) {
-		new Thread(() -> exporter.export(Locale.ENGLISH)).start();
-		new Thread(() -> exporter.export(Locale.FRENCH)).start();
-	}
+   public static void main(String[] args) {
+      SpringApplication.run(LifeApp.class);
+   }
+
+   @Autowired
+   private OrderExporter exporter;
+
+   // TODO [1] make singleton; multi-thread + mutable state = BAD
+   // TODO [2] instantiate manually, set dependencies, pass around; no AOP
+   // TODO [3] prototype scope + ObjectFactory or @Lookup. Did you said "Factory"? ...
+   // TODO [4] thread/request scope. HOW it works?! Leaks: @see SimpleThreadScope javadoc
+
+   public void run(String... args) {
+      new Thread(() -> exporter.export(Locale.ENGLISH)).start();
+      new Thread(() -> exporter.export(Locale.FRENCH)).start();
+   }
 }
+
 @Service
-class OrderExporter  {
-	private static final Logger log = LoggerFactory.getLogger(OrderExporter.class);
-	private final InvoiceExporter invoiceExporter;
-	private final ObjectFactory<LabelService> labelServiceFactory;
+class OrderExporter {
+   private static final Logger log = LoggerFactory.getLogger(OrderExporter.class);
+   private final InvoiceExporter invoiceExporter;
+   private final LabelServiceFactory labelServiceFactory;
 
-	OrderExporter(InvoiceExporter invoiceExporter, ObjectFactory<LabelService> labelServiceFactory) {
-		this.invoiceExporter = invoiceExporter;
-		this.labelServiceFactory = labelServiceFactory;
-	}
+   OrderExporter(InvoiceExporter invoiceExporter, LabelServiceFactory labelServiceFactory) {
+      this.invoiceExporter = invoiceExporter;
+      this.labelServiceFactory = labelServiceFactory;
+   }
 
-	public void export(Locale locale) {
-		log.debug("Running export in " + locale);
-		LabelService labelService = labelServiceFactory.getObject();
-		labelService.load(locale);
-		log.debug("Origin Country: " + labelService.getCountryName("rO"));
-		invoiceExporter.exportInvoice(labelService);
-	}
+
+   public void export(Locale locale) {
+      log.debug("Running export in " + locale);
+      LabelService labelService = labelServiceFactory.create(locale);
+      log.debug("Origin Country: " + labelService.getCountryName("rO"));
+      invoiceExporter.exportInvoice(labelService);
+   }
 }
+
+@Component
+class LabelServiceFactory {
+   private final ObjectFactory<LabelService> labelServiceFactory;
+
+   LabelServiceFactory(ObjectFactory<LabelService> labelServiceFactory) {
+      this.labelServiceFactory = labelServiceFactory;
+   }
+
+   public LabelService create(Locale locale) {
+      // encapsulated the temporal coupling COns: +1 class.
+      LabelService labelService = labelServiceFactory.getObject();
+      labelService.load(locale);
+      return labelService;
+   }
+}
+
 @Service
 class InvoiceExporter {
-	private static final Logger log = LoggerFactory.getLogger(InvoiceExporter.class);
+   private static final Logger log = LoggerFactory.getLogger(InvoiceExporter.class);
 
-	public void exportInvoice(LabelService labelService) {
-		log.debug("Invoice Country: " + labelService.getCountryName("ES"));
-	}
+   public void exportInvoice(LabelService labelService) {
+      log.debug("Invoice Country: " + labelService.getCountryName("ES"));
+   }
 }
 
 @Service
 @Scope("prototype")
 class LabelService { // statefull of ... S...TATE
-	private static final Logger log = LoggerFactory.getLogger(LabelService.class);
-	private final CountryRepo countryRepo;
+   private static final Logger log = LoggerFactory.getLogger(LabelService.class);
+   private final CountryRepo countryRepo;
 
-	public LabelService(CountryRepo countryRepo) {
-		System.out.println("+1 Label Service: " + this.hashCode());
-		this.countryRepo = countryRepo;
-	}
+   public LabelService(CountryRepo countryRepo) {
+      System.out.println("+1 Label Service: " + this.hashCode());
+      this.countryRepo = countryRepo;
+   }
 
-	private Map<String, String> countryNames;
+   private Map<String, String> countryNames;
 
-	public void load(Locale locale) {
-		log.debug("LabelService.load() on instance " + this.hashCode());
-		countryNames = countryRepo.loadCountryNamesAsMap(locale);
-	}
+   public void load(Locale locale) {
+      log.debug("LabelService.load() on instance " + this.hashCode());
+      countryNames = countryRepo.loadCountryNamesAsMap(locale);
+   }
 
-	public String getCountryName(String iso2Code) {
-		log.debug("LabelService.getCountryName() on instance " + this.hashCode());
-		return countryNames.get(iso2Code.toUpperCase());
-	}
+   public String getCountryName(String iso2Code) {
+      log.debug("LabelService.getCountryName() on instance " + this.hashCode());
+      return countryNames.get(iso2Code.toUpperCase());
+   }
 }
