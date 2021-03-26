0fbaba64acd59dce733e1ed60c63fb3551644d98
Victor Rentea <victorrentea@gmail.com>
2021-03-26 11:37:53 +0200

    Test Containers

--- from-start/src/test/java/com/example/fromstart/ArticleRepoTest.java
+++ from-start/src/test/java/com/example/fromstart/ArticleRepoTest.java
@@ -3,8 +3,7 @@ package com.example.fromstart;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
@@ -16,7 +15,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
-@DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
+@SpringBootTest// DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
 class CustomerRepositoryTest {
 
    @Container
