6240a98243ea39ac9426a09867707e4a782ee0b8
Victor Rentea <victorrentea@gmail.com>
2021-03-26 11:04:05 +0200

    Transactions END

--- spring-one/src/main/java/victor/training/spring/transactions/Playground.java
+++ spring-one/src/main/java/victor/training/spring/transactions/Playground.java
@@ -1,37 +1,64 @@
 package victor.training.spring.transactions;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
+import java.io.IOException;
+import java.sql.Connection;
 
 @Service
 @RequiredArgsConstructor
 public class Playground {
-    private final MessageRepo repo;
-    private final EntityManager em;
+    private static final Logger log = LoggerFactory.getLogger(Playground.class);
     private final JdbcTemplate jdbc;
     private final AnotherClass other;
-    private final MyBatisMapper mybatis;
 
-    @Transactional
+//    @Transactional
     public void transactionOne() {
-        jdbc.update("insert into MESSAGE(id, message) values ( 100,'ALO' )");
-        mybatis.search(100);
-        repo.save(new Message("jpa"));
+        log.info("START METHOD");
+
+        try {
+          other. doImport();
+        } catch (Exception e) {
+            other.persistError(e.getMessage());
+            // shaworma pattern
+        }
+        log.info("END METHOD");
     }
-    @Transactional
+
+
+
+   @Transactional
     public void transactionTwo() {
-        // TODO Repo API
-        // TODO @NonNullApi
     }
 }
-
 
 @Service
 @RequiredArgsConstructor // generates constructor for all final fields, used by Spring to inject dependencies
 class AnotherClass {
-    private final MessageRepo repo;
+    private final JdbcTemplate jdbc;
+
+    @Transactional
+   public void doImport() throws Exception {
+      jdbc.update("insert into MESSAGE(id, message) values ( 100,'ALO' )");
+      //        jdbc.update("insert into MESSAGE(id, message) values ( 101,'ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2ALO2' )");
+      booboo();
+   }
+
+//    @Transactional(rollbackFor = Exception.class)
+    public void booboo() throws Exception {
+        if (true)// throw new IOException(); // checked exceptions DO NOT cancel the current tx
+            throw new NullPointerException("intentional");
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void persistError(String message) {
+        jdbc.update("insert into MESSAGE(id, message) values ( 101,'ERROR:"+message+"' )");
+    }
+
 }
