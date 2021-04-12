8e78ea1c0dcfb8ed50a007fdadb051454f3139b6
Victor Rentea <victorrentea@gmail.com>
2021-04-12 13:17:57 +0300

    Properties

---
+++ first/src/main/java/com/example/first/FirstApplication.java
@@ -0,0 +1,30 @@
+package com.example.first;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class FirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApplication.class, args);
+	}
+
+}
+
+
+@RequiredArgsConstructor
+@RestController
+class MyRestController {
+	@Value("${welcome.welcomeMessage}")
+	private String message;
+	private final WelcomeInfo welcomeInfo;
+	@GetMapping
+	public String method() {
+	    return message;
+	}
+}
---
+++ first/src/main/java/com/example/first/WelcomeInfo.java
@@ -0,0 +1,39 @@
+package com.example.first;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Data // getters & setters are mandatory!
+@Component
+@ConfigurationProperties(prefix = "welcome")
+public class WelcomeInfo {
+    private String welcomeMessage;
+    private List<URL> supportUrls;
+    private Map<String,String> localContactPhone; // per country
+    private HelpInfo help;
+
+    @Data
+    public static class HelpInfo {
+        private Integer appId;
+        private File file;
+        private int someInt;
+    }
+
+    @PostConstruct
+    public void printMyself() {
+        // TODO check the help.file exists
+        log.info("My props: " + this);
+//        if (!help.file.isFile()) {
+//            throw new IllegalArgumentException("not a file: " + help.file);
+//        }
+    }
+}
