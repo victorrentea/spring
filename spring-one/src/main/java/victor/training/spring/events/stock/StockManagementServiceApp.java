778c56dfee2772d251ddc5bdbfd08073386f9a0f
Victor Rentea <victorrentea@gmail.com>
2021-04-13 12:15:25 +0300

    from classic Spring integration (@EnableBinding) to Functional Style (@Bean Function<>)

--- spring-one/src/main/java/victor/training/spring/events/stock/StockManagementServiceApp.java
+++ spring-one/src/main/java/victor/training/spring/events/stock/StockManagementServiceApp.java
@@ -2,29 +2,18 @@ package victor.training.spring.events.stock;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.Input;
-import org.springframework.cloud.stream.annotation.Output;
-import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.SubscribableChannel;
 import org.springframework.messaging.support.MessageBuilder;
 
+import java.util.function.Function;
 
-interface StockChannels {
-   @Input("oce_in")
-   SubscribableChannel orderCreatedEvents();
-   @Output("gi_out")
-   MessageChannel generateInvoice();
-}
+
 @Slf4j
 @SpringBootApplication
 @RequiredArgsConstructor
-@EnableBinding(StockChannels.class)
 public class StockManagementServiceApp {
 
    public static void main(String[] args) {
@@ -32,18 +21,17 @@ public class StockManagementServiceApp {
    }
    private int stock = 3; // silly implem :D
 
-   @Autowired
-   StockChannels channels;
-
-   @ServiceActivator(inputChannel = "oce_in", outputChannel = "gi_out")
-   public Message<String> process(String orderId) {
-      log.info("Checking stock for products in order " + orderId);
-      if (stock == 0) {
-         throw new IllegalStateException("Out of stock");
-      }
-      stock--;
-      log.info(">> PERSIST new STOCK!!");
+   @Bean
+   public Function<String, Message<String>> checkStock() {
+      return orderId -> {
+         log.info("Checking stock for products in order " + orderId);
+         if (stock == 0) {
+            throw new IllegalStateException("Out of stock");
+         }
+         stock--;
+         log.info(">> PERSIST new STOCK!!");
 //      return new OrderIsInStockEvent(event.getOrderId());
-      return MessageBuilder.withPayload("Order in STOCK " + orderId).build();
+         return MessageBuilder.withPayload("Order in STOCK " + orderId).build();
+      };
    }
 }
