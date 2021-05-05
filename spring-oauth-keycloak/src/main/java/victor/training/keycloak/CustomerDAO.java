e24a2b02f3851e5232939fbf336f1f7d36ab36cc
Victor Rentea <victorrentea@gmail.com>
2021-05-06 00:54:42 +0300

    cleanup

--- spring-oauth-keycloak/src/main/java/victor/training/keycloak/CustomerDAO.java
+++
@@ -1,7 +0,0 @@
-package victor.training.keycloak;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerDAO extends CrudRepository<Customer, Long> {
-
-}
--- spring-websockets/src/main/java/victor/training/spring/websockets/auction/Bid.java
+++
@@ -1,9 +0,0 @@
-package victor.training.spring.websockets.auction;
-
-import lombok.Data;
-
-@Data
-public class Bid {
-  private String name;
-  private int bid;
-}
