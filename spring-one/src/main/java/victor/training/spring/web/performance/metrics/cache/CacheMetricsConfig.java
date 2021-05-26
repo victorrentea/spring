e71bf9171f0958fc6e3d40438c837e36fcb391b8
Victor Rentea <victorrentea@gmail.com>
2021-05-26 17:28:55 +0300

    comment out custom cache metrics.

--- spring-one/src/main/java/victor/training/spring/web/performance/metrics/cache/CacheMetricsConfig.java
+++ spring-one/src/main/java/victor/training/spring/web/performance/metrics/cache/CacheMetricsConfig.java
@@ -1,26 +1,26 @@
-package victor.training.spring.web.performance.metrics.cache;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.CachingConfigurerSupport;
-import org.springframework.cache.support.SimpleCacheManager;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-
-import static java.util.stream.Collectors.toList;
-
-@Configuration
-public class CacheMetricsConfig extends CachingConfigurerSupport {
-   @Value("${spring.cache.cache-names}")
-   private List<String> cacheNames;
-
-   @Bean
-   @Override
-   public CacheManager cacheManager() {
-      SimpleCacheManager cacheManager = new SimpleCacheManager();
-      cacheManager.setCaches(cacheNames.stream().map(ConcurrentMapCacheMetricsWrapper::new).collect(toList()));
-      return cacheManager;
-   }
-}
+//package victor.training.spring.web.performance.metrics.cache;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.cache.CacheManager;
+//import org.springframework.cache.annotation.CachingConfigurerSupport;
+//import org.springframework.cache.support.SimpleCacheManager;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.List;
+//
+//import static java.util.stream.Collectors.toList;
+//
+//@Configuration
+//public class CacheMetricsConfig extends CachingConfigurerSupport {
+//   @Value("${spring.cache.cache-names}")
+//   private List<String> cacheNames;
+//
+//   @Bean
+//   @Override
+//   public CacheManager cacheManager() {
+//      SimpleCacheManager cacheManager = new SimpleCacheManager();
+//      cacheManager.setCaches(cacheNames.stream().map(ConcurrentMapCacheMetricsWrapper::new).collect(toList()));
+//      return cacheManager;
+//   }
+//}
