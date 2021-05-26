12485176271b0cdb20ddd0ea51b3d29f9ad5aa0e
Victor Rentea <victorrentea@gmail.com>
2021-05-26 12:07:42 +0300

    new app from start.spring.io

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+@RestController
+class MyFirstRest {
+
+	@Value("${prop}")
+	private String prop;
+
+	@GetMapping("/boot")
+	public String hello() {
+		return "Hello boot " + prop;
+	}
+}
+
+
