30032bc34ae20b332f2aa19eeb4e6e8c3fe985d6
Victor Rentea <victorrentea@gmail.com>
2021-05-26 11:21:13 +0300

    clearable thread scope

--- spring-one/src/main/java/victor/training/spring/life/LifeApp.java
+++ spring-one/src/main/java/victor/training/spring/life/LifeApp.java
@@ -1,8 +1,5 @@
 package victor.training.spring.life;
 
-import java.util.Locale;
-import java.util.Map;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.CustomScopeConfigurer;
@@ -10,18 +7,20 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.SimpleThreadScope;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.Locale;
+import java.util.Map;
 
 @SpringBootApplication
 public class LifeApp implements CommandLineRunner{
 	@Bean
 	public static CustomScopeConfigurer defineThreadScope() {
 		CustomScopeConfigurer configurer = new CustomScopeConfigurer();
-		// WARNING: Leaks memory. Prefer 'request' scope or read here: https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/context/support/SimpleThreadScope.html
-		configurer.addScope("thread", new SimpleThreadScope());
+		// WARNING: Can leaks memory if data remains on thread. Prefer 'request' scope or read here: https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/context/support/SimpleThreadScope.html
+		// TODO finally { ClearableThreadScope.clearAllThreadData(); }
+		configurer.addScope("thread", new ClearableThreadScope());
 		return configurer;
 	}
 	
