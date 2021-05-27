35d4d4a9ae714686fc0d550d6d4f94edddb4334b
Victor Rentea <victorrentea@gmail.com>
2021-05-27 12:23:08 +0300

    END of transactions basic

--- spring-one/src/main/java/victor/training/spring/transactions/Playground.java
+++ spring-one/src/main/java/victor/training/spring/transactions/Playground.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.io.IOException;
 
 @Slf4j
 @Service
@@ -21,28 +23,57 @@ public class Playground {
     @Transactional
     public void transactionOne() {
         jdbc.update("insert into MESSAGE(id, message) values ( 100,'ALO' )");
-        em.persist(new Message("JPA"));
-        repo.save(new Message("Spring Data JPA"));
+        repo.save(new Message("null"));
+//        em.persist(new Message(null));
+
+        try {
+            other.altaMetodaTranzactata();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         System.out.println(repo.findByMessage("JPA")); // causes a flush() pentru ca ceea ce avea de pus in baza ar putea influenta rez queryului.
         log.debug("Ies din metoda");
     }
 
 
-//    @Transactional(readOnly = true)
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     public void transactionTwo() {
 
         Message message = repo.findById(100L).get();
         message.setMessage("alt mesaj");
-
-        // TODO Repo API
-        // TODO @NonNullApi
     }
-}
+    @Transactional
+    public void transactionThree(String mesajDePeCoada) throws IOException {
+        try {
+            Boolean eroare = null;
+            repo.save(new Message("Am primit: " + mesajDePeCoada));
+            if (eroare) {
+                throw new IOException("NASPA FRATE");
+            }
+        } catch (Exception e) {
+//            log.error(e.getMessage(), e);
+            other.persistErorrInDifferentTx(e);
+            throw e;
+        }
+        em.persist(new Message("In veci in DB"));
+    }
 
+}
 
 @Service
 @RequiredArgsConstructor // generates constructor for all final fields, used by Spring to inject dependencies
 class AnotherClass {
     private final MessageRepo repo;
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void persistErorrInDifferentTx(Exception e) {
+        repo.save(new Message("EROARE: " + e.getMessage()));
+    }
+
+    @Transactional
+    public void altaMetodaTranzactata() {
+        repo.save(new Message("Inca una mai flacai!"));
+        throw new RuntimeException("Intentionata");
+    }
 }
