48fa690c8b7e0a69b4c352e99776ca58ea8362c2
Victor Rentea <victorrentea@gmail.com>
2021-05-28 14:24:28 +0300

    Scheduled

--- spring-one/src/main/java/victor/training/spring/web/SpaApplication.java
+++ spring-one/src/main/java/victor/training/spring/web/SpaApplication.java
@@ -3,11 +3,13 @@ package victor.training.spring.web;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableMBeanExport;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableCaching
 @EnableSwagger2
 @SpringBootApplication
+@EnableMBeanExport
 public class SpaApplication {
 
 	public static void main(String[] args) {
