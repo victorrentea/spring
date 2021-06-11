d62bcda8d83fc1120cf8c0952711dc4aed7e7cb5
Victor Rentea <victorrentea@gmail.com>
2021-06-11 15:13:15 +0300

    JPQL injection: incearca sa cauti dupa stringul
    ' OR '1'='1

--- spring-one/src/main/java/victor/training/spring/web/controller/TrainingController.java
+++ spring-one/src/main/java/victor/training/spring/web/controller/TrainingController.java
@@ -4,9 +4,14 @@ import org.owasp.html.PolicyFactory;
 import org.owasp.html.Sanitizers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 import victor.training.spring.web.controller.dto.TrainingDto;
 import victor.training.spring.web.controller.dto.TrainingSearchCriteria;
+import victor.training.spring.web.domain.Training;
+import victor.training.spring.web.repo.TrainingRepo;
+import victor.training.spring.web.security.SecurityUser;
 import victor.training.spring.web.service.TrainingService;
 
 import java.text.ParseException;
@@ -15,55 +20,76 @@ import java.util.List;
 @RestController
 @RequestMapping("api/trainings")
 public class TrainingController {
-	@Autowired
-	private TrainingService trainingService;
+   @Autowired
+   private TrainingService trainingService;
 
-	@GetMapping
-	public List<TrainingDto> getAllTrainings() {
-		return trainingService.getAllTrainings();
-	}
+   @GetMapping
+   public List<TrainingDto> getAllTrainings() {
+      return trainingService.getAllTrainings();
+   }
 
-	@GetMapping("{id}")
+   @GetMapping("{id}")
+	@PreAuthorize("@securityService.hasAccessOnTraining(#id)")
 	public TrainingDto getTrainingById(@PathVariable Long id) {
-		TrainingDto dto = trainingService.getTrainingById(id);
+      TrainingDto dto = trainingService.getTrainingById(id);
 
-		PolicyFactory sanitizer = Sanitizers.FORMATTING.and(Sanitizers.BLOCKS);
-		dto.description = sanitizer. sanitize(dto.description);
+      PolicyFactory sanitizer = Sanitizers.FORMATTING.and(Sanitizers.BLOCKS);
+      dto.description = sanitizer.sanitize(dto.description);
 
-		return dto;
-	}
+      return dto;
+   }
 
-	// TODO @Valid
-	@PostMapping
-	public void createTraining(@RequestBody TrainingDto dto) throws ParseException {
-		trainingService.createTraining(dto);
-	}
-	@PreAuthorize("hasAuthority('training.edit')")
-	@PutMapping("{id}")
+   // TODO @Valid
+   @PostMapping
+   public void createTraining(@RequestBody TrainingDto dto) throws ParseException {
+      trainingService.createTraining(dto);
+   }
+
+   @PreAuthorize("hasAuthority('training.edit') && @securityService.hasAccessOnTraining(#id)")
+   @PutMapping("{id}")
 	public void updateTraining(@PathVariable Long id, @RequestBody TrainingDto dto) throws ParseException {
+
 //		HtmlPolicyBuilder html = new HtmlPolicyBuilder();
 //		html.allowElements("b", "i","u");
 //		dto.description = html.
-		PolicyFactory sanitizer = Sanitizers.FORMATTING.and(Sanitizers.BLOCKS);
-		dto.description = sanitizer. sanitize(dto.description);
+      PolicyFactory sanitizer = Sanitizers.FORMATTING.and(Sanitizers.BLOCKS);
+      dto.description = sanitizer.sanitize(dto.description);
 
-		trainingService.updateTraining(id, dto);
-	}
-	// TODO Allow only for role 'ADMIN'... or POWER or SUPER
-	// TODO Allow for authority 'training.delete'
-	// TODO Requirement: The current user manages the the teacher of that training (User.getManagedTeacherIds)
-	// TODO @accessController.canDeleteTraining(#id)
-	// TODO PermissionEvaluator
+      trainingService.updateTraining(id, dto);
+   }
+   // TODO Allow only for role 'ADMIN'... or POWER or SUPER
+   // TODO Allow for authority 'training.delete'
+   // TODO Requirement: The current user manages the the teacher of that training (User.getManagedTeacherIds)
+   // TODO @accessController.canDeleteTraining(#id)
+   // TODO PermissionEvaluator
 
-	@DeleteMapping("{id}")
+   @DeleteMapping("{id}")
 //	@PreAuthorize("hasRole('ADMIN')")
-	@PreAuthorize("hasAuthority('training.delete')")
-	public void deleteTrainingById(@PathVariable Long id) {
-		trainingService.deleteById(id);
-	}
+   @PreAuthorize("hasAuthority('training.delete') && @securityService.hasAccessOnTraining(#id)")
+//	@PreAuthorize("hasPermission(#id, 'TRAINING', 'DELETE')")
+   public void deleteTrainingById(@PathVariable Long id) {
+//      securityService.hasAccessOnTraining(id);
 
-	@PostMapping("search")
-	public List<TrainingDto> search(@RequestBody  TrainingSearchCriteria criteria) {
-		return trainingService.search(criteria);
-	}
+      trainingService.deleteById(id);
+   }
+
+   @Autowired
+   SecurityService securityService;
+
+   @PostMapping("search")
+   public List<TrainingDto> search(@RequestBody TrainingSearchCriteria criteria) {
+      return trainingService.search(criteria);
+   }
+}
+
+@Component
+class SecurityService {
+   public boolean hasAccessOnTraining(Long id) {
+      Training training = trainingRepo.findById(id).get();
+      SecurityUser securityUser = (SecurityUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+      return securityUser.getManagedTeacherIds().contains(training.getTeacher().getId());
+   }
+
+   @Autowired
+   private TrainingRepo trainingRepo;
 }
--- spring-one/src/main/java/victor/training/spring/web/repo/TrainingRepoImpl.java
+++ spring-one/src/main/java/victor/training/spring/web/repo/TrainingRepoImpl.java
@@ -1,23 +1,29 @@
 package victor.training.spring.web.repo;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import victor.training.spring.web.controller.dto.TrainingSearchCriteria;
 import victor.training.spring.web.domain.Training;
+import victor.training.spring.web.security.SecurityUser;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class TrainingRepoImpl implements TrainingRepoCustom {
    private final EntityManager entityManager;
    @Override
    public List<Training> search(TrainingSearchCriteria criteria) {
-      String visibleTeacherIdCsv = "1"; // TODO
+      SecurityUser securityUser = (SecurityUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+      String visibleTeacherIdCsv = securityUser.getManagedTeacherIds().stream().map(Objects::toString).collect(Collectors.joining(","));
       // DON'T DO THIS: JPQL injection!
       String jpql = "SELECT t FROM Training t " +
                     "WHERE t.teacher.id IN (" + visibleTeacherIdCsv + ")";
       jpql += " AND t.name = '" + criteria.name + "'";
+//      jpql += " AND t.name = ' OR '1'='1'";
 
       TypedQuery<Training> query = entityManager.createQuery(jpql, Training.class);
 
