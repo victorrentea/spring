c1d0d21c1f665daacb4c8d4947a18e79dc2cfd14
Victor Rentea <victorrentea@gmail.com>
2021-06-11 12:07:01 +0300

    CORS

--- spring-one/src/main/java/victor/training/spring/web/SecurityConfig.java
+++ spring-one/src/main/java/victor/training/spring/web/SecurityConfig.java
@@ -10,7 +10,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -21,19 +24,56 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter /*implements W
 
 //    @Override
 //    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("http://localhost:8080");
+////        registry.addMapping("http://localhost:8080");
 ////        registry.addMapping("http://api.coface.com"); // aici deployezi tu
-//        registry.addMapping("http://cdn.coface.com");
+//        registry.addMapping("/api/**").allowedOrigins("http://cdn.coface.com").allowCredentials(true);
+//        registry.addMapping("/api/**").allowedOrigins("http://127.0.0.1:9999").allowCredentials(true);
 //    }
+//@Bean
+//public CorsConfigurationSource corsConfigurationSource() {
+//    return new CorsConfigurationSource() {
+//        @Override
+//        public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+//            CorsConfiguration config = new CorsConfiguration();
+//            config.addAllowedOrigin("http://127.0.0.1:9999");
+//            config.setAllowCredentials(true);
+//            return config;
+//        }
+//    };
+//}
+
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+
+
+        org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:9999"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "content-type", "x-requested-with",
+            "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "x-auth-token", "x-app-id", "Origin",
+            "Accept", "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers",
+            "Authorization", "Cache-Control"));
+        configuration.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 //HttpServletRequest request;
 //request.getSession().invalidate();
-       http//.//cors().configurationSource().and()
-           .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+       http.cors().and()//.//cors().configurationSource().and()
+//           .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
 
-//           .csrf().disable()
+           .csrf().disable()
 
             .authorizeRequests()
                .anyRequest().authenticated()
--- spring-one/src/main/java/victor/training/spring/web/controller/VictimController.java
+++ spring-one/src/main/java/victor/training/spring/web/controller/VictimController.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("victim")
 public class VictimController {
+//   @CrossOrigin(origins = "http://127.0.0.1:9999") //
    @PostMapping("transfer-rest")
-//   @CrossOrigin(origins = "*",allowCredentials = "true")  //or  implement WebMvcConfigurer with @Configuration + cors
+//   @CrossOrigin(origins = "http://127.0.0.1:9999",allowCredentials = "true")  //or  implement WebMvcConfigurer with @Configuration + cors
    public void transferRest(@RequestBody MoneyTransferRequest request) {
       log.warn("Transferring {} to {}, IBAN = {}", request.getAmount(), request.getRecipientFullName(), request.getRecipientIban());
    }
