8b5be4ed12b3dbdf1051fb2146a46fa859c4149b
Victor Rentea <victorrentea@gmail.com>
2021-06-11 15:39:28 +0300

    Am securizat de tot vizibilitatea la training pe jurizdictii + bugfixes

--- spring-one/src/main/java/victor/training/spring/web/repo/TrainingRepo.java
+++ spring-one/src/main/java/victor/training/spring/web/repo/TrainingRepo.java
@@ -1,8 +1,14 @@
 package victor.training.spring.web.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import victor.training.spring.web.domain.Training;
 
 public interface TrainingRepo extends JpaRepository<Training, Long>, TrainingRepoCustom {
     Training getByName(String name);
+
+    @Override
+    @PreAuthorize("hasAuthority('training.delete') && @securityService.hasAccessOnTraining(#id)")
+
+    void deleteById(Long id);
 }
