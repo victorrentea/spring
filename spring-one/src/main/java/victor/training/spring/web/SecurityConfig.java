1544e0024f79d074e7263d9b93322aa07200b4c5
Victor Rentea <victorrentea@gmail.com>
2021-07-14 12:54:15 +0300

    Doamne-ajuta

--- spring-one/src/main/java/victor/training/spring/web/SecurityConfig.java
+++ spring-one/src/main/java/victor/training/spring/web/SecurityConfig.java
@@ -1,27 +1,26 @@
-//package victor.training.spring.web;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Profile;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//
-//@EnableWebSecurity
+package victor.training.spring.web;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Profile("!test")
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+
 
     // TODO [SEC] Start with ROLE-based authorization on URL-patterns
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
        http
-//           .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
            .csrf().disable()
            .authorizeRequests()
                .mvcMatchers("unsecured/**").anonymous()
