8ffae9d39cdd1c117c6e0401ef04e76978412d65
Victor Rentea <victorrentea@gmail.com>
2021-07-15 12:16:52 +0300

    @Async te muta pe alt thread si INTOTDEAUNA pierzi Tranzactia si JDBC connectionul pe care erai.

--- spring-one/src/main/java/victor/training/spring/transactions/Playground.java
+++ spring-one/src/main/java/victor/training/spring/transactions/Playground.java
@@ -2,8 +2,10 @@ package victor.training.spring.transactions;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import victor.training.spring.ThreadUtils;
 
 import javax.persistence.EntityManager;
 
@@ -15,9 +17,14 @@ public class Playground {
     private final JdbcTemplate jdbc;
     private final AnotherClass other;
 
+    @Transactional
     public void transactionOne() {
         jdbc.update("insert into MESSAGE(id, message) values ( 100, ? )", "ALO");
         repo.save(new Message("null"));
+        jdbc.update("insert into MESSAGE(id, message) values ( 105, ? )", "ALO2");
+        other.parpad1();
+        ThreadUtils.sleep(300);
+
         repo.save(new Message(null));
     }
     @Transactional
@@ -26,10 +33,13 @@ public class Playground {
         // TODO @NonNullApi
     }
 }
-
-
 @Service
 @RequiredArgsConstructor // generates constructor for all final fields, used by Spring to inject dependencies
 class AnotherClass {
     private final MessageRepo repo;
+    @Async
+    public void parpad1() {
+        repo.save(new Message("parpad"));
+        ThreadUtils.sleep(1000);
+    }
 }
