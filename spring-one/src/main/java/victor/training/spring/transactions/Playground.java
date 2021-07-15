ded94510e25aa6be13671b2057efc579311f843f
Victor Rentea <victorrentea@gmail.com>
2021-07-15 12:30:10 +0300

    Cum poti sa startezi o tranzctie noua ad-hoc: 1) autoinject @Transactional 2) muti in alta clasa metoda @Transactional 3) TransactionTemplate (fara adnotari, dar Yuck!)

--- spring-one/src/main/java/victor/training/spring/transactions/Playground.java
+++ spring-one/src/main/java/victor/training/spring/transactions/Playground.java
@@ -1,10 +1,14 @@
 package victor.training.spring.transactions;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 import victor.training.spring.ThreadUtils;
 
 import javax.persistence.EntityManager;
@@ -16,17 +20,37 @@ public class Playground {
     private final EntityManager em;
     private final JdbcTemplate jdbc;
     private final AnotherClass other;
+    private TransactionTemplate txTemplate;
+//    private OurTransactionTemplate newTransaction;
 
-    @Transactional
+    @Autowired
+    public void setTxTemplate(PlatformTransactionManager txManager) {
+        this.txTemplate = new TransactionTemplate(txManager);
+        txTemplate.setPropagationBehaviorName("REQUIRES_NEW");
+        txTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_DEFAULT);
+    }
+
+    //    @Autowired
+//    private Playground myself;
+//    @Transactional
     public void transactionOne() {
-        jdbc.update("insert into MESSAGE(id, message) values ( 100, ? )", "ALO");
-        repo.save(new Message("null"));
-        jdbc.update("insert into MESSAGE(id, message) values ( 105, ? )", "ALO2");
+        txTemplate.execute(status -> {
+            atomicaFrate();
+            return null;
+        });
         other.parpad1();
         ThreadUtils.sleep(300);
 
-        repo.save(new Message(null));
+        repo.save(new Message("frumos"));
     }
+
+//    @Transactional
+    public void atomicaFrate() {
+        jdbc.update("insert into MESSAGE(id, message) values ( 100, ? )", "ALO");
+        repo.save(new Message("null"));
+        jdbc.update("insert into MESSAGE(id, message) values ( 105, null )");
+    }
+
     @Transactional
     public void transactionTwo() {
         // TODO Repo API
@@ -39,6 +63,7 @@ class AnotherClass {
     private final MessageRepo repo;
     @Async
     public void parpad1() {
+        System.out.println("Cate mesaje vad in baza " + repo.count());
         repo.save(new Message("parpad"));
         ThreadUtils.sleep(1000);
     }
