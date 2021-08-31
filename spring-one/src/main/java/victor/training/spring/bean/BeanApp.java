e2a21bfe286ed2f761bd99b94a7e1bdbb2bc3373
Victor Rentea <victorrentea@gmail.com>
2021-08-31 12:03:51 +0300

    a local @Bean call is not a call, but a reference

--- spring-one/src/main/java/victor/training/spring/bean/BeanApp.java
+++ spring-one/src/main/java/victor/training/spring/bean/BeanApp.java
@@ -1,9 +1,16 @@
 package victor.training.spring.bean;
 
 import lombok.Data;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class BeanApp implements CommandLineRunner {
@@ -11,16 +18,69 @@ public class BeanApp implements CommandLineRunner {
         SpringApplication.run(BeanApp.class);
     }
 
+    @Autowired
+    private Conversation conversation;
+
     @Override
     public void run(String... args) throws Exception {
-        Conversation conversation = new Conversation(new Person("John"), new Person("Jane"));
-        conversation.start();
         // TODO manage all with Spring
+//        Conversation conversation = new Conversation(new Person("John"), new Person("Jane"));
+
+        conversation.start();
 
         // TODO alternative: "Mirabela Dauer" story :)
     }
 }
 
+@Configuration
+class MyConfig {
+//    @Autowired
+//    @Qualifier("john")
+//    private Person john;
+//    @Autowired
+//    @Qualifier("jane")
+//    private Person jane;
+
+    @Bean
+    public Conversation conversation(/*Person john, Person jane*/) {
+        System.out.println("C1");
+        return new Conversation(john(), jane());
+    }
+    @Bean
+    public Conversation conversation2(Person john, Person jane) {
+        System.out.println("C2");
+        return new Conversation(john(), jane());
+    }
+    @Bean
+    public Person john() {
+        System.out.println("John is born");
+        return new Person("John");
+    }
+    @Bean
+    public Person jane() {
+        return new Person("Jane");
+    }
+}
+@Aspect
+@Component
+class ForSimon {
+    @Around("execution(* john(..))")
+    public Object method(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println("Invoked john");
+        return pjp.proceed();
+    }
+}
+//class OMG extends MyConfig {
+//    @Override
+//    public Person john() {
+//        if (john is not created ){
+//            return super.john();
+//            store john
+//        } else {
+//            return from singleton map
+//        }
+//    }
+//}
 @Data
 class Conversation {
     private final Person one;
@@ -48,5 +108,8 @@ class Person {
         return "Hello! Here is " + name + " from " + OldClass.getInstance().getCurrentCountry();
     }
 }
+
+
+
 
 
