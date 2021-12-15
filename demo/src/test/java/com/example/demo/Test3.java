794ad2b3c75530b574cbac81bb2ecf30baadcdec
Victor Rentea <victorrentea@gmail.com>
2021-12-15 15:11:54 +0200

    Parallel tests with Spring Boot

---
+++ demo/src/test/java/com/example/demo/Test3.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = "custom=aa")
+@Execution(ExecutionMode.CONCURRENT)
+class Test3 {
+	private static final Logger log = LoggerFactory.getLogger(Test3.class);
+
+	@Test
+	public void first() throws Exception{
+		log.info("start");
+		Thread.sleep(500);
+		log.info("end");
+	}
+	@Test
+	public void second() throws Exception{
+		log.info("start");
+		Thread.sleep(500);
+		log.info("end");
+	}
+
+}
