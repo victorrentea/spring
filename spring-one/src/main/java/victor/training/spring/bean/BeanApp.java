85a0f5f0136cbfaa37471769c88ea0cc940d2c83
Victor Rentea <victorrentea@gmail.com>
2022-01-25 12:13:22 +0200

    BANG! HORROR; @Bean methods are a fake if called from the same configuration

--- spring-one/src/main/java/victor/training/spring/bean/BeanApp.java
+++ spring-one/src/main/java/victor/training/spring/bean/BeanApp.java
@@ -2,11 +2,12 @@ package victor.training.spring.bean;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+// @Configuration
 @SpringBootApplication
 public class BeanApp implements CommandLineRunner {
     public static void main(String[] args) {
@@ -25,9 +26,9 @@ public class BeanApp implements CommandLineRunner {
         conversation.start();
     }
     @Bean
-    public Person john() {
-        System.out.println("John is born!");
-        return new Person("John");
+    public Person john(@Value("${john.name}")String johnName) {
+        System.out.println(johnName+" is born!");
+        return new Person(johnName);
     }
     @Bean
     public Person jane() {
@@ -35,10 +36,23 @@ public class BeanApp implements CommandLineRunner {
         return new Person("Jane");
     }
     @Bean
-    public Conversation conversation(Person john, Person jane) {
-        return new Conversation(john, jane);
+    public Conversation conversation() {
+        System.out.println("now new Conversation");
+        return new Conversation(john(null), john(null)); // how the hell these 2 method calls lead to one single "John is born!" message in the console
+
+        // THE MAGIC of @Bean in a @Configuration:
+        // any other @Bean method you call, you get into a @OVerride that Spring generated for THAT method.
     }
 }
+
+//class SpringHahahaha extends BeanApp {
+//    @Override
+//    public Person john() {
+//        if (already there) return that one;
+//        ....
+//        return super.john();
+//    }
+//}
 
 
 // ------------- you are not allowed to edit code below, because it's a library code --------------
