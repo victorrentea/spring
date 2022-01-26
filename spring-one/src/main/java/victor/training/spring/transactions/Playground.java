97ab8ed9504afdb70fed016dfabbf7f6c5668658
Victor Rentea <victorrentea@gmail.com>
2022-01-26 11:01:21 +0200

    Transaction end

--- spring-one/src/main/java/victor/training/spring/transactions/Playground.java
+++ spring-one/src/main/java/victor/training/spring/transactions/Playground.java
@@ -2,15 +2,24 @@ package victor.training.spring.transactions;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Service
+@Component
+@Transactional
+@Retention(RetentionPolicy.RUNTIME)
+@interface Facade {
+}
+
+@Facade
 @RequiredArgsConstructor
 public class Playground {
     private final MessageRepo repo;
@@ -21,7 +30,6 @@ public class Playground {
 
         ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-    @Transactional
     public void transactionOne() {
 //        em.lock(message, LockModeType.PESSIMISTIC_WRITE);
         // SELECT FOR UPDATE * FROM MESSAGE WHERE ID = 1; COMMIT/ROLLBACK
@@ -37,12 +45,14 @@ public class Playground {
     }
 
     // commit at the end if everything goes well. On exception: rollback
-    @Transactional
+    //@Transactional // "inherited" from @Facade
     public void transactionTwo() {
         // TODO Repo API
         // TODO @NonNullApi
     }
 }
+
+
 @Service
 @RequiredArgsConstructor // generates constructor for all final fields, used by Spring to inject dependencies
 class AnotherClass {
@@ -51,18 +61,14 @@ class AnotherClass {
     public void someOtherTransactedMethod() {
         throw new RuntimeException("e");
     }
+
 //    @Transactional(propagation = Propagation.REQUIRES_NEW)
-
     @Transactional(propagation = Propagation.NOT_SUPPORTED) //also works
-
     // beause repo.save() has an internal @Transactional in the implem that sees NO active TX and creates a brand new one
     public void saveError(Exception e) {
 
-
-//        Connection connection;
-//        connection.setAutoCommit(false);
-//        connection.rollback();
-
+        // no tx on current thread
+        //jdbc.update("insert into MESSAGE(id, message) values ( 100,'ALO' )"); // auto-commit method
 
         repo.save(new Message("ERROR: " + e.getMessage()));
     }
