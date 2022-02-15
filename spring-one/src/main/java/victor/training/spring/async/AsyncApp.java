0eab1fd4e01bdb513b93b4edcacd25a6596025f0
Victor Rentea <victorrentea@gmail.com>
2022-02-15 17:31:05 +0200

    non-blocking web endpoint + 2 thread pools (differently sized) involved to protect external fragile systems.

--- spring-one/src/main/java/victor/training/spring/async/AsyncApp.java
+++ spring-one/src/main/java/victor/training/spring/async/AsyncApp.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
@@ -14,9 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import victor.training.spring.ThreadUtils;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.Future;
+import java.util.concurrent.CompletableFuture;
 
 @EnableAsync
 @SpringBootApplication
@@ -31,14 +30,25 @@ public class AsyncApp {
 //	private int barmanThreadCount;
 
 	@Bean
-//	@ConfigurationProperties
-	public ThreadPoolTaskExecutor executor(@Value("${barman.thread.count}")
+	public ThreadPoolTaskExecutor beerPool(@Value("${beer.thread.count}")
 															 int barmanThreadCount) {
 		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
 		executor.setCorePoolSize(barmanThreadCount);
 		executor.setMaxPoolSize(barmanThreadCount);
 		executor.setQueueCapacity(500);
-		executor.setThreadNamePrefix("bar-");
+		executor.setThreadNamePrefix("beer-");
+		executor.initialize();
+		executor.setWaitForTasksToCompleteOnShutdown(true);
+		return executor;
+	}
+	@Bean
+	public ThreadPoolTaskExecutor vodkaPool(@Value("${vodka.thread.count}")
+															 int barmanThreadCount) {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(barmanThreadCount);
+		executor.setMaxPoolSize(barmanThreadCount);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("vodka-");
 		executor.initialize();
 		executor.setWaitForTasksToCompleteOnShutdown(true);
 		return executor;
@@ -53,40 +63,49 @@ class Drinker {
 	private Barman barman;
 
 //	private static final ExecutorService pool = Executors.newFixedThreadPool(2);
-@Autowired
-private ThreadPoolTaskExecutor pool;
-
 	// TODO [1] inject and use a ThreadPoolTaskExecutor.submit
 	// TODO [2] make them return a CompletableFuture + @Async + asyncExecutor bean
 	// TODO [3] Messaging...
 	@GetMapping("drink")
-	public List<Object> drink() throws Exception {
+	public CompletableFuture<DillyDilly> drink() throws Exception {
 		log.debug("Submitting my order");
 
-		Future<Beer> futureBeer = pool.submit(() -> barman.getOneBeer());
-		Future<Vodka> futureVodka = pool.submit(() -> barman.getOneVodka());
+		CompletableFuture<Beer> futureBeer = barman.getOneBeer(); // nothing happens YET
+		CompletableFuture<Vodka> futureVodka = barman.getOneVodka(); // = promise
 
-		Beer beer = futureBeer.get(); // how much time is the HTTP thread blocked here ? 1s
-		Vodka vodka = futureVodka.get(); // how much time is the HTTP thread blocked here ? 0s
+		// IT IS ILLEGAL TO DO GET on a completableFuture
+//		Beer beer = futureBeer.get(); // how much time is the HTTP thread blocked here ? 1s
+//		Vodka vodka = futureVodka.get(); // how much time is the HTTP thread blocked here ? 0s
 
-		log.debug("Got my order! Thank you lad! " + Arrays.asList(beer, vodka));
-		return List.of(beer,vodka);
+		CompletableFuture<DillyDilly> futureDilly = futureBeer.thenCombineAsync(futureVodka,
+			(beer, vodka) -> new DillyDilly(beer, vodka));
+		// Mono.zip
+
+		log.debug("Got my order! ");
+		return futureDilly;
 	}
+}
+
+@Data
+class DillyDilly {
+	private final Beer beer;
+	private final Vodka vodka;
 }
 
 @Slf4j
 @Service
 class Barman {
-	public Beer getOneBeer() {
+	@Async("beerPool") // means Proxy
+	public CompletableFuture<Beer> getOneBeer() { // 2 BEERS MAX IN PARALLEL
 		 log.debug("Pouring Beer...");
 		 ThreadUtils.sleep(1000); // REST CALL
-		 return new Beer();
+		 return CompletableFuture.completedFuture(new Beer());
 	 }
-	
-	 public Vodka getOneVodka() {
+	@Async("vodkaPool")
+	 public CompletableFuture<Vodka> getOneVodka() { //8 SHOTS IN PARALLEL
 		 log.debug("Pouring Vodka...");
 		 ThreadUtils.sleep(1000); // FAT PIG SQL
-		 return new Vodka();
+		 return CompletableFuture.completedFuture(new Vodka());
 	 }
 }
 
