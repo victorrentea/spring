2ae77c407e8d706010b7946c26860c867e1fd7b8
Victor Rentea <victorrentea@gmail.com>
2022-02-15 11:50:52 +0200

    cleaner 👌

--- spring-one/src/main/java/victor/training/spring/transactions/Playground.java
+++ spring-one/src/main/java/victor/training/spring/transactions/Playground.java
@@ -1,7 +1,6 @@
 package victor.training.spring.transactions;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -27,25 +26,12 @@ public class Playground {
         try {
             other.bigNastyLogic();
         } catch (Exception e) {
-            playground .persistError(e);
+            other.persistError(e);
         }
         repo.save(new Message("will not throw even if the current tx is ZOMBIE (kille dby the exception thrown before)"));
         System.out.println("End of method. The inserts happen AFTER this line.");
     }
 
-    @Autowired
-    private Playground playground;
-
-    // "Each thread has its own Stack of transaction" <<
-    // ThreadLocal<List<Transaction>>
-    // a proxy would have its own thread - NOT RIGHT
-    // all methods that you call are executed in a single thread. (unless you are using @Aync, Threads or ThreadPools .submit())
-
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-//    @Transactional(propagation = Propagation.NOT_SUPPORTED)
-    public void persistError(Exception e) {
-        repo.save(new Message("ERROR: " + e));
-    }
 
 
     @Transactional
@@ -67,6 +53,11 @@ public class Playground {
 class AnotherClass {
     private final MessageRepo repo;
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+//    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    public void persistError(Exception e) {
+        repo.save(new Message("ERROR: " + e));
+    }
 
 
 //    @TransactionAttribute(REQUIRES_NEW) // EJB
