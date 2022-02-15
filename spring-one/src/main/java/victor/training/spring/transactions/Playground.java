565dd27601081452159e91591e4c3745a99e8aa3
Victor Rentea <victorrentea@gmail.com>
2022-02-15 12:05:48 +0200

    programmatic transaction management with Transaction Template

--- spring-one/src/main/java/victor/training/spring/transactions/Playground.java
+++ spring-one/src/main/java/victor/training/spring/transactions/Playground.java
@@ -1,10 +1,14 @@
 package victor.training.spring.transactions;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.persistence.EntityManager;
 import java.io.IOException;
@@ -16,8 +20,9 @@ public class Playground {
     private final JdbcTemplate jdbc;
     private final EntityManager em;
     private final AnotherClass other;
+    private TransactionTemplate txTemplate;
 
-//    @Transactional
+    //    @Transactional
     public void transactionOne() {
         repo.save(new Message("Ok"));
 //        repo.flush(); // bad practice. Instead, go through JPA and it will auto-flush, as belo
@@ -26,12 +31,27 @@ public class Playground {
         try {
             other.bigNastyLogic();
         } catch (Exception e) {
-            other.persistError(e);
+//            other.persistError(e);
+
+            // txTemplate under the hood does:
+            // wraps the execution of the lambda in a new transaction, created inside and commited at the end (unless exception occur)
+
+            // get conncetion, start transaction, call your lamda, commit/rollback
+            txTemplate.executeWithoutResult( s -> persistErrorLocal(e)  );
+
         }
         repo.save(new Message("will not throw even if the current tx is ZOMBIE (kille dby the exception thrown before)"));
         System.out.println("End of method. The inserts happen AFTER this line.");
     }
 
+    @Autowired
+    public void configureTransactionTemplate(PlatformTransactionManager transactionManager) {
+        txTemplate = new TransactionTemplate(transactionManager);
+        txTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+    }
+    private void persistErrorLocal(Exception e) {
+        repo.save(new Message("ERROR: " + e));
+    }
 
 
     @Transactional
