5ae7fefa08b48df7d31c40bbf36e999a5a1ffc4d
Victor Rentea <victorrentea@gmail.com>
2022-02-15 09:52:35 +0200

    Persisntence abstraction layers

--- spring-one/src/main/java/victor/training/spring/transactions/Playground.java
+++ spring-one/src/main/java/victor/training/spring/transactions/Playground.java
@@ -14,12 +14,22 @@ public class Playground {
     private final EntityManager em;
     private final JdbcTemplate jdbc;
     private final AnotherClass other;
-    private final MyBatisMapper mybatis;
 
     @Transactional
     public void transactionOne() {
-        jdbc.update("insert into MESSAGE(id, message) values ( 100,'ALO' )");
-        mybatis.search(100);
+
+        // level 1 plain JDBC
+//        Connection connection;
+//        connection.prepareStatement("")
+
+        // level 2 JDBCTemplate for direct SQL but helped by Spring
+        jdbc.update("insert into MESSAGE(id, message) values ( 100, ?)", "ALO");
+
+        // level 3 builds on Level1 ; Entity Manager = Session (hibernate)
+        // em.createNativeQuery("insert into MESSAGE(id, message) values ( 100, ?)") // bad practice when using JPA
+//        em.persist(new Message("plain JPA"));
+
+        // level 4 (on top of 3) = Spring Data
         repo.save(new Message("jpa"));
     }
     @Transactional
