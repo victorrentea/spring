c372f5e061ac98c0c16b70ed45d522fa6526d5b9
Victor Rentea <victorrentea@gmail.com>
2022-03-30 13:50:07 +0300

    ZOMBIE transaction in actin. at line :30 the insert is seen in log on a connection that later (after the method end) the proxy rolls back the tx on

--- spring-one/src/main/java/victor/training/spring/transactions/Playground.java
+++ spring-one/src/main/java/victor/training/spring/transactions/Playground.java
@@ -3,6 +3,7 @@ package victor.training.spring.transactions;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -16,16 +17,25 @@ public class Playground {
     private final AnotherClass other;
     private final MyBatisMapper mybatis;
 
-    @Transactional
+    // a JDBC connection is BOUND to a thread.
+    @Transactional // A
     public void transactionOne() {
-        jdbc.update("insert into MESSAGE(id, message) values ( 100,'ALO' )");
-        mybatis.search(100);
-        repo.save(new Message("jpa"));
+        // logic
+        try {
+            jdbc.update("insert into MESSAGE(id, message) values ( 101,'OK')");
+            other.stuff();
+        } catch (Exception e) {
+           other. saveError(e);
+        }
+            jdbc.update("insert into MESSAGE(id, message) values ( 1025,'NEVER')");
+        System.out.println("End of method");
     }
-    @Transactional
+
+
     public void transactionTwo() {
-        // TODO Repo API
-        // TODO @NonNullApi
+        // now works in autocommit mode
+        jdbc.update("insert into MESSAGE(id, message) values ( 601,'null')");
+        // row is commited in DB.💗
     }
 }
 
@@ -34,4 +44,15 @@ public class Playground {
 @RequiredArgsConstructor // generates constructor for all final fields, used by Spring to inject dependencies
 class AnotherClass {
     private final MessageRepo repo;
+    private final JdbcTemplate jdbc;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void stuff() {
+        throw new RuntimeException("BUM");
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void saveError(Exception e) {
+        jdbc.update("insert into MESSAGE(id, message) values ( 105,'ERROR: ' || ?)", e.getMessage());
+    }
 }
