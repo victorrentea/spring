4e931e4f80d847b246151c9de53f1a1b33bdb862
Victor Rentea <victorrentea@gmail.com>
2022-04-14 16:06:49 +0300

    fix script

--- spring-app/src/main/docker/keycloak/create-admin-user.bat
+++ spring-app/src/main/docker/keycloak/create-admin-user.bat
@@ -1,1 +1,2 @@
-docker exec keycloak-keycloak-1 /opt/jboss/keycloak/bin/add-user-keycloak.sh -u initial1 -p zaq1!QAZ
+docker container ls
+docker exec keycloak_keycloak_1 /opt/jboss/keycloak/bin/add-user-keycloak.sh -u initial1 -p zaq1!QAZ
