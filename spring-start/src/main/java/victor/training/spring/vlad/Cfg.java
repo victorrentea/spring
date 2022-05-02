8042c0ad40bd5e90ddfa38363aa099c90014fa6f
Victor Rentea <victorrentea@gmail.com>
2022-05-02 12:08:51 +0300

    +valdari

--- spring-start/src/main/java/victor/training/spring/vlad/Cfg.java
+++ spring-start/src/main/java/victor/training/spring/vlad/Cfg.java
@@ -2,9 +2,17 @@ package victor.training.spring.vlad;
 
 import lombok.Data;
 
+import javax.annotation.PostConstruct;
+import java.util.Objects;
+
 @Data
 public class Cfg {
-   private int x;
-   private int y;
+   private Integer x;
+   private Integer y;
 
+@PostConstruct
+   public void validate() {
+      Objects.requireNonNull(x);
+      Objects.requireNonNull(y);
+   }
 }
