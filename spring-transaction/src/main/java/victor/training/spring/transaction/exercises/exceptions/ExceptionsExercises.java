608300669518828fb1444bfa5247d57c7a798777
Victor Rentea <victorrentea@gmail.com>
2022-05-03 22:17:22 +0300

    ExceptionsExercises solved

--- spring-transaction/src/main/java/victor/training/spring/transaction/exercises/exceptions/ExceptionsExercises.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/exercises/exceptions/ExceptionsExercises.java
@@ -1,6 +1,8 @@
 package victor.training.spring.transaction.exercises.exceptions;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,16 +15,14 @@ public class ExceptionsExercises {
    public void runtimeExceptions_markCurrentTransactionForRollback() {
       // Someone calls this method within their transaction
       // TODO kill their transaction
-      throw new RuntimeException();
    }
    @Transactional
    public void checkedExceptions_doNOTCauseRollback() throws Exception{
       // TODO prove that by default throwing any Checked exception (ex: some IOException)
       //  does not break the caller transaction
-      throw new IOException();
    }
 
-   @Transactional(rollbackFor = Exception.class)
+   @Transactional
    public void rollbackForChecked() throws Exception{
       // TODO break the caller transaction on any checked exceptions. Hint: rollbackFor=
       throw new IOException();
@@ -33,9 +33,60 @@ public class ExceptionsExercises {
       throw new RuntimeException();
    }
 
-   @Transactional(propagation = Propagation.REQUIRES_NEW)// (propagation=?)
+   @Transactional// (propagation=?)
    public void separateTransaction_isRolledbackIndependently() {
       // TODO avoid breaking the caller transaction via propagation=
       throw new RuntimeException();
    }
+}
+
+
+@Service
+@RequiredArgsConstructor
+class Caller {
+   private final ExceptionsExercises exercises;
+   private final ExceptionalEntityRepo repo;
+
+   @Transactional
+   public void runtimeExceptions_markCurrentTransactionForRollback() {
+      repo.save(new ExceptionalEntity("before"));
+      exercises.runtimeExceptions_markCurrentTransactionForRollback();
+      repo.save(new ExceptionalEntity("after"));
+   }
+
+   @Transactional
+   public void checkedExceptions_doNOTCauseRollback() throws Exception {
+      repo.save(new ExceptionalEntity("before"));
+      exercises.checkedExceptions_doNOTCauseRollback();
+      repo.save(new ExceptionalEntity("after"));
+   }
+
+   @Transactional
+   public void rollbackForChecked() throws Exception {
+      repo.save(new ExceptionalEntity("before"));
+      exercises.rollbackForChecked();
+      repo.save(new ExceptionalEntity("after"));
+   }
+
+   @Transactional
+   public void runtimeException_ifUnseenByProxy_doesntMarkForRollback() {
+      repo.save(new ExceptionalEntity("before"));
+
+      // TODO Stop the runtime exception from going through a Transactional Proxy!
+      exercises.runtimeException_ifUnseenByProxy_doesntMarkForRollback();
+
+      repo.save(new ExceptionalEntity("after"));
+   }
+
+   @Transactional
+   public void separateTransaction_isRolledbackIndependently() {
+      repo.save(new ExceptionalEntity("before"));
+      try {
+         exercises.separateTransaction_isRolledbackIndependently();
+      } catch (Exception e) {
+         System.out.println("Recovering from " + e);
+      }
+      repo.save(new ExceptionalEntity("after"));
+   }
+
 }
