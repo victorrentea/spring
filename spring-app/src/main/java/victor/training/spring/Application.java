1d9521c42416a5ca09a6e5b538ac14daa56ce392
victorrentea <victorrentea@gmail.com>
2022-06-09 13:44:28 +0300

    y

--- spring-app/src/main/java/victor/training/spring/Application.java
+++ spring-app/src/main/java/victor/training/spring/Application.java
@@ -46,8 +46,9 @@ public class Application {
 //            @Override
 //            public void addCorsMappings(CorsRegistry registry) {
 //                registry.addMapping("/api/**")
+//                        .allowedMethods("*")
 //                        .allowCredentials(true) // allows receiving session cookie (if using cookies)
-//                        .allowedOriginPatterns("http://localhost:8081/") // NodeJS
+//                        .allowedOriginPatterns("http://localhost:8081") // eg NodeJS
 ////					 .allowedOriginPatterns("http://*") // Too broad
 //                ;
 //                // also don't forget to add .cors() to spring security
