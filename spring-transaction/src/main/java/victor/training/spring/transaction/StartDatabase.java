fdfd859c27b74d69c79f1a591faa7f4da253ddce
victorrentea <victorrentea@gmail.com>
2022-06-09 13:25:08 +0300

    clean previous db at startup

--- spring-transaction/src/main/java/victor/training/spring/transaction/StartDatabase.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/StartDatabase.java
@@ -1,5 +1,6 @@
 package victor.training.spring.transaction;
 
+import java.io.File;
 import java.sql.SQLException;
 
 public class StartDatabase {
@@ -7,8 +8,23 @@ public class StartDatabase {
 		System.out.println("Started DB...");
 		System.out.println("Connecting to 'jdbc:h2:tcp://localhost:9092/~/test' will auto-create a database file 'test.mv.db' in user home (~)...");
 
+		deletePreviousDB();
+
 		// Allow auto-creating new databases on disk at first connection
 		org.h2.tools.Server.createTcpServer("-ifNotExists").start();
 	}
 
+	private static void deletePreviousDB() {
+		File dbFile = new File(System.getProperty("user.home"), "test.mv.db");
+		System.out.println("Db file: " +dbFile.getAbsolutePath());
+
+		if (dbFile.isFile()) {
+			boolean r = dbFile.delete();
+			if (!r) {
+				System.err.println("Could not delete previous db content!");
+			} else {
+				System.out.println("Previous DB contents wiped out");
+			}
+		}
+	}
 }
