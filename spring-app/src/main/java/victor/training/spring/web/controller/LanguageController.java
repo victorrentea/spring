8b4dccc1ff67a1944d894f76fd980a0c753547f0
victorrentea <victorrentea@gmail.com>
2022-06-16 12:29:22 +0300

    nu ai inteles proxyuri aici : aai chemat o fucntie local si ai vrut sa-ti mearga adnotarile. SI nu intelegi de ce nu merg. Si citesti.. Faza: tu cand chemi local, nu treci printr-o referinta injecatat de Spring >> no magic for you.

--- spring-app/src/main/java/victor/training/spring/web/controller/LanguageController.java
+++ spring-app/src/main/java/victor/training/spring/web/controller/LanguageController.java
@@ -1,6 +1,7 @@
 package victor.training.spring.web.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,16 +12,35 @@ import victor.training.spring.web.repo.ProgrammingLanguageRepo;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/languages")
 public class LanguageController {
     private final ProgrammingLanguageRepo repo;
+    private final LanguageService languageService;
 
     @GetMapping
     // TODO cache
     // TODO evict via dedicated endpoint (called from script)
     public List<LanguageDto> getAll() {
+        log.debug("Chem fct pe " + languageService.getClass());
+        return getLanguages();
+    }
+
+    @Cacheable("countries")
+    public List<LanguageDto> getLanguages() {
+        log.debug("in fct");
         return repo.findAll().stream().map(LanguageDto::new).collect(Collectors.toList());
     }
+
 }
+
+
+//class Tzeapa extends LanguageService {
+//    @Override
+//    public List<LanguageDto> getLanguages() {
+//daca am in cache da din cacjhe altfel cheam-o si apoi pune0n cache
+//        return super.getLanguages();
+//    }
+//}
---
+++ spring-app/src/main/java/victor/training/spring/web/controller/LanguageService.java
@@ -0,0 +1,24 @@
+package victor.training.spring.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import victor.training.spring.web.controller.dto.LanguageDto;
+import victor.training.spring.web.repo.ProgrammingLanguageRepo;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class LanguageService {
+    private final ProgrammingLanguageRepo repo;
+
+//    @Cacheable("countries")
+//    public List<LanguageDto> getLanguages() {
+//        log.debug("in fct");
+//        return repo.findAll().stream().map(LanguageDto::new).collect(Collectors.toList());
+//    }
+}
