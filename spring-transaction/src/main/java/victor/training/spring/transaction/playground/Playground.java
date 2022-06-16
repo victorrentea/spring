e2d9674687cd0d59da8114f1d01d1986704b89c9
victorrentea <victorrentea@gmail.com>
2022-06-16 16:30:44 +0300

    JPA ca write cache

--- spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
@@ -1,12 +1,16 @@
 package victor.training.spring.transaction.playground;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.h2.engine.Session;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class Playground {
@@ -17,9 +21,18 @@ public class Playground {
 
     @Transactional
     public void transactionOne() {
-        jdbc.update("insert into MESSAGE(id, message) values ( 100,'ALO' )");
-        repo.save(new Message("jpa"));
+        repo.save(new Message("a")); //asta nu scrie inca in DB ci il lasa in
+        // Persistence COntext (aka Session) care functioneaza aici ca un WRITE CACHE
+        // nu trimite direct INSERTUL ci asteapta sf tranzactiei cu succes.
+        // de ce ? pt performanta. Va putea ulterior sa batchuiasca mai multe inserturi impreuna
+        jdbc.update("insert into MESSAGE(id, message) values ( 100,null )");
+
+        // faci insert invalid aici dar crapa 7 metode mai sus cand se face in cele din urma COMMIT. ==> Uaaaa!! :((((9
+        //entityManager.flush();// folosesti temporar, doar pt debug ca afecteaza perf
+
+        log.debug("Dupa ce ies din fct proxyul reia controlul si da commit la conn");
     }
+
     @Transactional
     public void transactionTwo() {
     }
