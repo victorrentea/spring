9a02a3610db0fa7baaa577ce9c4ccd771e2a10d8
victorrentea <victorrentea@gmail.com>
2022-06-17 10:50:30 +0300

    undo la mesaje ca sa nu ai nevoie de rabbit pornit

--- spring-app/src/main/java/victor/training/spring/web/controller/MessageSenderController.java
+++ spring-app/src/main/java/victor/training/spring/web/controller/MessageSenderController.java
@@ -1,45 +1,45 @@
-package victor.training.spring.web.controller;
-
-
-import lombok.RequiredArgsConstructor;
-import lombok.Value;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.stream.function.StreamBridge;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.function.Consumer;
-
-
-@Slf4j
-@RequiredArgsConstructor
-@RestController
-@Configuration
-public class MessageSenderController {
-    static int i; // valeu!
-    private final StreamBridge streamBridge;
-
-    @GetMapping("api/send-message")
-    public String sendMessage() {
-        String reason = "Give me money! #" + ++i;
-        TransferMessage message = new TransferMessage("IBAN", 100, reason);
-        streamBridge.send("paymentRequest-out-0", message);
-        return "Sent message: " + message;
-    }
-
-    @Value
-    public static class TransferMessage {
-        String iban;
-        int amount;
-        String reason;
-    }
-
-    @Bean
-    public Consumer<String> receiveTransactionId() {
-        return id -> {
-            log.info("Received tid " + id);
-        };
-    }
-}
+//package victor.training.spring.web.controller;
+//
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.Value;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.cloud.stream.function.StreamBridge;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.function.Consumer;
+//
+//
+//@Slf4j
+//@RequiredArgsConstructor
+//@RestController
+//@Configuration
+//public class MessageSenderController {
+//    static int i; // valeu!
+//    private final StreamBridge streamBridge;
+//
+//    @GetMapping("api/send-message")
+//    public String sendMessage() {
+//        String reason = "Give me money! #" + ++i;
+//        TransferMessage message = new TransferMessage("IBAN", 100, reason);
+//        streamBridge.send("paymentRequest-out-0", message);
+//        return "Sent message: " + message;
+//    }
+//
+//    @Value
+//    public static class TransferMessage {
+//        String iban;
+//        int amount;
+//        String reason;
+//    }
+//
+//    @Bean
+//    public Consumer<String> receiveTransactionId() {
+//        return id -> {
+//            log.info("Received tid " + id);
+//        };
+//    }
+//}
--- spring-micro/src/main/java/victor/training/micro/MessageListener.java
+++ spring-micro/src/main/java/victor/training/micro/MessageListener.java
@@ -1,43 +1,43 @@
-package victor.training.micro;
-
-import lombok.Value;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.Message;
-
-import java.util.UUID;
-import java.util.function.Consumer;
-import java.util.function.Function;
-
-@Slf4j
-@Configuration
-public class MessageListener {
-
-    @Value
-    public static class TransferMessage {
-        String iban;
-        int amount;
-        String reason;
-    }
-    @Bean
-    public Function<Message<TransferMessage>, String> paymentRequestSubscriber() {
-        return message -> {
-            log.debug("Processing " + message);
-//            throw new RuntimeException("INTENTIONAT crap " + message.getPayload());
-            String transactionId = UUID.randomUUID().toString();
-            log.debug("reply with " + transactionId);
-            return transactionId;
-        };
-    }
-
-
-//    public class CustomErrorHandler implements ErrorHandler {
-//        @Override
-//        public void handleError(Throwable t) {
-//            if (!(t.getCause() instanceof BusinessException)) {
-//                throw new AmqpRejectAndDontRequeueException("Error Handler converted exception to fatal", t);
-//            }
-//        }
+//package victor.training.micro;
+//
+//import lombok.Value;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.messaging.Message;
+//
+//import java.util.UUID;
+//import java.util.function.Consumer;
+//import java.util.function.Function;
+//
+//@Slf4j
+//@Configuration
+//public class MessageListener {
+//
+//    @Value
+//    public static class TransferMessage {
+//        String iban;
+//        int amount;
+//        String reason;
 //    }
-}
+//    @Bean
+//    public Function<Message<TransferMessage>, String> paymentRequestSubscriber() {
+//        return message -> {
+//            log.debug("Processing " + message);
+////            throw new RuntimeException("INTENTIONAT crap " + message.getPayload());
+//            String transactionId = UUID.randomUUID().toString();
+//            log.debug("reply with " + transactionId);
+//            return transactionId;
+//        };
+//    }
+//
+//
+////    public class CustomErrorHandler implements ErrorHandler {
+////        @Override
+////        public void handleError(Throwable t) {
+////            if (!(t.getCause() instanceof BusinessException)) {
+////                throw new AmqpRejectAndDontRequeueException("Error Handler converted exception to fatal", t);
+////            }
+////        }
+////    }
+//}
