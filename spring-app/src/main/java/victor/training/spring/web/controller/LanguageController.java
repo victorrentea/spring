2a57ebbec8c05e74262bdf43618dfe144fd7ef0a
victorrentea <victorrentea@gmail.com>
2022-06-28 15:43:39 +0300

    Cacheable merge acum prin proxy-uri injectat in controller

--- spring-app/src/main/java/victor/training/spring/web/controller/LanguageController.java
+++ spring-app/src/main/java/victor/training/spring/web/controller/LanguageController.java
@@ -1,7 +1,10 @@
 package victor.training.spring.web.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,16 +14,40 @@ import victor.training.spring.web.repo.ProgrammingLanguageRepo;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/languages")
 public class LanguageController {
-    private final ProgrammingLanguageRepo repo;
+    private final LanguageService service;
 
     @GetMapping
     // TODO cache
     // TODO evict via dedicated endpoint (called from script)
     public List<LanguageDto> getAll() {
-        return repo.findAll().stream().map(LanguageDto::new).collect(Collectors.toList());
+        System.out.println("Vorbesc cu un proxy " + service.getClass());
+        return service.getAll();
     }
 }
+
+@Service
+@RequiredArgsConstructor
+class LanguageService {
+    private final ProgrammingLanguageRepo repo;
+
+    @Cacheable("languagesX")
+    public List<LanguageDto> getAll() {
+        System.out.println("Acum execut functia");
+        return repo.findAll()
+                .stream()
+                .map(LanguageDto::new)
+                .collect(toList());
+    }
+}
+// un interceptor(proxy) care primeste toate apelurile venite catre metode din clasa LanguageService ????
+    // la runtime, Spring genereaza o subclasa a lui Language Service
+    // si iti injecteaza la linia 23, subclasa lui.
+// interceptorul se uita ce metoda chemi, si daca vede ca invoci o metoda adnotata cu @Cacheable atunci
+// daca deja ai datele in memorie, le intoarce direct, altfel lasa apelul real sa execute si retine ulterior datele intoarse.
+
---
+++ spring-app/src/main/java/victor/training/spring/web/controller/ProxyuriDeMana.java
@@ -0,0 +1,49 @@
+package victor.training.spring.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cglib.proxy.Callback;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+public class ProxyuriDeMana {
+    public static void main(String[] args) {
+         // tu nu vei face asa ceva urmatorii 5 ani probabil
+        // ma prefac c-as fi spring
+
+        Mate realObject = new Mate();
+
+        Callback h = new MethodInterceptor() {
+            @Override
+            public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+                System.out.println("Sunt si eu aici si vad ca invoci metoda " + method.getName() + " cu arg " + Arrays.toString(args));
+//                method.isAnnotationPresent(Cacheable.class)
+                return method.invoke(realObject, args);
+            }
+        };
+        Mate proxy = (Mate) Enhancer.create(Mate.class, h);
+        Client client = new Client(proxy);
+        client.clasa2();
+    }
+}
+
+
+@RequiredArgsConstructor
+class Client {
+    private final Mate mate; // ceea ce ne da springu nu e defapt clasa reala a noasta ci un proxy.
+
+    public void clasa2() {
+        System.out.println("Tu cine esti ?!? Un Proxy" + mate.getClass());
+        System.out.println(mate.suma(17, 23));
+    }
+}
+
+class Mate {
+    public int suma(int a, int b) {
+        return a+ b;
+    }
+}
