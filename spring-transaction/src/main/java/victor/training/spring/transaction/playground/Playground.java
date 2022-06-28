7f5f8aebd8d13a3b569ec10954d6ce4cad2a83bc
victorrentea <victorrentea@gmail.com>
2022-06-28 17:15:05 +0300

    ThreadLocal WTF? !

--- spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
@@ -2,6 +2,7 @@ package victor.training.spring.transaction.playground;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,8 +22,14 @@ public class Playground {
         jdbc.update("insert into MESSAGE(id, message) values ( 100,'ALO' )");
         System.out.println("😁");
 
-        OtherClass.var.set("a");
+        OtherClass.var.set("10");
+        ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
         other.m();
+        // ce sta pe tjhread:
+        // 1) JDBC Connection < Transaction
+        // 2) PersistenceContext(JPA)
+        // 3) Security Context (cine e logat, acceul la JWT token, usernameul)
+        // 4) Lomback MDC
     }
 
     @Transactional
