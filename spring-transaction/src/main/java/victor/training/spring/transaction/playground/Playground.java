960fb56a96f98e74bf7fdb13d36373b9c60db93a
victorrentea <victorrentea@gmail.com>
2022-06-28 17:47:52 +0300

    cu propagation=REQUIRES_NEW poti sa fortezi obtinerea + commitul unei alte tranzactii, separate de cea a apelantului.

--- spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
@@ -2,13 +2,11 @@ package victor.training.spring.transaction.playground;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import java.io.IOException;
-import java.sql.Connection;
 
 @Service
 @RequiredArgsConstructor
@@ -19,13 +17,16 @@ public class Playground {
     private final OtherClass other;
 
     @Transactional
-    public void transactionOne() throws IOException {
+    public void transactionOne() {
         jdbc.update("insert into MESSAGE(id, message) values ( 100,'ALO' )");
         System.out.println("😁");
 
         System.out.println(repo.findAllByMessage("ALO"));
-
-        other.m();
+        try {
+            other.bizLogicRiscant();
+        } catch (Exception e) {
+            other.persistError(e);
+        }
     }
 
     @Transactional
@@ -38,9 +39,17 @@ public class Playground {
 class OtherClass {
     private final MessageRepo repo;
 
-    public void m() throws IOException {
-        repo.save(new Message("tranzactia"));
-        repo.save(new Message("count.amount -- "));
-        throw new IOException("BUM");
+    @Transactional // atomic , dar vine si proxy aici.
+    public void bizLogicRiscant() {
+        repo.save(new Message("INSERT tranzactia"));
+        repo.save(new Message("UPDATE count.amount -- "));
+        throw new RuntimeException("BUM");
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void persistError(Exception e) {
+        e.printStackTrace();
+        Message errorToInsert = new Message("ERROR: " + e.getMessage());
+        repo.save(errorToInsert); // aici scriu intr-o tranzactie deja moarta (zombie)
     }
 }
