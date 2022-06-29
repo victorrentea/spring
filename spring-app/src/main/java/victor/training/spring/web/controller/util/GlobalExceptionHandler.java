06ef3f489bf588ca40772743329126b525901fd6
victorrentea <victorrentea@gmail.com>
2022-06-29 17:09:54 +0300

    yolo

--- spring-app/src/main/java/victor/training/spring/web/controller/util/GlobalExceptionHandler.java
+++ spring-app/src/main/java/victor/training/spring/web/controller/util/GlobalExceptionHandler.java
@@ -2,22 +2,25 @@ package victor.training.spring.web.controller.util;
 
 import javax.servlet.http.HttpServletRequest;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
-	private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-	
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ResponseStatus(INTERNAL_SERVER_ERROR) // http response status
 	@ExceptionHandler(value = Exception.class)
-	public String defaultErrorHandler(HttpServletRequest request, Exception exception) throws Exception {
+	public String defaultErrorHandler(Exception exception, HttpServletRequest request) throws Exception {
 		log.error(exception.getMessage(), exception);
-		// you may want to translate a message code in the request.getLocale()
-		return exception.getMessage();
+		// you may want to translate a message code in the request.getLocale() / OpenID user token
+		return exception.getMessage(); // Security Best Practice: hide stack traces from API responses.
 	}
 
 }
