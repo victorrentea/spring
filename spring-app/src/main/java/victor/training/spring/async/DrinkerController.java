51249302041f6539034efe509ba3977fa7d66785
victorrentea <victorrentea@gmail.com>
2022-06-30 15:40:54 +0300

    thread pools stilu classic

--- spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
+++ spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
@@ -2,12 +2,16 @@ package victor.training.spring.async;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import victor.training.spring.async.drinks.Beer;
 import victor.training.spring.async.drinks.DillyDilly;
 import victor.training.spring.async.drinks.Vodka;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
 
 import static java.lang.System.currentTimeMillis;
 
@@ -16,6 +20,8 @@ import static java.lang.System.currentTimeMillis;
 public class DrinkerController {
    @Autowired
    private Barman barman;
+   @Autowired
+   private ThreadPoolTaskExecutor executor;
 
    // TODO [1] inject and submit work to a ThreadPoolTaskExecutor
    // TODO [2] mark pour* methods as @Async
@@ -25,8 +31,14 @@ public class DrinkerController {
       log.debug("Submitting my order");
       long t0 = currentTimeMillis();
 
-      Beer beer = barman.pourBeer();
-      Vodka vodka = barman.pourVodka();
+
+      // cand ceri o executie asincrona cu threadpooluri in Java obtii un Future.
+      //pe care poti ulterior sa blochezi sa astepti rezultatul
+      Future<Beer> futureBeer = executor.submit(() -> barman.pourBeer());
+      Future<Vodka> futureVodka = executor.submit(() -> barman.pourVodka());
+      log.debug("A plecat garcon cu comanda");
+      Beer beer = futureBeer.get();
+      Vodka vodka = futureVodka.get();
 
       long t1 = currentTimeMillis();
       log.debug("Got my drinks in {} millis", t1-t0);
