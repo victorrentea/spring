ce4f5acc30fc82c68b6dc9d9f67ecc4479040c29
victorrentea <victorrentea@gmail.com>
2022-08-31 11:20:06 +0300

    beans have names !!§

--- spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
+++ spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
@@ -7,7 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
@@ -18,12 +17,11 @@ public class BeanApp {
 
 
     @Bean
-    @Primary
     public Person john() {
         return new Person("John");
     }
     @Bean
-    public Person jane() {
+    public Person jane() { // method name = bean name
         return new Person("Jane");
     }
 }
@@ -31,29 +29,33 @@ public class BeanApp {
 class SecondConfig implements CommandLineRunner{
     @Autowired
     private Conversation conversation;
-
     @Override
     public void run(String... args) throws Exception {
         // TODO convince Spring to do for you the line above
         conversation.start();
     }
-
 }
+
+//@Qualifier
+//@Retention(RetentionPolicy.RUNTIME)
+//@interface First {}
+//
 
 @Data
 @Component
-class Conversation {
-    private final Person one;
-    private final Person two;
 
-    public Conversation(Person one, Person two) {
-        this.one = one;
-        this.two = two;
-    }
+class Conversation { // by default the name of this bean is = "conversation"
+    private final Person john;
+    private final Person jane;
+
+//    public Conversation(Person john,  Person jane) { // if the injection point name matches a bean name => no @Qualifier is needed is needed
+//        this.one = john;
+//        this.two = jane;
+//    }
 
     public void start() {
-        System.out.println(one.sayHello());
-        System.out.println(two.sayHello());
+        System.out.println(john.sayHello());
+        System.out.println(jane.sayHello());
     }
 }
 
