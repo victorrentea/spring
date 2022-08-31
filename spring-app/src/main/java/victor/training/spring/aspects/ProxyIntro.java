f2b512fbfe9d98e9986c9572df9cef866ce90999
victorrentea <victorrentea@gmail.com>
2022-08-31 15:31:17 +0300

    How proxies work

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -1,23 +1,27 @@
 package victor.training.spring.aspects;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cglib.proxy.Callback;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 @Slf4j
 public class ProxyIntro {
     public static void main(String[] args) {
         // pretend to BE Spring here
-        Maths maths = new Maths() {
+        Maths real = new Maths();
+        Callback h = new MethodInterceptor() {
             @Override
-            public int sum(int a, int b) {
-                log.info("sum of " + a + " + " + b);
-                return super.sum(a, b);
-            }
-
-            @Override
-            public int product(int a, int b) {
-                log.info("product of " + a + " + " + b);
-                return super.product(a, b);
+            public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+                log.info(method.getName() + " of " + Arrays.toString(args));
+                return method.invoke(real, args);
             }
         };
+        Maths maths = (Maths) Enhancer.create(Maths.class, h);
         SecondGrade secondGrade = new SecondGrade(maths);
 
         // TODO maniac father wants to log all the math operations done during school
@@ -34,20 +38,29 @@ class SecondGrade {
     }
 
     public void mathClass() {
+        System.out.println("Who am I talking to really here? " + maths.getClass());
         System.out.println(maths.sum(2, 4));
         System.out.println(maths.sum(1, 5));
         System.out.println(maths.product(2, 3));
+        System.out.println(maths.product(3, 3));
     }
 }
 
 @Slf4j
-class Maths {
+/*final*/ class Maths { // explosion
     public int sum(int a, int b) {
         return a + b;
     }
 
-    public int product(int a, int b) {
-        return a * b;
+    public /*final*/ int product(int a, int b) { // silently skip proxying this method. DON'T USE
+//        return a * b;
+
+        int prod = 0;
+        for (int i = 0; i < a; i++) {
+            prod = sum(prod, b); // NEVER audited because is NOT going through the proxy.
+        }
+        return prod;
+
     }
 }
 
