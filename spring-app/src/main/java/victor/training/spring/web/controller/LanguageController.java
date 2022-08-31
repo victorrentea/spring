72a2e4577ae94fcac4a0218a65349c0822d5a9da
victorrentea <victorrentea@gmail.com>
2022-08-31 16:24:07 +0300

    cache eviction

--- spring-app/src/main/java/victor/training/spring/web/controller/LanguageController.java
+++ spring-app/src/main/java/victor/training/spring/web/controller/LanguageController.java
@@ -1,6 +1,8 @@
 package victor.training.spring.web.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,8 +23,18 @@ public class LanguageController {
     @GetMapping
     // TODO cache
     // TODO evict via dedicated endpoint (called from script)
+    // at the end of the .sh script that inserts data, you need to tell the running apps to evict their cache.
     public List<LanguageDto> getAll() {
         return languageService.getEntities().stream().map(LanguageDto::new).collect(Collectors.toList());
+    }
+
+    private final CacheManager cacheManager; // programmatic alternative to @Cache*
+
+    @GetMapping("evict")
+//    @CacheEvict("languages")
+    public void killLanguageCache() {
+        cacheManager.getCache("languages").clear();
+        // empty method. but please do not delete it. it's here only for the proxy in front to wipe the cache out
     }
 
 }
