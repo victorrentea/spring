d62672d3f021c753a3a88e7f59c51186c8ba93a7
victorrentea <victorrentea@gmail.com>
2022-08-31 15:06:38 +0300

    Spring boot magic.

--- spring-start/src/main/java/victor/training/spring/props/PropApp.java
+++ spring-start/src/main/java/victor/training/spring/props/PropApp.java
@@ -2,10 +2,18 @@ package victor.training.spring.props;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PropApp {
     public static void main(String[] args) {
         SpringApplication.run(PropApp.class, args);
     }
+
+//    @Bean
+//    public DataSource dataSource() {
+//
+//    }
 }
