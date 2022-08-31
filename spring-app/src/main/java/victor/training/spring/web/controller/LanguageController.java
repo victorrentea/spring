6681f11c2cf098a5e6130c55cfc4cf792df4f9e8
victorrentea <victorrentea@gmail.com>
2022-08-31 16:13:07 +0300

    REMEMBER: @Cacheable works via proxies, which dont work in local method calls within the same class.

--- spring-app/src/main/java/victor/training/spring/web/controller/LanguageController.java
+++ spring-app/src/main/java/victor/training/spring/web/controller/LanguageController.java
@@ -2,10 +2,12 @@ package victor.training.spring.web.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import victor.training.spring.web.controller.dto.LanguageDto;
+import victor.training.spring.web.entity.ProgrammingLanguage;
 import victor.training.spring.web.repo.ProgrammingLanguageRepo;
 
 import java.util.List;
@@ -15,12 +17,37 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @RequestMapping("api/languages")
 public class LanguageController {
-    private final ProgrammingLanguageRepo repo;
-
+    private final LanguageService languageService;
     @GetMapping
     // TODO cache
     // TODO evict via dedicated endpoint (called from script)
     public List<LanguageDto> getAll() {
-        return repo.findAll().stream().map(LanguageDto::new).collect(Collectors.toList());
+        return languageService.getEntities().stream().map(LanguageDto::new).collect(Collectors.toList());
+    }
+
+}
+
+@Service
+@RequiredArgsConstructor
+class LanguageService  {
+    private final ProgrammingLanguageRepo repo;
+
+    @Cacheable("languages")
+    public List<ProgrammingLanguage> getEntities() {
+        return repo.findAll();
     }
 }
+
+
+@RestController
+@RequiredArgsConstructor
+class AnotherController {
+    private final LanguageService languageService;
+
+    @GetMapping("two")
+    public List<ProgrammingLanguage> two() {
+        return languageService.getEntities();
+    }
+}
+
+
