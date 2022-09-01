67598661ed6e70320e06a4e6e3c8000a719d1c45
victorrentea <victorrentea@gmail.com>
2022-09-01 12:41:09 +0300

    Conn starvation issue fixed. moved the atomic part away from the blocking call

--- spring-app/src/main/java/victor/training/spring/performance/TransactedService.java
+++ spring-app/src/main/java/victor/training/spring/performance/TransactedService.java
@@ -1,19 +1,36 @@
 package victor.training.spring.performance;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import victor.training.spring.web.entity.Message;
 import victor.training.spring.web.repo.MessageRepo;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class TransactedService {
    private final ExpensiveApiClient apiClient;
    private final MessageRepo messageRepo;
+   private final OtherService service;
+
    public void flow() {
       String data = apiClient.blockingCall();
+      service.atomic(data);
+   }
+
+}
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+class OtherService {
+
+   private final MessageRepo messageRepo;
+
+   @Transactional
+   public void atomic(String data) {
+      messageRepo.save(new Message(data));
       messageRepo.save(new Message(data));
    }
 }
