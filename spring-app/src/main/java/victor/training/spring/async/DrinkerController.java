870e2c70e195111a84afad34374650013e5333b6
victorrentea <victorrentea@gmail.com>
2022-09-01 16:09:48 +0300

    In face of large parallel load, you can protect Tomcats' thread by non-blocking your endpoints and returning a CompletableFuture(aka promise ) to Spring

--- spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
+++ spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
@@ -9,11 +9,10 @@ import victor.training.spring.async.drinks.Beer;
 import victor.training.spring.async.drinks.DillyDilly;
 import victor.training.spring.async.drinks.Vodka;
 
-import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.stream.Collectors;
 
 import static java.lang.System.currentTimeMillis;
 
@@ -31,24 +30,23 @@ public class DrinkerController {
     // TODO [2] mark pour* methods as @Async
     // TODO [3] Build a non-blocking web endpoint
     @GetMapping("api/drink")
-    public DillyDilly drink() throws Exception {
+    public CompletableFuture<DillyDilly> drink() throws Exception {
         log.debug("Submitting my order");
         long t0 = currentTimeMillis();
 
-        Future<Beer> futureBeer = barExecutor.submit(() -> barman.pourBeer());
-        Future<Vodka> futureVodka = barExecutor.submit(() -> barman.pourVodka());
-
         log.debug("Here, the waiter left with my 2 commands");
 
-        Vodka vodka = futureVodka.get(); // how much time does the HTTP thread wait here? 1 sec
-        Beer beer = futureBeer.get(); // how much time does the HTTP thread wait here? 0 sec
+        CompletableFuture<Beer> beerCF = CompletableFuture.supplyAsync(() -> barman.pourBeer());
+        CompletableFuture<Vodka> vodkaCF = CompletableFuture.supplyAsync(() -> barman.pourVodka());
 
 
+        CompletableFuture<DillyDilly> dillyCF = beerCF.thenCombineAsync(vodkaCF,
+                (beer, vodka) -> new DillyDilly(beer, vodka));
 
         barExecutor.submit(() -> barman.curse("$!#%!*%&!&*!")); // fire-and-forget action
 
         long t1 = currentTimeMillis();
         log.debug("Got my drinks in {} millis", t1 - t0);
-        return new DillyDilly(beer, vodka);
+        return dillyCF;
     }
 }
