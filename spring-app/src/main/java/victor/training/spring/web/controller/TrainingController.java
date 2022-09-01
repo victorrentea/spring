69e633b88e334aae55fb7bde0ea91b46f3ccd278
victorrentea <victorrentea@gmail.com>
2022-09-01 15:33:47 +0300

    wow

--- spring-app/src/main/java/victor/training/spring/web/controller/TrainingController.java
+++ spring-app/src/main/java/victor/training/spring/web/controller/TrainingController.java
@@ -56,5 +56,6 @@ public class TrainingController {
 	public List<TrainingDto> search(TrainingSearchCriteria criteria) {
 		return trainingService.search(criteria);
 	}
+	// Hint: try direcly @GetMapping with no @RequestBody annot
 
 }
