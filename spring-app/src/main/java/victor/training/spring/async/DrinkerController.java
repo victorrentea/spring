9f171756dbb24bd9a6d6df0fe44b28657f0a60ff
victorrentea <victorrentea@gmail.com>
2022-09-01 15:44:45 +0300

    Java standard executors

--- spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
+++ spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
@@ -8,27 +8,38 @@ import victor.training.spring.async.drinks.Beer;
 import victor.training.spring.async.drinks.DillyDilly;
 import victor.training.spring.async.drinks.Vodka;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import static java.lang.System.currentTimeMillis;
 
 @Slf4j
 @RestController
 public class DrinkerController {
-   @Autowired
-   private Barman barman;
+    @Autowired
+    private Barman barman;
 
-   // TODO [1] inject and submit work to a ThreadPoolTaskExecutor
-   // TODO [2] mark pour* methods as @Async
-   // TODO [3] Build a non-blocking web endpoint
-   @GetMapping("api/drink")
-   public DillyDilly drink() throws Exception {
-      log.debug("Submitting my order");
-      long t0 = currentTimeMillis();
+    // TODO [1] inject and submit work to a ThreadPoolTaskExecutor
+    // TODO [2] mark pour* methods as @Async
+    // TODO [3] Build a non-blocking web endpoint
+    @GetMapping("api/drink")
+    public DillyDilly drink() throws Exception {
+        log.debug("Submitting my order");
+        long t0 = currentTimeMillis();
 
-      Beer beer = barman.pourBeer();
-      Vodka vodka = barman.pourVodka();
+        ExecutorService pool = Executors.newFixedThreadPool(2);
+        Future<Beer> futureBeer = pool.submit(() -> barman.pourBeer());
+        Future<Vodka> futureVodka = pool.submit(() -> barman.pourVodka());
 
-      long t1 = currentTimeMillis();
-      log.debug("Got my drinks in {} millis", t1-t0);
-      return new DillyDilly(beer, vodka);
-   }
+        log.debug("Here, the waiter left with my 2 commands");
+
+        Vodka vodka = futureVodka.get(); // how much time does the HTTP thread wait here? 1 sec
+        Beer beer = futureBeer.get(); // how much time does the HTTP thread wait here? 0 sec
+
+
+        long t1 = currentTimeMillis();
+        log.debug("Got my drinks in {} millis", t1 - t0);
+        return new DillyDilly(beer, vodka);
+    }
 }
