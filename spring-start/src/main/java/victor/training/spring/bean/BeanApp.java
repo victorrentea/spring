6392da9482903efa189ebcba6eae0344fbace575
Victor Rentea <victorrentea@gmail.com>
2022-09-26 10:49:54 +0300

    pitfall of prototype scoped : why not to use it !

--- spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
+++ spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
@@ -1,9 +1,15 @@
 package victor.training.spring.bean;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class BeanApp implements CommandLineRunner {
@@ -11,11 +17,32 @@ public class BeanApp implements CommandLineRunner {
         SpringApplication.run(BeanApp.class);
     }
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
+
     @Override
     public void run(String... args) throws Exception {
-        Conversation conversation = new Conversation(new Person("John"), new Person("Jane"));
+        Person p1 = applicationContext.getBean(Person.class); // dangerous
+        System.out.println(p1);
+        Person p2 = applicationContext.getBean(Person.class);
+        System.out.println(p2);
+        Conversation conversation = new Conversation(new Person(), new Person());
         // TODO convince Spring to do for you the line above
         conversation.start();
+    }
+}
+
+@RestController
+class MyController {
+    private final Person person; // Pitfall: injecting a prototype-scoped into a singleton! => only creates a single prototype
+
+    MyController(Person person) {
+        this.person = person;
+    }
+    @GetMapping
+    public String method() {
+        return "different instance for each call ? " + person;
     }
 }
 
@@ -36,11 +63,13 @@ class Conversation {
 }
 
 
+@Component
+@Scope("prototype")
 class Person {
     private final String name;
 
-    public Person(String name) {
-        this.name = name;
+    public Person() {
+        this.name = "name";
     }
     public String sayHello() {
         return "Hello! Here is " + name;
