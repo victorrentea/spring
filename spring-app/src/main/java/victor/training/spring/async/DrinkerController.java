eaa77f0d0a1e85189a769a35fb3dfd624429f213
Victor Rentea <victorrentea@gmail.com>
2022-09-27 12:40:30 +0300

    controlling the size of the thread pool

--- spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
+++ spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
@@ -2,6 +2,7 @@ package victor.training.spring.async;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import victor.training.spring.async.drinks.Beer;
@@ -11,6 +12,8 @@ import zipkin2.Call;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static java.lang.System.currentTimeMillis;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
@@ -29,8 +32,9 @@ public class DrinkerController {
       log.debug("Submitting my order");
       long t0 = currentTimeMillis();
 
-      CompletableFuture<Beer> futureBeer = supplyAsync(() -> barman.pourBeer());
-      CompletableFuture<Vodka> futureVodka = supplyAsync(() -> barman.pourVodka());
+      ExecutorService pool = Executors.newFixedThreadPool(2);
+      CompletableFuture<Beer> futureBeer = supplyAsync(() -> barman.pourBeer(), pool);
+      CompletableFuture<Vodka> futureVodka = supplyAsync(() -> barman.pourVodka(), pool);
       Beer beer = futureBeer.get(); // 1 sec
       Vodka vodka = futureVodka.get(); // 0 sec
 
