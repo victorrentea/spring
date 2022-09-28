f87480edc2a2fac71c08a3ef36f4812ce0d4ab1f
Victor Rentea <victorrentea@gmail.com>
2022-09-28 09:28:12 +0300

    cleanup unused code

--- spring-app-cdn/run-nodejs.bat
+++
@@ -1,4 +0,0 @@
-rem this is a more traditional approach: serving static content from a NodeJS instance.
-
-call npm install --global http-server
-http-server ../spring-app/src/main/resources/static -p 8081
