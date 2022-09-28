b148d74ed69e9df91fa7573a0dcdb7558560b58a
Victor Rentea <victorrentea@gmail.com>
2022-09-28 15:07:48 +0300

    header auth

---
+++ spring-app/src/main/java/victor/training/spring/web/security/preauth/PreAuthHeaderFilter.java
@@ -0,0 +1,37 @@
+package victor.training.spring.web.security.preauth;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class PreAuthHeaderFilter extends AbstractPreAuthenticatedProcessingFilter {
+    public PreAuthHeaderFilter(AuthenticationManager authenticationManager) {
+        setAuthenticationManager(authenticationManager);
+    }
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    @Override
+    protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
+        String username = request.getHeader("X-User");
+        String rolesStr = request.getHeader("X-User-Roles");
+        if (username == null || rolesStr == null) return null;
+        if (username.isBlank() || rolesStr.isBlank()) return null;
+        List<String> roles = List.of(rolesStr.split(","));
+        Map<String, String> attributesMap = Map.of(); // todo read from other headers
+        return new PreAuthHeaderPrincipal(username, roles, attributesMap);
+    }
+
+    @Override
+    protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
+        return "null";
+    }
+}
---
+++ spring-app/src/main/java/victor/training/spring/web/security/preauth/PreAuthHeaderSecurityConfig.java
@@ -0,0 +1,44 @@
+package victor.training.spring.web.security.preauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+// this will allow going in just using headers, eg:
+// curl http://localhost:8080/api/trainings -H 'X-User: user' -H 'X-User-Roles: USER'
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class PreAuthHeaderSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // as I don't ever take <form> POSTs
+                .addFilterBefore(preAuthHeaderFilter(), BasicAuthenticationFilter.class)
+                .authenticationProvider(preAuthenticatedProvider())
+                .authorizeRequests().anyRequest().authenticated();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        // trick to expose the standard AuthenticationManager bean
+        return super.authenticationManagerBean();
+    }
+    @Bean
+    public PreAuthHeaderFilter preAuthHeaderFilter() throws Exception {
+        return new PreAuthHeaderFilter(authenticationManagerBean());
+    }
+
+    @Bean
+    public AuthenticationProvider preAuthenticatedProvider() {
+        PreAuthenticatedAuthenticationProvider provider = new PreAuthenticatedAuthenticationProvider();
+        provider.setPreAuthenticatedUserDetailsService(token -> (PreAuthHeaderPrincipal) token.getPrincipal());
+        return provider;
+    }
+}
