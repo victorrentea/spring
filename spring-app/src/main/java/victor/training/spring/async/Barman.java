5ecc4a55a40bbb8338706133a33e6b4cabc62e01
Victor Rentea <victorrentea@gmail.com>
2022-10-03 17:40:50 +0300

    non-blocking http endpoints fara WebFlux

--- spring-app/src/main/java/victor/training/spring/async/Barman.java
+++ spring-app/src/main/java/victor/training/spring/async/Barman.java
@@ -9,15 +9,14 @@ import victor.training.spring.async.drinks.Vodka;
 
 import java.util.concurrent.CompletableFuture;
 
-import static java.util.concurrent.CompletableFuture.*;
 import static java.util.concurrent.CompletableFuture.completedFuture;
 
 @Slf4j
 @Service
 public class Barman {
    @Async
-   public CompletableFuture<Beer> pourBeer() {
-      if (true) throw new IllegalStateException("NU MAI E BERE in Vama");
+   public CompletableFuture<Beer> pourBeer()  {
+//      if (true) throw new RuntimeException("NU MAI E BERE in Vama");
       log.debug("Pouring Beer (SOAP CALL)...");
       ThreadUtils.sleepq(1000);
       return completedFuture(new Beer());
@@ -27,5 +26,14 @@ public class Barman {
       log.debug("Pouring Vodka (REST CALL)...");
       ThreadUtils.sleepq(1000);
       return completedFuture(new Vodka());
+   }
+
+   @Async
+   public void longRunProcessingFoolowingARESTCALl(String uratura) {
+      if (uratura != null)
+         throw new RuntimeException("Iti fac buzunar/Te casez");
+      // eg: process uploaded file
+      // generate export file / batch / sending notifications ....
+      ThreadUtils.sleepq(10000);
    }
 }
--- spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
+++ spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
@@ -9,7 +9,10 @@ import victor.training.spring.async.drinks.Beer;
 import victor.training.spring.async.drinks.DillyDilly;
 import victor.training.spring.async.drinks.Vodka;
 
+import javax.servlet.AsyncContext;
+import javax.servlet.http.HttpServletRequest;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import static java.lang.System.currentTimeMillis;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
@@ -28,28 +31,41 @@ public class DrinkerController {
    // TODO [2] mark pour* methods as @Async
    // TODO [3] Build a non-blocking web endpoint
    @GetMapping("api/drink")
-   public DillyDilly drink() throws Exception {
+   public CompletableFuture<DillyDilly> drink() throws ExecutionException, InterruptedException {
       log.debug("Submitting my order");
       long t0 = currentTimeMillis();
 
 
-      CompletableFuture<Beer> futureBeer = null;
-      try {
-         System.out.println("Oare cum e posibil? eu chem o functie, si aia nu ruleaza ?");
-         System.out.println("Oare barmanul cine este ? " + barman.getClass());
-         futureBeer = barman.pourBeer();
-      } catch (Exception e) {
-         // toDO
-         System.out.println("NICIODATA NU INTRA AICI! DE CE ?!!!");
-      }
+      System.out.println("Oare cum e posibil? eu chem o functie, si aia nu ruleaza ?");
+      System.out.println("Oare barmanul cine este ? " + barman.getClass());
+      CompletableFuture<Beer> futureBeer = barman.pourBeer();
       CompletableFuture<Vodka> futureVodka = barman.pourVodka();
 
-      Beer beer = futureBeer.get();
-      Vodka vodka = futureVodka.get();
+//      Beer beer = futureBeer.get(); // < aici tocmai a murit 0.5% (1 din max 200) din Tomcat pt 1 secunda
+//      Vodka vodka = futureVodka.get();
 
+      CompletableFuture<DillyDilly> futureDilly =
+              futureBeer.thenCombine(futureVodka,
+                      (beer, vodka) -> new DillyDilly(beer, vodka));
 
+      //      barman.longRunProcessingFoolowingARESTCALl("mama tata #!*$&%!#&%^&*!#^%&*!^");
+      log.info("Ajung in patuc");
       long t1 = currentTimeMillis();
       log.debug("Got my drinks in {} millis", t1-t0);
-      return new DillyDilly(beer, vodka);
+      return futureDilly;
    }
+//      futureDilly.thenAccept(dilly -> httpRespon.write(dilly))
 }
+
+//class Acu10AniAIesitServlet3_0 {
+//   @GetMapping
+//   public void method(HttpServletRequest request) {
+//      AsyncContext asyncContext = request.startAsync();
+//
+//      // din alt thread
+////      {
+////         asyncContext.getResponse().getWriter().write(dilly);
+////         asyncContext.complete(); // aici ii spui lui Tomcat sa inchida conn TCP IP
+////      }
+//   }
+//}
