291f3b0164ccc6e919825fa71f1d605fd1bcf28e
Victor Rentea <victorrentea@gmail.com>
2022-10-17 14:53:52 +0300

    cum aflu daca am tx pornita?

--- spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
@@ -1,9 +1,15 @@
 package victor.training.spring.transaction.playground;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import javax.persistence.EntityManager;
 
@@ -15,10 +21,14 @@ public class Playground {
     private final JdbcTemplate jdbc;
     private final OtherClass other;
 
-    @Transactional // face ca tot sau nimic ce e in metoda se va persista
+//    @Transactional // face ca tot sau nimic ce e in metoda se va persista = ATOMIC
     public void transactionOne() {
         jdbc.update("insert into MESSAGE(id, message) values ( 100,'ALO' )");
-        repo.save(new Message("jpa"));
+        repo.save(new Message("jpa")); // nici un insert in log.
+        System.out.println("saveul de mai sus NU AJUNGE IN DB decat DUPA FINALUL metodei");
+
+
+        f();
         // 0 p6spy
         // 1 Cause a rollback by breaking NOT NULL, throw Runtime, throw CHECKED
         // 2 Tx propagates with your calls (in your thread😱)
@@ -26,6 +36,15 @@ public class Playground {
         // 4 Game: persist error from within zombie transaction: REQUIRES_NEW or NOT_SUPPORTED
         // 5 Performance: connection starvation issues : debate: avoid nested transactions
     }
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+    private void f() {
+        System.out.println(" E activa? " + TransactionSynchronizationManager.isActualTransactionActive());
+    }
+
+    //    @Transactional
 
 
     @Transactional
