086b808477c0f0e3617dfb41b416c8a38d9bfe4e
Victor Rentea <victorrentea@gmail.com>
2022-10-17 11:33:56 +0300

    Panica pentru Vlad Mihalcea: pentru ca realizat ca orice req REST pastreaza deschisa connexiunea JDBC (±tranzactie) pana la trimiterea raspunsului HTTP catre client
    - Vlad: nu e eficient! Frate tu tii connex pana la final HTTP request? pai asta blocheaza acea connex putand duce la Connection Starvation issues (TimeoutWaitingForConnection sau intarzieri neexplicate in toate req)
    
    - daca vii din web, va merge by default lazy loading. din alte threaduri insa NU va merge.

--- spring-app/src/main/java/victor/training/spring/web/service/LazyLoadExceptions.java
+++ spring-app/src/main/java/victor/training/spring/web/service/LazyLoadExceptions.java
@@ -3,7 +3,8 @@ package victor.training.spring.web.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import victor.training.spring.web.entity.Teacher;
 import victor.training.spring.web.repo.TeacherRepo;
 
@@ -17,13 +18,39 @@ public class LazyLoadExceptions {
 //    @Transactional // fix1 pros:
     @Scheduled(initialDelay = 1000, fixedDelay = 10000000)
     public void method() {
+        creepy();
+    }
+
+    // daca chemi creepy din REST Controller -> merge lazy
+    // daca o chemi din @Schedule, @MessageListener, @Async, (orice mod de a porni exec nu prin HTTP) -> o sa crape cu lazy load init ex.
+
+    public void creepy() {
         System.out.println("ONCE!");
-//        List<Teacher> teachers = teacherRepo.findAll(); // NU asa cand vrei si copiiiiii pe usecaseul asta
-        List<Teacher> teachers = teacherRepo.findAllWithChildren();
+        List<Teacher> teachers = teacherRepo.findAll(); // NU asa cand vrei si copiiiiii pe usecaseul asta
+        //        List<Teacher> teachers = teacherRepo.findAllWithChildren();
         System.out.println("AM adus teacheri fara sa incarc inca trainingurile lor");
         for (Teacher teacher : teachers) {
             System.out.println(teacher.getName() + " tine + " + teacher.getTrainings());
         }
         System.out.println("AFTER");
+    }
+}
+
+
+@RestController
+@RequiredArgsConstructor
+class LazyLoadController {
+    private final TeacherRepo teacherRepo;
+    private final  LazyLoadExceptions lazyLoadExceptions;
+
+    @GetMapping("api/lazy")
+    public String lazy() {
+        lazyLoadExceptions.creepy();
+        return ":)";
+    }
+
+    @GetMapping("api/jr")
+    public List<Teacher> jr() { // developerii e prosti
+        return teacherRepo.findAll();
     }
 }
