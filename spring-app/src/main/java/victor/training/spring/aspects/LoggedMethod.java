d743dd7062da10fb05bba5510069daebf786e92a
Victor Rentea <victorrentea@gmail.com>
2022-10-17 13:06:04 +0300

    polish aspect

---
+++ spring-app/src/main/java/victor/training/spring/aspects/LoggedMethod.java
@@ -0,0 +1,8 @@
+package victor.training.spring.aspects;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoggedMethod {
+}
