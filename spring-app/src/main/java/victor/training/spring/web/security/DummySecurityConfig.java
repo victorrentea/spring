aca6ef928b011953e848521905b26b3e7727311d
Victor Rentea <victorrentea@gmail.com>
2022-10-19 12:31:48 +0300

    Las-o asa ca merge

--- spring-app/src/main/java/victor/training/spring/web/security/DummySecurityConfig.java
+++ spring-app/src/main/java/victor/training/spring/web/security/DummySecurityConfig.java
@@ -11,7 +11,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true) // @Secured @PreAuthorized
@@ -21,12 +23,19 @@ public class DummySecurityConfig extends WebSecurityConfigurerAdapter {
    protected void configure(HttpSecurity http) throws Exception {
       CookieCsrfTokenRepository csrfTokenRepository = new CookieCsrfTokenRepository();
       csrfTokenRepository.setCookieHttpOnly(false);
-      http
 //          .addFilterBefore(new HttpRequestFilterPrintingHeaders(), WebAsyncManagerIntegrationFilter.class)
 //          .cors().and()
 //          .csrf().disable() // OK since I don't ever take <form> POSTs
-              .csrf().csrfTokenRepository(csrfTokenRepository).and()
-          .authorizeRequests().anyRequest().authenticated();
+
+      // Filtru de CORS nu merge decat daca zici asta.
+      http.cors(); // copii si eu dupa alta app care merge :)
+
+
+//      http.csrf().csrfTokenRepository(csrfTokenRepository);
+
+      http.csrf().disable(); // ca nu fac POST ci doar AJAX
+
+      http.authorizeRequests().anyRequest().authenticated();
 
       http.formLogin().permitAll().defaultSuccessUrl("/",true);
 
