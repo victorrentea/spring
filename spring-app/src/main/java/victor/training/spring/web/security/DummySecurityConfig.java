d9b2f60823e2b588b80bb6f1597e3e387da6fc49
Victor Rentea <victorrentea@gmail.com>
2022-10-19 15:24:05 +0300

    Merge integrarea cu kc ca mi-am pus client secretul in application.properties

--- spring-app/src/main/java/victor/training/spring/web/security/DummySecurityConfig.java
+++ spring-app/src/main/java/victor/training/spring/web/security/DummySecurityConfig.java
@@ -1,69 +1,69 @@
-package victor.training.spring.web.security;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.Map;
-
-import static org.springframework.http.HttpMethod.DELETE;
-
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true) // @Secured @PreAuthorized
-public class DummySecurityConfig extends WebSecurityConfigurerAdapter {
-
-   @Override
-   protected void configure(HttpSecurity http) throws Exception {
-      CookieCsrfTokenRepository csrfTokenRepository = new CookieCsrfTokenRepository();
-      csrfTokenRepository.setCookieHttpOnly(false);
-//          .addFilterBefore(new HttpRequestFilterPrintingHeaders(), WebAsyncManagerIntegrationFilter.class)
-//          .cors().and()
-//          .csrf().disable() // OK since I don't ever take <form> POSTs
-
-      // Filtru de CORS nu merge decat daca zici asta.
-//      http.cors(); // copii si eu dupa alta app care merge :)
-
-
-
-//      http.csrf().csrfTokenRepository(csrfTokenRepository);
-
-      http.csrf().disable(); // ca nu fac POST ci doar AJAX
-
-      http.authorizeRequests()
-
-              // NICIODATA! reject la PR
-//              .mvcMatchers(DELETE, "/api/trainings/*").hasRole("ADMIN")
-              .anyRequest().authenticated()
-
-      ;
-
-      http.formLogin().permitAll().defaultSuccessUrl("/",true);
-
-      http.addFilter(new ApiKeyFilter());
-
-      http.httpBasic();
-   }
-
-   // *** Dummy users 100% in-mem - NEVER USE IN PRODUCTION
-   @Bean
-   public UserDetailsService userDetailsService() {
-      UserDetails userDetails = User.withDefaultPasswordEncoder()
-          .username("user").password("user").roles("USER").build();
-      UserDetails adminDetails = User.withDefaultPasswordEncoder()
-          .username("admin").password("admin").roles("ADMIN").build();
-      UserDetails power = User.withDefaultPasswordEncoder()
-          .username("power").password("power").roles("POWER").build();
-      return new InMemoryUserDetailsManager(userDetails, adminDetails, power);
-   }
-
-}
+//package victor.training.spring.web.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpSession;
+//import java.util.Map;
+//
+//import static org.springframework.http.HttpMethod.DELETE;
+//
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true) // @Secured @PreAuthorized
+//public class DummySecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//   @Override
+//   protected void configure(HttpSecurity http) throws Exception {
+//      CookieCsrfTokenRepository csrfTokenRepository = new CookieCsrfTokenRepository();
+//      csrfTokenRepository.setCookieHttpOnly(false);
+////          .addFilterBefore(new HttpRequestFilterPrintingHeaders(), WebAsyncManagerIntegrationFilter.class)
+////          .cors().and()
+////          .csrf().disable() // OK since I don't ever take <form> POSTs
+//
+//      // Filtru de CORS nu merge decat daca zici asta.
+////      http.cors(); // copii si eu dupa alta app care merge :)
+//
+//
+//
+////      http.csrf().csrfTokenRepository(csrfTokenRepository);
+//
+//      http.csrf().disable(); // ca nu fac POST ci doar AJAX
+//
+//      http.authorizeRequests()
+//
+//              // NICIODATA! reject la PR
+////              .mvcMatchers(DELETE, "/api/trainings/*").hasRole("ADMIN")
+//              .anyRequest().authenticated()
+//
+//      ;
+//
+//      http.formLogin().permitAll().defaultSuccessUrl("/",true);
+//
+//      http.addFilter(new ApiKeyFilter());
+//
+//      http.httpBasic();
+//   }
+//
+//   // *** Dummy users 100% in-mem - NEVER USE IN PRODUCTION
+//   @Bean
+//   public UserDetailsService userDetailsService() {
+//      UserDetails userDetails = User.withDefaultPasswordEncoder()
+//          .username("user").password("user").roles("USER").build();
+//      UserDetails adminDetails = User.withDefaultPasswordEncoder()
+//          .username("admin").password("admin").roles("ADMIN").build();
+//      UserDetails power = User.withDefaultPasswordEncoder()
+//          .username("power").password("power").roles("POWER").build();
+//      return new InMemoryUserDetailsManager(userDetails, adminDetails, power);
+//   }
+//
+//}
