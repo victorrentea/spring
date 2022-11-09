8d00a474f27f87fd7b82125cf1b477f023e9d6e5
Victor Rentea <victorrentea@gmail.com>
2022-11-09 10:26:16 +0200

    hack @Lazy

--- spring-start/src/main/java/victor/training/spring/di/Y.java
+++ spring-start/src/main/java/victor/training/spring/di/Y.java
@@ -1,23 +1,30 @@
 package victor.training.spring.di;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import subp.X;
 
 @Service
 public class Y {
-    @Autowired
-    private  Z z;
+    private final Z z;
 
+    public Y(Z z) {
+        this.z = z;
+    }
+
+    public static void method() {
+//        new Y(new Z(new Y()))
+    }
     // #3 constructor injection (no @Autowired needed since Spring 4.3)
 //    public Y(Z z) {
 //        this.z = z;
 //    }
 
-    @Autowired
-    public void method(X x) {
-        System.out.println("La startup: " + x);
-    }
+//    @Autowired
+//    public void method(X x) {
+//        System.out.println("La startup: " + x);
+//    }
 
     public int prod() {
         System.out.println(z);
--- spring-start/src/main/java/victor/training/spring/di/Z.java
+++ spring-start/src/main/java/victor/training/spring/di/Z.java
@@ -1,9 +1,17 @@
 package victor.training.spring.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Z {
+
+    private final Y y;
+    @Lazy  // hack pentru vremuri de restriste cand te bati cu vreo circulara din frameowkr
+    public Z(Y y) {
+        this.y = y;
+    }
 
     public int prod() {
         return 1;
