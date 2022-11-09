fbb3901f64f2d52208f2082d10da2963454a1428
Victor Rentea <victorrentea@gmail.com>
2022-11-09 16:23:11 +0200

    yolo

--- spring-app/src/main/java/victor/training/spring/aspects/LoggingAspect.java
+++ spring-app/src/main/java/victor/training/spring/aspects/LoggingAspect.java
@@ -31,12 +31,13 @@ public class LoggingAspect {
         }
     }
 
-    @Around("@within(victor.training.spring.aspects.Facade))") // all methods inside classes annotated with @Facade
+    @Around("@within(victor.training.spring.aspects.Facade))") // all methods of classes annotated with @Facade
 //    @Around("@annotation(victor.training.spring.aspects.LoggedMethod))") // all methods annotated with @LoggedMethod
 //    @Around("execution(* org.springframework.data.jpa.repository.JpaRepository+.*(..))") // all subtypes of JpaRepository
 
+    // -- DANGER ZONE --
 //    @Around("execution(* *.get*(..))") // all methods starting with "get" everywhere!! = naming convention = dangerous
-//    @Around("execution(* victor.training.spring.web..*.*(..))") // orice metoda chemata dintr-o clasa de sub pachetul web.
+//    @Around("execution(* victor.training.spring.web..*.*(..))") // any method of any class in a sub-package of 'web'
     public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
 
         if (log.isDebugEnabled()) {
