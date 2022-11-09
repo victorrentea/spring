d38bdd6e7739fa29ea96e7801cde943337f75cc0
Victor Rentea <victorrentea@gmail.com>
2022-11-09 10:57:21 +0200

    lmost done

--- spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
+++ spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
@@ -1,19 +1,36 @@
 package victor.training.spring.bean;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
-public class BeanApp implements CommandLineRunner {
+//@Configuration // in cele din e si asta
+public class BeanApp  {
     public static void main(String[] args) {
         SpringApplication.run(BeanApp.class);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        Conversation conversation = new Conversation(new Person("John"), new Person("Jane"));
+    @Bean
+    public Conversation conversation() {
+        return new Conversation(new Person("John"), new Person("Jane"));
+    }
+}
+@Component
+class Startup {
+
+    @Autowired
+    private Conversation conversation;
+
+    @PostConstruct
+    public void run() throws Exception {
         // TODO convince Spring to do for you the line above
         conversation.start();
     }
