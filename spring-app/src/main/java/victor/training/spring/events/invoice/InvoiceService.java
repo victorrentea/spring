7c319c8d5011d7bddc2ead956527314f72992b06
Victor Rentea <victorrentea@gmail.com>
2022-11-10 14:27:39 +0200

    Order pe handleri

--- spring-app/src/main/java/victor/training/spring/events/invoice/InvoiceService.java
+++ spring-app/src/main/java/victor/training/spring/events/invoice/InvoiceService.java
@@ -2,6 +2,7 @@ package victor.training.spring.events.invoice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 import victor.training.spring.events.events.OrderPlacedEvent;
 
@@ -9,9 +10,11 @@ import victor.training.spring.events.events.OrderPlacedEvent;
 @Service
 public class InvoiceService {
    @EventListener
+   @Order(2)
    public void onOrderPlacedEvent(OrderPlacedEvent event) {
       sendInvoice(event.getOrderId());
    }
+
    public void sendInvoice(long orderId) {
       log.info("Generating invoice for order " + orderId);
       // if (random() < .3) throw new RuntimeException("Invoice Generation Failed");
--- spring-app/src/main/java/victor/training/spring/events/stock/StockManagementService.java
+++ spring-app/src/main/java/victor/training/spring/events/stock/StockManagementService.java
@@ -2,15 +2,17 @@ package victor.training.spring.events.stock;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 import victor.training.spring.events.events.OrderPlacedEvent;
 
 @Slf4j
 @Service
 public class StockManagementService {
-   private int stock = 3; // silly implem :D
+   private int stock = 0; // silly implem :D
 
    @EventListener
+   @Order(1)
    public void onOrderPlaced(OrderPlacedEvent event) {
       checkStock(event.getOrderId());
    }
