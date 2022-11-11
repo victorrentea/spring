635d107990b775f142eccffa6bcb78ee01314315
Victor Rentea <victorrentea@gmail.com>
2022-11-11 10:31:05 +0200

    Form logic + Basic auth  pe spring security

--- spring-app/src/main/java/victor/training/spring/web/security/DummySecurityConfig.java
+++ spring-app/src/main/java/victor/training/spring/web/security/DummySecurityConfig.java
@@ -1,41 +1,43 @@
-//package victor.training.spring.web.security;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//
-//@EnableWebSecurity // (debug = true) // see the filter chain in use
-//@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-//public class DummySecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable(); // OK since I never take <form> POSTs
-//
-//        http.cors(); // needed only if .js files are served by a CDN (eg)
-//
-//        http.authorizeRequests()
-//                .anyRequest().authenticated();
-//
-//        http.formLogin().defaultSuccessUrl("/", true);
-//
-//        http.httpBasic(); // also accept Authorization: Basic ...
-//    }
-//
-//   // *** Dummy users 100% in-mem - NEVER USE IN PRODUCTION
-//   @Bean
-//   public UserDetailsService userDetailsService() {
-//      UserDetails userDetails = User.withDefaultPasswordEncoder()
-//          .username("user").password("user").roles("USER").authorities("training.delete").build();
-//      UserDetails adminDetails = User.withDefaultPasswordEncoder()
-//          .username("admin").password("admin").roles("ADMIN").build();
-//      return new InMemoryUserDetailsManager(userDetails, adminDetails);
-//   }
-//
-//}
+package victor.training.spring.web.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity // (debug = true) // see the filter chain in use
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true) // iti permite sa folosesti @PreAuthorized si/sau @Secured in app
+public class DummySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable(); // OK sa disable protectia default de la Spring
+        // since I never take <form> POSTs (faceam asta cand ;ucram cu JPS,JSF,VAADIN
+        // daca tu ai REST API-uri (doar JSOANe raspunzi) -> nu poti fi atacat cu CSRF
+
+        http.cors(); // needed only if .js files are served by a CDN (eg)
+
+        http.authorizeRequests()
+                .anyRequest().authenticated();
+
+        http.formLogin().defaultSuccessUrl("/", true);
+
+        http.httpBasic(); // also accept Authorization: Basic ...
+    }
+
+   // *** Dummy users 100% in-mem - NEVER USE IN PRODUCTION
+   @Bean
+   public UserDetailsService userDetailsService() {
+      UserDetails userDetails = User.withDefaultPasswordEncoder()
+          .username("user").password("user").roles("USER").authorities("training.delete").build();
+      UserDetails adminDetails = User.withDefaultPasswordEncoder()
+          .username("admin").password("admin").roles("ADMIN").build();
+      return new InMemoryUserDetailsManager(userDetails, adminDetails);
+   }
+
+}
