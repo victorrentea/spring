1e5fc0370c1acf9a1640afabdd08189f0ab180e7
Victor Rentea <victorrentea@gmail.com>
2022-11-14 20:46:24 +0200

    add open api @ConfigurationProperties @Bean

---
+++ spring-app/src/main/java/victor/training/spring/props/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package victor.training.spring.props;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    // (A) Inject properties and manually configure an external class (OpenAPI)
+    @Value("${api.info.version}")
+    String version;
+    @Value("${api.info.title}")
+    String title;
+    @Value("${api.info.contact.name}")
+    String contactName; // + 10 more
+
+    @Bean
+    // (B) Auto-inject properties in @Bean
+    //    @ConfigurationProperties("api")
+    // Any available property starting with "api." is injected in any property(getter/setter) of the returned object
+    // eg: api.info.version=1.0  -> api().setInfo(new Info().setVersion("1.0"))
+    public OpenAPI api() {
+        return new OpenAPI()
+                // (B) does this automatically
+                .info(new Info()
+                    .title(title)
+                    .version(version)
+                    .contact(new Contact().name(contactName)))
+                ;
+    }
+}
