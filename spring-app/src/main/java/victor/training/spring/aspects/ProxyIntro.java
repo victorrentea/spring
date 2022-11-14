c92c763d60f54e75edb1cd42fe0aefeece89cf07
Victor Rentea <victorrentea@gmail.com>
2022-11-14 18:12:34 +0200

    intercepting methods using subclassing of target dependency

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -3,19 +3,29 @@ package victor.training.spring.aspects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class ProxyIntro {
     public static void main(String[] args) {
-        // pretend to BE Spring here
-        Maths maths = new Maths();
+        // pretend to BE Spring here - doing DI
+        Maths maths = new Maths() { // anonymous extends
+
+            @Override
+            public int sum(int a, int b) {
+                System.out.println("calling sum with  " + a + " and " + b);
+                return super.sum(a, b);
+            }
+        };
         SecondGrade secondGrade = new SecondGrade(maths);
+        // TODO because my daughter has a maniac father, the father wants to intercept all calls to
+        // maths methods and their results, to check them
         new ProxyIntro().run(secondGrade);
     }
 
-    //    public static void main(String[] args) {SpringApplication.run(ProxyIntro.class, args);}
-
+//    //    public static void main(String[] args) {SpringApplication.run(ProxyIntro.class, args);}
+//
     @Autowired
     public void run(SecondGrade secondGrade) {
         System.out.println("At runtime...");
@@ -23,6 +33,11 @@ public class ProxyIntro {
     }
 
 }
+// spring under the hood   "decorator pattern py"
+// @g
+// f()
+// ---------------- line -------------------------------
+// application code
 
 @Service
 class SecondGrade {
@@ -39,7 +54,8 @@ class SecondGrade {
     }
 }
 
-@Facade
+//@Facade
+@Service
 class Maths {
     public int sum(int a, int b) {
         return a + b;
