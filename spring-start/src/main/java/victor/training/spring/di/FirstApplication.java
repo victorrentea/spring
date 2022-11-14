a1942577f3398fb2bea8d01e1f92f74ce8724915
Victor Rentea <victorrentea@gmail.com>
2022-11-14 16:02:48 +0200

    Pragmatic dep injection via an invisible constructor.

--- spring-start/src/main/java/victor/training/spring/di/FirstApplication.java
+++ spring-start/src/main/java/victor/training/spring/di/FirstApplication.java
@@ -1,5 +1,7 @@
 package victor.training.spring.di;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -37,7 +39,6 @@ public class FirstApplication implements CommandLineRunner{
 
 //@Controller // in the ages of jsp (html on server) < avoid if exposing REST endpoints
 //@RestController // REST apoi
-@Service  // = business logic
 //@Component // a kind of util/ technica infra stuff
 //@Repository // DB access
 
@@ -46,6 +47,10 @@ public class FirstApplication implements CommandLineRunner{
 //@interface Facade {}
 //
 //@Facade
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
 class X {
 	// #1 field injection by type
 	private final Y y;
@@ -57,18 +62,6 @@ class X {
 	private final Y y7;
 	private final Y y8;
 
-	// #3 ctor based injection = better because you can instantiate (in tests) the class w/o the framework
-	public X(Y y, Y y2, Y y3, Y y4, Y y5, Y y6, Y y7, Y y8) {
-		this.y = y;
-		this.y2 = y2;
-		this.y3 = y3;
-		this.y4 = y4;
-		this.y5 = y5;
-		this.y6 = y6;
-		this.y7 = y7;
-		this.y8 = y8;
-	}
-
 	// #2 method (setter) injection (rarely used)
 //	private Z z;
 //	@Autowired
@@ -77,6 +70,7 @@ class X {
 //	}
 
 	public int prod() {
+//		log.debug();
 		return 1 + y.prod();
 	}
 }
