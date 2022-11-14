d53d5182244743ad11eeb7b560ebac8416cef7c8
Victor Rentea <victorrentea@gmail.com>
2022-11-14 17:15:01 +0200

    profile pitfalls

--- spring-start/src/main/java/victor/training/spring/di/Z1.java
+++ spring-start/src/main/java/victor/training/spring/di/Z1.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+// pitfall 1:
+//@Profile("prod") // NEVER do this, because this means it has to be only production
+// pitfall 2: in prod they had to activate 8 profiles. < profile mania.
 public class Z1 implements ZInterface {
     @Override
     public int prod() {
