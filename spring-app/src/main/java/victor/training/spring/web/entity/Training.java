720070854b8f331e86a771d27f976c37e1ae3934
Victor Rentea <victorrentea@gmail.com>
2022-11-15 18:00:36 +0200

    GraphQL playing around

--- spring-app/src/main/java/victor/training/spring/web/entity/Training.java
+++ spring-app/src/main/java/victor/training/spring/web/entity/Training.java
@@ -19,6 +19,8 @@ public class Training {
 	@ManyToOne
 	private ProgrammingLanguage programmingLanguage;
 
+//	List<Tag> tags;
+
 	public Training() {
 	}
 	
--- spring-app/src/main/java/victor/training/spring/web/repo/ProgrammingLanguageRepo.java
+++ spring-app/src/main/java/victor/training/spring/web/repo/ProgrammingLanguageRepo.java
@@ -3,5 +3,8 @@ package victor.training.spring.web.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import victor.training.spring.web.entity.ProgrammingLanguage;
 
+import java.util.Collection;
+
 public interface ProgrammingLanguageRepo extends JpaRepository<ProgrammingLanguage, Long> {
+//  Collection<Object> findAllByTrainingId(Long trainingId);
 }
