4f7e9de5c74cd9626569dbeed9cd5e6356f6413e
Victor Rentea <victorrentea@gmail.com>
2022-11-15 16:32:35 +0200

    If the called method is ran on a differnt thread -> you loose the transaction

--- spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
@@ -6,26 +6,29 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @RequiredArgsConstructor
 public class Playground {
     private final MessageRepo repo;
 
-//    @Transactional
+    @Transactional
     public void transactionOne() {
         repo.save(new Message("jpa1"));
-        repo.save(new Message(null));
+//        justAnotherMethod();
+        CompletableFuture.runAsync(() -> repo.save(new Message(null)));
 
-
-
-        // 0 p6spy
         // 1 Cause a rollback by breaking NOT NULL, throw Runtime, throw CHECKED
         // 2 Tx propagates with your calls (in your thread😱)
         // 3 Difference with/out @Transactional on f() called: zombie transactions; mind local calls⚠️
         // 4 Game: persist error from within zombie transaction: REQUIRES_NEW or NOT_SUPPORTED
         // 5 Performance: connection starvation issues : debate: avoid nested transactions
     }
+
+    private void justAnotherMethod() {
+    }
+
     @Transactional
     public void transactionTwo() {
     }
