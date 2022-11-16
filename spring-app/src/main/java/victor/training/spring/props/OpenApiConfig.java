b0318d903745b95388a952eb080f70446ae0030f
Victor Rentea <victorrentea@gmail.com>
2022-11-16 18:32:25 +0200

    END

--- spring-app/src/main/java/victor/training/spring/props/OpenApiConfig.java
+++ spring-app/src/main/java/victor/training/spring/props/OpenApiConfig.java
@@ -8,6 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//@ConditionalOnProduction
 @Configuration
 public class OpenApiConfig {
 
--- spring-app/src/main/java/victor/training/spring/scheduled/ScheduledActions.java
+++ spring-app/src/main/java/victor/training/spring/scheduled/ScheduledActions.java
@@ -1,13 +1,27 @@
 package victor.training.spring.scheduled;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import victor.training.spring.varie.ThreadUtils;
+
+import javax.annotation.PostConstruct;
 
 @Slf4j
 @Component
 public class ScheduledActions {
 
+    @Autowired
+    public void method(ApplicationContext applicationContext) {
+        for (String name : applicationContext.getBeanDefinitionNames()) {
+            System.out.println("Found " + name + " with  type " + applicationContext.getBean(name).getClass());
+//            app.getbean
+        }
+    }
+
+//    @Scheduled(fixedRate = 60_000)
     // TODO 1 Should run every 5 seconds / configurable / cron "*/5 * * * * *"
     // TODO 3 Play with delays. cron vs fixedRate? Overlapping executions?
     // TODO 4 Should run on a separate 1-thread pool
