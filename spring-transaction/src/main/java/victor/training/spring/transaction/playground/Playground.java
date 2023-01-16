b4196c5ac7b0a371e9ac6e6c984feacb5b6773f7
Victor Rentea <victorrentea@gmail.com>
2023-01-16 14:28:51 +0200

    o sa vezi si o sa te sperii. da merge

--- spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
@@ -2,8 +2,10 @@ package victor.training.spring.transaction.playground;
 
 import lombok.RequiredArgsConstructor;
 import org.aspectj.runtime.CFlow;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -19,15 +21,35 @@ public class Playground {
 
     @Transactional
     public void transactionOne() {
-        repo.save(new Message("jpa")); // NU ramane in DB => a fost ATOMICA cu save #2;
+        try {
+            bizLogic();
+        } catch (Exception e) {
+            playgroundWTF.altaMetoda(e);
+            throw new RuntimeException(e);
+        }
+        System.out.println("TOTU BUN ACUM!");
         // cum e posibil?
-        CompletableFuture.runAsync(() -> metoda());
         // 0 p6spy
         // 1 Cause a rollback by breaking NOT NULL, throw Runtime, throw CHECKED
         // 2 Tx propagates with your calls (in your thread😱)
         // 3 Difference with/out @Transactional on f() called: zombie transactions; mind local calls⚠️
         // 4 Game: persist error from within zombie transaction: REQUIRES_NEW or NOT_SUPPORTED
         // 5 Performance: connection starvation issues : debate: avoid nested transactions
+    }
+
+    @Autowired
+    private Playground playgroundWTF;
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void altaMetoda(Exception e) {
+        repo.save(new Message("Valeu a crapat cu " + e));
+    }
+
+    private void bizLogic() {
+        repo.save(new Message("jpa")); // NU ramane in DB => a fost ATOMICA cu save #2;
+        if (Math.random() < .5) {
+            throw new IllegalArgumentException("Crapau!");
+        }
     }
 
     private void metoda() {
