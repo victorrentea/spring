ff3fc2c1826f46d7b449e61de91965e70373fb2d
Victor Rentea <victorrentea@gmail.com>
2023-01-16 13:27:58 +0200

    pranz

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -1,53 +1,43 @@
 package victor.training.spring.aspects;
 
-import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cglib.proxy.Callback;
-import org.springframework.cglib.proxy.Enhancer;
-import org.springframework.cglib.proxy.MethodInterceptor;
-import org.springframework.cglib.proxy.MethodProxy;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.lang.reflect.Method;
-import java.util.Arrays;
 
 @SpringBootApplication
 public class ProxyIntro {
 
 
-        public static void main(String[] args) {
-            SpringApplication.run(ProxyIntro.class, args);
-        }
+  public static void main(String[] args) {
+    SpringApplication.run(ProxyIntro.class, args);
+  }
 
-    @Autowired
-    public void run(SecondGrade secondGrade) {
-        System.out.println("At runtime...");
-        secondGrade.mathClass();
-    }
+  @Autowired
+  public void run(SecondGrade secondGrade) {
+    System.out.println("At runtime...");
+    secondGrade.mathClass();
+  }
 
-//    public static void main(String[] args) {
-//        // pretend to BE Spring here
-//        Maths maths = new Maths();
-//        // the real story: spring face asa :
-//        Callback h = new MethodInterceptor() {
-//            @Override
-//            public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
-//                System.out.println("Cheama fiimea metoda " + method.getName() + " cu param " + Arrays.toString(args));
-//                return method.invoke(maths, args);
-//            }
-//        };
-//        Maths proxy = (Maths) Enhancer.create(Maths.class, h);
-//        SecondGrade secondGrade = new SecondGrade(proxy);
-//        new ProxyIntro().run(secondGrade);
-//    }
+  //    public static void main(String[] args) {
+  //        // pretend to BE Spring here
+  //        Maths maths = new Maths();
+  //        // the real story: spring face asa :
+  //        Callback h = new MethodInterceptor() {
+  //            @Override
+  //            public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+  //                System.out.println("Cheama fiimea metoda " + method.getName() + " cu param " + Arrays.toString(args));
+  //                return method.invoke(maths, args);
+  //            }
+  //        };
+  //        Maths proxy = (Maths) Enhancer.create(Maths.class, h);
+  //        SecondGrade secondGrade = new SecondGrade(proxy);
+  //        new ProxyIntro().run(secondGrade);
+  //    }
 }
+
 //class TataManiac extends Maths {
 //    private final Maths original;
 //
@@ -71,33 +61,36 @@ public class ProxyIntro {
 @RequiredArgsConstructor
 @Service
 class SecondGrade {
-    private final Maths maths; // Congratulations, it's a proxy!
-    public void mathClass() {
-        System.out.println("Oare cu cine vorbesc?!?! "+ maths.getClass());
-        System.out.println(maths.sum(2, 4));
-        System.out.println(maths.sum(1, 5));
-        System.out.println(maths.sum(2, 5));
-        System.out.println(maths.product(2, 3));
-    }
+  private final Maths maths; // Congratulations, it's a proxy!
+
+  public void mathClass() {
+    System.out.println("Oare cu cine vorbesc?!?! " + maths.getClass());
+    System.out.println(maths.sum(2, 4));
+    System.out.println(maths.sum(1, 5));
+    System.out.println(maths.sum(2, 5));
+    System.out.println(maths.product(2, 3));
+  }
 }
+
 @Facade
 @Slf4j
-//@Service
-class Maths {
-//    @Transactional
-//    @Cacheable
-//    @PReAuthorized
-//    @Retyable
-//    @Timed
-//    @SEcured
-//    @LoggedMethod
-    public int sum(int a, int b) {
-        return a + b;
+        /*final crapa*/ class Maths {
+  //    @Transactional
+  //    @Secured("ADMIN")
+  public int sum(int a, int b) {
+    return a + b;
+  }
+
+  public int product(int a, int b) {
+    //        return a * b;
+    int r = 0;
+    for (int i = 0; i < a; i++) {
+      r = sum(r, b);
+      // apelurile locale de metode nu trec prin proxy-uri!! - cel mai criminala capcana de la proxy-uri in Sprng
+      // adica (RO): daca ai adnotari pe acea metoda ele nu vor mai face nimic daca e chema din aceeasi clasa.
     }
-//    @LoggedMethod
-    public int product(int a, int b) {
-        return a * b;
-    }
+    return r;
+  }
 }
 
 
