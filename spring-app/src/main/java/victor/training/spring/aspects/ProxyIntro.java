84304428a571ab69a35094f5b3c3392f3898463b
Victor Rentea <victorrentea@gmail.com>
2023-01-16 11:57:29 +0200

    inteceptie de metode facute cu Java curat

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -1,18 +1,14 @@
 package victor.training.spring.aspects;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class ProxyIntro {
-    public static void main(String[] args) {
-        // pretend to BE Spring here
-        Maths maths = new Maths();
-        SecondGrade secondGrade = new SecondGrade(maths);
-        new ProxyIntro().run(secondGrade);
-    }
+
 
     //    public static void main(String[] args) {SpringApplication.run(ProxyIntro.class, args);}
 
@@ -22,29 +18,50 @@ public class ProxyIntro {
         secondGrade.mathClass();
     }
 
+    public static void main(String[] args) {
+        // pretend to BE Spring here
+        Maths maths = new Maths();
+        SecondGrade secondGrade = new SecondGrade(new TataManiac(maths));
+        new ProxyIntro().run(secondGrade);
+    }
 }
+class TataManiac extends Maths {
+    private final Maths original;
 
+    TataManiac(Maths original) {
+        this.original = original;
+    }
+
+    @Override
+    public int sum(int a, int b) {
+        System.out.println("log");
+        return original.sum(a, b);
+    }
+
+    @Override
+    public int product(int a, int b) {
+        System.out.println("log");
+        return original.product(a, b);
+    }
+}
+////-------------------------------------------
+@RequiredArgsConstructor
 @Service
 class SecondGrade {
     private final Maths maths;
-
-    SecondGrade(Maths maths) {
-        this.maths = maths;
-    }
-
     public void mathClass() {
         System.out.println(maths.sum(2, 4));
         System.out.println(maths.sum(1, 5));
         System.out.println(maths.product(2, 3));
     }
 }
-
-@Facade
+//@Facade
+@Slf4j
+@Service
 class Maths {
     public int sum(int a, int b) {
         return a + b;
     }
-
     public int product(int a, int b) {
         return a * b;
     }
