e8114fa9a7ee561369cb4673637075269e1be43d
Victor Rentea <victorrentea@gmail.com>
2023-01-16 16:03:40 +0200

    PRAPAD: daca tii date pe singletong

--- spring-transaction/src/main/java/victor/training/spring/transaction/playground/PlaygroundJpa.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/playground/PlaygroundJpa.java
@@ -3,6 +3,8 @@ package victor.training.spring.transaction.playground;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 @RequiredArgsConstructor
@@ -10,12 +12,21 @@ public class PlaygroundJpa {
   private final MessageRepo repo;
   private Long id;
 
-  private String currentUser;
+  private String currentUser; // foarte rau sa pui date specifice requestului web pe camp al singleton => race bugs
   private String stationId;
 
+  @GetMapping
   @Transactional
-  public void transactionOne() {
+  public void transactionOne(@RequestParam("user") String user) {
+    currentUser = "user curent din token" + user;
     id = repo.save(new Message("JPA")).getId();
+    f();
+
+  }
+
+  private void f() {
+    System.out.println("Useru curent tinut temporar intr-un camp"
+                       + currentUser);
   }
 
   @Transactional
