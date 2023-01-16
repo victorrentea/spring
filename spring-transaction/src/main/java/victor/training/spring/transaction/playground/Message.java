bdb4dea705a1fe6ff61efad2a92ff8d602206d49
Victor Rentea <victorrentea@gmail.com>
2023-01-16 14:04:22 +0200

    Pana aici a fost clar: o tx cu mai multe op cu DB in ea.

--- spring-transaction/src/main/java/victor/training/spring/transaction/playground/Message.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/playground/Message.java
@@ -24,3 +24,5 @@ public class Message {
         this.message = message;
     }
 }
+
+
--- spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
@@ -8,17 +8,18 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 
 @Service
+
 @RequiredArgsConstructor
 public class Playground {
     private final MessageRepo repo;
-    private final EntityManager entityManager;
-    private final JdbcTemplate jdbc;
     private final OtherClass other;
 
     @Transactional
     public void transactionOne() {
-        jdbc.update("insert into MESSAGE(id, message) values ( 100,'ALO' )");
         repo.save(new Message("jpa"));
+        repo.save(new Message("null"));
+
+
         // 0 p6spy
         // 1 Cause a rollback by breaking NOT NULL, throw Runtime, throw CHECKED
         // 2 Tx propagates with your calls (in your thread😱)
