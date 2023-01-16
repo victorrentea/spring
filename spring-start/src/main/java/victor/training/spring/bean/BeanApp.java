20421e159cbbfb21d076d301849d58df76906b44
Victor Rentea <victorrentea@gmail.com>
2023-01-16 11:16:38 +0200

    @Bean ce cere ca param 2 beanuri

--- spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
+++ spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
@@ -1,23 +1,49 @@
 package victor.training.spring.bean;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-public class BeanApp implements CommandLineRunner {
+public class BeanApp {
     public static void main(String[] args) {
         SpringApplication.run(BeanApp.class);
     }
 
+    @Bean
+    public Person john() {
+        return new Person("John");
+    }
+
+    @Bean
+    public Person jane() {
+        return new Person("Jane");
+    }
+
+    @Bean
+    public Conversation conversation(Person john, Person jane) { // injectie de dep in param metodei @Bean
+        return new Conversation(john, jane);
+    }
+
+
+
+
+}
+@Component
+class CandCoduSeImbacsesteMereuEBineSaIeiOGuraDeAerProaspat_ClasaNoua implements CommandLineRunner{
+    @Autowired
+    private Conversation conversation;
+
     @Override
     public void run(String... args) throws Exception {
-        Conversation conversation = new Conversation(new Person("John"), new Person("Jane"));
-        // TODO convince Spring to do for you the line above
         conversation.start();
     }
 }
+/// ---- sub linie consideram librarie NU POTI modifica
 
 @Data
 class Conversation {
@@ -30,6 +56,7 @@ class Conversation {
     }
 
     public void start() {
+        System.out.println("Starting conversation...");
         System.out.println(one.sayHello());
         System.out.println(two.sayHello());
     }
