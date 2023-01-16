3b42282bd2d76355fbb005b3720827ab07833d3c
Victor Rentea <victorrentea@gmail.com>
2023-01-16 16:13:20 +0200

    JPA2: write behind: amana scrierile pana la COMMIT.

--- spring-transaction/src/main/java/victor/training/spring/transaction/playground/PlaygroundJpa.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/playground/PlaygroundJpa.java
@@ -15,15 +15,18 @@ public class PlaygroundJpa {
   @Transactional
   public void transactionOne() {
     id = repo.save(new Message("JPA")).getId();
+    repo.save(new Message("#sieu"));
+    // inserturile (orice scriere in DB) JPA o "amana"
+    // pana la COMMIT = "write behind"
+    // 1) poate da rollback si nu le mai face (pesismist)
+    // 2) ca sa faca batching (sa reduca round-tripurile catre DB)
+    System.out.println("Si acum trimit Rabbit");
   }
 
   @Transactional
   public void transactionTwo() {
     Message message = repo.findById(id).orElseThrow();
     message.setMessage("Altu");
-    // JPA face automat flush la modificarile pe
-    // @Entity luate din JPA la final de tx daca erai in @Transaction
-    // => buguri cand din neatentie 'carpesti' un camp intr-o entity
-    // si ramane carpit in DB UPDATE =
+
   }
 }
