d9b0c87f34332a4ea8febfa6300f16402aec85c8
Victor Rentea <victorrentea@gmail.com>
2023-02-13 13:20:47 +0200

    validation + @Config.Properties

---
+++ spring-start/src/main/java/victor/training/spring/bean/WelcomeInfo.java
@@ -0,0 +1,77 @@
+package victor.training.spring.bean;
+
+import lombok.Data;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.annotation.PostConstruct;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+
+@Slf4j
+@Data //get+set+tostring
+@Component // makes spring see this
+@ConfigurationProperties(prefix = "welcome") // spring to inject from yaml the props under welcome:
+@Validated // calls for validation (spring to use reflection on the fields bellow and check javax.validation annotations0
+public class WelcomeInfo {
+    @NotNull // javax.validation
+    private String welcomeMessage;
+    @Size(min = 1)
+    private List<URL> supportUrls;
+    private Map<Locale, String> localContactPhone;
+    private HelpInfo help;
+
+    @Data
+    public static class HelpInfo {
+        Integer appId;
+        File file;
+    }
+
+    @PostConstruct
+    public void printMyselfAtStartup() {
+        if (!help.file.isFile()) {
+            throw new IllegalArgumentException("NOT A FILE: " + help.file);
+        }
+
+        // TODO validate help.file exists on disk
+        // TODO validate welcome message is not null and at least 10 chars
+        // TODO use javax.validation for the previous task. Hint: annotate class with @Validated
+        log.info("My props: \n" + this);
+    }
+}
+
+
+//@Slf4j
+//@Value
+//@ConstructorBinding
+//@ConfigurationProperties(prefix = "welcome")
+//public class WelcomeInfo {
+//    String welcomeMessage;
+//    List<URL> supportUrls;
+//    Map<Locale, String> localContactPhone;
+//    HelpInfo help;
+//
+//    @Value
+//    public static class HelpInfo {
+//        Integer appId;
+//        File file;
+//    }
+//
+//    @PostConstruct
+//    public void printMyselfAtStartup() {
+//        // TODO validate help.file exists on disk
+//        // TODO validate welcome message is not null and at least 10 chars
+//        // TODO use javax.validation for the previous task. Hint: annotate class with @Validated
+//        log.debug("My props: " + this);
+//    }
+//}
--- spring-start/src/main/java/victor/training/spring/supb/Deep.java
+++ spring-start/src/main/java/victor/training/spring/supb/Deep.java
@@ -1,7 +1,19 @@
 package victor.training.spring.supb;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class Deep {
+  @Value("${minOrderAmountForDiscount}") // fails if prop not define
+//  @Value("${minOrderAmountForDiscount:0}") // provide a default < avoid this
+  // you have to scan the code to find the available props to set.
+  private int minOrderAmountForDiscount;
+
+  @PostConstruct
+  public void logic() {
+    System.out.println("value: "+ minOrderAmountForDiscount);
+  }
 }
