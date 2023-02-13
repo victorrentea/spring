e4e6e6c9bd6e515efe3ce4774d0aa501bd97d936
Victor Rentea <victorrentea@gmail.com>
2023-02-13 12:54:03 +0200

    Pitfalls of @Scope(request): if you leave the thread you loose the data. (fix: tomorrow0

--- spring-start/src/main/java/victor/training/spring/life/ScopeRequestController.java
+++ spring-start/src/main/java/victor/training/spring/life/ScopeRequestController.java
@@ -2,6 +2,7 @@ package victor.training.spring.life;
 
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 // what kind of component can intercept all http request/response and do stuff on them
 @RequiredArgsConstructor
@@ -23,11 +26,12 @@ class MyFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest r = (HttpServletRequest) request;
         String httpRequestLocale = r.getLocale().toString();
-        requestMetadata.setMetadata(httpRequestLocale);
+//        requestMetadata.setMetadata(httpRequestLocale);
         chain.doFilter(request, response);
     }
 }
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class ScopeRequestController {
@@ -35,13 +39,16 @@ public class ScopeRequestController {
     private final AnotherBean anotherBean;
 
     @GetMapping("scope-request") //t?lang=EN
-    public String requestScope(@RequestParam String lang) {
-//        requestMetadata.setMetadata(lang); // usually available in all requests
-        return anotherBean.method();
+    public String requestScope(@RequestParam String lang) throws ExecutionException, InterruptedException {
+        requestMetadata.setMetadata(lang); // usually available in all requests
+        log.info("Start in controller");
+        String result = CompletableFuture.supplyAsync(()->anotherBean.method()).get();
+        return result;
     }
 }
+
 @Component
-@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS) // DOES NOT PROPAGATE AUTOMATICALLY OVER NEW THREADS
 class RequestMetadata {
     private String metadata; // mutable!!
     public String getMetadata() {
@@ -51,15 +58,18 @@ class RequestMetadata {
         this.metadata = metadata;
     }
 }
+@Slf4j
 @Service
 @RequiredArgsConstructor
 class AnotherBean {
     private final RequestMetadata requestMetadata;
     @SneakyThrows
     public String method() {
-        System.out.println("Who am i talking to ?! " + requestMetadata.getClass() );
+        log.info("Who am i talking to ?! " + requestMetadata.getClass() );
 //        if(true) throw new RuntimeException("Intetional");
         Thread.sleep(2000);
         return "I need the request metadata: USER_LANG=" + requestMetadata.getMetadata();
+        // crash because on the thread this line runs now there is no ACTIVE HTTP REQUEST anymore, as you are not
+        // running in the Tomcat thread anymomre
     }
 }
