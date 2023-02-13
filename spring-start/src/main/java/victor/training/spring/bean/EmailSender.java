5c9ac78d3c7013ce62cbcad999779752c3489481
Victor Rentea <victorrentea@gmail.com>
2023-02-13 11:44:56 +0200

    @Profile + @Primary combo

--- spring-start/src/main/java/victor/training/spring/bean/EmailSender.java
+++ spring-start/src/main/java/victor/training/spring/bean/EmailSender.java
@@ -1,6 +1,7 @@
 package victor.training.spring.bean;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,7 @@ public interface EmailSender {
 }
 @Component
 // a spring profile is wasy
-@Profile("!local")
+//@Profile("!local")
 //@Profile("prod") // <- pitfall of profile: # I don’t always test my code, but when I do, I do it in production. YOLO
   // Profile-mania!!! WARNING: 12 profiles activate do work in prod.
 // Best practice0-profiles in the ONE production I have
@@ -24,6 +25,8 @@ class ProdEmailSender implements EmailSender{ // implem1
 }
 @Component
 @Profile("local")
+@Primary // every time spring has to choose between this bean and any other for injection, this wins
+  // application: src/test/java TestEmailSender.java  (aka "Fake" test double)
 class LocalEnvEmailSender implements EmailSender { // implem2
   public void sendEmail(String email) {
     System.out.println(email); // in my local I want to do this
