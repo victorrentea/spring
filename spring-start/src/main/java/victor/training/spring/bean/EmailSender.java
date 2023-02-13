c45bc3f890392526f16a4d68ece48d57e753f6bd
Victor Rentea <victorrentea@gmail.com>
2023-02-13 11:41:27 +0200

    profiles (!) best practices

---
+++ spring-start/src/main/java/victor/training/spring/bean/EmailSender.java
@@ -0,0 +1,42 @@
+package victor.training.spring.bean;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+// Strategy design pattern
+public interface EmailSender {
+  void sendEmail(String email);
+}
+@Component
+// a spring profile is wasy
+@Profile("!local")
+//@Profile("prod") // <- pitfall of profile: # I don’t always test my code, but when I do, I do it in production. YOLO
+  // Profile-mania!!! WARNING: 12 profiles activate do work in prod.
+// Best practice0-profiles in the ONE production I have
+class ProdEmailSender implements EmailSender{ // implem1
+  public void sendEmail(String email) {
+     throw new IllegalArgumentException("in my local throws exception");
+  }
+}
+@Component
+@Profile("local")
+class LocalEnvEmailSender implements EmailSender { // implem2
+  public void sendEmail(String email) {
+    System.out.println(email); // in my local I want to do this
+  }
+}
+@RequiredArgsConstructor
+@Service
+class SomeServiceThatNeedsUserDate {
+  private final EmailSender emailSender;
+
+  @PostConstruct
+  public void method() {
+    emailSender.sendEmail("this email");
+  }
+
+}
