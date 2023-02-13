6c7965be27bdfebc4fd04496e7bc8ff8b4553e49
Victor Rentea <victorrentea@gmail.com>
2023-02-13 11:28:39 +0200

    @Qualifier and param name @ injection point

--- spring-start/src/main/java/victor/training/spring/bean/Conversation.java
+++ spring-start/src/main/java/victor/training/spring/bean/Conversation.java
@@ -1,25 +1,30 @@
 package victor.training.spring.bean;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Qualifier;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component// ("conversation") by default the bean name of a @Component&co is the class name with a small first letter
 @Data
+@RequiredArgsConstructor
 public class Conversation {
-  private final Person one;
-  private final Person two;
+  private final Person john;
+  private final Person jane;
 
   // @Qualifier at injection point
-  public Conversation(@Qualifier("john") Person one,
-                      @Qualifier("jane") Person two) {
-    this.one = one;
-    this.two = two;
-  }
+//  public Conversation(@Qualifier("john") Person one,
+//                      @Qualifier("jane") Person two) {
+
+  // or since 5y ago:
+//  public Conversation(Person john, Person jane) {
+//    // match the param name; Lombok-friendly. Strage to match param name to bean name far away
+//    this.one = john;
+//    this.two = jane;
+//  }
 
   public void start() {
     System.out.println("This ?");
-    System.out.println(one.sayHello());
-    System.out.println(two.sayHello());
+    System.out.println(john.sayHello());
+    System.out.println(jane.sayHello());
   }
 }
