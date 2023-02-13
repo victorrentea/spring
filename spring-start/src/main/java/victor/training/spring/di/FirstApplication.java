84a970c92329937e007045da05674bd9bf9da9f5
Victor Rentea <victorrentea@gmail.com>
2023-02-13 10:52:23 +0200

    defining and wiring beans basics + running code at startup

--- spring-start/src/main/java/victor/training/spring/di/FirstApplication.java
+++ spring-start/src/main/java/victor/training/spring/di/FirstApplication.java
@@ -1,17 +1,24 @@
 package victor.training.spring.di;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
@@ -44,7 +51,6 @@ public class FirstApplication implements CommandLineRunner{
 @interface Mapper {}
 //  registers the class in the spring context (aka bucket of instances)
 	// Spring will create 1 single instance of this aka Singleton pattern
-//@Component // use for ....? idk garbage TBD  Util .. CustomerMapper
 
 //@Service // business logic
 
@@ -54,22 +60,49 @@ public class FirstApplication implements CommandLineRunner{
 //@RestController // REST API
 // NOT @Entity <- hibernate instantiates those.
 
-@Mapper
+//@Mapper
+@Slf4j // <-- adds this private static final Logger log = LoggerFactory.getLogger(X.class);
+@RequiredArgsConstructor // <- defines contructor in .class file with all the final fields
+@Service // use for ....? idk garbage TBD  Util .. CustomerMapper
 class X {
+//	@Autowired
+//	private Y y; // field based injection
+
+	// constructor-injection is recommended. bescause of tests.
+	// but in practice we unit test classes with @InjectMocks who works with a) @Autowire b) constructor
+	private final Y y;
+
+	private Y y2;
 	@Autowired
-	private Y y;
+	public X anything(Y y2) {
+		this.y2 = y2;
+		return this;
+	}
 
 	public int prod() {
 		return 1 + y.prod();
 	}
 }
 @Service
-class Y {
+class Y implements CommandLineRunner{
 	private final Z z;
 
 	// #3 constructor injection (no @Autowired needed since Spring 4.3)
 	public Y(Z z) {
 		this.z = z;
+	}
+
+
+//	@Autowired // abit of abuse ?!
+//	@PostConstruct // recommended, but is pretty coarse-grained about WHEN?
+	@EventListener(ApplicationStartedEvent.class) // just one of the 12 lifecycle events thrown by Spring at startup
+	public void helloSwift() {
+		System.out.println("Moving swiftly. WHY?! What to do in here?");
+		System.out.println("Eg.v warm up a cache, load some more config, ping redis, send a message, validate a folder exists");
+	}
+	@Override // spring sees your bean implementing the CLR interface => calls this method with the program argumens
+	public void run(String... args) throws Exception {
+		System.out.println("Mee too");
 	}
 
 	public int prod() {
