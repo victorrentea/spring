b42cfe0fab3ed2cc00b37ffa176a43390e1f981f
Victor Rentea <victorrentea@gmail.com>
2023-02-13 12:38:59 +0200

    THE PROBLEM reproduces

--- spring-start/src/main/java/victor/training/spring/life/ScopeRequestController.java
+++ spring-start/src/main/java/victor/training/spring/life/ScopeRequestController.java
@@ -1,9 +1,13 @@
 package victor.training.spring.life;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -12,33 +16,31 @@ public class ScopeRequestController {
     private final RequestMetadata requestMetadata;
     private final AnotherBean anotherBean;
 
-    @GetMapping("scope-request")
-    public String requestScope() {
-        requestMetadata.setMetadata("Extracted from request: " + "language=EN"); // usually available in all requests
+    @GetMapping("scope-request") //t?lang=EN
+    public String requestScope(@RequestParam String lang) {
+        requestMetadata.setMetadata(lang); // usually available in all requests
         return anotherBean.method();
     }
 }
-
 @Component
+//@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 class RequestMetadata {
-    private String metadata;
-
+    private String metadata; // mutable!!
     public String getMetadata() {
         return metadata;
     }
-
-    public RequestMetadata setMetadata(String metadata) {
+    public void setMetadata(String metadata) {
         this.metadata = metadata;
-        return this;
     }
 }
-
 @Service
 @RequiredArgsConstructor
 class AnotherBean {
     private final RequestMetadata requestMetadata;
 
+    @SneakyThrows
     public String method() {
-        return "I need the request metadata: " + requestMetadata.getMetadata();
+        Thread.sleep(2000);
+        return "I need the request metadata: USER_LANG=" + requestMetadata.getMetadata();
     }
 }
