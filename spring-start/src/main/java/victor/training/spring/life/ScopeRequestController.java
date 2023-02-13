e2380389e3f44dc148109b774765b4c29883e28c
Victor Rentea <victorrentea@gmail.com>
2023-02-13 12:23:05 +0200

    undo scope request <--PICK

--- spring-start/src/main/java/victor/training/spring/life/ScopeRequestController.java
+++ spring-start/src/main/java/victor/training/spring/life/ScopeRequestController.java
@@ -1,9 +1,6 @@
 package victor.training.spring.life;
 
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,27 +9,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 public class ScopeRequestController {
-    private final RequestScoped requestScoped;
+    private final RequestMetadata requestMetadata;
     private final AnotherBean anotherBean;
+
     @GetMapping("scope-request")
     public String requestScope() {
-        requestScoped.setMetadata("Extracted from request"); //  ideally available in all requests
+        requestMetadata.setMetadata("Extracted from request: " + "language=EN"); // usually available in all requests
         return anotherBean.method();
     }
 }
 
-@Data
 @Component
-@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
-class RequestScoped {
+class RequestMetadata {
     private String metadata;
+
+    public String getMetadata() {
+        return metadata;
+    }
+
+    public RequestMetadata setMetadata(String metadata) {
+        this.metadata = metadata;
+        return this;
+    }
 }
 
 @Service
 @RequiredArgsConstructor
 class AnotherBean {
-    private final RequestScoped requestScoped;
+    private final RequestMetadata requestMetadata;
+
     public String method() {
-        return "Obtained magically: " + requestScoped.getMetadata();
+        return "I need the request metadata: " + requestMetadata.getMetadata();
     }
 }
