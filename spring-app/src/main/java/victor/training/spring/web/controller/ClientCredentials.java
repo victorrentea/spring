9c2d6e6a2a9e86e0e8e21d0f1994814e125d5ee4
Victor Rentea <victorrentea@gmail.com>
2023-02-22 00:05:44 +0200

    yolo

--- spring-app/src/main/java/victor/training/spring/web/controller/ClientCredentials.java
+++ spring-app/src/main/java/victor/training/spring/web/controller/ClientCredentials.java
@@ -1,6 +1,6 @@
 package victor.training.spring.web.controller;
 
-import io.micrometer.core.annotation.Timed;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,35 +14,36 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class ClientCredentials { // CLient=Applicatie
-   @Autowired
-   OAuth2RestTemplate oauth2RestTemplate;
-   // https://medium.com/@bcarunmail/securing-rest-api-using-keycloak-and-spring-oauth2-6ddf3a1efcc2
-   @GetMapping("/api/client-credentials")
-   public String clientCredentials() {
-      return oauth2RestTemplate.getForObject("http://localhost:8082", String.class);
-   }
+  private final OAuth2RestTemplate restWithClientCredentials;
 
-   @Scheduled(fixedRate = 60*60*1000)
-  public void vreauSaExecutUnApelRESTCatreAltMicroserviciuDarNuinNumeleUnuiUser_ciInNumeleAplicatiei() {
-     // echivalentu lui apikey/basic
+  // https://medium.com/@bcarunmail/securing-rest-api-using-keycloak-and-spring-oauth2-6ddf3a1efcc2
+  @GetMapping("/api/client-credentials")
+  public String clientCredentials() {
+    return restWithClientCredentials.getForObject("http://localhost:8082", String.class);
+  }
 
+  @Scheduled(fixedRate = 2 * 1000)
+  public void scheduled() {
+    String value = restWithClientCredentials.getForObject("http://localhost:8082", String.class);
+    log.info("@Scheduled got value: " + value);
   }
 
 
-   @Configuration
-   public static class ConfigureOAuth2RestTemplate {
-      @Bean
-      public OAuth2RestTemplate oauth2RestTemplate(OAuth2ProtectedResourceDetails details) {
-         log.info("Trying to get the Access Token with Client Credential ...");
-         OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(details);
-         //Prepare by getting the AccessToken once
-          OAuth2AccessToken token = oAuth2RestTemplate.getAccessToken();
-          System.out.println( token);
-         log.info("SUCCESS");
-         return oAuth2RestTemplate;
-      }
-   }
+  @Configuration
+  public static class ConfigureOAuth2RestTemplate {
+    @Bean
+    public OAuth2RestTemplate restWithClientCredentials(OAuth2ProtectedResourceDetails details) {
+      log.info("Trying to get the Access Token with Client Credential ...");
+      OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(details);
+      //Prepare by getting the AccessToken once
+      OAuth2AccessToken token = oAuth2RestTemplate.getAccessToken();
+      System.out.println(token);
+      log.info("SUCCESS");
+      return oAuth2RestTemplate;
+    }
+  }
 
 }
 
