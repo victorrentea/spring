01238c9f3fd9cfec91fea9253f22215fd0ef533b
Victor Rentea <victorrentea@gmail.com>
2023-02-28 11:10:18 +0200

    startup code

--- spring-start/src/main/java/victor/training/spring/first/X.java
+++ spring-start/src/main/java/victor/training/spring/first/X.java
@@ -12,11 +12,25 @@ import org.springframework.stereotype.Service;
 //@Repository // => DB access; Note: if using Spring Data Mongo you will just define an interface that extends a MongoRepository
 //@Mapper // my own subtype of component
 public class X {
-  @Autowired
-  private Y y; // field injection
 
-  // (rarely used) method injection
-  // public void setY(Y y) {this.y = y;}
+//  @Autowired
+//  private Y y; // field injection : bad becauase
+  // 1) mutable field
+  // 2) hard to test (bullshit because mockit can inject even private fields @InjectMocks)
+
+  private final Y y;
+  public X(Y y) { // constructor injection < recommended way
+    this.y = y;
+  }
+
+  // (rarely used) 3 method injection
+  private Y y2;
+  @Autowired
+  public void init(Y y) { // spring will call this method automatically when initializign hthe bean,
+    // before it becomes ready to use
+    this.y2 = y;
+    System.out.println("I was given an instance of Y: " + y);
+  }
 
   public int logic() {
     return 1 + y.logic();
