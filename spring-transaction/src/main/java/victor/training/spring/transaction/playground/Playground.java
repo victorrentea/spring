0ddc5ff4fbfccdfc8cae4640dd47423fb8993ae8
Victor Rentea <victorrentea@gmail.com>
2023-03-01 13:32:03 +0200

    loosing the thread means loosing the current tx.

--- spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
@@ -2,10 +2,9 @@ package victor.training.spring.transaction.playground;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManager;
 
 @Service
 @RequiredArgsConstructor
@@ -13,17 +12,23 @@ public class Playground {
     private final JdbcTemplate jdbc;
     private final OtherClass otherClass;
 
-    @Transactional // THE PROXY: one connection is acquired from the CONNECTION POOL (Hikari 20 max by default)
     // because there was no tx open yet on this
+    @Transactional // THE PROXY: one connection is acquired from the CONNECTION POOL (Hikari 20 max by default)
     public void transactionOne() {
         jdbc.update("insert into MESSAGE(id, message) values ( 100,'ALO' )");
         // any jooq insert/update you send to db in a @Transactional flow, you will see the INSERT going do DB,
         // but the DB will hold it just in your transaction until later, the app send a COMMIT
         // on that same JDBC connection
         System.out.println("Count = " + jdbc.queryForObject("select count(*) from message", Long.class));
-        otherClass.anotherMethod();
-        if (true) throw new RuntimeException("Intentional");
+        try {
+            otherClass.bizLogicTransacted();
+        } catch (Exception e) {
+            otherClass.saveError();
+            throw new RuntimeException(e);
+        }
+        // requirement: all the errors processing this request should be INSERTED in the DB
     }
+
     // here, after the method, the PROXY sends the COMMIT to DB
     // 0 p6spy
     // 1 Cause a rollback by breaking NOT NULL, throw Runtime, throw CHECKED
@@ -40,8 +45,14 @@ public class Playground {
 class OtherClass {
     private final JdbcTemplate jdbc;
     @Transactional // this proxy allows the existing tx on the current thread to enter the method
-    public void anotherMethod() {
+    public void bizLogicTransacted() {
         jdbc.update("insert into MESSAGE(id, message) values ( 101,'ALO' )");
         jdbc.update("insert into MESSAGE(id, message) values ( 102,'ALO' )");
+        if (true) throw new RuntimeException("Biz validation failed bla bla bla tomatoes bla");
+    }
+
+    @Async
+    public void saveError() {
+        jdbc.update("insert into MESSAGE(id, message) values ( 109,'ERROR: ')");
     }
 }
