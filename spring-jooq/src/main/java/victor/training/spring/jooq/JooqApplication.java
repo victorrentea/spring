4b31baf314ae482bf39f789e5a175103cd9c3cfe
Victor Rentea <victorrentea@gmail.com>
2023-03-02 09:33:38 +0200

    WIP

--- spring-jooq/src/main/java/victor/training/spring/jooq/JooqApplication.java
+++ spring-jooq/src/main/java/victor/training/spring/jooq/JooqApplication.java
@@ -3,6 +3,7 @@ package victor.training.spring.jooq;
 import lombok.SneakyThrows;
 import lombok.Value;
 import org.jooq.DSLContext;
+import org.jooq.Records;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,12 +15,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import victor.training.spring.jooq.table.tables.Author;
+import victor.training.spring.jooq.table.tables.Book;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Random;
 
 import static victor.training.spring.jooq.table.Tables.AUTHOR;
 import static victor.training.spring.jooq.table.Tables.BOOK;
+import static victor.training.spring.jooq.table.tables.AuthorBook.AUTHOR_BOOK;
 
 @SuppressWarnings("ALL")
 @SpringBootApplication
@@ -54,13 +59,26 @@ public class JooqApplication {
   }
   @PostMapping("books")
   public void createBook(@RequestBody CreateBookRequest dto) {
-//    return dsl.select(BOOK.ID, BOOK.TITLE)
-//            .from(BOOK)
-//            .fetch()
-//            .map(r -> new BookDto(r.value1(), r.value2()));
-
+    Integer id = new Random().nextInt(100000); // dirty hack :)
+    dsl.insertInto(Book.BOOK)
+            .set(Book.BOOK.ID,id )
+            .set(Book.BOOK.TITLE, dto.title)
+            .execute();
+    for (Integer authorId : dto.authorIds) {
+      dsl.insertInto(AUTHOR_BOOK)
+              .set(AUTHOR_BOOK.AUTHOR_ID, authorId)
+              .set(AUTHOR_BOOK.BOOK_ID, id)
+              .execute();
+    }
   }
 
+  @Value
+  public static class AuthorDto {
+    Integer id;
+    String firstName;
+    String lastName;
+    String bio;
+  }
   @GetMapping("authors")
   public List<AuthorDto> authors() {
     return dsl.select(AUTHOR.ID, AUTHOR.FIRST_NAME, AUTHOR.LAST_NAME)
@@ -78,11 +96,5 @@ public class JooqApplication {
   }
 
 
-  @Value
-  public static class AuthorDto {
-    Integer id;
-    String firstName;
-    String lastName;
-    String bio;
-  }
+
 }
