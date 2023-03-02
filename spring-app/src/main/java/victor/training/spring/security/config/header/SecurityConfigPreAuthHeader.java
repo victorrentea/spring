7e69f2369972678c097f22e375d1cf9deec73e68
Victor Rentea <victorrentea@gmail.com>
2023-03-02 11:43:32 +0200

    we logged in using preauth http headers added to the requests by the Gateway that we started on port 80

--- spring-app/src/main/java/victor/training/spring/security/config/header/SecurityConfigPreAuthHeader.java
+++ spring-app/src/main/java/victor/training/spring/security/config/header/SecurityConfigPreAuthHeader.java
@@ -16,14 +16,15 @@ import org.springframework.security.web.authentication.preauth.PreAuthenticatedA
 @Profile("header")
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+// allows me to use annotations to control access to methods
 public class SecurityConfigPreAuthHeader extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable() // as I don't ever take <form> POSTs
+    http.csrf().disable() // as I don't ever take  <form method="post" action="url..."><input type="submit"></form>
             .authorizeRequests().anyRequest().authenticated().and()
             .addFilter(preAuthHeaderFilter())
             .authenticationProvider(preAuthenticatedProvider())
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
     ;
   }
 
@@ -36,7 +37,8 @@ public class SecurityConfigPreAuthHeader extends WebSecurityConfigurerAdapter {
   @Bean
   public PreAuthHeaderFilter preAuthHeaderFilter() throws Exception {
     return new PreAuthHeaderFilter(authenticationManagerBean());
-  }
+  }// after we return the instance, spring looks at it and : processes additional
+  // @Autowired, runs @PostConstruct hooks, may proxy if needed
 
   @Bean
   public AuthenticationProvider preAuthenticatedProvider() {
