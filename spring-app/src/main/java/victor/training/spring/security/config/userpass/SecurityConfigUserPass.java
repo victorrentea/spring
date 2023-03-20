86f8db1fd2a4afab1aa327d2a49afdfdef70ed4c
Victor Rentea <victorrentea@gmail.com>
2023-03-20 15:39:10 +0200

    FIXED with a token sent by the server to the CORRECT tab and replayed back with EVERY Request. (the impact is huge: all the requests to the server have to come with +1 fields in a form or +1 header in the request)

--- spring-app/src/main/java/victor/training/spring/security/config/userpass/SecurityConfigUserPass.java
+++ spring-app/src/main/java/victor/training/spring/security/config/userpass/SecurityConfigUserPass.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Profile("userpass")
 @EnableWebSecurity // (debug = true) // see the filter chain in use
@@ -18,8 +19,11 @@ public class SecurityConfigUserPass extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable(); // OK since I never take <form> POSTs
+//    http.csrf().disable(); // OK since I never take <form> POSTs
 
+    CookieCsrfTokenRepository csrfTokenRepository = new CookieCsrfTokenRepository();
+    csrfTokenRepository.setCookieHttpOnly(false);
+    http.csrf().csrfTokenRepository(csrfTokenRepository);
     // http.cors(); // needed only if .js files are served by a CDN (eg)
 
     http.authorizeRequests().anyRequest().authenticated();
