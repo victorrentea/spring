ea8ee238ccb2443f104904e91de600a8c4abedf2
Victor Rentea <victorrentea@gmail.com>
2023-03-21 11:49:01 +0200

    Function-level authorization @Secured using ROLES

--- spring-app/src/main/java/victor/training/spring/web/controller/TrainingController.java
+++ spring-app/src/main/java/victor/training/spring/web/controller/TrainingController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import victor.training.spring.web.controller.dto.TrainingDto;
@@ -57,7 +58,8 @@ public class TrainingController {
 	//  -> use SpEL: @accessController.canDeleteTraining(#id)
 	//  -> hasPermission + PermissionEvaluator [GEEK]
 	@DeleteMapping("{trainingId}")
-	@PreAuthorize("hasRole('ADMIN')")
+	@Secured("ROLE_ADMIN")
+//	@PreAuthorize("hasRole('ADMIN')")
 	public void delete(@PathVariable Long trainingId) {
 		trainingService.deleteById(trainingId);
 	}
