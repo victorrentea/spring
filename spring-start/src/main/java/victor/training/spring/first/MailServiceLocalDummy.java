c85427e79e2569337487f3e35e6f0dd72a565407
Victor Rentea <victorrentea@gmail.com>
2023-04-03 11:57:55 +0300

    conditional on missing bean

--- spring-start/src/main/java/victor/training/spring/first/MailServiceLocalDummy.java
+++ spring-start/src/main/java/victor/training/spring/first/MailServiceLocalDummy.java
@@ -1,17 +1,36 @@
 package victor.training.spring.first;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Slf4j
-@Service
-@Profile("local") //2: @Profile("test") + you keep the calss in src/test/java
-@Primary
+//@Service("mailservice2")
+
+//@Profile("local") //2: @Profile("test") + you keep the calss in src/test/java
 // TODO when starting the app locally, don't send any emails, log then instead
+
+//@ConditionalOnProperty(name = "mail.sender", havingValue = "dummy")
+//@ConditionalOnMissingBean(MailService.class)
+
+//@Primary
 public class MailServiceLocalDummy implements MailService {
   public void sendEmail(String subject) {
     System.out.println("DUMMY EMAIL SENDER sending an email with subject=" + subject);
+  }
+}
+
+
+@Configuration
+@ConditionalOnMissingBean(MailService.class)
+class SomeConfig {
+  @Bean
+  MailServiceLocalDummy mailServiceLocalDummy() {
+    return new MailServiceLocalDummy();
   }
 }
