8729a962175631f3ac6328e4932de848071436b6
Victor Rentea <victorrentea@gmail.com>
2023-04-03 14:54:40 +0300

    config properties

--- spring-app/src/main/java/victor/training/spring/props/WelcomeInfo.java
+++ spring-app/src/main/java/victor/training/spring/props/WelcomeInfo.java
@@ -6,8 +6,12 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 import javax.annotation.PostConstruct;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.File;
 import java.net.URL;
 import java.util.List;
@@ -19,26 +23,47 @@ import java.util.Map;
 //  and @ConfigurationPropertiesScan on a @Configuration
 // https://stackoverflow.com/questions/26137932/immutable-configurationproperties
 
+//@Component
+//class DirectValue {
+//  @Value("${welcome.welcomeMessage:8}") // fails if the prop is not defined
+//  private String welcomeMessage;
+//  @PostConstruct
+//  public void method() {
+//    System.out.println("message=" + welcomeMessage);
+//  }
+//}
+
 @Slf4j
 @Data
-@Component // (A) mutable
-// @Value @ConstructorBinding // (B) immutable
+@Component
 @ConfigurationProperties(prefix = "welcome")
+@Validated
 public class WelcomeInfo {
-  String welcomeMessage; // TODO validate is not null and size >= 4; with javax.validation annotations?
-  List<URL> supportUrls; // TODO validate at least 1 element
+  @NotNull
+  String welcomeMessage = "surprise"; // TODO validate is not null and size >= 4; with javax.validation annotations?
+  //  @NotEmpty
+  @Size(min = 2)
+  List<URL> supportUrls; // TODO validate at least 2 element
   Map<Locale, String> localContactPhone;
   HelpInfo help;
 
-  @Data // (A) mutable
-  // @Value // (B) immutable
+  @Data
   public static class HelpInfo {
     Integer appId;
     File file; // TODO validate exists on disk
   }
 
+  @AssertTrue
+  public boolean hasHelpFile() {
+    System.out.println("Called!!!");
+    return help.file.isFile();
+  }
+
   @PostConstruct
   public void printMyselfAtStartup() throws JsonProcessingException {
-    // log.info("WelcomeInfo:\n" + new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(this));
+//    if (!help.file.isFile()) {
+//      throw new IllegalArgumentException("Oups: no file:  " + help.file);
+//    }
+    log.info("WelcomeInfo:\n" + new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(this));
   }
 }
