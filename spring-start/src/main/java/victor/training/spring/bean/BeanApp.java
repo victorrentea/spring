7c3ade3eeb896a541622fdeb64b3f449365b5903
Victor Rentea <victorrentea@gmail.com>
2023-04-03 11:13:33 +0300

    @QUalifier

--- spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
+++ spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
@@ -2,10 +2,14 @@ package victor.training.spring.bean;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class BeanApp {
@@ -13,23 +17,36 @@ public class BeanApp {
         SpringApplication.run(BeanApp.class);
     }
 
-//    @Autowired
-//    private Conversation conversation;
+    @Autowired
+    private Conversation conversation;
 
     @EventListener(ApplicationStartedEvent.class)
     public void onStart() {
-        Conversation conversation = new Conversation(new Person("John"), new Person("Jane"));
+
         // TODO register the two persons and the conversation as Spring beans
         conversation.start();
     }
-}
 
+}
+@Configuration
+class MyConfig {
+    @Bean
+    public Person john() { // Beans have names! here the name is "john"
+        return new Person("John");
+    }
+    @Bean
+    public Person jane() {
+        return new Person("Jane");
+    }
+}
+@Component
 @Data
 class Conversation {
     private final Person one;
     private final Person two;
 
-    public Conversation(Person one, Person two) {
+    public Conversation(@Qualifier("john") Person one,
+                        @Qualifier("jane") Person two) {
         this.one = one;
         this.two = two;
     }
@@ -40,6 +57,7 @@ class Conversation {
 }
 
 
+//@Component // i need this class to be a MULTI-TON: 2 instances per app
 class Person {
     private final String name;
 
