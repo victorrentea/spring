a692bd96aca309377a6bf6997c02f9c72a25ec5c
Victor Rentea <victorrentea@gmail.com>
2023-04-03 10:57:25 +0300

    Event listener

--- spring-start/src/main/java/victor/training/spring/first/Cris.java
+++ spring-start/src/main/java/victor/training/spring/first/Cris.java
@@ -2,20 +2,40 @@ package victor.training.spring.first;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.event.ApplicationContextInitializedEvent;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.boot.context.event.ApplicationStartingEvent;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
 @RequiredArgsConstructor
-public class Cris {
+public class Cris implements CommandLineRunner {
   private final X x;
   @Value("${mail.sender}")
   private final String prop;
 
-  @PostConstruct
+//  @PostConstruct
+//  @EventListener(ApplicationStartingEvent.class)
+//  @EventListener(ApplicationContextInitializedEvent.class)
+//  @EventListener(ContextRefreshedEvent.class)
+  @EventListener(ApplicationStartedEvent.class) // when the app is fully configured
   public void logicMethod() {
     System.out.println("Cris logic: " + prop);
+  }
+
+//  @EventListener
+//  public void onAppStart(ApplicationStartingEvent event) {
+//    event.getSpringApplication().add
+//  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println("Workers/batches:  " + args);
   }
 }
 
