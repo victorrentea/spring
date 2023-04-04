9de73e743ddca1c5261c570396bab3fbff0d5a64
Victor Rentea <victorrentea@gmail.com>
2023-04-04 10:49:45 +0300

    works because the @Async submits to a spring-managed [decorated] thread pool

--- spring-micro/src/main/java/victor/training/micro/MicroApp.java
+++ spring-micro/src/main/java/victor/training/micro/MicroApp.java
@@ -1,15 +1,24 @@
 package victor.training.micro;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
+
 @Slf4j
 @RestController
+@EnableAsync
 @SpringBootApplication
 public class MicroApp {
    public static void main(String[] args) {
@@ -27,15 +36,30 @@ public class MicroApp {
       String username = SecurityContextHolder.getContext().getAuthentication().getName();
       log.info("Serving user {}", username);
       threadLocal.set(username);
-      someOther();
+      microApp.someOther();
       return "Amazing bio for teacher id=" + teacherId + " retrieved from remote API as username=" + username;
    }
 
-   public void someOther() {
-      String user = threadLocal.get();
-      log.info("Again user " + user);
-   }
+   @Autowired
+   @Lazy
+   private MicroApp microApp;
 
+   @Async// ("executor") // tells spring to start this method on a thread pool named bean "executor"
+   public void someOther() throws InterruptedException {
+      String user = threadLocal.get();
+      Thread.sleep(2000);
+      log.info("Again user " + user); // the log line has at its start the traceId of the original request
+      // proving that the Sleuth request metadata was magically propagated over an @Async call
+   }
    private static final ThreadLocal<String> threadLocal = new ThreadLocal<>();
-//   private static String currentSessionId;
+
+   @Bean
+   public ThreadPoolTaskExecutor executor() {
+      ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+      executor.setCorePoolSize(7);
+      executor.setMaxPoolSize(7);
+      executor.setThreadNamePrefix("ex-");
+      executor.setQueueCapacity(200);
+      return executor;
+   }
 }
