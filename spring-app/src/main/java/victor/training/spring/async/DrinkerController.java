8f3f2e8f1d26bc3228a8d0bcea413fa78ed2c314
Victor Rentea <victorrentea@gmail.com>
2023-04-04 11:47:01 +0300

    polish

--- spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
+++ spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
@@ -16,9 +16,9 @@ public class DrinkerController {
    @Autowired
    private Barman barman;
 
-   // TODO [1] inject and submit work to a ThreadPoolTaskExecutor
+   // TODO [1] autowire and submit tasks to a ThreadPoolTaskExecutor
    // TODO [2] mark pour* methods as @Async
-   // TODO [3] Build a non-blocking web endpoint
+   // TODO [3] Make this endpoint non-blocking
    @GetMapping("api/drink")
    public DillyDilly drink() throws Exception {
       log.debug("Submitting my order");
@@ -27,8 +27,7 @@ public class DrinkerController {
       Beer beer = barman.pourBeer();
       Vodka vodka = barman.pourVodka();
 
-      long t1 = currentTimeMillis();
-      log.debug("Got my drinks in {} millis", t1-t0);
+      log.debug("Method completed in {} millis", currentTimeMillis() - t0);
       return new DillyDilly(beer, vodka);
    }
 }
