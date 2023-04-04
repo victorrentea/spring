c95bcb2a6a1a787437c5afa7a6515af3d6e87bd0
Victor Rentea <victorrentea@gmail.com>
2023-04-04 11:44:27 +0300

    best practice: always pass a spring-managed magic executor to any ...Async method of a CompletableFuture.

--- spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
+++ spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
@@ -2,12 +2,15 @@ package victor.training.spring.async;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import victor.training.spring.async.drinks.Beer;
 import victor.training.spring.async.drinks.DillyDilly;
 import victor.training.spring.async.drinks.Vodka;
 
+import javax.servlet.AsyncContext;
+import javax.servlet.http.HttpServletRequest;
 import java.util.concurrent.CompletableFuture;
 
 import static java.lang.System.currentTimeMillis;
@@ -19,6 +22,9 @@ public class DrinkerController {
    @Autowired
    private Barman barman;
 
+   @Autowired
+   private ThreadPoolTaskExecutor executor;
+
    // TODO [1] inject and submit work to a ThreadPoolTaskExecutor
    // TODO [2] mark pour* methods as @Async
    // TODO [3] Build a non-blocking web endpoint
@@ -27,8 +33,8 @@ public class DrinkerController {
       log.debug("Submitting my order");
       long t0 = currentTimeMillis();
 
-      CompletableFuture<Beer> futureBeer = supplyAsync(() -> barman.pourBeer());
-      CompletableFuture<Vodka> futureVodka = supplyAsync(() -> barman.pourVodka());
+      CompletableFuture<Beer> futureBeer = supplyAsync(() -> barman.pourBeer(), executor);
+      CompletableFuture<Vodka> futureVodka = supplyAsync(() -> barman.pourVodka(), executor);
 
 //      Beer beer = futureBeer.get(); // the Tomcat threads have to wait 1 second here
 //      Vodka vodka = futureVodka.get(); // no wait, the vodka was already poured
@@ -37,9 +43,20 @@ public class DrinkerController {
       CompletableFuture<DillyDilly> f = futureBeer.thenCombine(futureVodka, (b, v) -> new DillyDilly(b, v));
       long t1 = currentTimeMillis();
       log.debug("Method returns in  {} millis", t1-t0);
+//      f.thenAccept(dilly-> jackson asyncContext.write)
       return f;
 //            .thenAccept(dillyDilly -> log.debug("Got my drinks in {} millis", currentTimeMillis()-t0));
 
 //      return new DillyDilly(beer, vodka);
    }
+
+
+   public void method(HttpServletRequest request) {
+      AsyncContext asyncContext = request.startAsync();
+
+
+   }
+   // in a different thread
+//      asyncContext.getResponse().getWriter().write(jackson.toStrin("data");
+//      asyncContext.complete();
 }
