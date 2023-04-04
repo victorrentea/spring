9283a6b80c85f70fe7cd4736f47eff4d3daf04d6
Victor Rentea <victorrentea@gmail.com>
2023-04-04 13:10:48 +0300

    metrics

--- spring-app/src/main/java/victor/training/spring/performance/BetsController.java
+++ spring-app/src/main/java/victor/training/spring/performance/BetsController.java
@@ -1,7 +1,11 @@
 package victor.training.spring.performance;
 
+import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,16 +13,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BetsController {
 
+  @Autowired
+  private JackpotRepo jackpotRepo;
+
   // TODO report total bets -> counter
   // TODO report histogram of bets -> summary
   @Autowired
-  private MeterRegistry meterRegistry;
+  private MeterRegistry micrometer;
 
   @GetMapping("/bet/{amount}")
-  public void bet(@PathVariable int amount) {
+  @Timed("bet.time") // or registry.timer()
+  public String bet(@PathVariable int amount) {
     System.out.println("Bet " + amount);
+    jackpotRepo.addJackpot((int) (0.1f * amount));
+    micrometer.counter("bets").increment(amount);
+    micrometer.summary("bets.histo").record(amount);
+    return "🍀";
+  }
+
+  @EventListener(ApplicationStartedEvent.class)
+  public void initGauge() {
+    micrometer.gauge("jackpot", jackpotRepo, JackpotRepo::getJackpot);
   }
 
 
+}
 
+@Repository
+class JackpotRepo {
+  //
+  int jackpot;
+
+  public int getJackpot() {
+    return jackpot;
+  }
+
+  public void addJackpot(int amount) {
+    jackpot += amount;
+  }
 }
