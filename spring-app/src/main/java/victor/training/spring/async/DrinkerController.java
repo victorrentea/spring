89c49095412d5aca9a2c12a6f930e207d171d76e
Victor Rentea <victorrentea@gmail.com>
2023-04-04 11:16:31 +0300

    level1 : more tomcat threads

--- spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
+++ spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
@@ -8,7 +8,10 @@ import victor.training.spring.async.drinks.Beer;
 import victor.training.spring.async.drinks.DillyDilly;
 import victor.training.spring.async.drinks.Vodka;
 
+import java.util.concurrent.CompletableFuture;
+
 import static java.lang.System.currentTimeMillis;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 @Slf4j
 @RestController
@@ -24,8 +27,12 @@ public class DrinkerController {
       log.debug("Submitting my order");
       long t0 = currentTimeMillis();
 
-      Beer beer = barman.pourBeer();
-      Vodka vodka = barman.pourVodka();
+      CompletableFuture<Beer> futureBeer = supplyAsync(() -> barman.pourBeer());
+      CompletableFuture<Vodka> futureVodka = supplyAsync(() -> barman.pourVodka());
+
+      Beer beer = futureBeer.get(); // the Tomcat threads have to wait 1 second here
+      Vodka vodka = futureVodka.get(); // no wait, the vodka was already poured
+      // if 200 parallel requests hit this endpoint, Tomcat will be left out of threads for 1 sec.
 
       long t1 = currentTimeMillis();
       log.debug("Got my drinks in {} millis", t1-t0);
