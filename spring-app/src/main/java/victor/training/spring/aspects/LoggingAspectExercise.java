beed586e5c5ff22679d99852f305b1b87a8cd3d0
Victor Rentea <victorrentea@gmail.com>
2023-05-15 16:17:18 +0300

    aspect

--- spring-app/src/main/java/victor/training/spring/aspects/LoggingAspectExercise.java
+++ spring-app/src/main/java/victor/training/spring/aspects/LoggingAspectExercise.java
@@ -1,13 +1,18 @@
 package victor.training.spring.aspects;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
 /**
  * Methods in a class annotated with @Facade
- * @Around("@within(victor.training.spring.aspects.Facade))")
+ * @Around("@within(victor.training.spring.aspects.Facade)")
  * <p>
  * Methods annotated with @LoggedMethod:
  * @Around("@annotation(victor.training.spring.aspects.LoggedMethod))")
@@ -20,11 +25,31 @@ import javax.annotation.PostConstruct;
  * <p>
  * Methods starting with "get" in any class = too much? 😱
  * @Around("execution(* *.get*(..))")
+ *
+ * Methods starting with "get" in any class = too much? 😱
+ * @Around("execution(* victor.training.spring.aspects.Maths.sum(..))")
  */
+@Slf4j
+@Aspect
+@Component
 public class LoggingAspectExercise {
   // TODO 1 log a message for any call to some methods, using any of the techniques above
   // TODO 2 print the method name, arguments and return value to the console
+//  @Around("@within(victor.training.spring.aspects.Facade)")
+   @Around("execution(* victor.training.spring.aspects.Maths.sum(..))")
 
+  public Object interceptCall(ProceedingJoinPoint joinPoint) throws Throwable {
+//    joinPoint.getSignature().getName() // numele metodei chemate
+//    joinPoint.getArgs() // argumentele
+//     transaction start
+    Object r = joinPoint.proceed(); // cheama metoda reala din joinpoint
+//     transaction commit
+//     catch {
+//       tx.rollback
+//     }
+    log.info("calling {}.({}) = {}", joinPoint.getSignature().getName(), joinPoint.getArgs(), r);
+    return r;
+  }
 }
 
 @Component
