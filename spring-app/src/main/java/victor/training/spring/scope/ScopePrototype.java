d47e1160bcac141c7c007aba9d010b4a3702a4ad
Victor Rentea <victorrentea@gmail.com>
2023-05-15 11:56:37 +0300

    scopeuri

--- spring-app/src/main/java/victor/training/spring/scope/ScopePrototype.java
+++ spring-app/src/main/java/victor/training/spring/scope/ScopePrototype.java
@@ -1,7 +1,10 @@
 package victor.training.spring.scope;
 
 import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,23 +14,34 @@ import victor.training.spring.varie.ThreadUtils;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @RestController
 public class ScopePrototype {
   @Autowired
+//  private ApplicationContext applicationContext;
   private DataValidator dataValidator;
+
 
   @GetMapping("api/scope/prototype")
   public List<ValidationMessage> requestScope() {
     String foo = "some text to validate";
     String bar = "a pub";
+//    DataValidator dataValidator = applicationContext.getBean(DataValidator.class); // instanta noua de fiecare data
     dataValidator.getValidationMessages().clear();
+    log.info("Sters");
     ThreadUtils.sleepMillis(3000); // allow the race
     dataValidator.validateFoo(foo);
     dataValidator.validateBar(bar);
+    log.info("adaugat");
     return dataValidator.getValidationMessages();
   }
 }
 @Component
+@Scope("prototype") // de fiecare data cand Spring are nevoie de o instanta -> face NEW
+//@Scope("request") // traieste ob cat traieste un req http
+//@Scope("session") // traieste obiectul cat dureaza sesiunea http 30min
+//@Scope("job") // Spring Batch
+//@Scope("step") // Spring Batch
 class DataValidator {
   private final List<ValidationMessage> validationMessages = new ArrayList<>();
   public void validateFoo(String foo) {
