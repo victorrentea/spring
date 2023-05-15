d747b33b43ee504cb60b657ef65e33d3dfe9e4d4
Victor Rentea <victorrentea@gmail.com>
2023-05-15 11:04:37 +0300

    NU E BINE @Lazy

--- spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
+++ spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
@@ -5,33 +5,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class BeanApp {
     public static void main(String[] args) {
         SpringApplication.run(BeanApp.class);
     }
-
-//    @Autowired
-//    private Conversation conversation;
+    @Autowired
+    private Conversation conversation;
 
     @EventListener(ApplicationStartedEvent.class)
     public void onStart() {
-        Conversation conversation = new Conversation(new Person("John"), new Person("Jane"));
+//        Conversation conversation = new Conversation(
+//                new Person("John"),
+//                new Person("Jane"));
         // TODO register the two persons and the conversation as Spring beans
         conversation.start();
     }
-}
 
+    @Bean
+    public Person john() {
+        return new Person("John");
+    }
+    @Bean
+    public Person jane() {
+        return new Person("Jane");
+    }
+}
 @Data
+@Component
 class Conversation {
     private final Person one;
     private final Person two;
-
-    public Conversation(Person one, Person two) {
-        this.one = one;
-        this.two = two;
+    public Conversation(@Lazy Person john,@Lazy Person jane) {
+        this.one = john;
+        this.two = jane;
     }
 
     public void start() {
--- spring-start/src/main/java/victor/training/spring/first/Y.java
+++ spring-start/src/main/java/victor/training/spring/first/Y.java
@@ -1,8 +1,14 @@
 package victor.training.spring.first;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import victor.training.spring.sub.X;
 
+import javax.annotation.PostConstruct;
+import java.sql.SQLOutput;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -10,16 +16,20 @@ import java.util.List;
 public class Y {
 //  @Qualifier("mailServiceImpl")
 //  private final MailService mailService; // polymorphic injection
-
+//  @Lazy
+//  private final X x;
 //  private final MailServiceImpl mailService;
-
   private final MailService mailService;
-
   private final List<MailService> mailServiceToate;
-
   public int logic() {
     System.out.println("TOate de acel tip: " + mailServiceToate);
     mailService.sendEmail("I like 4 topics");
     return 1;
+  }
+
+  //@PostConstruct
+  @EventListener(ApplicationStartedEvent.class)
+  public void method() {
+    System.out.println("ASTA");
   }
 }
