a4aa354bdcb78f2a98ab6d08d6d8e7c19dd1c7ec
Victor Rentea <victorrentea@gmail.com>
2023-05-15 14:48:43 +0300

    proxy-uri facute cu plain java

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -1,16 +1,19 @@
 package victor.training.spring.aspects;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class ProxyIntro {
     public static void main(String[] args) {
         // Play the role of Spring here ...
         Maths maths = new Maths();
-        SecondGrade secondGrade = new SecondGrade(maths);
+        SecondGrade secondGrade = new SecondGrade(new Wrapper(maths));
         new ProxyIntro().run(secondGrade);
     }
 
@@ -22,15 +25,34 @@ public class ProxyIntro {
         secondGrade.mathClass();
     }
 }
-
-@Service
-class SecondGrade {
+class Wrapper extends  Maths {
+    private static final Logger log = LoggerFactory.getLogger(ProxyIntro.class);
     private final Maths maths;
 
-    SecondGrade(Maths maths) {
+    public Wrapper(Maths maths) {
         this.maths = maths;
     }
 
+    @Override
+    public int sum(int a, int b) {
+        log.info("{}+{}",a,b);
+        return maths.sum(a, b);
+    }
+
+    @Override
+    public int product(int a, int b) {
+        log.info("{}+{}",a,b);
+        return maths.product(a, b);
+    }
+}
+// codu din framework
+// --------------
+// codu app mele
+class SecondGrade {
+    private final Maths maths;
+    SecondGrade(Maths maths) { // spring nu va injecta clasa REALA Maths ci un wrapper peste. o clasa care extinde pe-a mea!!
+        this.maths = maths;
+    }
     public void mathClass() {
         System.out.println("2 + 4 = " + maths.sum(2, 4));
         System.out.println("1 + 5 = " + maths.sum(1, 5));
@@ -38,7 +60,7 @@ class SecondGrade {
     }
 }
 
-@Facade
+@Slf4j
 class Maths {
     public int sum(int a, int b) {
         return a + b;
