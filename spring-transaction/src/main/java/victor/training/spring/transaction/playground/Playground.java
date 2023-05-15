8b2760a8d2fa26d673976736356fb7c66245eb36
Victor Rentea <victorrentea@gmail.com>
2023-05-15 17:11:57 +0300

    am reusit sa exec o metoda pe alt thread cu @Async -> bravo! d
    dar am pierdut tranzactia :(

--- spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
@@ -1,7 +1,9 @@
 package victor.training.spring.transaction.playground;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,15 +22,14 @@ public class Playground {
     // proxy creaza tranzactie si o PUNE PE THREAD (ThreadLocal)
     public void transactionOne() {
         repo.nativeQueryInSpringDataJPA("ALO");
-        altaMetoda();
+        otherClass.altaMetoda();
         // 3 Difference with/out @Transactional on f() called: zombie transactions; mind local calls⚠️
         // 4 Game: persist error from within zombie transaction: REQUIRES_NEW or NOT_SUPPORTED
         // 5 Performance: connection starvation issues : debate: avoid nested transactions
     }
 
-    private void altaMetoda() {
-        repo.save(new Message(null));
-    }
+    @Autowired
+    private OtherClass otherClass;
 
     @Transactional
     public void transactionTwo() {
@@ -39,4 +40,8 @@ public class Playground {
 @RequiredArgsConstructor
 class OtherClass {
     private final MessageRepo repo;
+    @Async
+    public void altaMetoda() {
+        repo.save(new Message(null));
+    }
 }
