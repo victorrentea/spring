6bb844c04294f5fb93e9a6b7ae0ec01cc32bcbc6
Victor Rentea <victorrentea@gmail.com>
2023-05-15 11:06:10 +0300

    mandru fara @Lazy

--- spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
+++ spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -27,6 +28,10 @@ public class BeanApp {
         conversation.start();
     }
 
+}
+@Configuration
+class ProjectConfiguration {
+
     @Bean
     public Person john() {
         return new Person("John");
@@ -41,7 +46,7 @@ public class BeanApp {
 class Conversation {
     private final Person one;
     private final Person two;
-    public Conversation(@Lazy Person john,@Lazy Person jane) {
+    public Conversation(Person john,Person jane) {
         this.one = john;
         this.two = jane;
     }
