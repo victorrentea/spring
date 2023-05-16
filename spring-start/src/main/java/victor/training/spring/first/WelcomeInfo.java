6d74b9873d7d6bb3abc9113afd5bfd866f828d07
Victor Rentea <victorrentea@gmail.com>
2023-05-16 07:25:12 +0300

    aaa

--- spring-start/src/main/java/victor/training/spring/first/WelcomeInfo.java
+++ spring-start/src/main/java/victor/training/spring/first/WelcomeInfo.java
@@ -40,13 +40,15 @@ public class WelcomeInfo {
 //   or create a new component in which to inject WelcomeInfo and use a property
 // TODO 1 inject welcome.welcomeMessage property in 'welcomeMessage' field
 //   Hint: @Value("${
-//   Question: what happens if the property is NOT defined
+//   Question: what happens if the property is NOT defined ?
 //     TODO 1* provide a default value by @Value("${...:defaultvalue}")
 // TODO 2 inject welcome.gate in the 'gate' field'
 //   Question: what happens if the value of the property is not an INT ?
 // TODO 3 inject all properties at once using @ConfigurationProperties(prefix=...)
 //   Ref: https://www.google.com/search?q=configuration+properties+baledung
 // TODO 4 validate (see above)
+//   - using if at startup
+//   - using javax.validation annotations + @Validated on the class
 //   Trick: provide a default to a property by assigning the field = "defaultvalue"
 // TODO 5[PRO] make this class immutable: lombok.@Value + @ConstructorBinding + @ConfigurationPropertiesScan on a @Configuration
 //  Hint: https://stackoverflow.com/questions/26137932/immutable-configurationproperties
