85a8f9c9a3c90b307b8a96bde617701db6dbc5c3
Victor Rentea <victorrentea@gmail.com>
2023-06-15 12:45:21 +0300

    prima vers de interceptie subclasand tipurile tale

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -6,16 +6,25 @@ import org.springframework.stereotype.Service;
 public class ProxyIntro {
     public static void main(String[] args) {
         // WE play the role of Spring here ...
-        Maths maths = new Maths();
+        Maths maths = new Maths() {
+            @Override
+            public int sum(int a, int b) {
+                int result = super.sum(a, b); // real call
+//                System.out.println("sum \{a} + \{b} = \{result}"); // java24
+                System.out.println("SRI: sum "+ a+ " + " + b +" = "+result); // java21
+                return result;
+            }
+        };
         SecondGrade secondGrade = new SecondGrade(maths);
+        // TODO logeaza param si rezultatele tutoro metodelor din Maths chemate de SecondGrade
+        // -- pana aici ar face spring in startup
+        // la runtime
         secondGrade.mathClass();
     }
 }
-
-// ------------------------
+// ------------------------  n-ai voie sa scrii nimic sub linia asta: fii-mea nu tre sa afle --------
 class SecondGrade {
     private final Maths maths;
-
     SecondGrade(Maths maths) {
         this.maths = maths;
     }
@@ -26,12 +35,10 @@ class SecondGrade {
         System.out.println("4 x 3 = " + maths.product(4, 3));
     }
 }
-
 class Maths {
     public int sum(int a, int b) {
         return a + b;
     }
-
     public int product(int a, int b) {
         return a * b;
     }
