27a61899110ebd00d4793b78ad3de1e6afe06318
Victor Rentea <victorrentea@gmail.com>
2023-06-16 10:24:30 +0300

    CompletableFuture sa protejezi Tomcatu

--- spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
+++ spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
@@ -2,17 +2,15 @@ package victor.training.spring.async;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import victor.training.spring.async.drinks.Beer;
 import victor.training.spring.async.drinks.DillyDilly;
 import victor.training.spring.async.drinks.Vodka;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.CompletableFuture;
 
 import static java.lang.System.currentTimeMillis;
 
@@ -25,23 +23,36 @@ public class DrinkerController {
    @Autowired
    private ThreadPoolTaskExecutor barPool;
 
+//   @PostMapping("accept-payment")
+//   public void method() {
+//
+//   }
    // TODO [1] autowire and submit tasks to a ThreadPoolTaskExecutor
    // TODO [2] mark pour* methods as @Async
    // TODO [3] Make this endpoint non-blocking
    @GetMapping("api/drink")
-   public DillyDilly drink() throws Exception {
+   public CompletableFuture<DillyDilly> drink() throws Exception {
       log.debug("Submitting my order: "+barPool.getClass());
       long t0 = currentTimeMillis();
 
       // never in spring
 //      ExecutorService threadPool = Executors.newFixedThreadPool(2);
-      Future<Beer> futureBeer = barman.pourBeer();
-      Future<Vodka> futureVodka = barman.pourVodka();
+      CompletableFuture<Beer> futureBeer = barman.pourBeer();
+      CompletableFuture<Vodka> futureVodka = barman.pourVodka();
 
-      Beer beer = futureBeer.get(); // BLOCK threadul tomcatului pt 1 sec
-      Vodka vodka = futureVodka.get(); // BLOCK threadul tomcatului pt 0
+//      Beer beer = futureBeer.get(); // BLOCK threadul tomcatului pt 1 sec
+//      Vodka vodka = futureVodka.get(); // BLOCK threadul tomcatului pt 0
+
+      CompletableFuture<DillyDilly> futureDilly = futureBeer.thenCombine(futureVodka,
+          (b, v) -> new DillyDilly(b, v));
 
       log.debug("Method completed in {} millis", currentTimeMillis() - t0);
-      return new DillyDilly(beer, vodka);
+//      futureDilly.thenAccept(dilly -> rsponse.write(dilly))
+      return futureDilly;
    }
+   //springu dupa tine agata de futureDilly un callback sa scrie pe socket la client peste HTTP
 }
+// daca vrei poti sa NU blochezi threadul tomcatului, returnand un CompletableFuture
+// procesare async non-blocanta Java8
+
+//
