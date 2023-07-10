52e10db77379c787cffba1607d3408d5a50f8c36
Victor Rentea <victorrentea@gmail.com>
2023-07-10 15:32:32 +0300

    @Bean

--- spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
+++ spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication //@Configuration
 public class BeanApp {
     public static void main(String[] args) {
@@ -25,20 +27,44 @@ public class BeanApp {
     }
 
 }
-@Configuration
+@Configuration// (proxyBeanMethods = false)
 class MyConfig {
     @Bean
     public Person john() {
+        System.out.println("John se naste");
         return new Person("John");
     }
     @Bean
     public Person jane() {
         return new Person("Jane");
     }
+    @Bean
+    public Conversation conversation() {
+        System.out.println("Conversatia");
+        return new Conversation(john(), jane());
+        // cand dintr-o metoda @bean apelezi alta metoda @Bean intr-un @Configuration,
+        // => nu chemi de fapt metoda reala ci Spring iti prinde apelul
+    }
+    @Bean
+    public Conversation impacarea() {
+        System.out.println("IMpacarea");
+        return new Conversation(john(), jane());
+    }
 }
+// spring, la runtime
+//class Hack extends MyConfig {
+//    @Override
+//    public Person john() {
+//        // daca am in cahce de singleton >> return
+//        altfel
+//        return super.john();
+// ruleaza post construct
+//        put in cache ...
+//    }
+//}
 
+//// --- linie : nu ai voie sa modifici nimic sub (.jar)
 @Data
-@Service
 class Conversation {
     private final Person john;
     private final Person jane;
