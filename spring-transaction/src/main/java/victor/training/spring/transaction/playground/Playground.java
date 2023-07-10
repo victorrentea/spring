e7a7fa699f72aa5c7cbdf5182e1ca03a24822c7e
Victor Rentea <victorrentea@gmail.com>
2023-07-10 17:22:28 +0300

    zombie tranzaction

--- spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
@@ -1,11 +1,13 @@
 package victor.training.spring.transaction.playground;
 
+import com.sun.xml.bind.v2.TODO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.sql.Connection;
 
 @Service
 @RequiredArgsConstructor
@@ -16,9 +18,14 @@ public class Playground {
 
     @Transactional
     public void transactionOne() {
-        jdbc.update("insert into MESSAGE(id, message)" +
-                    " values ( 100,'SuQiLi' )");
-        other.altaMetoda();
+        jdbc.update("insert into MESSAGE(id, message) values ( 100,'SuQiLi' )");
+        try {
+            other.altaMetoda();
+        } catch (Exception e) {
+            repo.save(new Message("EROARE TATA: " + e)); // faci .save intr-o
+            //  tranzactie omorata de proxy de la linia 44. se va face ROLLBACK la ce ai facut aici
+        }
+        repo.save(new Message("DUPA"));
         // 0 p6spy
         // 1 Cause a rollback by breaking NOT NULL, throw Runtime, throw CHECKED
         // 2 Tx propagates with your calls (in your thread😱)
@@ -39,8 +46,8 @@ class OtherClass {
         repo.save(new Message("OK"));
         repo.save(new Message("OK2"));
         System.out.println("Ies din metoda!");
-//        if (true) {
-//            throw new IllegalArgumentException("Biz validation exception care distruge tranzactia curenta");
-//        }
+        if (true) {
+            throw new IllegalArgumentException("Biz validation exception care distruge tranzactia curenta");
+        }
     }
 }
