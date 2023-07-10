b255b0a86fbeadfec084be8332d7bee806265d33
Victor Rentea <victorrentea@gmail.com>
2023-07-10 12:37:15 +0300

    am pus degetul pr proxy

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -1,34 +1,52 @@
 package victor.training.spring.aspects;
 
+import org.springframework.cglib.proxy.Callback;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 // de ce sa fii atent astea 45 min pana la masa ?
 // @Transactional @Secured/@PreAuthorized, @Cacheable, @Timed, @Retryable, @Aspect
 public class ProxyIntro {
     public static void main(String[] args) {
         // WE play the role of Spring here ...
-        Maths maths = new MathsInterceptat(new Maths()); // DECORATOR
-        SecondGrade secondGrade = new SecondGrade(maths);
+        Maths realBean = new Maths();
+
+        Callback callback = new MethodInterceptor() {
+            @Override
+            public Object intercept(Object o, Method method, Object[] arguments, MethodProxy methodProxy) throws Throwable {
+                System.out.println("Chem metoda '" + method.getName() + "' cu param " + Arrays.toString(arguments));
+                return method.invoke(realBean, arguments);
+            }
+        };
+        Maths proxy = (Maths) Enhancer.create(Maths.class, callback);
+        SecondGrade secondGrade = new SecondGrade(proxy);
         // imagine un Apel HTTP aici
         secondGrade.mathClass();
     }
 }
-class MathsInterceptat  extends Maths { // PROXY
-    private final Maths realBean;
-    MathsInterceptat(Maths real) {
-        this.realBean = real;
-    }
+//class MathsInterceptat  extends Maths { // PROXY
+//    private final Maths realBean;
+//    MathsInterceptat(Maths real) {
+//        this.realBean = real;
+//    }
+//
+//    @Override
+//    public int sum(int a, int b) {
+//        System.out.println("Chema suma intre " + a + " cu " + b);
+//        return realBean.sum(a, b);
+//    }
+//
+//    @Override
+//    public int product(int a, int b) {
+//        System.out.println("Chema produs intre " + a + " cu " + b);
+//        return realBean.product(a, b);
+//    }
+//}
 
-    @Override
-    public int sum(int a, int b) {
-        System.out.println("Chema suma intre " + a + " cu " + b);
-        return realBean.sum(a, b);
-    }
-
-    @Override
-    public int product(int a, int b) {
-        System.out.println("Chema produs intre " + a + " cu " + b);
-        return realBean.product(a, b);
-    }
-}
 // cerinta deasupra acestei linii scrie ceva asa incat sa pui in consola toate apelurile
 // pe care SecondGrade le face catre Maths (param+ return value)
 // ------------------------
@@ -38,13 +56,18 @@ class SecondGrade {
         this.maths = maths;
     }
     public void mathClass() {
-        System.out.println("8 + 4 = " + maths.sum(8, 4));
+        //cand eu chem o metoda pe 'maths', mai ruleaza cineva pana intru in acea metoda
+        System.out.println("Inainte de apel. TZEAPA. obiectul pe caer Spring ti l-a " +
+                           "injectat  NU E CEEA CE CREZI: "+maths.getClass());
+        int rezultat = maths.sum(8, 4);
+        System.out.println("8 + 4 = " + rezultat);
         System.out.println("6 + 6 = " + maths.sum(6, 6));
         System.out.println("4 x 3 = " + maths.product(4, 3));
     }
 }
 class Maths {
     public int sum(int a, int b) {
+        System.out.println("Am ajuns in metoda reala");
         return a + b;
     }
     public int product(int a, int b) {
