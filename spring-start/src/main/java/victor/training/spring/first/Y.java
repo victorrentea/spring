54e40873a750bfefbf9b5c78104b4d5df9efb9dd
Victor Rentea <victorrentea@gmail.com>
2023-07-10 12:13:48 +0300

    Lombok tweaking

--- spring-start/src/main/java/victor/training/spring/first/Y.java
+++ spring-start/src/main/java/victor/training/spring/first/Y.java
@@ -2,28 +2,21 @@ package victor.training.spring.first;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import victor.training.spring.bean.X;
 
-import java.util.Optional;
-
-@RequiredArgsConstructor
+@RequiredArgsConstructor // pune pe ctor @Lazy si @Value
+// de mai jos multumita lombok.config
 @Service
 public class Y {
-  @Autowired
   @Lazy // cheating
-  private X x;
+  private final X x;
+  private final MailService mailService;
+  @Value("${welcome.welcomeMessage}") // inject this from the configuration files
+  private final String message;
 
-
-  @Autowired
-//  @Qualifier("mailServiceLocalDummy") // numesc beanul dorit
-  private MailService mailService; // polymorphic injection
-
-  //  @Value("${welcome.welcomeMessage}") // inject this from the configuration files
-  private final String message = "HALO";
 
   public int logic() {
     mailService.sendEmail("I like 4 topics : " + message);
