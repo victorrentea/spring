e3f48733036ab77939cb257692aeea0c9e11889c
Victor Rentea <victorrentea@gmail.com>
2023-07-10 12:26:51 +0300

    am suprascris metoda ca sa fac chestii in plus

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -3,38 +3,42 @@ package victor.training.spring.aspects;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
 
-@SpringBootApplication
+// de ce sa fii atent astea 45 min pana la masa ?
+// @Transactional @Secured/@PreAuthorized, @Cacheable, @Timed, @Retryable, @Aspect
 public class ProxyIntro {
     public static void main(String[] args) {
         // WE play the role of Spring here ...
-        Maths maths = new Maths();
+        Maths maths = new MathsInterceptat();
         SecondGrade secondGrade = new SecondGrade(maths);
+        // imagine un Apel HTTP aici
         secondGrade.mathClass();
     }
 }
-
+class MathsInterceptat  extends Maths{
+    @Override
+    public int sum(int a, int b) {
+        System.out.println("Chema suma intre " + a + " cu " + b);
+        return super.sum(a, b);
+    }
+}
+// cerinta deasupra acestei linii scrie ceva asa incat sa pui in consola toate apelurile
+// pe care SecondGrade le face catre Maths (param+ return value)
 // ------------------------
-@Service
 class SecondGrade {
     private final Maths maths;
-
     SecondGrade(Maths maths) {
         this.maths = maths;
     }
-
     public void mathClass() {
         System.out.println("8 + 4 = " + maths.sum(8, 4));
         System.out.println("6 + 6 = " + maths.sum(6, 6));
         System.out.println("4 x 3 = " + maths.product(4, 3));
     }
 }
-
-@Facade
 class Maths {
     public int sum(int a, int b) {
         return a + b;
     }
-
     public int product(int a, int b) {
         return a * b;
     }
