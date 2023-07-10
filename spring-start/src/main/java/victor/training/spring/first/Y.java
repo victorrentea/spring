c6551928771b3d85dfcad28a1476ba83f0552efb
Victor Rentea <victorrentea@gmail.com>
2023-07-10 10:26:08 +0300

    cyclic deps - yuuuu🤮

--- spring-start/src/main/java/victor/training/spring/first/Y.java
+++ spring-start/src/main/java/victor/training/spring/first/Y.java
@@ -1,16 +1,24 @@
 package victor.training.spring.first;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+import victor.training.spring.bean.X;
 
 @Service
 public class Y {
+  @Autowired
+  @Lazy // cheating
+  private X x;
   @Qualifier("mailServiceImpl")
   private final MailService mailService; // polymorphic injection
-//  @Value("${welcome.welcomeMessage}") // inject this from the configuration files
+  //  @Value("${welcome.welcomeMessage}") // inject this from the configuration files
   private final String message = "HALO";
+
+
 
   // (recommended) constructor injection => 😏 replace with @RequiredArgsConstructor
   public Y(MailService mailService) {
