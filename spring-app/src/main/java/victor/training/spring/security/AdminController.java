1576d78e78eb636ca9f46d37419540ac29ee863c
Victor Rentea <victorrentea@gmail.com>
2023-07-17 18:07:51 +0300

    XSS fun

--- spring-app/src/main/java/victor/training/spring/security/AdminController.java
+++ spring-app/src/main/java/victor/training/spring/security/AdminController.java
@@ -1,14 +1,21 @@
 package victor.training.spring.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import victor.training.spring.props.WelcomeInfo;
 
 @RestController
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
 
-  // TODO URL-pattern restriction: admin/**
+  private static final Logger log = LoggerFactory.getLogger(AdminController.class);
+
   @GetMapping("admin/launch")
   public String restart() {
     return "What does this red button do?     ... [Missile Launched]";
@@ -17,6 +24,12 @@ public class AdminController {
   @GetMapping("admin/clear-cache")
   public String clearCaches() {
     return "Wipe out caches";
+  }
+
+  @GetMapping("api/salary/raise")
+  public String raiseSalary(@RequestParam String who, @RequestParam String amount) {
+    log.info("Raising salary of {} with {}", who, amount);
+    return "Salary raised by admin!";
   }
 
 }
