fe7515425061bf17d48183fc1bb29c7618b0d3bd
Victor Rentea <victorrentea@gmail.com>
2023-07-17 14:31:51 +0300

    CORS config global

--- spring-app/src/main/java/victor/training/spring/security/config/CorsGlobalConfig.java
+++ spring-app/src/main/java/victor/training/spring/security/config/CorsGlobalConfig.java
@@ -7,19 +7,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsGlobalConfig {
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/api/**")
-//                        .allowedMethods("*")
-//                        .allowCredentials(true) // allows receiving session cookie (if using cookies)
-//                        .allowedOriginPatterns("http://localhost:8081") // eg NodeJS
-////					 .allowedOriginPatterns("http://*") // Too broad
-//                ;
-//                // also don't forget to add .cors() to spring security
-//            }
-//        };
-//    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedMethods("*")
+                        .allowCredentials(true) // allows receiving session cookie (if using cookies)
+                        .allowedOriginPatterns("http://localhost:8081") // eg NodeJS
+//					 .allowedOriginPatterns("http://*") // Too broad
+                ;
+                // also don't forget to add .cors() to spring security
+            }
+        };
+    }
 }
