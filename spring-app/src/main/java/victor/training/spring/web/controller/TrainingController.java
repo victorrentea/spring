440a8aebc45b920ea0fa55d3b5f221b024958c46
Victor Rentea <victorrentea@gmail.com>
2023-07-17 15:48:44 +0300

    SQL injection fixed + ORDER BY whitelistat

--- spring-app/src/main/java/victor/training/spring/web/controller/TrainingController.java
+++ spring-app/src/main/java/victor/training/spring/web/controller/TrainingController.java
@@ -26,6 +26,7 @@ public class TrainingController {
 
 	@GetMapping
 	public List<TrainingDto> getAll() {
+
 		List<TrainingDto> list = trainingService.getAllTrainings();
 		for (TrainingDto trainingDto : list) {
 			trainingDto.description = sanitizeDescription(trainingDto.description);
