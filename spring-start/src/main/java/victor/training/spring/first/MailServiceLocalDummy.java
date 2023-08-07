1692fdb8ec8ba9b331393ac4f8291ccaa0fd3b87
Victor Rentea <victorrentea@gmail.com>
2023-08-07 11:55:54 +0300

    conditionalOn.... instead of @Profile

--- spring-start/src/main/java/victor/training/spring/first/MailServiceLocalDummy.java
+++ spring-start/src/main/java/victor/training/spring/first/MailServiceLocalDummy.java
@@ -1,13 +1,17 @@
 package victor.training.spring.first;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("local")
+@ConditionalOnProperty(name = "environment.name",
+        havingValue = "local",
+        matchIfMissing = false)
+//@Profile("local")
 @Primary
 public class MailServiceLocalDummy implements MailService {
   public void sendEmail(String subject) {
