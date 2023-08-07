2cf7e8f9b0fe38808abf08bbeec5bed95252c993
Victor Rentea <victorrentea@gmail.com>
2023-08-07 13:49:15 +0300

    @Bean calling other @Bean in a configuration; calls are proxies

--- spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
+++ spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
@@ -1,57 +1,89 @@
 package victor.training.spring.bean;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Scope;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-@SpringBootApplication
+@SpringBootApplication//(proxyBeanMethods = false)
 public class BeanApp {
     public static void main(String[] args) {
         SpringApplication.run(BeanApp.class);
     }
 
-//    @Autowired
-//    private Conversation conversation;
-
-    @EventListener(ApplicationStartedEvent.class)
-    public void onStart() {
-        Conversation conversation = new Conversation(
-                new Person("John"),
-                new Person("Jane"));
-        // TODO register the two persons and the conversation as Spring beans
-        conversation.start();
-    }
-
     // i need 2+ instances of the same type as beans
-    // i need manual config of the instances 
-    @Bean
-    public Person john() { // creates manually a bean named 'john' of type Person
-        return new Person("John");
-    }
+    // i need manual config of the instances
+    // i want to add a class from a library to my spring context
     @Bean
     public Person jane() {
         return new Person("Jane");
     }
+    @Bean // in CLASS annotated with @Configuration
+//    @Profile()
+//    @ConfigurationProperties
+//    @Primary
+//    @Scope
+    public  Person john() { // creates manually a bean named 'john' of type Person
+        System.out.println("John born👶");
+        return new Person("John");
+    }
+    @Bean
+    public Conversation conversation() {
+        System.out.println("That's impossible1");
+        return new Conversation(john(), jane());
+    }
+    @Bean
+    public Conversation brainstorm() {
+        System.out.println("That's impossible2");
+        return new Conversation(john(), jane());
+    }
+}
+// spring creates a subclass of every @Configuration class in order to
+// override the public @Bean methods to enforce the spring semantic (singleton, autowqired,
+// WARNING: this is THE ONLY place in Spring Framework where LOCAL METHOD CALLS ARE INTERCEPTED
+//class SubClass extends BeanApp {
+//    @Override
+//    public Person john() {
+//        // spring is in control:
+//        return super.john();
+//    }
+//}
+
+
+
+
+@Component
+class SecondClass {
+    @Autowired
+    Conversation conversation;
+    @EventListener(ApplicationStartedEvent.class)
+    public void onStart() {
+        conversation.start();
+    }
 }
 
+// ----
+// below this line the code comes from a library that I cannot touch
 @Data
-@Component
 class Conversation {
-    private final Person one;
-    private final Person two;
+    private final Person john;
+    private final Person jane;
 
-    public Conversation(@Qualifier("john") Person one, @Qualifier("jane")Person two) {
-        this.one = one;
-        this.two = two;
+    public Conversation(Person john, Person jane) {
+        this.john = john;
+        this.jane = jane;
     }
 
     public void start() {
-        System.out.println(one.getName() + " talks with " + two.getName());
+        System.out.println(john.getName() + " talks with " + jane.getName());
     }
 }
 
