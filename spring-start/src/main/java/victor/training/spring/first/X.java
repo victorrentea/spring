b0bc163e40b915bfd76db2eec5dfa312a2483a61
Victor Rentea <victorrentea@gmail.com>
2023-08-07 10:16:28 +0300

    programmatic bean retrival - bad

--- spring-start/src/main/java/victor/training/spring/first/X.java
+++ spring-start/src/main/java/victor/training/spring/first/X.java
@@ -1,17 +1,29 @@
 package victor.training.spring.first;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
-@Service // TODO what other annotation register this class as a bean
+@Service // TODO what other annotation register this class as a "bean"
 public class X {
-  @Autowired
-  private Y y; // #2 field injection
+//  private final Y y;
+//  // constructor based injection ❤️
+//  public X(Y y) {
+//    this.y = y;
+//  }
 
-  // #3 method injection (rarely used)
-  // @Autowired public void init(Y y) {this.y2 = y;}
+//  @Autowired // method injection
+//  public void init(Yyy y, Zzz z) {
+//  }
+
+  @Autowired
+  ApplicationContext applicationContext;
 
   public int logic() {
-    return 1 + y.logic();
+    System.out.println("At runtime, 30 min later when ***that*** REST call happens");
+    // risky: runtime failure if there is no Y bean defined in the app
+    Y bean = applicationContext.getBean(Y.class); // manual bean fetching
+
+    return 1 + bean.logic();
   }
 }
--- spring-start/src/main/java/victor/training/spring/first/Y.java
+++ spring-start/src/main/java/victor/training/spring/first/Y.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 public class Y {
   @Qualifier("mailServiceImpl")
   private final MailService mailService; // polymorphic injection
