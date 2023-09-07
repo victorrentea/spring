b96083bc61660c530dc4ca0216d0441ad2734e2d
Victor Rentea <victorrentea@gmail.com>
2023-09-07 16:31:44 +0300

    moved atomic part in a @Transactional method in in a separate class so a proxy kicks in between

--- spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/playground/Playground.java
@@ -1,9 +1,14 @@
 package victor.training.spring.transaction.playground;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @Service
 @RequiredArgsConstructor
@@ -24,11 +29,34 @@ public class Playground {
     var dataFromRemote = "data retrieved via an API call";
     // restTemplate.getForObject / webClient.block() => JDBC Connection Pool Starvation
 
+//    Playground myself = (Playground) AopContext.currentProxy(); // #2 also this!! OMG
+
     other.atomicPart(dataFromRemote);
+
+//    transactionTemplate.executeWithoutResult(status -> { // #4 programatic no aspects
+//      repo.save(new Message("JPA with " + dataFromRemote));
+//      repo.save(new Message(null));
+//    });
   }
 
+  private TransactionTemplate transactionTemplate;
   @Autowired
-  private OtherClass other; // proxy injected here !
+  public void initTxTemplate(PlatformTransactionManager transactionManager) {
+    transactionTemplate = new TransactionTemplate(transactionManager);
+    transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+    transactionTemplate.setTimeout(1);
+  }
+  @Autowired
+//  @Lazy
+//  private Playground myself; // proxy injected here ! ME #3
+  private OtherClass other; // #1 move method to other class and inject it proxy injected here !
+    // breaking complexity apart is generally GOOD
+
+  @Transactional
+  public void atomicPart(String dataFromRemote) {
+    repo.save(new Message("JPA with " + dataFromRemote));
+    repo.save(new Message(null));
+  }
 
   public void transactionTwo() {}
 }
