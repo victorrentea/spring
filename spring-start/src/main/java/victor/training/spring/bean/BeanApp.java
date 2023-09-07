8834f487be85649dfe17fea70ac72b1254669049
Victor Rentea <victorrentea@gmail.com>
2023-09-07 18:18:29 +0300

    @Qualifier

--- spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
+++ spring-start/src/main/java/victor/training/spring/bean/BeanApp.java
@@ -1,11 +1,17 @@
 package victor.training.spring.bean;
 
 import lombok.Data;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class BeanApp {
@@ -22,14 +28,23 @@ public class BeanApp {
         // TODO register the two persons and the conversation as Spring beans
         conversation.start();
     }
-}
 
+    @Bean
+    public Person john() {
+        return new Person("John");
+    }
+    @Bean
+    public Person jane() {
+        return new Person("Jane");
+    }
+}
 @Data
+@Component
 class Conversation {
     private final Person one;
     private final Person two;
 
-    public Conversation(Person one, Person two) {
+    public Conversation(@Qualifier("john") Person one, @Qualifier("jane") Person two) {
         this.one = one;
         this.two = two;
     }
@@ -38,7 +53,6 @@ class Conversation {
         System.out.println(one.getName() + " talks with " + two.getName());
     }
 }
-
 
 class Person {
     private final String name;
