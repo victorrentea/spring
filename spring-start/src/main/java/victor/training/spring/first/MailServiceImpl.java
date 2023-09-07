0dba720179b931e170765ee8768b87b8c2dae785
Victor Rentea <victorrentea@gmail.com>
2023-09-07 12:14:05 +0300

    conditional bean backed off when LocalDummy bean is defined (thanks to profile)

--- spring-start/src/main/java/victor/training/spring/first/MailServiceImpl.java
+++ spring-start/src/main/java/victor/training/spring/first/MailServiceImpl.java
@@ -1,10 +1,16 @@
 package victor.training.spring.first;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Profile;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
 @Service
+//@Profile("prod") // BAD PRACTICE: only to run in production env
+//@Profile("!local") // garbage only for local env dev
+@ConditionalOnMissingBean(MailService.class)
 @RequiredArgsConstructor
 public class MailServiceImpl implements MailService {
   //  private final MailSender sender; // TODO uncomment and watch it failing because it requires properties to be auto-defined
--- spring-start/src/main/java/victor/training/spring/first/MailServiceLocalDummy.java
+++ spring-start/src/main/java/victor/training/spring/first/MailServiceLocalDummy.java
@@ -1,9 +1,16 @@
 package victor.training.spring.first;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+@Service
 @Slf4j
+//@ConditionalOnProperty(value = "mail.sender.dummy",havingValue = "true")
+@Profile("local")
+
 // TODO when starting the app locally, don't send any emails, log then instead
 public class MailServiceLocalDummy implements MailService {
   public void sendEmail(String subject) {
