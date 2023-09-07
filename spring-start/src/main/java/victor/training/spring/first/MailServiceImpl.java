04c840be296e0f0acb9941cb74472789cacecf7f
Victor Rentea <victorrentea@gmail.com>
2023-09-07 12:25:46 +0300

    app fails to start because an unneeded prop is missing

--- spring-start/src/main/java/victor/training/spring/first/MailServiceImpl.java
+++ spring-start/src/main/java/victor/training/spring/first/MailServiceImpl.java
@@ -1,20 +1,30 @@
 package victor.training.spring.first;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Profile;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 @Service
 //@Profile("prod") // BAD PRACTICE: only to run in production env
 //@Profile("!local") // garbage only for local env dev
-@ConditionalOnMissingBean(MailServiceLocalDummy.class) // coupling to LOCAL env hacks
-@RequiredArgsConstructor
+//@ConditionalOnMissingBean(MailServiceLocalDummy.class) // coupling to LOCAL env hacks
 public class MailServiceImpl implements MailService {
+  @Value("${prod.stuff.only}")
+  private String cool;
+  public MailServiceImpl() {
+    System.out.println("Instantiated REAL");
+  }
   //  private final MailSender sender; // TODO uncomment and watch it failing because it requires properties to be auto-defined
-
+  @PostConstruct
+  public void init() {
+    System.out.println("init REAL");
+  }
   public void sendEmail(String body) {
     SimpleMailMessage message = new SimpleMailMessage();
     message.setFrom("noreply@victorrentea.ro");
--- spring-start/src/main/java/victor/training/spring/first/MailServiceLocalDummy.java
+++ spring-start/src/main/java/victor/training/spring/first/MailServiceLocalDummy.java
@@ -2,17 +2,27 @@ package victor.training.spring.first;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 @Service
 @Slf4j
 //@ConditionalOnProperty(value = "mail.sender.dummy",havingValue = "true")
 @Profile("local")
-
+@Primary
 // TODO when starting the app locally, don't send any emails, log then instead
 public class MailServiceLocalDummy implements MailService {
+  public MailServiceLocalDummy() {
+    System.out.println("Instantiated DUMMY");
+  }
+  @PostConstruct
+  public void init() {
+    System.out.println("init DUMMY");
+  }
   public void sendEmail(String subject) {
     System.out.println("DUMMY EMAIL SENDER sending an email with subject=" + subject);
   }
