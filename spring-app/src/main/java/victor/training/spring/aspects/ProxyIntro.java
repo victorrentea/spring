b9337c25e421ba7c492b6914374de9faab9fbb96
Victor Rentea <victorrentea@gmail.com>
2023-09-07 14:58:33 +0300

    EXCATCLY what spring does under the hood with its good old friend CGLIB

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -1,35 +1,56 @@
 package victor.training.spring.aspects;
 
 import lombok.extern.slf4j.Slf4j;
+import org.mapstruct.ap.internal.model.Decorator;
+import org.springframework.cglib.proxy.Callback;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 
 public class ProxyIntro {
     public static void main(String[] args) {
         // WE play the role of Spring here ...
-        Maths maths = new Decorator(new Maths());
-        SecondGrade secondGrade = new SecondGrade(maths);
+        Maths realInstance = new Maths();
+
+        Callback h = new MethodInterceptor() {
+            @Override
+            public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+                System.out.println(method.getName() + " with " + Arrays.toString(args));
+                Object r = method.invoke(realInstance, args);
+                System.out.println("returned " + r);
+                return r;
+            }
+        };
+        Maths proxy = (Maths) Enhancer.create(Maths.class, h);
+
+        SecondGrade secondGrade = new SecondGrade(proxy);
 
         secondGrade.mathClass();
     }
 }
-@Slf4j
-class Decorator extends Maths { // proxy instance is a SEPARATE instance from
-    // your real bean instance.
-    private final Maths delegate;
-    Decorator(Maths delegate) {
-        this.delegate = delegate;
-    }
-    @Override
-    public int sum(int a, int b) {
-        log.info("Calling sum(" + a + "," + b + ")");
-        return delegate.sum(a, b);
-    }
-    @Override
-    public int product(int a, int b) {
-        log.info("Calling product(" + a + "," + b + ")");
-        return delegate.product(a, b);
-    }
-}
+//@Slf4j
+//class Decorator extends Maths { // proxy instance is a SEPARATE instance from
+//    // your real bean instance.
+//    private final Maths delegate;
+//    Decorator(Maths delegate) {
+//        this.delegate = delegate;
+//    }
+//    @Override
+//    public int sum(int a, int b) {
+//        log.info("Calling sum(" + a + "," + b + ")");
+//        return delegate.sum(a, b);
+//    }
+//    @Override
+//    public int product(int a, int b) {
+//        log.info("Calling product(" + a + "," + b + ")");
+//        return delegate.product(a, b);
+//    }
+//}
+
 // TODO write code above this line to intercept and log args of any call that SecondGrade does on Maths
 // ------------------------
 class SecondGrade {
