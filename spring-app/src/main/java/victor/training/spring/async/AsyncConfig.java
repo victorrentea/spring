c8637220ce2f4a1f3a1235bf8182a2da6d12862e
Victor Rentea <victorrentea@gmail.com>
2023-09-08 10:09:42 +0300

    fire and forget

--- spring-app/src/main/java/victor/training/spring/async/AsyncConfig.java
+++ spring-app/src/main/java/victor/training/spring/async/AsyncConfig.java
@@ -2,6 +2,7 @@ package victor.training.spring.async;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -11,9 +12,9 @@ public class AsyncConfig {
 	@Bean
 	public ThreadPoolTaskExecutor executor() {
 		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(1);
-		executor.setMaxPoolSize(1);
-		executor.setQueueCapacity(500);
+		executor.setCorePoolSize(4); // minimum size
+		executor.setMaxPoolSize(4); // max how much it can extend under pressure
+		executor.setQueueCapacity(500); // how many tasks can be waiting at some point
 		executor.setThreadNamePrefix("bar-");
 		executor.initialize();
 		executor.setWaitForTasksToCompleteOnShutdown(true);
--- spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
+++ spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
@@ -1,20 +1,27 @@
 package victor.training.spring.async;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import victor.training.spring.async.drinks.Beer;
 import victor.training.spring.async.drinks.DillyDilly;
 import victor.training.spring.async.drinks.Vodka;
 
+import java.util.concurrent.Future;
+
 import static java.lang.System.currentTimeMillis;
 
+@RequiredArgsConstructor
 @Slf4j
 @RestController
 public class DrinkerController {
-   @Autowired
-   private Barman barman;
+   private final Barman barman;
+//   @Qualifier("executor") // ok
+   private final ThreadPoolTaskExecutor executor;
 
    // TODO [1] autowire and submit tasks to a ThreadPoolTaskExecutor
    // TODO [2] mark pour* methods as @Async
@@ -24,10 +31,16 @@ public class DrinkerController {
       log.debug("Submitting my order");
       long t0 = currentTimeMillis();
 
-      Beer beer = barman.pourBeer();
-      Vodka vodka = barman.pourVodka();
+      Future<Beer> futureBeer = executor.submit(() -> barman.pourBeer());
+      Future<Vodka> futureVodka = executor.submit(() -> barman.pourVodka());
 
-      barman.auditCocktail("Dilly");
+      Beer beer = futureBeer.get(); // the HTTP thread waits for 1 second here
+      Vodka vodka = futureVodka.get(); // the HTTP thread waits for 0 second here, vodka already poured
+
+      executor.submit(() -> barman.auditCocktail("Dilly"));
+      // "FIRE-AND-FORGET"
+      // dear biz, do we need to WAIT for AUDIT when preparing a drink? NO
+      // if an error occurs in audit, should we NOT give the drink to the guy? WRONG. give the drink anyway
 
       log.debug("Method completed in {} millis", currentTimeMillis() - t0);
       return new DillyDilly(beer, vodka);
