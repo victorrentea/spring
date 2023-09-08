8c3989d3dcfe60c1afaee13b64efc01656aad44e
Victor Rentea <victorrentea@gmail.com>
2023-09-08 10:27:54 +0300

    freed the tomcat thread and allow it to return to the pool to handle otjer HTTP requests

--- spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
+++ spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
@@ -2,8 +2,6 @@ package victor.training.spring.async;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +9,10 @@ import victor.training.spring.async.drinks.Beer;
 import victor.training.spring.async.drinks.DillyDilly;
 import victor.training.spring.async.drinks.Vodka;
 
-import java.util.concurrent.Future;
+import java.util.concurrent.CompletableFuture;
 
 import static java.lang.System.currentTimeMillis;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -27,15 +26,16 @@ public class DrinkerController {
    // TODO [2] mark pour* methods as @Async
    // TODO [3] Make this endpoint non-blocking
    @GetMapping("api/drink")
-   public DillyDilly drink() throws Exception {
+   public CompletableFuture<DillyDilly> drink() throws Exception {
       log.debug("Submitting my order");
       long t0 = currentTimeMillis();
 
-      Future<Beer> futureBeer = executor.submit(() -> barman.pourBeer());
-      Future<Vodka> futureVodka = executor.submit(() -> barman.pourVodka());
+      // promise (JS) === CompletableFuture (Java8+)
+      CompletableFuture<Beer> promiseBeer = supplyAsync(() -> barman.pourBeer());
+      CompletableFuture<Vodka> promiseVodka = supplyAsync(() -> barman.pourVodka());
 
-      Beer beer = futureBeer.get(); // the HTTP thread waits for 1 second here
-      Vodka vodka = futureVodka.get(); // the HTTP thread waits for 0 second here, vodka already poured
+      CompletableFuture<DillyDilly> promiseDilly = promiseBeer.thenCombineAsync(promiseVodka,
+          (beer, vodka) -> new DillyDilly(beer, vodka));
 
       executor.submit(() -> barman.auditCocktail("Dilly"));
       // "FIRE-AND-FORGET"
@@ -43,6 +43,6 @@ public class DrinkerController {
       // if an error occurs in audit, should we NOT give the drink to the guy? WRONG. give the drink anyway
 
       log.debug("Method completed in {} millis", currentTimeMillis() - t0);
-      return new DillyDilly(beer, vodka);
+      return promiseDilly;
    }
 }
