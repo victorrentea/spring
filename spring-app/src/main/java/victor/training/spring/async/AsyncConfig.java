29947b277f20c7f5231db75929be51f69e4d86ce
Victor Rentea <victorrentea@gmail.com>
2023-09-08 10:42:02 +0300

    parameterized th pool

--- spring-app/src/main/java/victor/training/spring/async/AsyncConfig.java
+++ spring-app/src/main/java/victor/training/spring/async/AsyncConfig.java
@@ -1,5 +1,6 @@
 package victor.training.spring.async;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -9,11 +10,13 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @EnableAsync
 @Configuration
 public class AsyncConfig {
+
+
 	@Bean
-	public ThreadPoolTaskExecutor executor() {
+	public ThreadPoolTaskExecutor executor(@Value("${bar.pool.size}") int poolSize) {
 		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(4); // minimum size
-		executor.setMaxPoolSize(4); // max how much it can extend under pressure
+		executor.setCorePoolSize(poolSize); // minimum size
+		executor.setMaxPoolSize(poolSize); // max how much it can extend under pressure
 		executor.setQueueCapacity(500); // how many tasks can be waiting at some point
 		executor.setThreadNamePrefix("bar-");
 		executor.initialize();
