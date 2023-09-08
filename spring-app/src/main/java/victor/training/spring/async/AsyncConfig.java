995a1f8324e4fbe0b71aa6fdffc2ca7b68d5a69d
Victor Rentea <victorrentea@gmail.com>
2023-09-08 10:43:10 +0300

    @@ConfigurationProperties on @Bean

--- spring-app/src/main/java/victor/training/spring/async/AsyncConfig.java
+++ spring-app/src/main/java/victor/training/spring/async/AsyncConfig.java
@@ -1,6 +1,7 @@
 package victor.training.spring.async;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -13,12 +14,13 @@ public class AsyncConfig {
 
 
 	@Bean
-	public ThreadPoolTaskExecutor executor(@Value("${bar.pool.size}") int poolSize) {
+	@ConfigurationProperties(prefix = "bar.pool")
+	public ThreadPoolTaskExecutor executor() {
 		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(poolSize); // minimum size
-		executor.setMaxPoolSize(poolSize); // max how much it can extend under pressure
+//		executor.setCorePoolSize(poolSize); // minimum size
+//		executor.setMaxPoolSize(poolSize); // max how much it can extend under pressure
 		executor.setQueueCapacity(500); // how many tasks can be waiting at some point
-		executor.setThreadNamePrefix("bar-");
+//		executor.setThreadNamePrefix("bar-");
 		executor.initialize();
 		executor.setWaitForTasksToCompleteOnShutdown(true);
 		return executor;
