43cc3227f234689e5b586f1f224e151cd7efd1c4
Victor Rentea <victorrentea@gmail.com>
2023-09-27 11:42:46 +0300

    merge din nou pt ca Lombok copiaza acum adnotarea dep e campul final pe param de ctor

--- spring-start/src/main/java/victor/training/spring/first/Config.java
+++ spring-start/src/main/java/victor/training/spring/first/Config.java
@@ -14,7 +14,7 @@ public class Config {
   // :default nu recomand pt ca vreau toate props prezente in config
   // nu vreau sa alerg prin cod sa vad ce pot customiza
 
-  @Value("${db.password}") //
+  @Value("${db.password}") // ca si cum ai pune asta pe param din ctor!
   private final String pass;
   private final X x;
 
