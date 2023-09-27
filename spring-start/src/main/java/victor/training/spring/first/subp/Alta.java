1631958231d24bff416f4db1469dc14b631ed09c
Victor Rentea <victorrentea@gmail.com>
2023-09-27 10:34:24 +0300

    cyclic dep = bad design

--- spring-start/src/main/java/victor/training/spring/first/subp/Alta.java
+++ spring-start/src/main/java/victor/training/spring/first/subp/Alta.java
@@ -1,11 +1,21 @@
 package victor.training.spring.first.subp;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
+import victor.training.spring.first.PreDI;
 
 @Component // cu de toate, eg un mapper
 //@Service // business logic: treburi de prin requirementuri
 //@Repository // Manual DB access, nu e necesar daca extinzi o interfata din Spring Data
+//@RequiredArgsConstructor
 public class Alta {
+
+  @Autowired
+//  @Lazy // de evitat
+  private PreDI preDI;
+
   public void f() {
     System.out.println("Sa mearga");
   }
