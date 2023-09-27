41c08907b9463504da84bb959511382ad36f9cc5
Victor Rentea <victorrentea@gmail.com>
2023-09-27 10:55:47 +0300

    Eventuri

--- spring-start/src/main/java/victor/training/spring/first/subp/Alta.java
+++ spring-start/src/main/java/victor/training/spring/first/subp/Alta.java
@@ -1,16 +1,20 @@
 package victor.training.spring.first.subp;
 
-import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import victor.training.spring.first.PreDI;
+
+import javax.annotation.PostConstruct;
 
 @Component // cu de toate, eg un mapper
 //@Service // business logic: treburi de prin requirementuri
 //@Repository // Manual DB access, nu e necesar daca extinzi o interfata din Spring Data
 //@RequiredArgsConstructor
-public class Alta {
+public class Alta implements CommandLineRunner {
 //  @Autowired
 ////  @Lazy // de evitat
 //  private PreDI preDI;
@@ -19,9 +23,38 @@ public class Alta {
     System.out.println("Sa mearga");
   }
 
+  @PostConstruct // nu poti insera in DB din el: pt ca ruleaza prea devreme in startup
   public void init() {
     System.out.println("verific un folder sa fie acolo");
+  }
+
+  @EventListener(ApplicationStartedEvent.class) // spring arunca o gramada de eventuri cand porneste
+  public void candToataAppEGata() {
     System.out.println("Iau din DB ceva eg lista de tari");
     System.out.println("dau un mail: m-am nascut!");
+    eventPublisher.publishEvent(new EventuMeu(1));
+  }
+
+  @Autowired
+  private ApplicationEventPublisher eventPublisher;
+
+  @Override
+  public void run(String... args) throws Exception { // da acces la args de command line
+    System.out.println("Pt batch joburi");
+  }
+}
+@Value
+class EventuMeu {
+  int id;
+}
+@Component
+class AltaClasaUndevaDeparte {
+  @EventListener
+  public void method(EventuMeu eventuMeu) {
+    System.out.println("Am primit event printr-un event bus in memory al SPring " + eventuMeu.getId());
+  }
+  @EventListener
+  public void sieu(EventuMeu eventuMeu) {
+    System.out.println("#sieu");
   }
 }
