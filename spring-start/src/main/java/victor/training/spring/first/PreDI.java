683774163ee8b9a57b98c4639898ab1231c1de67
Victor Rentea <victorrentea@gmail.com>
2023-09-27 10:28:34 +0300

    DI mechanisms

--- spring-start/src/main/java/victor/training/spring/first/PreDI.java
+++ spring-start/src/main/java/victor/training/spring/first/PreDI.java
@@ -1,5 +1,9 @@
 package victor.training.spring.first;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +14,25 @@ import victor.training.spring.first.subp.Alta;
 // sunt scanate automat la startup cf @ComponentScan
 @RestController // REST API
 //@Controller // .JSP JSF VAADIN GWT Freemarker Velocity > generau HTML pe server
+@RequiredArgsConstructor // ❤️ genereaza ctor cu toate campurile finale
+@Slf4j // ❤️ adauga un camp slf4j Logger 'log' ca mai jos
 public class PreDI {
-  @Autowired // Springule, injecteaza-mi aici o instanta d-aia
-  private Alta alta;
+//  private static final Logger log = LoggerFactory.getLogger(PreDI.class);
+
+//  @Autowired // field greu de injectat un mock din @Test
+//  private Alta alta;
+
+  private final Alta alta;
+
+//  public PreDI(Alta alta) { // constructor injection
+//    this.alta = alta;
+//  }
+
+  @Autowired
+  public void cuAlta(Alta alta) { // method/setter injection
+    log.info("Am primit alta: " + alta);
+  }
+
   @GetMapping("prima")
   public String method() {
     alta.f();
