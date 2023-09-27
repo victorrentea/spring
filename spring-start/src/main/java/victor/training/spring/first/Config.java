946564c66dbdb5d79f7bc8c882bddedb306d4ca9
Victor Rentea <victorrentea@gmail.com>
2023-09-27 11:23:17 +0300

    default value

--- spring-start/src/main/java/victor/training/spring/first/Config.java
+++ spring-start/src/main/java/victor/training/spring/first/Config.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Component;
 // TODO print DB pass in console at startup
 @Component
 public class Config {
-  @Value("${db.password:defaultVal}")
+//  @Value("${db.password:defaultVal}")
+  // :default nu recomand pt ca vreau toate props prezente in config
+  // nu vreau sa alerg prin cod sa vad ce pot customiza
+  @Value("${db.password}")
   private String pass;
 
   @EventListener(ApplicationStartedEvent.class)
