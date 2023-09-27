d01c87f5bd31940f3d0de691b1c092146a55e779
Victor Rentea <victorrentea@gmail.com>
2023-09-27 12:09:59 +0300

    modificarea config property = VALEU!!!!

---
+++ spring-start/src/main/java/victor/training/spring/first/CumSeFolosesteConfig.java
@@ -0,0 +1,22 @@
+package victor.training.spring.first;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@RestController
+@RequiredArgsConstructor
+public class CumSeFolosesteConfig {
+  private final WelcomeInfo welcomeInfo;
+  @GetMapping("hello")
+  public /*@Size(min = 5, max = 100) @NotNull*/ String method() {
+    /// intr-un colt al unei app de 100K de linii de cod (mare)
+    // cineva din greseala face:
+    welcomeInfo.setWelcomeMessage("oups!"); // cum previn
+
+    return welcomeInfo.getWelcomeMessage();
+  }
+}
