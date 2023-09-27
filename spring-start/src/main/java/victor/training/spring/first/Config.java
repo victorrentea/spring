4f5702e1b49d28169f32fef22608fd3f310b99e1
Victor Rentea <victorrentea@gmail.com>
2023-09-27 11:38:54 +0300

    final config

--- spring-start/src/main/java/victor/training/spring/first/Config.java
+++ spring-start/src/main/java/victor/training/spring/first/Config.java
@@ -11,8 +11,12 @@ public class Config {
 //  @Value("${db.password:defaultVal}")
   // :default nu recomand pt ca vreau toate props prezente in config
   // nu vreau sa alerg prin cod sa vad ce pot customiza
-  @Value("${db.password}")
-  private String pass;
+
+  private final String pass;
+
+  public Config(@Value("${db.password}") String pass) {
+    this.pass = pass;
+  }
 
   @EventListener(ApplicationStartedEvent.class)
   public void method() {
