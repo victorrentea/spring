855fc5ccedcd9998f46d4a9506ea72c30f5b8455
Victor Rentea <victorrentea@gmail.com>
2023-09-27 15:35:09 +0300

    Proxying mechanims under the hood (BASICS)

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -1,41 +1,66 @@
 package victor.training.spring.aspects;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Service;
+import java.lang.reflect.Method;
 
 public class ProxyIntro {
-    public static void main(String[] args) {
-        // WE play the role of Spring here ...
-        Maths maths = new Maths();
-        SecondGrade secondGrade = new SecondGrade(maths);
+  public static void main(String[] args) {
+    // WE play the role of Spring here ...
+    Maths reala = new Maths();
+    Maths maths = new UnaCareSaOExtinda(reala);
+    // scritzi aci <<
 
-        secondGrade.mathClass();
-    }
+    SecondGrade secondGrade = new SecondGrade(maths);
+
+    secondGrade.mathClass();
+  }
 }
-// ------------------------
-@Service
+//Exact asa face springu!: iti ia clasa ta marcata cu eg @service si o subclaseaza,
+// @Overridand toate metodele publice.
+// Ulterior, oricine va avea nevoie sa fie injectat cu un Maths, va primi o instanta din aceasta subclasa
+class UnaCareSaOExtinda extends Maths {
+  private final Maths delegate;
+
+  public UnaCareSaOExtinda(Maths delegate) {
+    this.delegate = delegate;
+  } // DECORATOR PATTERN
+  @Override
+  public int sum(int a, int b) {
+    int r = delegate.sum(a, b);
+    System.out.println("sum("+a+","+b+")="+r);
+    return r;
+  }
+
+  @Override
+  public int product(int a, int b) {
+    int r = delegate.product(a, b);
+    System.out.println("produs("+a+","+b+")="+r);
+    return r;
+  }
+}
+// sa pp ca Emma are un tatic paranoic care vrea sa vada toate calculele facute la scoale sa le verifice/auditeze
+// taticu nu vrea sa se prinda copilul
+// TODO interceptezi orice apel facut de SecondGrade la orice metoda din Maths
+//  sa printezi param si return value (pt audit de tatic)
+// ------------------------ NU AI VOIE SA SCRII NIMIC SUB LINIA ASTA
 class SecondGrade {
-    private final Maths maths;
-    SecondGrade(Maths maths) {
-        this.maths = maths;
-    }
-
-    public void mathClass() {
-        System.out.println("8 + 4 = " + maths.sum(8, 4));
-        System.out.println("6 + 6 = " + maths.sum(6, 6));
-        System.out.println("4 x 3 = " + maths.product(4, 3));
-    }
+  private final Maths maths;
+  public SecondGrade(Maths maths) {
+    this.maths = maths;
+  }
+  public void mathClass() {
+    System.out.println("8 + 4 = " + maths.sum(8, 4));
+    System.out.println("6 + 6 = " + maths.sum(6, 6));
+    System.out.println("4 x 3 = " + maths.product(4, 3));
+  }
 }
 
-@Facade
 class Maths {
-    public int sum(int a, int b) {
-        return a + b;
-    }
-
-    public int product(int a, int b) {
-        return a * b;
-    }
+  public int sum(int a, int b) {
+    return a + b;
+  }
+  public int product(int a, int b) {
+    return a * b;
+  }
 }
 
 
