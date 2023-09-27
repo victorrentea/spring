77fd4cbb55a56e0bcaf3b5c1477e855a5414b744
Victor Rentea <victorrentea@gmail.com>
2023-09-27 12:01:12 +0300

    validari pe @ConfigurationProperties

--- spring-start/src/main/java/victor/training/spring/first/WelcomeInfo.java
+++ spring-start/src/main/java/victor/training/spring/first/WelcomeInfo.java
@@ -7,8 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 import javax.annotation.PostConstruct;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.io.File;
 import java.net.URL;
 import java.util.List;
@@ -19,9 +23,16 @@ import java.util.Map;
 @Data // Lombok generates getters + setters
 @Component
 @ConfigurationProperties(prefix = "welcome")
+@Validated
 public class WelcomeInfo {
   int gate;
+//  @Email
+//  @Pattern(regexp = "\\d+")
+  @Size(min = 5, max = 100)
+  @NotNull // adnotarea asta singura nu face nimic. trebuie ca altcineva sa zica @Validated pe instanta asta
   String welcomeMessage; // TODO 4a validate is not null and size >= 4
+  @Size(min = 1)
+  @NotNull // mereu dupa Size la brat
   List<URL> supportUrls; // TODO 4b validate list contains at least 1 element
   Map<Locale, String> localContactPhone;
   HelpInfo help;
