08fb5afb3eced7ef309c901786fd73480ec19e1d
Victor Rentea <victorrentea@gmail.com>
2023-09-28 15:45:29 +0300

    prima solutie paralelizand beer cu vodka

--- spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
+++ spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
@@ -8,7 +8,11 @@ import victor.training.spring.async.drinks.Beer;
 import victor.training.spring.async.drinks.DillyDilly;
 import victor.training.spring.async.drinks.Vodka;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+
 import static java.lang.System.currentTimeMillis;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 @Slf4j
 @RestController
@@ -24,11 +28,20 @@ public class DrinkerController {
       log.debug("Submitting my order");
       long t0 = currentTimeMillis();
 
-      Beer beer = barman.pourBeer();
-      Vodka vodka = barman.pourVodka();
+      // TODO optimizati viteza
+      //  new Thread(new Runnable // niciodata
+      //  ExecutorService.... // interzis in Spring
+      // CompletableFuture === promise din FE
+      CompletableFuture<Beer> beerPromise = supplyAsync(() -> barman.pourBeer());
+      CompletableFuture<Vodka> vodkaPromise = supplyAsync(() -> barman.pourVodka());
+
+      // pe CF nu prea da bine faci .get
+      Beer beer = beerPromise.get(); //
+      Vodka vodka = vodkaPromise.get();
 
       barman.auditCocktail("Dilly");
 
+      // TODO daca nu mai e bere ?!! Andreea
       log.debug("Method completed in {} millis", currentTimeMillis() - t0);
       return new DillyDilly(beer, vodka);
    }
