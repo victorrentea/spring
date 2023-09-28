8e311780a576bd4c907d5bd6eebe4aae6fdfb41f
Victor Rentea <victorrentea@gmail.com>
2023-09-28 16:49:52 +0300

    teste tata!

--- demo/src/test/java/com/example/demo/LocalProfileTests.java
+++ demo/src/test/java/com/example/demo/LocalProfileTests.java
@@ -4,13 +4,16 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class DemoApplicationTests {
+@ActiveProfiles("local")
+class LocalProfileTests {
 	@Autowired
 	ReservationRepo reservationRepo;
 	@Test
-	void noDummyDataIsInserted() {
-		Assertions.assertThat(reservationRepo.count()).isZero();
+	void contextLoads() {
+		// din pachetul assertj
+		Assertions.assertThat(reservationRepo.count()).isNotZero();
 	}
 }
