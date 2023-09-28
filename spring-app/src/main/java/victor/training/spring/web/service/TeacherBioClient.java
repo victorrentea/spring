df6364e61d34df3ccc665212ecd575e24f62e7c6
Victor Rentea <victorrentea@gmail.com>
2023-09-28 14:40:27 +0300

    RestTemplate

--- spring-app/src/main/java/victor/training/spring/web/service/TeacherBioClient.java
+++ spring-app/src/main/java/victor/training/spring/web/service/TeacherBioClient.java
@@ -4,23 +4,12 @@ import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.keycloak.KeycloakPrincipal;
-import org.keycloak.KeycloakSecurityContext;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
 import victor.training.spring.varie.ThreadUtils;
 
 import java.net.URI;
-import java.util.List;
-import java.util.Map;
-
-import static java.lang.System.currentTimeMillis;
 
 @RequiredArgsConstructor
 @Component
@@ -34,7 +23,7 @@ public class TeacherBioClient {
   private final TeacherBioFeignClient feignClient;
 
   // don't do "new RestTemplate()" but take it from Spring, to allow Sleuth to propagate 'traceId'
-  private final RestTemplate rest;
+  private final RestTemplate restTemplate;
 
 
   // TODO cacheable
@@ -66,12 +55,12 @@ public class TeacherBioClient {
 
   @SneakyThrows
   public String callUsingRestTemplate(long teacherId) {
-    Map<String, List<String>> header = Map.of();// Map.of("Authorization", List.of("Bearer " + accessToken));
-    ResponseEntity<String> response = rest.exchange(new RequestEntity<>(
-            CollectionUtils.toMultiValueMap(header),
-            HttpMethod.GET,
-            new URI(teacherBioUriBase + "/api/teachers/" + teacherId + "/bio")), String.class);
-    return response.getBody();
+//    ResponseEntity<String> response = rest.exchange(new RequestEntity<>(
+//            HttpMethod.GET,
+//            new URI(teacherBioUriBase + "/api/teachers/" + teacherId + "/bio")), String.class);
+    String response = restTemplate.getForObject(
+            new URI(teacherBioUriBase + "/api/teachers/" + teacherId + "/bio"), String.class);
+    return response;
   }
 
 }
