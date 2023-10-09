3aeb812a9dc3d2288b8f727cf55a66d972bf0e1b
Victor Rentea <victorrentea@gmail.com>
2023-10-09 10:18:30 +0300

    springu cauta in pachetul in care ai pus @SpringBootApplicaation si orice subpachet clasele tale adnotate cu @Component & friends.

--- spring-start/src/main/java/victor/training/spring/Y.java
+++ spring-start/src/main/java/victor/training/spring/Y.java
@@ -1,4 +1,4 @@
-package victor.training.spring.first;
+package victor.training.spring;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,12 +6,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+import victor.training.spring.first.MailService;
 
 @Service
 public class Y {
-  @Autowired
-//  @Lazy
-  private  X x;
   @Qualifier("mailServiceImpl")
   private final MailService mailService; // polymorphic injection
 //  @Value("${welcome.welcomeMessage}") // inject this from the configuration files
