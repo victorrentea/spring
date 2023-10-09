22f08ff29bcd3f89a6690b160273bd2449501047
Victor Rentea <victorrentea@gmail.com>
2023-10-09 10:37:53 +0300

    @EventListener pt eventuri de startup de spring sau pt eventurile mele.

--- spring-start/src/main/java/victor/training/spring/first/ClasaNoua.java
+++ spring-start/src/main/java/victor/training/spring/first/ClasaNoua.java
@@ -1,7 +1,9 @@
 package victor.training.spring.first;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -17,13 +19,30 @@ public class ClasaNoua implements CommandLineRunner {
     System.out.println("Hello Spring!");
   }
 
-  @EventListener(ApplicationStartedEvent.class)
-  public void eventHandler() {
-    System.out.println("Event la startup - pot accesa orice alt bean, totul merge deja");
-  }
-
   @Override
   public void run(String... args) throws Exception {
     System.out.println("Bun cand startezi joburi spring din command line ");
+  }
+
+  @Autowired
+  ApplicationEventPublisher eventPublisher;
+  @EventListener(ApplicationStartedEvent.class)
+  public void eventHandler() {
+    System.out.println("Event la startup - pot accesa orice alt bean, totul merge deja");
+    eventPublisher.publishEvent(new EventuMeu(12));
+  }
+}
+record EventuMeu(int treaba) {}
+//spring poate sa-ti care eventurile TALE de colo colo
+
+@Component
+class PesteMariSiTari {
+  @EventListener
+  public void method(EventuMeu eventuMeu) {
+    System.out.println("Uite ce-a venit de la spring:" + eventuMeu);
+  }
+  @EventListener
+  public void method2(EventuMeu eventuMeu) {
+    System.out.println("Din nou:" + eventuMeu);
   }
 }
