d4d9e78b7a067716f08eb2936c1a8044e4e1afc8
Victor Rentea <victorrentea@gmail.com>
2023-10-09 10:02:13 +0300

    cum declaram beanuri cu adnotari pe clasa

--- spring-start/src/main/java/victor/training/spring/first/X.java
+++ spring-start/src/main/java/victor/training/spring/first/X.java
@@ -1,9 +1,21 @@
 package victor.training.spring.first;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service // TODO what other annotation register this class as a bean
+// declari aceasta clasa ca Bean in spring, o va instantia O DATA
+//@Service // = business logic  BR-4581
+//@Repository // access la DB a ta
+//@RestController // @GetMapping REST
+//@Controller // = HTML de pe server cu .jsp .jsf vaadin thymeleaf
+@Component // cand nimic de sus nu merge. aka gunoi/util
+//@SpringBootApplication
 public class X {
   @Autowired
   private Y y; // #2 field injection
