6bf4dfa7257976f9fedd18c28c446fbac88b0dc1
Victor Rentea <victorrentea@gmail.com>
2023-10-09 10:15:32 +0300

    cyclic deps are a code smell

--- spring-start/src/main/java/victor/training/spring/first/Y.java
+++ spring-start/src/main/java/victor/training/spring/first/Y.java
@@ -1,12 +1,17 @@
 package victor.training.spring.first;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Y {
+  @Autowired
+//  @Lazy
+  private  X x;
   @Qualifier("mailServiceImpl")
   private final MailService mailService; // polymorphic injection
 //  @Value("${welcome.welcomeMessage}") // inject this from the configuration files
