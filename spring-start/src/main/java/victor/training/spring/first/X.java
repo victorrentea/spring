38a19d1d3103516bd4a2360a4aedb293ae09ed3c
Victor Rentea <victorrentea@gmail.com>
2023-10-09 10:23:11 +0300

    custom annotation

--- spring-start/src/main/java/victor/training/spring/first/X.java
+++ spring-start/src/main/java/victor/training/spring/first/X.java
@@ -5,15 +5,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import victor.training.spring.first.subp.Y;
 
+import java.lang.annotation.Retention;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 // declari aceasta clasa ca Bean in spring, o va instantia O DATA
 //@Service // = business logic  BR-4581
 //@Repository // access la DB a ta
 //@RestController // @GetMapping REST
 //@Controller // = HTML de pe server cu .jsp .jsf vaadin thymeleaf
 //@SpringBootApplication
+//@Facade
 
 @Slf4j
-@Component // cand nimic de sus nu merge. aka gunoi/util
+@Component
 @RequiredArgsConstructor // lombok = dovada ca java e '95
 public class X {
   private final Y y;
@@ -22,3 +27,7 @@ public class X {
     return 1 + y.logic();
   }
 }
+
+@Component
+@Retention(RUNTIME) // stops javac from removing it at compilation
+@interface Facade {}
