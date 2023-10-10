384f69cc1acdc66cc50eebef7cfcbd66726c56d4
Victor Rentea <victorrentea@gmail.com>
2023-10-10 12:50:35 +0300

    programatic Tx control

--- spring-transaction/src/main/java/victor/training/spring/transaction/playground/PlaygroundJpa.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/playground/PlaygroundJpa.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -17,6 +18,11 @@ public class PlaygroundJpa {
 
   @Transactional
   public void transactionOne()  {
+//    TransactionTemplate tx;
+//    tx.executeWithoutResult(s-> {
+//      insert
+//      insert
+//    });
     repo.save(new Message("Ceva"));  //INSERT
     repo.saveAndFlush(new Message("Ceva")); //INSERT
     // fortezi un flush mai devreme: cu..
@@ -25,10 +31,10 @@ public class PlaygroundJpa {
 //    System.out.println(repo.findByMessage("Doi"));// orice SELECT in DB hibernate il precedeaza cu un FLUSH (scrie iN DB tot ce mai avea de scris)
     System.out.println("Ies din functie: rabbit.send(), kafka.send() --- ");
   }
+  @Transactional
   public void transactionTwo() {
     Message message = repo.findById(1L).orElseThrow();
     message.setMessage("altu"); //SOC: modificarea unei @Entity in cadrul unei metode @Transactional ajunge automat UPDATE in DB
-    repo.save(message);
   }
 }
 @Service
