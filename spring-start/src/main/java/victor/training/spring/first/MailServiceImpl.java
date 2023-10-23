e8a10537388d051aa9b4929f61f3f757435f4b2a
Victor Rentea <victorrentea@gmail.com>
2023-10-23 11:31:34 +0300

    circular dependency

--- spring-start/src/main/java/victor/training/spring/first/MailServiceImpl.java
+++ spring-start/src/main/java/victor/training/spring/first/MailServiceImpl.java
@@ -1,7 +1,11 @@
 package victor.training.spring.first;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +15,12 @@ import org.springframework.stereotype.Service;
 @Service
 //@Profile("!local") // pollution
 @RequiredArgsConstructor
+@ConditionalOnProperty(name = "some.prop")
+//@ConditionalOnMissingBean( MailServiceLocalDummy.class)
 public class MailServiceImpl implements MailService {
-  //  private final MailSender sender; // TODO uncomment and watch it failing because it requires properties to be auto-defined
+  private final MailSender sender; // TODO uncomment and watch it failing because it requires properties to be auto-defined
+  @Value("${some.prop}")
+  private String prop;
 
   public void sendEmail(String body) {
     SimpleMailMessage message = new SimpleMailMessage();
