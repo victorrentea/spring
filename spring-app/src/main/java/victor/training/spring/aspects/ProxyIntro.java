e86cc221383b7cc9af25c47461c8ccdb6711e9ef
Victor Rentea <victorrentea@gmail.com>
2023-10-23 15:16:19 +0300

    proxyies without spring

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -1,30 +1,48 @@
 package victor.training.spring.aspects;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Service;
-
 public class ProxyIntro {
     public static void main(String[] args) {
         // WE play the role of Spring here ...
-        Maths maths = new Maths();
-        SecondGrade secondGrade = new SecondGrade(maths);
+        Maths real = new Maths();
+        Maths proxy = new MyHack(real);
+        SecondGrade secondGrade = new SecondGrade(proxy);
 
         secondGrade.mathClass();
     }
 }
+class MyHack extends Maths {
+    private final Maths delegate;
+    MyHack(Maths delegate) {
+        this.delegate = delegate;
+    }
+    @Override
+    public int sum(int a, int b) {
+        System.out.println("Calling sum with " + a + " " + b);
+        return delegate.sum(a, b);
+    }
+
+    @Override
+    public int product(int a, int b) {
+        System.out.println("Calling product with " + a + " " + b);
+        return delegate.product(a, b);
+    }
+}
+// CR: the father wants to audit all the method calls (method call+params) to Maths!!!! to check for errors.
+// restriction: don't edit code below the following line
+
 // ------------------------
 class SecondGrade {
     private final Maths maths;
-    SecondGrade(Maths maths) {
+    public SecondGrade(Maths maths) {
         this.maths = maths;
     }
-
     public void mathClass() {
         System.out.println("8 + 4 = " + maths.sum(8, 4));
         System.out.println("6 + 6 = " + maths.sum(6, 6));
         System.out.println("4 x 3 = " + maths.product(4, 3));
     }
 }
+
 class Maths {
     public int sum(int a, int b) {
         return a + b;
