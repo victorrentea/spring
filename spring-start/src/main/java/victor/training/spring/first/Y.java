b9040e334d2f7d5431c11accdbfa6c01a70bcd54
Victor Rentea <victorrentea@gmail.com>
2023-10-23 11:29:50 +0300

    circular dependency

--- spring-start/src/main/java/victor/training/spring/first/Y.java
+++ spring-start/src/main/java/victor/training/spring/first/Y.java
@@ -1,14 +1,19 @@
 package victor.training.spring.first;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import victor.training.spring.first.pack.X;
 
 @Service
 public class Y {
 //  @Value("${welcome.welcomeMessage}") // inject this from the configuration files
   private final String message = "HALO";
+
+  @Autowired
+  private X x;
 
   // (recommended) constructor injection => 😏 replace with @RequiredArgsConstructor
 }
--- spring-start/src/main/java/victor/training/spring/first/pack/X.java
+++ spring-start/src/main/java/victor/training/spring/first/pack/X.java
@@ -2,6 +2,9 @@ package victor.training.spring.first.pack;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 import victor.training.spring.first.MailService;
 import victor.training.spring.first.Y;
@@ -22,18 +25,22 @@ import javax.annotation.PostConstruct;
 @Component // = everything else
 //@RequiredArgsConstructor // makes javac generate the constructor in the .class file
 @Slf4j
+//@Lazy
 public class X {
   @Autowired
   private Y y;
   @Autowired
   private MailService mailService;
 
+//  @Value("${some.inexisting.prop}")
+//  private String crashAtRuntime;
+
   @PostConstruct
   public void init() {
     System.out.println("What was I injected: " + mailService);
   }
 
-  @Autowired
+  @Autowired // at startup
   public void init(Y y, Y y2) {
     // only when you want to DO stuff with those dependencies, not just store them as fields
     System.out.println("Y: " + y);
