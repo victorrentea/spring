404a3ec68e2e8d17e564a3db73b9956d654d1b7f
Victor Rentea <victorrentea@gmail.com>
2023-10-23 12:33:07 +0300

    event listeners

--- spring-start/src/main/java/victor/training/spring/first/FirstApplication.java
+++ spring-start/src/main/java/victor/training/spring/first/FirstApplication.java
@@ -1,5 +1,11 @@
 package victor.training.spring.first;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Component;
 import victor.training.spring.first.pack.X;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -24,6 +30,7 @@ import org.springframework.context.event.EventListener;
 
 // - @Value(${}) + @ConfigurationProperties
 @SpringBootApplication
+@EnableAsync
 //@ComponentScan(basePackages = {
 //    "victor.training.spring.first",
 //    "another.pack"})
@@ -32,6 +39,7 @@ import org.springframework.context.event.EventListener;
 //    X.class
 //}) // we want control what classes we define as bean
 
+@Slf4j
 public class FirstApplication implements CommandLineRunner {
   public static void main(String[] args) {
     SpringApplication.run(FirstApplication.class);
@@ -42,12 +50,45 @@ public class FirstApplication implements CommandLineRunner {
 
   @Override // from CommandLineRunner
   public void run(String... args) {
-    System.out.println(x.logic());
+    log.info(" " + x.logic());
   }
 
+//  @Transactional // this opens a tx on the current thread
   @EventListener(ApplicationReadyEvent.class)
   public void onAppStart() {
-    System.out.println("App started OK 🎉");
+    log.info("App started OK 🎉");
+    // those listnres will share the same tx with publisher
+    eventPublisher.publishEvent(new MyEvent(87665));
+  }
+  @Autowired
+  private ApplicationEventPublisher eventPublisher;
+}
+record MyEvent(int data) {} //my event
+//- ------------- boundary
+
+@Slf4j
+@Component
+class AnotherClassInADifferentModuleOfMyModulutih {
+  @EventListener
+//  @Order(200) // DONT DO THIS; listeners should not matter in what order they run
+  @Async//("virtThrExe") // DONT makes the event run on a separate thread; no errors come back in publisher. publisher doesn't have to wait.
+  // listeners may have to wait their turn in a waiting queue to get to run = volatile. Imagine a server crash
+     /// ==> if you want "persistent @Async" => send yourself a durable message over a Q (eg rabbit)
+
+  //@Bulkhead ()//restrict max 20 executions in parallel
+
+  //  @TransactionalEventListener(AFTER_COMMIT) // runs after the COMMIT of the tx in which the event was published
+  public void onSomethingInteresting(MyEvent event) {
+    log.info("Runs via an event listener rabbit.send(), email.send('success..') " + event.data());
+
   }
 }
-
+@Slf4j
+@Component
+class MeTooo {
+  @EventListener
+//  @Order(100)
+  public void onSomethingInteresting(MyEvent event) {
+    log.info("Bis " + event.data());
+  }
+}
--- spring-start/src/main/java/victor/training/spring/first/Y.java
+++ spring-start/src/main/java/victor/training/spring/first/Y.java
@@ -2,9 +2,13 @@ package victor.training.spring.first;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import victor.training.spring.first.pack.X;
+
+import javax.annotation.PostConstruct;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +27,19 @@ public class Y {
 //  public Y(@Lazy X x) {
 //    this.x = x;
 //  }
+
+  @Value("${db.password}")
+  private final String dbPass;
+
+  @PostConstruct
+  public void postConstruct() {
+    System.out.println("Too Early sometimes: dbpass: " + dbPass);
+  }
+
+  @EventListener(ApplicationStartedEvent.class) // listening for an even thrown by spring during startup
+  public void atAppStartup() {
+    System.out.println("at startup dbpass: " + dbPass);
+  }
 
   // (recommended) constructor injection => 😏 replace with @RequiredArgsConstructor
 }
