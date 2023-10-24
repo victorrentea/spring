8708063c043081e3b6ab89c886b70a31d01b82dd
Victor Rentea <victorrentea@gmail.com>
2023-10-24 10:56:48 +0300

    moved to completable future to NOT block tomcat's thread in high-RPS systems to avoid thread pool starvation

--- spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
+++ spring-app/src/main/java/victor/training/spring/async/DrinkerController.java
@@ -10,9 +10,7 @@ import victor.training.spring.async.drinks.Beer;
 import victor.training.spring.async.drinks.DillyDilly;
 import victor.training.spring.async.drinks.Vodka;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.CompletableFuture;
 
 import static java.lang.System.currentTimeMillis;
 
@@ -31,31 +29,26 @@ public class DrinkerController {
    // TODO [2] mark pour* methods as @Async
    // TODO [3] Make this endpoint non-blocking
    @GetMapping("api/drink")
-   public DillyDilly drink() throws Exception {
+   public CompletableFuture<DillyDilly> drink() throws Exception {
       log.debug("Submitting my order " + restTemplate.getClass());
       long t0 = currentTimeMillis();
-
       // How to lose the TraceID on outbound request/message:
       // Pitfall#1:
       //new RestTemplate().getForObject()// this call does not carry the request header!!! instead use a Spring-injected RestTemplate/WebClient/RabbitTemplate...
-
 //      restTemplate.getForObject// the http request will carry automatcally the TraceId to all other microservices that I call
 //      rabbitTemplate.send(message// the message will carry automatcally the TraceId to all other microservices that I call
-
       // Pitfall#2: move to another thread without Spring awareness (using a custom-made executor/thread)
 //      ExecutorService threadPool = Executors.newFixedThreadPool(3); // DO NOT EVER USE Executors in a Spring application
-
-      Future<Beer> beerF = executor.submit(()->barman.pourBeer());
-      Future<Vodka> vodkaF = executor.submit(()->barman.pourVodka());
+      CompletableFuture<Beer> beerPromise = CompletableFuture.supplyAsync(()->barman.pourBeer());
+      CompletableFuture<Vodka> vodkaPromise = CompletableFuture.supplyAsync(()->barman.pourVodka());
 
       executor.submit(()->barman.auditCocktail("Dilly"));// fire-and-forget call: start but never wait to complete/throw
 
-      Beer beer = beerF.get();
-      Vodka vodka = vodkaF.get();
-      DillyDilly dillyDilly = new DillyDilly(beer, vodka);
+      CompletableFuture<DillyDilly> dillyPromise = beerPromise.thenCombine(vodkaPromise,
+          (beer, vodka) -> new DillyDilly(beer, vodka));
 
       log.debug("Method completed in {} millis", currentTimeMillis() - t0);
-      return dillyDilly;
+      return dillyPromise;
    }
 
 }
