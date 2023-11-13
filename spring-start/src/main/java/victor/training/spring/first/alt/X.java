71bfb028aad5dee88e1c84025d0894966381680d
Victor Rentea <victorrentea@gmail.com>
2023-11-13 10:26:58 +0200

    Injection styles

--- spring-start/src/main/java/victor/training/spring/first/alt/X.java
+++ spring-start/src/main/java/victor/training/spring/first/alt/X.java
@@ -1,6 +1,11 @@
 package victor.training.spring.first.alt;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
@@ -17,12 +22,35 @@ import victor.training.spring.first.Y;
 //@Service // business logic
 //@Repository // clasa persista date intr-o DB (SQL, Mongo,...)
 
+@Slf4j // genreaza un private static final Logger log = LoggerFactory.getLogger(X.class);
 @Component // pe orice altceva (chestii tehnice)
-// TODO Defining Beans (whose life is managed by Spring Container)
+@RequiredArgsConstructor // ii spune lui lombok sa genreze ctor pt toate campurile finale
 public class X {
+
+  // TODO Dependency Injection:
+// // Springule, cauta prin componentele existente una compatibila si daca gaseste,
+//  // seteaza acest camp la o referinta la acel obiect.
+//  // Spring/Hibernate nu au nici o problema cu a scrie/citi din campuri private
+//  @Autowired
+//  private Y y; // injection point
+  // field injection e descurajat pt ca
+  // 1. un camp privat nu poate fi setat manual din @Teste = Mockito.mock(Y.class);
+  //      => @InjectMocks
+  //      => reflection din teste ReflectionUtils.setField("y", mock(Y.class));
+  // 2. promoveaza campuri ne-finale
+
+  private final Y y; // constructor-injection
+
+//  public X(Y y) { // "injection point" este contructor param
+//    this.y = y;
+//  }// din teste new X(mock(Y.class))
+
+  // method injection: folositi cand nu vrei doar sa stochezi dependinta,
+  // ci sa EXECUTI ceva cu ea la startup
   @Autowired
-  private Y y;
-  // TODO Injection
+  public void orice(Y y2, Y y3) {
+    System.out.println("Chemata la startup de Spring cu dep dorite " + y2);
+  }
 
   public int logic() {
     return 1 + y.logic();
