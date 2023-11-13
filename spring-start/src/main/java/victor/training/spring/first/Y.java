2b9a3b8d65a97092e2ae42e96144f033339c6fe5
Victor Rentea <victorrentea@gmail.com>
2023-11-13 11:26:58 +0200

    dependency retrieve programatic prin ApplicationContext - nu folosi

--- spring-start/src/main/java/victor/training/spring/first/Y.java
+++ spring-start/src/main/java/victor/training/spring/first/Y.java
@@ -2,6 +2,7 @@ package victor.training.spring.first;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,14 +15,22 @@ public class Y {
   @Autowired
   //@Qualifier("gigel") <- nu mai e necesar, poti doar sa numesti punctul de inejctie cum trebuie
   private PersonService gigel;
+  // DI crapa la startup ❤️❤️❤️❤️❤️
 
   // (recommended) constructor injection => 😏 replace with @RequiredArgsConstructor
   public Y(MailService mailService) {
     this.mailService = mailService;
   }
 
+  @Autowired
+  private ApplicationContext applicationContext;
+
   public int logic() {
     mailService.sendEmail("I like 4 topics : " + message);
+    // risk: s-a schimbat numele crapa
+    // crapa abia la callul metodei, potential la 2 zile dupa punerea in prod
+//    var x = applicationContext.getBean("gigel");
+//    var y = applicationContext.getBean("gigel",PersonService.class);
     gigel.method();
     return 1;
   }
