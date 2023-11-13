8cd7ad07ba945cc5570cfd5f8faee52828035397
Victor Rentea <victorrentea@gmail.com>
2023-11-13 15:45:02 +0200

    log peste tot

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -1,35 +1,38 @@
 package victor.training.spring.aspects;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Service;
-
 public class ProxyIntro {
-    public static void main(String[] args) {
-        Maths maths = new Maths();
-        SecondGrade secondGrade = new SecondGrade(maths);
+  public static void main(String[] args) {
+    Maths maths = new Maths();
+    SecondGrade secondGrade = new SecondGrade(maths);
 
-        secondGrade.mathClass();
-    }
+    secondGrade.mathClass();
+  }
 }
+
 // ------------------------
 class SecondGrade {
-    private final Maths maths;
-    SecondGrade(Maths maths) {
-        this.maths = maths;
-    }
+  private final Maths maths;
 
-    public void mathClass() {
-        System.out.println("8 + 4 = " + maths.sum(8, 4));
-        System.out.println("6 + 6 = " + maths.sum(6, 6));
-        System.out.println("4 x 3 = " + maths.product(4, 3));
-    }
+  SecondGrade(Maths maths) {
+    this.maths = maths;
+  }
+
+  public void mathClass() {
+    System.out.println("8 + 4 = " + maths.sum(8, 4));
+    System.out.println("6 + 6 = " + maths.sum(6, 6));
+    System.out.println("4 x 3 = " + maths.product(4, 3));
+  }
 }
-class Maths {
-    public int sum(int a, int b) {
-        return a + b;
-    }
 
-    public int product(int a, int b) {
-        return a * b;
-    }
+class Maths {
+  // TODO sa logam orice operatie face Maths
+  public int sum(int a, int b) {
+    System.out.println("sum(" + a + "," + b + ")");
+    return a + b;
+  }
+
+  public int product(int a, int b) {
+    System.out.println("product(" + a + "," + b + ")");
+    return a * b;
+  }
 }
