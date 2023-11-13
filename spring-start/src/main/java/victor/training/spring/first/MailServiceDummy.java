3dcaca3ac00aee810a21ce6dea305a5c3c6a0518
Victor Rentea <victorrentea@gmail.com>
2023-11-13 11:36:50 +0200

    @Profile pentru a zice springului pe ce mediu porneste app. primu draft

--- spring-start/src/main/java/victor/training/spring/first/MailServiceDummy.java
+++ spring-start/src/main/java/victor/training/spring/first/MailServiceDummy.java
@@ -1,9 +1,12 @@
 package victor.training.spring.first;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
-// nu e bean pentru spring
+@Component
 @Slf4j
+@Profile("local")
 // TODO use solely on local dev env
 public class MailServiceDummy implements MailService {
   public void sendEmail(String subject) {
