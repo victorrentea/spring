cffecce8f831208c07d99c7a7696eb0ba975d301
Victor Rentea <victorrentea@gmail.com>
2023-11-13 16:02:32 +0200

    Am folosit Enhancer (o lib de-a springului) sa-mi generez un class-proxy pe care orice metoda chemi mi-am scris un handler generic de methodcall 'h' care delega si la metoda reala

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -1,32 +1,48 @@
 package victor.training.spring.aspects;
 
+import org.springframework.cglib.proxy.Callback;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class ProxyIntro {
   public static void main(String[] args) {
     Maths real = new Maths();
-    Maths proxy = new MathsProxy(real);
+    Callback h = new MethodInterceptor() {
+      @Override
+      public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+        Object r = method.invoke(real, args);
+        System.out.println("Call intercepted: " + method.getName() + Arrays.toString(args)  + "="+ r);
+        return r;
+      }
+    };
+    Maths proxy = (Maths) Enhancer.create(Maths.class, h);
     SecondGrade secondGrade = new SecondGrade(proxy);
 
     secondGrade.mathClass();
   }
 }
-class MathsProxy extends Maths {
-  private final Maths delegate;
-  MathsProxy(Maths delegate) {
-    this.delegate = delegate;
-  }
-  @Override
-  public int sum(int a, int b) {
-    int r = delegate.sum(a, b);
-    System.out.println("sum(" + a + "," + b + ")="+r);
-    return r;
-  }
-  @Override
-  public int product(int a, int b) {
-    int r = delegate.product(a, b);
-    System.out.println("product(" + a + "," + b + ")="+r);
-    return r;
-  }
-}
+//class MathsProxy extends Maths {
+//  private final Maths delegate;
+//  MathsProxy(Maths delegate) {
+//    this.delegate = delegate;
+//  }
+//  @Override
+//  public int sum(int a, int b) {
+//    int r = delegate.sum(a, b);
+//    System.out.println("sum(" + a + "," + b + ")="+r);
+//    return r;
+//  }
+//  @Override
+//  public int product(int a, int b) {
+//    int r = delegate.product(a, b);
+//    System.out.println("product(" + a + "," + b + ")="+r);
+//    return r;
+//  }
+//}
 
 // TODO sa logam orice operatie face Maths, dar nu ai voie sa scrii cod sub linia asta:
 // -------------------------------------------------------------
@@ -38,6 +54,7 @@ class SecondGrade {
   }
 
   public void mathClass() {
+    System.out.println("Oare ce Math class mi-a fost injectat? " +maths.getClass());
     System.out.println("8 + 4 = " + maths.sum(8, 4));
     System.out.println("6 + 6 = " + maths.sum(6, 6));
     System.out.println("4 x 3 = " + maths.product(4, 3));
