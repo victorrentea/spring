6a626a42f413a9921e06d6744c6d00004ccc0d9c
Victor Rentea <victorrentea@gmail.com>
2024-03-04 10:03:28 +0200

    dependency injection

--- spring-start/src/main/java/victor/training/spring/first/X.java
+++ spring-start/src/main/java/victor/training/spring/first/X.java
@@ -5,12 +5,27 @@ import org.springframework.stereotype.Service;
 
 // TODO Define Beans
 @Service
-public class X {
-  // TODO Injection
+public class X { //
+//  @Autowired // field-based in @Test
+//  private Y y; // requires spring to inject a reference to a bean of type Y for its
+  // context (bucket of objects)
+
+  private final Y y;
+  public X(Y y) { // constructor based injection in production everywhere
+    this.y = y;
+  }
+  // method-based injection for startup logic
   @Autowired
-  private Y y;
+  public void met(Y y) { // imagine I wanted to send a kafka event only at the app start up
+    System.out.println("I got a y: " + y);
+  }
 
   public int logic() {
+    System.out.println("X.logic");
     return 1 + y.logic();
   }
 }
+
+//class InATest {
+//  X x = new X(y)
+//}
