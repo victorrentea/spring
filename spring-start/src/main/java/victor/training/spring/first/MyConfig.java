3bd980619e3ee217ec901961da6e8ad670556b40
Victor Rentea <victorrentea@gmail.com>
2024-03-04 10:42:36 +0200

    better: no cycle!!

--- spring-start/src/main/java/victor/training/spring/first/MyConfig.java
+++ spring-start/src/main/java/victor/training/spring/first/MyConfig.java
@@ -2,24 +2,14 @@ package victor.training.spring.first;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Lazy;
 
-//@Configuration //no application logic, only Spring glue, beans, annotation ,.. etc
-@Import({
-  YConfig.class
-})
 public class MyConfig {// implements somth from Spring
-  private final Y y;
-
-  public MyConfig(Y y) {
-    this.y = y;
-  }
 
   @Bean
   public X x() {
-    return new X(y);
+    return new X(y());
   }
-}
-class YConfig {
   @Bean
   public Y y() {
     return new Y();
