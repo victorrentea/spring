642efc0f98c6aaaf464c48b8f1a892b9f60d02ff
Victor Rentea <victorrentea@gmail.com>
2024-03-05 10:39:24 +0200

    Event+async for long running deferreed

--- spring-start/src/main/java/victor/training/spring/first/events/inventory/InvoiceService.java
+++ spring-start/src/main/java/victor/training/spring/first/events/inventory/InvoiceService.java
@@ -2,7 +2,7 @@ package victor.training.spring.first.events.inventory;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
-import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import victor.training.spring.first.events.invoicing.StockReservedEvent;
 import victor.training.spring.first.events.order.OrderPlacedEvent;
@@ -10,13 +10,21 @@ import victor.training.spring.first.events.order.OrderPlacedEvent;
 @Slf4j
 @Service
 public class InvoiceService {
-   @EventListener
+  @EventListener
 //   @Order(20) // order of the listeners should never be important
-   public void onOrderPlaced(StockReservedEvent event) {
-      log.info("Generating invoice for order " + event.orderId());
-      // if (random() < .3) throw new RuntimeException("Invoice Generation Failed");
-      log.info(">> PERSIST Invoice!!");
-   }
+  public void onOrderPlaced(StockReservedEvent event) {
+    log.info("Generating invoice for order " + event.orderId());
+    // if (random() < .3) throw new RuntimeException("Invoice Generation Failed");
+    log.info(">> PERSIST Invoice!!");
+  }
+
+  @EventListener
+  @Async // makes the caller not wait for the listener to finish.
+  // also, any exception thrown here will be not visible to the caller
+  public void longRunningFlow(OrderPlacedEvent event) {
+    // heavy CPU
+//    sending emails
+  }
 }
 
 // avoid events, they make the code harder to navigate/reason about
