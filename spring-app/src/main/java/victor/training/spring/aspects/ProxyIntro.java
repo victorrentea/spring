4cf55719d8a2c5e4d00c087586bf85ee75db8f0a
Victor Rentea <victorrentea@gmail.com>
2024-06-12 15:06:49 +0300

    CLASS PROXY facut cu mana. practic pe sub, Spring asa isi face proxy-urile necesare adnotarilor magice pe care le folosesti tu.

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -6,12 +6,33 @@ import org.springframework.stereotype.Service;
 public class ProxyIntro {
     public static void main(String[] args) {
         // WE play the role of Spring here ...
-        Maths maths = new Maths();
+        Maths maths = new MathProxy(new Maths()); // a-la DECORATOR pattern
         SecondGrade secondGrade = new SecondGrade(maths);
 
         secondGrade.mathClass();
     }
 }
+class MathProxy extends Maths {
+    private final Maths maths;
+    MathProxy(Maths maths) {
+        this.maths = maths;
+    }
+
+    @Override
+    public int sum(int a, int b) {
+        System.out.println("Calling sum with " + a + " and " + b);
+        return maths.sum(a, b);
+    }
+
+    @Override
+    public int product(int a, int b) {
+        System.out.println("Calling product with " + a + " and " + b);
+        return maths.product(a, b);
+    }
+}
+// TODO aici
+// scriind doar deasupra acestei linii, printeaza pe System.out orice apel
+// la sum sau produs cu param dati
 // ------------------------
 @Service
 class SecondGrade {
@@ -26,13 +47,11 @@ class SecondGrade {
         System.out.println("4 x 3 = " + maths.product(4, 3));
     }
 }
-
 @Service
 class Maths {
     public int sum(int a, int b) {
         return a + b;
     }
-
     public int product(int a, int b) {
         return a * b;
     }
