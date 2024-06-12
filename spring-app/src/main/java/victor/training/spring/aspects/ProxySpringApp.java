278d98da187cf4e60efa165fc996c53913e17be7
Victor Rentea <victorrentea@gmail.com>
2024-06-12 16:47:55 +0300

    yolo

--- spring-app/src/main/java/victor/training/spring/aspects/ProxySpringApp.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxySpringApp.java
@@ -3,14 +3,20 @@ package victor.training.spring.aspects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class ProxySpringApp {
     public static void main(String[] args) {
         SpringApplication.run(ProxySpringApp.class, args);
     }
+
     @Autowired
-    public void run(SecondGrade secondGrade) {
+    private SecondGrade secondGrade;
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void run() {
         System.out.println("Running Maths class...");
         secondGrade.mathClass();
     }
