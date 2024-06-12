5e381ded86de7c00f32d6598ad97c5170e727445
Victor Rentea <victorrentea@gmail.com>
2024-06-12 11:14:03 +0300

    ent

--- spring-start/src/main/java/victor/training/spring/first/Config.java
+++ spring-start/src/main/java/victor/training/spring/first/Config.java
@@ -2,18 +2,24 @@ package victor.training.spring.first;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import victor.training.spring.pachet.pachet.X;
 
 @Configuration
 public class Config
     implements CommandLineRunner {
-@Autowired
+
+  @Autowired
   private X x;
 
   @Override // from CommandLineRunner
   public void run(String... args) {
     System.out.println(x.logic());
+  }
+  @EventListeneevr(ApplicationStartedEvent.class)
+  public void start() {
+    System.out.println("Hello events");
   }
 }
