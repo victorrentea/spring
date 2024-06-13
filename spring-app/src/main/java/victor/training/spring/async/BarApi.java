8304bd01d869aa75fc088efcfcc41f61630c27f0
Victor Rentea <victorrentea@gmail.com>
2024-06-13 15:49:01 +0300

    commit

--- spring-app/src/main/java/victor/training/spring/async/BarApi.java
+++ spring-app/src/main/java/victor/training/spring/async/BarApi.java
@@ -1,5 +1,6 @@
 package victor.training.spring.async;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,22 +9,29 @@ import victor.training.spring.async.drinks.Beer;
 import victor.training.spring.async.drinks.DillyDilly;
 import victor.training.spring.async.drinks.Vodka;
 
+import java.util.concurrent.CompletableFuture;
+
 import static java.lang.System.currentTimeMillis;
+import static java.util.concurrent.CompletableFuture.*;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class BarApi {
-   @Autowired
-   private BarmanService barmanService;
+   private final BarmanService barmanService;
    @GetMapping("api/drink")
    public DillyDilly drink() throws Exception {
       log.debug("Submitting my order");
       long t0 = currentTimeMillis();
 
-      Beer beer = barmanService.pourBeer();
-      Vodka vodka = barmanService.pourVodka();
+      var beerPromise = supplyAsync(()->barmanService.pourBeer());
+      var vodkaPromise = supplyAsync(()->barmanService.pourVodka());
+      log.debug("Mi-a luat comanda");
+      Beer beer = beerPromise.get(); // http asteapta 1 sec
+      Vodka vodka = vodkaPromise.get(); // http asteapta 0, ca vodka e deja gata cand berea e gata
 
-      barmanService.auditCocktail("Dilly");
+      // fire-and-forget
+      runAsync(()->barmanService.auditCocktail("Dilly")); // 0.5 sec
 
       log.debug("Method completed in {} millis", currentTimeMillis() - t0);
       return new DillyDilly(beer, vodka);
