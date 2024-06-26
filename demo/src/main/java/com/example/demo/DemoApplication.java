46435ec56756515ef16cbdb869a257c879af61fc
Victor Rentea <victorrentea@gmail.com>
2024-06-26 15:52:12 +0300

    GET basics

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,20 +3,25 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 
-	@GetMapping
-	public String hello() {
-		return "Hello!";
-	}
+  // http://localhost:8080/hello/12?style=slay
+  @GetMapping("hello/{id}")
+  public String hello(
+      @PathVariable long id,
+		  @RequestParam String style) {
+    return "Hello " + id + " ? " + style;
+  }
 
-	//TODO ResponseEntity - de ce nu
+  //TODO ResponseEntity - de ce nu
 }
