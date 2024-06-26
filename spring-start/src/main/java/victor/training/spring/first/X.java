92e9f581c3c0ff0bddb5f2d6a7c899e698828ca2
Victor Rentea <victorrentea@gmail.com>
2024-06-26 10:38:52 +0300

    injection + singleton + appcontext.getBean

--- spring-start/src/main/java/victor/training/spring/first/X.java
+++ spring-start/src/main/java/victor/training/spring/first/X.java
@@ -5,7 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 // TODO Define Beans
 @Service // ii spune lui spring sa manageuie o instanta din aceasta clasa
@@ -16,6 +18,7 @@ public class X { // spring ii face "new"
 //  private Y y; // nici unui framework in Java nu ii pasa de private: Spring, Hibernate, EJB, CDI, Jackson
   // foloseste Java reflection sa acceseze campuri private
   private final Y y;
+  private final ApplicationContext applicationContext;
 
   @Autowired //method/setter- injection
   public void metoda(Y y) {
@@ -23,6 +26,11 @@ public class X { // spring ii face "new"
   }
 
   public int logic() {
+    // am obtinut programatic un obiect din context spring - de evitat!!
+    log.info("y=" + applicationContext.getBean(Y.class));
+//    log.info("y=" + applicationContext.getBean(Math.class)); // ar crapa abia la exec acestei metode, nu la deploy!
+    // + acelasi Y primit si aici si in "metoda"
+
     log.info("X.logic");
     return 1 + y.logic();
   }
