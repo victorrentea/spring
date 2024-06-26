35f2d950c617c66983c6b9edce20c69d2bae92fd
Victor Rentea <victorrentea@gmail.com>
2024-06-26 14:14:13 +0300

    subclasare cu apel la super.

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -5,34 +5,42 @@ import org.springframework.stereotype.Service;
 
 public class ProxyIntro {
     public static void main(String[] args) {
-        // WE play the role of Spring here ...
-        Maths maths = new Maths();
+        Maths maths = new MathsIntercept();
         SecondGrade secondGrade = new SecondGrade(maths);
-
         secondGrade.mathClass();
     }
 }
-// ------------------------
+// fara a modifica nimic sub linia asta, printati fiecare
+// apel de functie sum/produs cu parametrii ei
+// pe care-l face SecondGrade (sa interceptam apelul)
+class MathsIntercept extends Maths {
+    public int sum(int a, int b) {
+        System.out.println("Intercepted sum(" + a + ", " + b + ")");
+        return super.sum(a, b);
+    }
+    public int product(int a, int b) {
+        System.out.println("Intercepted product(" + a + ", " + b + ")");
+        return super.product(a, b);
+    }
+}
+// -----------------------------------------
 @Service
 class SecondGrade {
     private final Maths maths;
     SecondGrade(Maths maths) {
         this.maths = maths;
     }
-
     public void mathClass() {
         System.out.println("8 + 4 = " + maths.sum(8, 4));
         System.out.println("6 + 6 = " + maths.sum(6, 6));
         System.out.println("4 x 3 = " + maths.product(4, 3));
     }
 }
-
 @Service
 class Maths {
     public int sum(int a, int b) {
         return a + b;
     }
-
     public int product(int a, int b) {
         return a * b;
     }
