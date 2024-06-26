bfb5dc575463cb758b980a2970a3fb592bfcb3bf
Victor Rentea <victorrentea@gmail.com>
2024-06-26 15:37:02 +0300

    Hello!

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,21 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping
+	public String hello() {
+		return "Hello!";
+	}
+
+	//TODO ResponseEntity - de ce nu
 }
