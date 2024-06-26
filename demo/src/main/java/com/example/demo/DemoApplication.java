98e52d3c4339b9f1456fcb356268a5d7a3feb77a
Victor Rentea <victorrentea@gmail.com>
2024-06-26 16:41:24 +0300

    ResponseEntity pt status si headere

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -23,17 +24,29 @@ public class DemoApplication {
   @GetMapping("hello/{id}")
   public String hello(
       @PathVariable long id,
-		  @RequestParam(required = false) String style) {
+      @RequestParam(required = false) String style) {
     return "Hello " + id + " ? " + style;
   }
-  record ReservationDto(@Size(min=10) String name){}
-  @PostMapping
-  public void createReservation(@Validated @RequestBody ReservationDto dto) {
-    reservationService.create(dto);
-  }
+
   @Autowired
   ReservationService reservationService;
+
+  record ReservationDto(@Size(min = 10) String name) {
+  }
+
+  @PostMapping
+  public ResponseEntity<Void> createReservation(@Validated @RequestBody ReservationDto dto) {
+    try {
+      reservationService.create(dto);
+    } catch (IllegalArgumentException e) {
+      return ResponseEntity.badRequest().build();
+    }
+    return ResponseEntity.ok()
+        .header("X-Custom", "BUN") // motiv bun pentru ResponseEntity
+        .build();
+  }
 }
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
