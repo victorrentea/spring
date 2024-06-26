08b478e830fdad094f2d72169d3edc42887bc59e
Victor Rentea <victorrentea@gmail.com>
2024-06-26 11:18:19 +0300

    yo

--- spring-start/src/main/java/victor/training/spring/first/MailServiceDummy.java
+++ spring-start/src/main/java/victor/training/spring/first/MailServiceDummy.java
@@ -2,7 +2,6 @@ package victor.training.spring.first;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 // TODO for local dev only
 public class MailServiceDummy implements MailService {
   public void sendEmail(String subject) {
