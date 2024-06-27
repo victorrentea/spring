9119621b12862e114cb8ffbc08a6436dbc704acb
Victor Rentea <victorrentea@gmail.com>
2024-06-27 14:44:17 +0300

    @ConfigurationProperties pe @Bean

--- spring-app/src/main/java/victor/training/spring/async/AsyncConfig.java
+++ spring-app/src/main/java/victor/training/spring/async/AsyncConfig.java
@@ -1,6 +1,7 @@
 package victor.training.spring.async;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -9,19 +10,33 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @EnableAsync
 @Configuration
 public class AsyncConfig {
-	@Value("${executor.thread.count}")
-	int threadCount;
+
 	@Bean
+	@ConfigurationProperties(prefix = "executor")
 	public ThreadPoolTaskExecutor executor() {
 		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(threadCount);
-		executor.setMaxPoolSize(threadCount);
-		executor.setQueueCapacity(500);
-		executor.setThreadNamePrefix("bar-");
+//		executor.setCorePoolSize(threadCount);
+//		executor.setMaxPoolSize(threadCount);
+//		executor.setQueueCapacity(500);
+//		executor.setThreadNamePrefix("bar-");
 		executor.initialize();
 		executor.setWaitForTasksToCompleteOnShutdown(true);
 		return executor;
 	}
+//	@Bean // initial
+//	public ThreadPoolTaskExecutor executor(
+//			@Value("${executor.thread.count}")
+//			int threadCount
+//	) {
+//		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+//		executor.setCorePoolSize(threadCount);
+//		executor.setMaxPoolSize(threadCount);
+//		executor.setQueueCapacity(500);
+//		executor.setThreadNamePrefix("bar-");
+//		executor.initialize();
+//		executor.setWaitForTasksToCompleteOnShutdown(true);
+//		return executor;
+//	}
 }
 
 
