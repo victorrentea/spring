cc3357d84ffe65cc6ab8ffbfa32ce257fb1f31a1
Victor Rentea <victorrentea@gmail.com>
2024-06-27 11:54:38 +0300

    nici nu mai VEZI inserturi, ca interceptorul stie ca le-a facut si le-ar trimite oricum degeaba

--- spring-transaction/src/main/java/victor/training/spring/transaction/playground/Jpa.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/playground/Jpa.java
@@ -17,10 +17,11 @@ public class Jpa {
 //    Message m = new Message("ONE");
 //    repo.save(m);
 //    id = m.getId(); is same as:
-
     id = repo.save(new Message("ONE")).getId();
-    log.info("End of method ---"); // TODO write-behind
-  }
+    repo.save(new Message("TWO"));
+    if (true) throw new RuntimeException("e"); // #1 nici nu mai VEZI inserturi, ca interceptorul stie ca le-a facut si le-ar trimite oricum degeaba
+    log.info("End of method ---");
+  } // write-behind: DUPA ce ies din metoda TxInterc face FLUSH(trimite IN DB insert/update/delete) + COMMIT
 
   public void two() {
 //    Message e = repo.findById(id).orElseThrow();
