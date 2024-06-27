2cbd86ab81fc68fcae6493077cc1f06c5a13a46d
Victor Rentea <victorrentea@gmail.com>
2024-06-27 14:29:29 +0300

    primu draft. e plin de leakuri si greseli

--- spring-app/src/main/java/victor/training/spring/async/BarApi.java
+++ spring-app/src/main/java/victor/training/spring/async/BarApi.java
@@ -8,6 +8,10 @@ import victor.training.spring.async.drinks.Beer;
 import victor.training.spring.async.drinks.DillyDilly;
 import victor.training.spring.async.drinks.Vodka;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import static java.lang.System.currentTimeMillis;
 
 @Slf4j
@@ -16,16 +20,17 @@ public class BarApi {
    @Autowired
    private BarmanService barmanService;
 
-   // TODO [1] autowire and submit tasks to a ThreadPoolTaskExecutor
-   // TODO [2] mark pour* methods as @Async
-   // TODO [3] Make this endpoint non-blocking
    @GetMapping("api/drink")
    public DillyDilly drink() throws Exception {
       log.debug("Submitting my order");
       long t0 = currentTimeMillis();
 
-      Beer beer = barmanService.pourBeer();
-      Vodka vodka = barmanService.pourVodka();
+      ExecutorService threadPool = Executors.newFixedThreadPool(2);
+      Future<Beer> futureBeer = threadPool.submit(() -> barmanService.pourBeer());
+      Future<Vodka> futureVodka = threadPool.submit(() -> barmanService.pourVodka());
+
+      Beer beer = futureBeer.get();
+      Vodka vodka = futureVodka.get();
 
       barmanService.auditCocktail("Dilly");
 
