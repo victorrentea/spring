d7fb3032f60adb2eaacfdbd43287c928cc95caf1
Victor Rentea <victorrentea@gmail.com>
2024-06-27 14:58:20 +0300

    @Async pt fire and forget

--- spring-app/src/main/java/victor/training/spring/async/BarmanService.java
+++ spring-app/src/main/java/victor/training/spring/async/BarmanService.java
@@ -3,6 +3,7 @@ package victor.training.spring.async;
 import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import victor.training.spring.varie.Sleep;
@@ -34,9 +35,15 @@ public class BarmanService {
       return restTemplate.getForObject("http://localhost:8080/api/vodka", Vodka.class);
    }
 
+   @Async("executor") // numele beanului de tip ThreadPoolTaskExecutor
    public void auditCocktail(String name) {
       log.debug("Longer running task I don't want to wait for: auditing drink: " + name);
       Sleep.millis(500); // pretend send emails or import/export a file
-      log.debug("DONE");
+      if (true) throw new RuntimeException("Intentional"); // Spring o logeaza automat
+      log.debug("DONE Audit");
+      // utilizari practice de metoda @Async void = aka "fire and forget"
+      // - importuri de fisiere uploadate
+      // - generari de rapoarte ce sunt trimise pe mail cand sunt gata/downloadate ulterior
+      // - trimitere de notificari push/emailuri multe
    }
 }
