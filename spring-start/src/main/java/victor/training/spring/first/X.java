93953afd09e86d1ea4dce2c29eb2729d20ffb415
Victor Rentea <victorrentea@gmail.com>
2024-10-07 10:24:22 +0300

    Dependency Injection: 3 types

--- spring-start/src/main/java/victor/training/spring/first/X.java
+++ spring-start/src/main/java/victor/training/spring/first/X.java
@@ -5,18 +5,11 @@ import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Service;
 
 public class X {
-//  programmatic singleton = garbage;
-//  private static X INSTANCE;
-//  public static X getInstance() {
-//    if (INSTANCE == null) {
-//      INSTANCE = new X();
-//    }
-//    return INSTANCE;
-//  }
-//  private X() {
-//  }
-  @Autowired // tells spring to inject a Y instance here
-  private Y y;
+  private final Y y;
+
+  public X(Y y) { // constructor injection💖
+    this.y = y;
+  }
 
   public int logic() {
     return 1 + y.logic();
--- spring-start/src/main/java/victor/training/spring/first/X2.java
+++ spring-start/src/main/java/victor/training/spring/first/X2.java
@@ -4,8 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 
 public class X2 {
-  @Autowired // tells spring to inject a Y instance here
+  @Autowired // field injection 😡😡 not good; tells spring to inject a Y instance here
   private Y y;
+
+  // @Bean
+  // @GetMapping
+  // @Scheduled
+  // @EventListener
+  @Autowired // method injection 😡(not traditional)
+  public void method(Y y) {
+    System.out.println("Got "+ y + " called by Spring" +
+                       " that acts here as an IoC (Inversion of Control)" +
+                       " container");
+    // the hollywood principle: "Don't call us, we'll call you"
+
+    // RULE: injection happens at startup time,
+    // before the bean gets any calls from eg. Rest, Kafka...
+//    throw new RuntimeException("Proof:ouuuu crashes the startup of the app");
+  }
 
   public int logic() {
     return 1 + y.logic();
