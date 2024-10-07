3ae96b1cd07b0a13296668235ff00bccfa57cbce
Victor Rentea <victorrentea@gmail.com>
2024-10-07 13:24:23 +0300

    @Bean+@Configuration

--- spring-start/src/main/java/victor/training/spring/first/MyModuleConfig.java
+++ spring-start/src/main/java/victor/training/spring/first/MyModuleConfig.java
@@ -2,6 +2,7 @@ package victor.training.spring.first;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
@@ -22,12 +23,20 @@ import org.springframework.context.annotation.Profile;
     MailServiceDummy.class,
 //    ClassFromAJar.class,// wont work because the class needs manual initialization
 })
+@Configuration // when this appears on a class
+// then when a @Bean method calls another @Bean method
+// they don't really call each other.Spring tricks you!
+// when you call #theBeanName(null) from #externalClass
+// spring INTERCEPTS your call and fills the argument
+
+// these calls act as "bean references"
 public class MyModuleConfig {
   @Profile("local")
   @Bean // allows to create a bean in the Spring context programatically
 //  public ClassFromAJar theBeanName(@Value("${props.gate}") int gate) {
   public ClassFromAJar theBeanName(Props props) {
-    ClassFromAJar bean = new ClassFromAJar("init");
+    System.out.println("I am called now with props: " + props);
+    ClassFromAJar bean = new ClassFromAJar(props.welcomeMessage());
     bean.setState(props.gate()); // < props.gate
     return bean;
   }
@@ -37,6 +46,14 @@ public class MyModuleConfig {
 //  }
   @Bean
   public ExternalClass externalClass() {
+    return new ExternalClass(theBeanName(null));
+  }
+  @Bean
+  public ExternalClass externalClass1() {
+    return new ExternalClass(theBeanName(null));
+  }
+  @Bean
+  public ExternalClass externalClass2() {
     return new ExternalClass(theBeanName(null));
   }
 }
