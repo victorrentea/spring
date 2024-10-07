a877a8adbf94c9a64c0d66096faf1317b7e29035
Victor Rentea <victorrentea@gmail.com>
2024-10-07 11:09:57 +0300

    @Value in field

--- spring-start/src/main/java/victor/training/spring/first/Y2.java
+++ spring-start/src/main/java/victor/training/spring/first/Y2.java
@@ -1,4 +1,20 @@
 package victor.training.spring.first;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+
 public class Y2 {
+  Y2() { // in order to write in the gate field,
+    // spring first has to 'new' this class
+    System.out.println("Gate constructor: " + gate); // too early
+  }
+
+  @Value("${props.gate}")
+  private Integer gate;
+
+  @EventListener(ApplicationStartedEvent.class)
+  public void spring_pleaseCallThisWhenTheAppIsReadyToReceiveRequests() {
+    System.out.println("Gate @EventListener: " + gate);
+  }
 }
