b95aa0f95cad32e30effe2e96017a75325e5c028
Victor Rentea <victorrentea@gmail.com>
2024-10-07 11:19:18 +0300

    fails to start if a prop is not found

--- spring-start/src/main/java/victor/training/spring/first/Y2.java
+++ spring-start/src/main/java/victor/training/spring/first/Y2.java
@@ -2,18 +2,27 @@ package victor.training.spring.first;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.EventListener;
 
 public class Y2 {
   private final int gate;
 
-  Y2(@Value("${props.gate}") int gate) {
+  Y2(@Value("${props.gateOUPS}") int gate, ApplicationContext applicationContext) {
     this.gate = gate;
+    this.applicationContext = applicationContext;
     System.out.println("gate in ctor " + gate);
   }
+  private final ApplicationContext applicationContext;
 
   @EventListener(ApplicationStartedEvent.class)
   public void spring_pleaseCallThisWhenTheAppIsReadyToReceiveRequests() {
     System.out.println("Gate @EventListener: " + gate);
+
+//    System.out.println("From ctx:" + applicationContext.getEnvironment()
+//        .getProperty("props.gateOUPS").toUpperCase());
   }
 }
+
+// can Immutables objects return null from a getter?
+// NO. only empty Optional<>
