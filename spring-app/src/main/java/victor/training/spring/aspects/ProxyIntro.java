16d4848209f42ee6066fd2ad9932e6a66e886722
Victor Rentea <victorrentea@gmail.com>
2024-10-10 11:01:21 +0300

    works by copy-paste.

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -1,10 +1,5 @@
 package victor.training.spring.aspects;
 
-import io.micrometer.core.annotation.Timed;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 public class ProxyIntro {
   public static void main(String[] args) {
     Maths maths = new Maths();
@@ -12,35 +7,30 @@ public class ProxyIntro {
     secondGrade.mathClass();
   }
 }
-// ------------------------ THE LINE ------------------
+
 //CR: we need to LOG the arguments of sum() and product()
+// WITHOUT TOUCHING ANY CODE BELOW THE LINE
+// ------------------------ THE LINE ------------------
 class SecondGrade {
   private final Maths maths;
+
   SecondGrade(Maths maths) {
     this.maths = maths;
   }
-  public void mathClass() {
-    Util.measure(()->maths.sum(8,4));
 
+  public void mathClass() {
     System.out.println("8 + 4 = " + maths.sum(8, 4));
     System.out.println("6 + 6 = " + maths.sum(6, 6));
     System.out.println("4 x 3 = " + maths.product(4, 3));
   }
 }
+
 class Maths {
-//  @Timed // exposes a metric on how long this method takes at /actuator/promehteus
-//  @Transactional // start a tx before and commits/rollback after
-//  @Secured("ROLE_CAN_SUM_NUMBERS") // requires the requesting us er to have these roles
   public int sum(int a, int b) {
-    transaction.start
-    try{
-      long t0 = System.currentTimeMillis();
-      System.out.println("Sum " + a + " and " + b);
-      return a + b;
-      long t1 = System.currentTimeMillis();
-    transaction.commit
-     }catch(Ex) { transaction.rollback }
+    System.out.println("Sum " + a + " and " + b);
+    return a + b;
   }
+
   public int product(int a, int b) {
     System.out.println("Product " + a + " and " + b);
     return a * b;
@@ -48,6 +38,9 @@ class Maths {
 }
 
 
+//  @Timed // exposes a metric on how long this method takes at /actuator/promehteus
+//  @Transactional // start a tx before and commits/rollback after
+//  @Secured("ROLE_CAN_SUM_NUMBERS") // requires the requesting us er to have these roles
 // Key Points
 // - Class Proxy using CGLIB Enhancer to extend the proxied class
 // - Proxy limitations: final methods/classes, local calls
