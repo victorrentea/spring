679d96f3d228f02eba83025f4147a3fa0512b52a
Victor Rentea <victorrentea@gmail.com>
2024-10-10 10:27:25 +0300

    @async ftw. or not.

--- spring-app/src/main/java/victor/training/spring/events/InvoiceService.java
+++ spring-app/src/main/java/victor/training/spring/events/InvoiceService.java
@@ -3,6 +3,7 @@ package victor.training.spring.events;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import static java.lang.Math.random;
@@ -10,6 +11,7 @@ import static java.lang.Math.random;
 @Slf4j
 @Service
 public class InvoiceService {
+   @Async
    @Order(2)
    @EventListener
    public void process(OrderPlacedEvent event) {
