fc7e81071d95b3ca5264abfae45bb69cf29829db
Victor Rentea <victorrentea@gmail.com>
2024-10-30 11:25:49 +0200

    DI + un GET

---
+++ demo/src/main/java/com/example/demo/AController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class AController {
+  private final AService aService;
+
+  @GetMapping
+  public String hi() {
+    return "Hi!";
+  }
+}
---
+++ demo/src/main/java/com/example/demo/AService.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AService {
+}
