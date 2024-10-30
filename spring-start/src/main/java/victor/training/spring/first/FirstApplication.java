301554e9a81dd350f48af5633c9069dd50fd1536
Victor Rentea <victorrentea@gmail.com>
2024-10-30 10:04:54 +0200

    #asa da

--- spring-start/src/main/java/victor/training/spring/first/FirstApplication.java
+++ spring-start/src/main/java/victor/training/spring/first/FirstApplication.java
@@ -1,24 +1,18 @@
-package victor.training.spring.first.config;
+package victor.training.spring.first;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
-import victor.training.spring.first.MailServiceDummy;
-import victor.training.spring.first.X;
-import victor.training.spring.first.Y;
 
-@ComponentScan(basePackages = "aiurea")
-@Import({
-    X.class,
-    Y.class,
-    MailServiceDummy.class
-})
+//@ComponentScan(basePackages = "aiurea")
+//@Import({
+//    X.class,
+//    Y.class,
+//    MailServiceDummy.class
+//})
 @SpringBootApplication
 public class FirstApplication implements CommandLineRunner {
   public static void main(String[] args) {
