fba64990efc0afab0af0ce92dbad84e1fb32b612
Victor Rentea <victorrentea@gmail.com>
2024-10-30 14:37:10 +0200

    print la startup

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,19 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 @EnableConfigurationProperties(Props.class)
 //@ConfigurationPropertiesScan // a gasit-o @Teo
 public class DemoApplication {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
--- demo/src/main/java/com/example/demo/Props.java
+++ demo/src/main/java/com/example/demo/Props.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.event.EventListener;
 import org.springframework.validation.annotation.Validated;
 
 import java.time.LocalDate;
 
+@Slf4j
 @ConfigurationProperties("prop")
 @Validated
 public record Props(
@@ -14,4 +18,9 @@ public record Props(
     @NotNull
     LocalDate b
 ) {
+
+  @EventListener(ApplicationReadyEvent.class)
+  public void printProps() {
+    log.info(this.toString());
+  }
 }
