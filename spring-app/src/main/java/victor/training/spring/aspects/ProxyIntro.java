21644190a656a9f7f63ec5006ab5fad25acf69ae
Victor Rentea <victorrentea@gmail.com>
2024-10-30 16:20:52 +0200

    TZEPE in lucrul cu proxy

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -1,9 +1,11 @@
 package victor.training.spring.aspects;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.cglib.proxy.Callback;
 import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.cglib.proxy.MethodInterceptor;
 import org.springframework.cglib.proxy.MethodProxy;
+import org.springframework.security.access.annotation.Secured;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
@@ -20,6 +22,7 @@ public class ProxyIntro {
       }
     };
     Maths proxy = (Maths) Enhancer.create(Maths.class, callback); // subclasa dinamica
+    // nu exista bytecodeul acestei clase generate in /target in vreun fisier ci e totul in memorie la apel
     SecondGrade secondGrade = new SecondGrade(proxy);
     secondGrade.mathClass();
   }
@@ -47,27 +50,58 @@ public class ProxyIntro {
 // ------------------------ THE LINE ------------------
 class SecondGrade {
   private final Maths maths;
+
   SecondGrade(Maths maths) {
     this.maths = maths;
   }
+
   public void mathClass() {
+//    Maths maths = new Maths(); // nu mai iei dep injectata4
+    System.out.println("Miciuna: spring mi-a injectat un ... " + maths.getClass().getSimpleName());
     System.out.println("8 + 4 = " + maths.sum(8, 4));
     System.out.println("6 + 6 = " + maths.sum(6, 6));
     System.out.println("4 x 3 = " + maths.product(4, 3));
   }
 }
+/*final:CRASH*/ // de aia nici nu ai voie sa faci bean spring record
+@RequiredArgsConstructor
 class Maths {
-  public int sum(int a, int b) {
+  @Secured("ROLE_ALEX_ILIE")
+  public /*static:ignore*/ int sum(int a, int b) { // nu merge interceptia pe Util/Hellper
     return a + b;
   }
-  public int product(int a, int b) {
-    return a * b;
+  public /*final:weirdBug*/ int product(int a, int b) {
+//    return a * b;
+    int result = 0;
+    for (int i = 0; i < b; i++) {
+      //?? TOP TZEAPA: apelul local in cadrul aceleiasi clase nu e interceptat
+      result = sum(result, a);
+    }
+    return result;
   }
 }
+// Concluzii: nu merg proxy daca: cu ce m-am ales in viata
+// - nu faci clasa beanului final, nu record; dar DA campurile finale!
+// - nu faci metodele din beanuri final sau static
+// - nu faci 'new' la beanuri manageuite de spring
 
-// Key Points
-// - Class Proxy using CGLIB Enhancer to extend the proxied class
-// - Proxy limitations: final methods/classes, local calls
-// - Debug a Proxy
-// - Custom @Aspect
 
+// magia asta de proxy cand e folosita de Spring? la adnotari pe metode
+// @Transactional = proxy care incepe si commituieste tranzactia
+// @Async = executa metoda pe alt thread
+// @Secured("ROLE_ADMIN"),@RolesAllowed,@PreAuthorized = verifica autorizarea
+// @Cacheable = verifica cacheul
+// @Retryable = reincearca apelul metodei in caz de exceptie
+// @Validated = valideaza parametrii metodei
+// @Timed = masoara timpul de executie iese apoi pe http://localhost:8080/actuator/metrics
+// @Log (facuta de orange)
+
+
+// ==sa nu confunzi cu @ pt IoC = cand te cheama frameworkul pe tine
+// @Scheduled = proxy care executa metoda la interval regulat
+// @GetMapping
+// @PostConstruct
+// @EventListener
+
+
+// si Mockito.mock(X.class) e un proxy sau @Mock
