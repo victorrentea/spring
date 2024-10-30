3f4e1e400148521b11439d4be3bda519e8bf21b9
Victor Rentea <victorrentea@gmail.com>
2024-10-30 10:04:31 +0200

    fara component scan

--- spring-start/src/main/java/victor/training/spring/first/X.java
+++ spring-start/src/main/java/victor/training/spring/first/X.java
@@ -3,7 +3,21 @@ package victor.training.spring.first;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
+//@Retention(RetentionPolicy.RUNTIME)
+//@Component
+//@interface Manager {}
+
+// sa se ocupe springu de asta = "new". cum?!
+//@RestController // REST
+//@Repository // DB
+//@Controller // .jsp
+//@Component ce nu se incadreaza in cele de mai sus
+//@Manager
+
+//@Configuration // in clase cu configuri de spring, fara pic de logica, de ob cu metode @Bean
+// nu ruleaza cod per request
+
+@Service // = biz logic
 public class X {
   @Autowired
   private Y y;
--- spring-start/src/main/java/victor/training/spring/first/config/FirstApplication.java
+++ spring-start/src/main/java/victor/training/spring/first/config/FirstApplication.java
@@ -1,12 +1,24 @@
-package victor.training.spring.first;
+package victor.training.spring.first.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import victor.training.spring.first.MailServiceDummy;
+import victor.training.spring.first.X;
+import victor.training.spring.first.Y;
 
+@ComponentScan(basePackages = "aiurea")
+@Import({
+    X.class,
+    Y.class,
+    MailServiceDummy.class
+})
 @SpringBootApplication
 public class FirstApplication implements CommandLineRunner {
   public static void main(String[] args) {
