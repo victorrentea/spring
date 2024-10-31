680562365c9702311781a0e54ede28cb042934a7
Victor Rentea <victorrentea@gmail.com>
2024-10-31 15:29:01 +0200

    startup probe

---
+++ spring-app/src/main/java/victor/training/spring/actuator/StartupProbeController.java
@@ -0,0 +1,26 @@
+package victor.training.spring.actuator;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@RestController
+public class StartupProbeController {
+
+    private final AtomicBoolean appStarted = new AtomicBoolean(false);
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady() {
+        appStarted.set(true);
+    }
+
+    @GetMapping("/started")
+    public ResponseEntity<String> startup() {
+        return appStarted.get() ? ResponseEntity.ok("Application started") : ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Not ready");
+    }
+}
