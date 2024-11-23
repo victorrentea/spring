23963360128b39a68af9613059eb7bdb03988c5f
Victor Rentea <victorrentea@gmail.com>
2024-11-23 09:27:04 +0200

    END

--- spring-app/src/main/java/victor/training/spring/security/UserController.java
+++ spring-app/src/main/java/victor/training/spring/security/UserController.java
@@ -2,6 +2,7 @@ package victor.training.spring.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class UserController {
 
     log.info("Return current user");
     CurrentUserDto dto = new CurrentUserDto();
-    dto.username = "<todo-username>"; // TODO
+    dto.username = SecurityContextHolder.getContext().getAuthentication().getName();
 
     //<editor-fold desc="KeyCloak">
     //		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
--- spring-app/src/main/java/victor/training/spring/web/controller/MessageSenderController.java
+++ spring-app/src/main/java/victor/training/spring/web/controller/MessageSenderController.java
@@ -24,4 +24,6 @@ public class MessageSenderController {
 //        int amount;
 //        String reason;
 //    }
+
+//  Enhancer
 }
