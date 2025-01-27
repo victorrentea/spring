145ab01f78b21ecdf476bbf99f07c9e640071d43
Victor Rentea <victorrentea@gmail.com>
2025-01-27 15:28:40 +0200

    Cum sa stric interceptia metodelor (proxierea)

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -4,7 +4,10 @@ import org.springframework.cglib.proxy.Callback;
 import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.cglib.proxy.MethodInterceptor;
 import org.springframework.cglib.proxy.MethodProxy;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.Serial;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
@@ -45,24 +48,33 @@ public class ProxyIntro {
 //}
 // afiseaza param fiecarui apel de metoda in Maths, fara a scrie nimic sub linie
 // ------------------- LINIE ------------------
+// ce pot scrie sub linie sa nu mearga interceptia metodei
 class SecondGrade {
-  private final Maths maths;
+  private  Maths maths;
   SecondGrade(Maths maths) {
     this.maths = maths;
   }
   public void mathClass() {
+//    maths = new Maths(); // #5 nu mai intercepteaza pt ca nu mai lucrezi cu proxyul injectat
     System.out.println("8 + 4 = " + maths.sum(8, 4));
     System.out.println("6 + 6 = " + maths.sum(6, 6));
     System.out.println("8 + 8 = " + maths.sum(8, 8));
     System.out.println("4 x 3 = " + maths.product(4, 3));
   }
 }
+//@Service
+/*final class #3 error*/
+/*record #4 error ca e final*/
 class Maths {
-  public int sum(int a, int b) {
+  public /*static #1 ignored*/ int sum(int a, int b) {
     return a + b;
   }
-  public int product(int a, int b) {
-    return a * b;
+  public /*final #2 ignored/error*/ int product(int a, int b) {
+    int result = 0;
+    for (int i = 0; i < a; i++) {
+      result = sum(result, b); // #6 👑 apelurile locale de metode nu sunt interceptate
+    }
+    return result;
   }
 }
 
