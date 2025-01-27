1eff6fe9782be0529f157cd8d64964f01bacd774
Victor Rentea <victorrentea@gmail.com>
2025-01-27 15:14:18 +0200

    pana aici totu bine. aspecte

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -1,23 +1,48 @@
 package victor.training.spring.aspects;
 
+import org.springframework.cglib.proxy.Callback;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+import static java.lang.System.currentTimeMillis;
+
 public class ProxyIntro {
   public static void main(String[] args) {
-    Maths maths = new MathsProxy();
-    SecondGrade secondGrade = new SecondGrade(maths);
+    Maths real = new Maths();
+    Callback callback = new MethodInterceptor() {
+      @Override
+      public Object intercept(Object obj, Method method, Object[] params, MethodProxy proxy) throws Throwable {
+        System.out.println("Method " + method.getName() + " called with params " + Arrays.toString(params));
+        long t0 = currentTimeMillis();
+        try {
+          return method.invoke(real, params);
+        } finally {
+          long t1 = currentTimeMillis();
+          System.out.println("Method " + method.getName() + " took " + (t1 - t0) + " ms");
+        }
+      }
+    };
+    Maths proxy = (Maths) Enhancer.create(Maths.class, callback);
+
+    SecondGrade secondGrade = new SecondGrade(proxy);
     secondGrade.mathClass();
   }
 }
-class MathsProxy extends Maths {
-  Maths maths = new Maths();
-  public int sum(int a, int b) {
-    System.out.println("sum called with " + a + " and " + b);
-    return maths.sum(a, b);
-  }
-  public int product(int a, int b) {
-    System.out.println("product called with " + a + " and " + b);
-    return maths.product(a, b);
-  }
-}
+//class MathsProxy extends Maths {
+//  Maths maths = new Maths();
+//  public int sum(int a, int b) {
+//    System.out.println("sum called with " + a + " and " + b);
+//    return maths.sum(a, b);
+//  }
+//  public int product(int a, int b) {
+//    System.out.println("product called with " + a + " and " + b);
+//    return maths.product(a, b);
+//  }
+//}
 // afiseaza param fiecarui apel de metoda in Maths, fara a scrie nimic sub linie
 // ------------------- LINIE ------------------
 class SecondGrade {
@@ -28,6 +53,7 @@ class SecondGrade {
   public void mathClass() {
     System.out.println("8 + 4 = " + maths.sum(8, 4));
     System.out.println("6 + 6 = " + maths.sum(6, 6));
+    System.out.println("8 + 8 = " + maths.sum(8, 8));
     System.out.println("4 x 3 = " + maths.product(4, 3));
   }
 }
