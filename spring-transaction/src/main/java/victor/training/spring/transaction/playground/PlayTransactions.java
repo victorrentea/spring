d5cdb1ea3dce398e8ed3d910c496100790b23c55
Victor Rentea <victorrentea@gmail.com>
2025-01-28 14:55:37 +0200

    exceptiile checked sunt naspa

--- spring-transaction/src/main/java/victor/training/spring/transaction/playground/PlayTransactions.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/playground/PlayTransactions.java
@@ -6,6 +6,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+
 @Service
 @RequiredArgsConstructor
 public class PlayTransactions {
@@ -13,19 +15,21 @@ public class PlayTransactions {
   private final MessageRepo repo; // = Spring Data JPA, 2011
   private final OtherClass other;
 
-  @SneakyThrows
   @Transactional
-  public void play() {
+      (rollbackFor = Exception.class)  // 1 fix, pentru cine stie
+  // 2, mai bine renunta sa mai arunci vreodata exceptii CHECKED. oricum sunt greseli in limbaj
+  public void play() throws IOException {
     chemataLocal();
     // o tx face rollback daca sare orice exceptie din metoda @Transactional
     // de ex cand oricare din cele 2 queruri crapa
   }
 
-  private void chemataLocal() { // sonar se prinde
+  private void chemataLocal() throws IOException { // sonar se prinde
     repo.insert("SQL");
     repo.save(new Message("SQL2"));
-    if (Math.random()<1)throw new IllegalArgumentException("ghinion");
-
+//    if (Math.random()<1)throw new IllegalArgumentException("ghinion");
+    if (Math.random()<1) throw new IOException("N-am gasit un fisier. ca-n anii 90'"); // exceptie d-aia scarboasa de doar Java are
+    // soc: da commit ca asa se comprta si batranu EJB (Java EE) de udne a furat SPring suflete.
   }
 
 }
