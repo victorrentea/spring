1db9aa3a0a8093304f77f514cff39b2cef680309
Victor Rentea <victorrentea@gmail.com>
2025-02-24 13:39:19 +0200

    EventListener

---
+++ spring-start/src/main/java/victor/training/spring/first/EntryPoint.java
@@ -0,0 +1,77 @@
+package victor.training.spring.first;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EntryPoint {
+  private static final Logger log = LoggerFactory.getLogger(EntryPoint.class);
+  private final ApplicationEventPublisher applicationEventPublisher;
+
+  public EntryPoint(ApplicationEventPublisher applicationEventPublisher) {
+    this.applicationEventPublisher = applicationEventPublisher;
+  }
+
+  @EventListener(ApplicationStartedEvent.class)
+  @Order(7)
+  public void start() {
+    System.out.println("Hello Spring!");
+    CartEvent event = new CartEvent(List.of("Tomatoes", "Cucumber"), 10);
+    log.info("Before: " + event);
+    applicationEventPublisher.publishEvent(event);
+    log.info("After: " +event);
+  }
+}
+
+class Offer2 {
+  private static final Logger log = LoggerFactory.getLogger(Offer2.class);
+
+  @EventListener
+  @Order(6) // hard to maintain = global coupling point
+//  @Async // runs in a separate thread
+  public void onCartEvent(CartEvent event) {
+    log.info("Offer2: " + event);
+    // event handlers run by default in the same thread as publisher, one AFTER THE OTHER in an unspecified order
+    // if you buy cucumber and tomatoes -2
+    if (event.items.contains("Tomatoes") && event.items.contains("Cucumber")) {
+      event.price -= 2; // bad practice; don't show to anyone in Picnic
+//      event.appliedOffers.add(my discount(-2, "offer2")) .. better
+    }
+  }
+}
+
+class Offer1 {
+  private static final Logger log = LoggerFactory.getLogger(Offer1.class);
+
+  @EventListener
+  public void onCartEvent(CartEvent event) {
+    log.info("Offer1: " + event);
+    if (event.items.contains("Tomatoes")) {
+      event.price -= 1;
+    }
+  }
+}
+
+class CartEvent {
+  public List<String> items;
+  public Integer price;
+
+  public CartEvent(List<String> items, Integer price) {
+    this.items = items;
+    this.price = price;
+  }
+}
+
+
+
+// B) Fire a custom CartEvent and have multiple beans listening ( @EventListener )
+//    mutating the CartEvent payload
+// ✨ Spring MAGIC
--- spring-start/src/main/java/victor/training/spring/first/FirstApplication.java
+++ spring-start/src/main/java/victor/training/spring/first/FirstApplication.java
@@ -3,16 +3,14 @@ package victor.training.spring.first;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.TypeExcludeFilter;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.*;
 import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
-@Import({ SecondConfig.class, MyConfig.class})
+@Import({ SecondConfig.class, MyConfig.class, EntryPoint.class, Offer1.class, Offer2.class})
 @EnableConfigurationProperties(Props.class)
 @ComponentScan(basePackages = "kodagcidscgdhcgvdsuyfeqw")
 public class FirstApplication implements CommandLineRunner {
