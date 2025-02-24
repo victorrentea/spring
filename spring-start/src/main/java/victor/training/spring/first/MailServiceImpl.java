8d2f47e7ac4dd9c728e7aa4f0c85c1c0594f15a5
Victor Rentea <victorrentea@gmail.com>
2025-02-24 12:00:25 +0200

    config sources

--- spring-start/src/main/java/victor/training/spring/first/MailServiceImpl.java
+++ spring-start/src/main/java/victor/training/spring/first/MailServiceImpl.java
@@ -1,15 +1,21 @@
 package victor.training.spring.first;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Primary
+//@Profile("prod") // risky: may forget to enable in the HOLY PROD
+@Profile("!local")// if I enable this profile this bean will be ignored
 public class MailServiceImpl implements MailService {
   //  private final MailSender sender; // TODO this bean is automatically defined by spring
 
   public void sendEmail(String body) {
+//    System.getProperty() // vm props -Dbla=val
     SimpleMailMessage message = new SimpleMailMessage();
     message.setFrom("noreply@victorrentea.ro");
     message.setTo("victor@victorrentea.ro");
