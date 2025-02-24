61398e374d3350026c13e0d55459bf2ee1205452
Victor Rentea <victorrentea@gmail.com>
2025-02-24 11:15:09 +0200

    surprise: even though there are two implementations available for injection as a male service, the dummy one won because the injection point name happened to match the bean name

--- spring-start/src/main/java/victor/training/spring/first/MyConfig.java
+++ spring-start/src/main/java/victor/training/spring/first/MyConfig.java
@@ -8,12 +8,13 @@ import org.springframework.context.annotation.Import;
     X.class,
     victor.training.spring.first.config.X.class,
     Y.class,
+    MailServiceImpl.class // # impl1
     /*MailServiceDummy.class*/}) // name of the bean = "mailServiceDummy"
 public class MyConfig {
 
   // if I need to do manually "new" an object or configure it manually (eg. call init())
-  @Bean
-  public MailServiceDummy dummy2() { //  name of the bean = method name("dummy")
+  @Bean // # impl2
+  public MailServiceDummy dummy() { //  name of the bean = method name("dummy")
     MailServiceDummy mailServiceDummy = new MailServiceDummy(1);
     mailServiceDummy.init();
     System.out.println("I win in MyConfig");
--- spring-start/src/main/java/victor/training/spring/first/SecondConfig.java
+++ spring-start/src/main/java/victor/training/spring/first/SecondConfig.java
@@ -5,12 +5,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SecondConfig {
-  @Bean
-  public MailServiceDummy dummy() { //  name of the bean = method name("dummy")
-    MailServiceDummy mailServiceDummy = new MailServiceDummy(1);
-    mailServiceDummy.init();
-    System.out.println("I win in SecondConfig");
-    return mailServiceDummy;
-  }
+
 
 }
