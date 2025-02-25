328ffb90daca9fc00b7c4ba2fc5fe1b1436313f1
Victor Rentea <victorrentea@gmail.com>
2025-02-25 11:25:50 +0200

    manual-written proxy

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -2,18 +2,39 @@ package victor.training.spring.aspects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import static java.lang.System.currentTimeMillis;
 
 public class ProxyIntro {
   public static void main(String[] args) {
     // WE play the role of Spring here ...
     Maths maths = new Maths();
-    SecondGrade secondGrade = new SecondGrade(maths);
+    var decorator = new LoggingDecorator(maths);
+    SecondGrade secondGrade = new SecondGrade(decorator);
     secondGrade.mathClass();
   }
 }
-// Requirement: log the arguments of Maths#sum() and Maths#product() methods
+// this is how class-proxies (the normal ones) work in Spring
+// 💡 class with the same "interface" that will log the args before delegating the call to the real methods in Maths
+class LoggingDecorator extends Maths {
+  private static final Logger log = LoggerFactory.getLogger(LoggingDecorator.class);
+  private final Maths maths;
+  public LoggingDecorator(Maths maths) {
+    this.maths = maths;
+  }
+  public int sum(int a, int b) {
+    log.info("sum({},{})", a, b);
+    return maths.sum(a, b);
+  }
+  public int product(int a, int b) {
+    log.info("product({},{})", a, b);
+    return maths.product(a, b);
+  }
+}
+// Log the arguments of Maths#sum() and Maths#product() methods without changing any code below this line
 // ------------------- LINE ------------------
 class SecondGrade {
   private final Maths maths;
@@ -27,14 +48,10 @@ class SecondGrade {
   }
 }
 class Maths {
-  private static final Logger log = LoggerFactory.getLogger(Maths.class);
-
   public int sum(int a, int b) {
-    log.info("sum ({},{})", a, b);
     return a + b;
   }
   public int product(int a, int b) {
-    log.info("product ({},{})", a, b);
     return a * b;
   }
 }
