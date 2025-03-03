6dd8ad982ddf210dd4ae18e0f12b1ddfec85211d
Victor Rentea <victorrentea@gmail.com>
2025-03-03 12:26:35 +0200

    @Async + @Scheduled

--- spring-app/src/main/java/victor/training/spring/async/AsyncConfig.java
+++ spring-app/src/main/java/victor/training/spring/async/AsyncConfig.java
@@ -2,29 +2,40 @@ package victor.training.spring.async;
 
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.Metrics;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskDecorator;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @EnableAsync
+@EnableScheduling
 @Configuration
 public class AsyncConfig {
+//	@Bean
+//	public ThreadPoolTaskExecutor poolBar(@Value("${bar.pool.size}") int size) {
 	@Bean
-	public ThreadPoolTaskExecutor poolBar(TaskDecorator taskDecorator) {
+	@ConfigurationProperties(prefix = "bar.pool")
+	public ThreadPoolTaskExecutor poolBar() {
 		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(1);
-		executor.setMaxPoolSize(1);
+//		executor.setCorePoolSize(size);
+//		executor.setMaxPoolSize(size); // # of threads
 		executor.setQueueCapacity(500);
 		executor.setThreadNamePrefix("pool-bar-");
 		executor.initialize();
-		executor.setWaitForTasksToCompleteOnShutdown(true);
-		// copy MDC from parent thread to workerthread
+		executor.setWaitForTasksToCompleteOnShutdown(true); // !!!! must-have!
+
+		// copy MDC from parent thread to worker thread (eg: traceId)
 		executor.setTaskDecorator(new CopyMDCToWorker());
 
-		Gauge.builder( "poolbar.pool.size", executor::getPoolSize).register(Metrics.globalRegistry);
-		Gauge.builder( "poolbar.queue.size", executor::getQueueSize).register(Metrics.globalRegistry);
+		Gauge.builder( "poolbar_pool_size2", executor::getPoolSize).register(Metrics.globalRegistry);
+		Gauge.builder( "poolbar_queue_size", executor::getQueueSize).register(Metrics.globalRegistry);
+		// Usges:
+		// - raise alarm if queue size > 400
+		// - the instance could declare itself readyness => false
 		return executor;
 	}
 
--- spring-app/src/main/java/victor/training/spring/async/BarApi.java
+++ spring-app/src/main/java/victor/training/spring/async/BarApi.java
@@ -1,43 +1,83 @@
 package victor.training.spring.async;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import victor.training.spring.async.drinks.Beer;
 import victor.training.spring.async.drinks.DillyDilly;
 import victor.training.spring.async.drinks.Vodka;
 
-import javax.print.Doc;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
 
 import static java.lang.System.currentTimeMillis;
 
 @Slf4j
 @RestController
-@RequiredArgsConstructor
 public class BarApi {
-   private final Barman barman;
+  private final Barman barman;
+  private final ThreadPoolTaskExecutor poolBar;
 
-   @GetMapping("api/drink")
-   public DillyDilly drink() throws Exception {
-      log.debug("Submitting my order");
-      long t0 = currentTimeMillis();
+  public BarApi(Barman barman, ThreadPoolTaskExecutor poolBar) {
+    this.barman = barman;
+    this.poolBar = poolBar;
+  }
 
-      Beer beer = barman.pourBeer();
-      Vodka vodka = barman.pourVodka();
-      // #1 sequential calls that are independent
+  @GetMapping("api/drink")
+  public DillyDilly drink() throws Exception {
+    log.debug("Submitting my order");
+    long t0 = currentTimeMillis();
 
-      // #2: why do I wait for email to be sent? > run in back ground
-      // #3: failures on Email server are not critical
-      barman.sendNotification("Dilly");
+    Future<Beer> beerFuture = poolBar.submit(() -> barman.pourBeer());
+    Future<Vodka> vodkaFuture = poolBar.submit(() -> barman.pourVodka());
 
-      //a) Kafka/Rabbit send instead 💖
-      //b) store this notification in a db (outbox table pattern) to poll afterwards
+    Beer beer = beerFuture.get();// blocking
+    Vodka vodka = vodkaFuture.get();// blocking
 
-      log.debug("HTTP thread released in {} millis", currentTimeMillis() - t0);
-      return new DillyDilly(beer, vodka);
-   }
+    // #1 sequential calls that are independent
+    // WebFlux: mono1.zipWith(mono2, (beer, vodka) -> new DillyDilly(beer, vodka))
+
+    // #2: why do I wait for email to be sent? > run in back ground
+    // #3: failures on Email server are not critical
+    // Solution#1: "Fire and forget" [in memory] with:
+    //1) threadPool.submit
+    //2) CompletableFuture.runAsync(() -> barman.sendNotification("Dilly"), poolBar); 💖
+    barman.sendNotification("Dilly"); // call an @Async annotated method
+    //4) WebFlux: someService.reactiveMethod(..).subscribe();
+    // !!! check that you propagate request metadata (traceId, securityContext)
+
+
+    // !!! how to manage background errors ? scrap log, alarms,
+    // !!! what if my server crashes with items in the waiting queue?
+    // Fix:
+    // Solution#2: "Fire and forget" persisted
+    //a) Kafka/Rabbit send instead 💖 (durable queue)
+    //b) store this notification in a db (outbox table pattern) to poll afterwards every 5 sec (eg)
+
+    log.debug("HTTP thread released in {} millis", currentTimeMillis() - t0);
+    return new DillyDilly(beer, vodka);
+  }
+
+
+//  @Scheduled(cron = "0/5 * * * * *")// every 5 seconds cron
+  @Scheduled(fixedRateString = "${outbox.poll.ms}")// adjust rate via config
+  public void method() { // runs every 5 sec
+    // 1) what if it takes > 5 secods (rate): they will NEVER overlap?
+    // 2) two instance (pods) of this app will race eachother: scheduled will run on both.
+    // fix:
+    // a) enable scheduled on only ONE instance @Profile or @ConditionalOnProperty = risky
+    // b) distributed locking 💖(Redis locks, SQL-level locks: SELECT FOR UPDATE) @ShedulerLock// https://www.baeldung.com/shedlock-spring
+    // c) introduce random skew -@sander wants to take chances. risky
+    // d) SELECT from INBOX where id % 2 = 0: SPOF + autoscale=hell
+    // e) replace @Scheduler with @GetMapping/@Kafka|RabbitListener and have an external cron job fire a call/message
+    //    this call will be load balanced by the gateway and will run only on one instance
+    //    easier to test/manual call💖💖💖
+    log.info("Looking in the OUTBOX table for notifications to POST to the email server");
+  }
+
 
 //   @PostMapping
 //   public Mono<Void> saveToDb() {
