14c6d0be8eeeb6ce8db1491442c74d8ed51ede6f
Victor Rentea <victorrentea@gmail.com>
2025-07-14 10:39:39 +0300

    component scan

---
+++ spring-start/src/main/java/victor/training/spring/altu/X.java
@@ -0,0 +1,25 @@
+package victor.training.spring.altu;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import victor.training.spring.first.Y;
+
+//@RestController
+//class C {}
+//@Controller// pe vremuri se scria HTML in server .jsp, .jsf, Vaadin
+//@RestController // @GetMapping api REST
+@Service // logica de business
+//@Repository // acces la DB
+
+//@Component // = nimic de mai sus; respo tehnice: filtru http, interceptor, aspect
+
+//@Configuration // defineste @Bean
+public class X {
+  // un bean = o instanta managed by Spring din clasa ta
+  @Autowired
+  private Y y;
+
+  public int logic() {
+    return 1 + y.logic();
+  }
+}
--- spring-start/src/main/java/victor/training/spring/first/FirstApplication.java
+++ spring-start/src/main/java/victor/training/spring/first/FirstApplication.java
@@ -5,15 +5,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.event.EventListener;
+import victor.training.spring.altu.X;
 
-@SpringBootApplication
+@ComponentScan("victor.training.spring")
+@SpringBootApplication // defineste o app spring boot
 public class FirstApplication implements CommandLineRunner {
   public static void main(String[] args) {
     SpringApplication.run(FirstApplication.class);
   }
 
-  @Autowired
+  @Autowired // springule, da-mi o instanta de X
   private X x;
 
   @EventListener(ApplicationReadyEvent.class)
