a835650c3ee16237b5c2d70edd54c3ebf74e5dc3
Victor Rentea <victorrentea@gmail.com>
2025-09-16 15:57:36 +0300

    AOP facut de mana la coada vacii

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -1,29 +1,52 @@
 package victor.training.spring.aspects;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 public class ProxyIntro {
   public static void main(String[] args) {
-    // WE play the role of Spring here ...
-    Maths maths = new Maths();
-    SecondGrade secondGrade = new SecondGrade(maths);
+    Maths real = new Maths();
+    Maths decorator = new MathsDecorator(real);
+    SecondGrade secondGrade = new SecondGrade(decorator);
     secondGrade.mathClass();
   }
 }
+class MathsDecorator extends Maths {
+  private final Maths delegate;
+
+  MathsDecorator(Maths delegate) {
+    this.delegate = delegate;
+  }
+
+  @Override
+  public int sum(int a, int b) {
+    System.out.println("Calling sum with params: " + a + ", " + b);
+    return delegate.sum(a, b);
+  }
+
+  @Override
+  public int product(int a, int b) {
+    System.out.println("Calling product with params: " + a + ", " + b);
+    return delegate.product(a, b);
+  }
+}
+// TODO: printati param si val returnata de orice metoda chemata din Maths de catre SecondGrade
+//  FARA SA MODIFICI CODUL SUB LINIE
 // ------------------- LINE ------------------
 @Service
+@RequiredArgsConstructor
 class SecondGrade {
   private final Maths maths;
-  SecondGrade(Maths maths) {
-    this.maths = maths;
-  }
   public void mathClass() {
+    System.out.println("Oare cu cine vorbesc??? " + maths.getClass());
     System.out.println("8 + 4 = " + maths.sum(8, 4));
     System.out.println("6 + 6 = " + maths.sum(6, 6));
     System.out.println("4 x 3 = " + maths.product(4, 3));
   }
 }
+@Slf4j
 @Component
 class Maths {
   public int sum(int a, int b) {
