ade413becc1bbe259534725e93bc4c1d78d9d532
Victor Rentea <victorrentea@gmail.com>
2025-09-16 15:46:44 +0300

    Add AOP exercise

---
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxySpringApp.java
@@ -0,0 +1,29 @@
+package victor.training.spring.aspects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+public class ProxySpringApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ProxySpringApp.class, args);
+    }
+
+    @Autowired
+    private SecondGrade secondGrade;
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void run() {
+        System.out.println("Running Maths class...");
+        secondGrade.mathClass();
+    }
+}
+//AI Prompts:
+//1. I have a Spring Boot 3 application. Write an aspect that intercepts all method calls to any class in package "victor.training.spring.aspects" and log the method name and parameters. Assume I have all the necessary dependencies in the pom.xml.
+//2. Change the aspect to target only method annotated with a custom annotation
+//3. If I annotate the class, all methods should be intercepted.
+//4. Write another aspect to measure the execution time of these methods.
+// sample: https://chatgpt.com/share/3cdeb17b-ce60-4c11-a9b1-76af8cd929e1
