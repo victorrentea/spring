1783239c21b842edc9aca3555aba72540bf36e1b
Victor Rentea <victorrentea@gmail.com>
2025-10-10 11:29:40 +0300

    how to mess with proxies, how to STOP magic from working by accident

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -38,31 +38,51 @@ public class ProxyIntro {
 //  @Override
 //  public int product(int a, int b) {
 //    System.out.println("product called with " + a + " and " + b);
-//    return delegate.product(a, b);
+//    return delegate.product(a, b); // WHAT SPRING DOES
+//    return this.product(a, b); // NOT WHAT SPRING DOES
 //  }
 //}
 // whenever SecondGrade calls sum() or product(), print the params, without changing any code below the line
 // ------------------- LINE ------------------
-// Change something below this line to stop proxies from working
 class SecondGrade {
   private final Maths maths;
   SecondGrade(Maths maths) {
     this.maths = maths;
   }
   public void mathClass() {
+//    Maths maths = new Maths();😶  new MyImmutable, new MyRecord, new Dto, new JooqRecord
     System.out.println("What maths class did Spring injected me here? " + maths.getClass());
     System.out.println("8 + 4 = " + maths.sum(8, 4));
     System.out.println("6 + 6 = " + maths.sum(6, 6));
     System.out.println("4 x 3 = " + maths.product(4, 3));
-    System.out.println("4 x 4 = " + maths.product(4, 4));
+    System.out.println("4 x 4 = " + maths.curveIntegral(4, 4));
   }
 }
+// Edit some code below this line that will stop the proxy from working
+/*final🚫*/
+/*record🚫*/
 class Maths {
-  public int sum(int a, int b) {
+  // @Async starts the method on another thread
+  // @Timed/@Observed // expose a metric on how long this method took
+  // @Transactional commit all SQL interactions of this method ATOMICALLY
+  // @Secured(ROLE_ADMIN) // only allow call if your user does have ADMIN ROLE
+   // ✅ called from OUTSIDE of this class works
+   // 🚫 called from INSIDE of this class does not work- allows ANY role to call it
+  public /*final😶*/ int sum(int a, int b) {
     return a + b;
   }
-  public int product(int a, int b) {
-    return a * b;
+  public /*static😶*/ int product(int a, int b) {
+//    return a * b;
+    System.out.println("What class am I now in ? " + this.getClass());
+    int result = 0;
+    for (int i = 0; i < a; i++) {
+      result = sum(result, b); // 👑THE BIGGEST PITFALL OF SPRING!
+      // local method calls don't get INTERCEPTED ❌❌❌❌
+    }
+    return result;
+  }
+  public int curveIntegral(int a, int b) {
+    return 42;
   }
 }
 
