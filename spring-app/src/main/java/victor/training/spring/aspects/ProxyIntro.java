e4f58e2a3709e526a670e35c370d137f5e4c4a2a
Victor Rentea <victorrentea@gmail.com>
2025-10-13 13:46:12 +0300

    #ship #shit!

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -1,18 +1,34 @@
 package victor.training.spring.aspects;
 
+import io.micrometer.core.annotation.Timed;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 public class ProxyIntro {
   public static void main(String[] args) {
-    // WE play the role of Spring here ...
-    Maths maths = new Maths();
+    Maths maths = new NamingIsHardCaIncaHabarNAmCeFacAici_Maths();
     SecondGrade secondGrade = new SecondGrade(maths);
     secondGrade.mathClass();
   }
 }
+class NamingIsHardCaIncaHabarNAmCeFacAici_Maths extends Maths {
+  @Override
+  public int sum(int a, int b) {
+    System.out.println("sum called with " + a + " and " + b);
+    return super.sum(a, b);
+  }
+  @Override
+  public int product(int a, int b) {
+    System.out.println("product called with " + a + " and " + b);
+    return super.product(a, b);
+  }
+}
+// logeaza param primiti de metodele din Maths fara sa modifici cod sub linie
+// sau: ti-e #rusine sa copii cod de 7 ori
 // ------------------- LINE ------------------
-@Service
 class SecondGrade {
   private final Maths maths;
   SecondGrade(Maths maths) {
@@ -24,7 +40,6 @@ class SecondGrade {
     System.out.println("4 x 3 = " + maths.product(4, 3));
   }
 }
-@Component
 class Maths {
   public int sum(int a, int b) {
     return a + b;
