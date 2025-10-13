2064dbac07a0901dd4729a1a327a99f29abc09d1
Victor Rentea <victorrentea@gmail.com>
2025-10-13 13:50:44 +0300

    proxyuri facute de mana, ca sa intelegi ce face Spring pe sub

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -1,29 +1,27 @@
 package victor.training.spring.aspects;
 
-import io.micrometer.core.annotation.Timed;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
 public class ProxyIntro {
   public static void main(String[] args) {
-    Maths maths = new NamingIsHardCaIncaHabarNAmCeFacAici_Maths();
+    Maths real = new Maths();
+    Maths maths = new LoggingProxy(real);
     SecondGrade secondGrade = new SecondGrade(maths);
     secondGrade.mathClass();
   }
 }
-class NamingIsHardCaIncaHabarNAmCeFacAici_Maths extends Maths {
+class LoggingProxy extends Maths {
+  private final Maths delegate;
+  public LoggingProxy(Maths delegate) {
+    this.delegate = delegate;
+  }
   @Override
   public int sum(int a, int b) {
     System.out.println("sum called with " + a + " and " + b);
-    return super.sum(a, b);
+    return delegate.sum(a, b);
   }
   @Override
   public int product(int a, int b) {
     System.out.println("product called with " + a + " and " + b);
-    return super.product(a, b);
+    return delegate.product(a, b);
   }
 }
 // logeaza param primiti de metodele din Maths fara sa modifici cod sub linie
