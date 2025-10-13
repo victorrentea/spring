31bb80e6e8d5b6f663ea589c3dc6534e04f1ec91
Victor Rentea <victorrentea@gmail.com>
2025-10-13 13:55:09 +0300

    cica au inteles.

--- spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
+++ spring-app/src/main/java/victor/training/spring/aspects/ProxyIntro.java
@@ -1,29 +1,45 @@
 package victor.training.spring.aspects;
 
+import org.springframework.cglib.proxy.Callback;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class ProxyIntro {
   public static void main(String[] args) {
     Maths real = new Maths();
-    Maths maths = new LoggingProxy(real);
+//    Maths maths = new LoggingProxy(real);
+    Callback handler = new MethodInterceptor() {
+      @Override
+      public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+        System.out.println(method.getName() + " called with " + Arrays.toString(args));
+        return method.invoke(real, args);
+      }
+    };
+    Maths maths = (Maths) Enhancer.create(Maths.class, handler); //scrie codul scris de noi dar dinamic la runtime
     SecondGrade secondGrade = new SecondGrade(maths);
     secondGrade.mathClass();
   }
 }
-class LoggingProxy extends Maths {
-  private final Maths delegate;
-  public LoggingProxy(Maths delegate) {
-    this.delegate = delegate;
-  }
-  @Override
-  public int sum(int a, int b) {
-    System.out.println("sum called with " + a + " and " + b);
-    return delegate.sum(a, b);
-  }
-  @Override
-  public int product(int a, int b) {
-    System.out.println("product called with " + a + " and " + b);
-    return delegate.product(a, b);
-  }
-}
+//class LoggingProxy extends Maths {
+//  private final Maths delegate;
+//  public LoggingProxy(Maths delegate) {
+//    this.delegate = delegate;
+//  }
+//  @Override
+//  public int sum(int a, int b) {
+//    System.out.println("sum called with " + a + " and " + b);
+//    return delegate.sum(a, b);
+//  }
+//  @Override
+//  public int product(int a, int b) {
+//    System.out.println("product called with " + a + " and " + b);
+//    return delegate.product(a, b);
+//  }
+//}
 // logeaza param primiti de metodele din Maths fara sa modifici cod sub linie
 // sau: ti-e #rusine sa copii cod de 7 ori
 // ------------------- LINE ------------------
@@ -34,7 +50,7 @@ class SecondGrade {
   }
   public void mathClass() {
     System.out.println("8 + 4 = " + maths.sum(8, 4));
-    System.out.println("6 + 6 = " + maths.sum(6, 6));
+    System.out.println("6 + 7 = " + maths.sum(6, 7));
     System.out.println("4 x 3 = " + maths.product(4, 3));
   }
 }
