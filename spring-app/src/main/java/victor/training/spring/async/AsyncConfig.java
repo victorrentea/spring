45d5740d570f94d90cfcb16ff89177e6cf0a895f
Victor Rentea <victorrentea@gmail.com>
2025-10-17 11:05:54 +0300

    scheduled

--- spring-app/src/main/java/victor/training/spring/async/AsyncConfig.java
+++ spring-app/src/main/java/victor/training/spring/async/AsyncConfig.java
@@ -6,10 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskDecorator;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @EnableAsync
 @Configuration
+@EnableScheduling
 public class AsyncConfig {
 	@Bean
 	public ThreadPoolTaskExecutor poolBar() {
---
+++ spring-app/src/main/java/victor/training/spring/async/ReloadClientCache.java
@@ -0,0 +1,16 @@
+package victor.training.spring.async;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReloadClientCache {
+
+//  @Scheduled(cron = "0 */5 * * * *")
+  @Scheduled( fixedRateString = "1000") //spring drives
+
+  // @PutMapping =REST api called via a curl command by cron job in OS
+  public void pullsAllClientData() {
+    System.out.println("Downloading to refresh my cache");
+  }
+}
