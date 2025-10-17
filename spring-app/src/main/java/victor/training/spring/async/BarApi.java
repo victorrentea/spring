daf8cf2c423ed29894b78f66487f0f504959ba73
Victor Rentea <victorrentea@gmail.com>
2025-10-17 09:56:53 +0300

    multithreading with spring-web

--- spring-app/src/main/java/victor/training/spring/async/BarApi.java
+++ spring-app/src/main/java/victor/training/spring/async/BarApi.java
@@ -2,31 +2,38 @@ package victor.training.spring.async;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import victor.training.spring.async.drinks.Beer;
 import victor.training.spring.async.drinks.DillyDilly;
 import victor.training.spring.async.drinks.Vodka;
 
+import java.util.concurrent.CompletableFuture;
+
 import static java.lang.System.currentTimeMillis;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class BarApi {
-   private final Barman barman;
+  private final Barman barman;
+  private final ThreadPoolTaskExecutor poolBar;
 
-   @GetMapping("api/drink")
-   public DillyDilly drink() throws Exception {
-      log.debug("Submitting my order");
-      long t0 = currentTimeMillis();
+  @GetMapping("api/drink")
+  public DillyDilly drink() throws Exception {
+    log.debug("Submitting my order");
+    long t0 = currentTimeMillis();
 
-      Beer beer = barman.pourBeer();
-      Vodka vodka = barman.pourVodka();
+    CompletableFuture<Beer> beerFuture = supplyAsync(() -> barman.pourBeer(), poolBar); // 1s
+    Vodka vodka = barman.pourVodka(); // 1s
+    Beer beer = beerFuture.get();// blocks the black = 0s
 
-      barman.sendNotification("Dilly");
+    barman.sendNotification("Dilly"); // 0.5s // takes time and can fail
 
-      log.debug("HTTP thread released in {} millis", currentTimeMillis() - t0);
-      return new DillyDilly(beer, vodka);
-   }
+    log.debug("HTTP thread released in {} millis", currentTimeMillis() - t0);
+    return new DillyDilly(beer, vodka);
+  }
 }
