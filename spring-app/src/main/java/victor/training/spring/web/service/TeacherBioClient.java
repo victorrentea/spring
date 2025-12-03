18dec8c23c542c87f0c61cef4cb3b737b3f5ada5
Victor Rentea <victorrentea@gmail.com>
2025-12-04 00:54:16 +0200

    cleanup

--- spring-app/src/main/java/victor/training/spring/web/service/TeacherBioClient.java
+++ spring-app/src/main/java/victor/training/spring/web/service/TeacherBioClient.java
@@ -26,17 +26,14 @@ public class TeacherBioClient {
   private final String teacherBioUriBase;
 
   private final TeacherBioFeignClient feignClient;
-
-  // don't do "new RestTemplate()" but take it from Spring, to allow Sleuth to propagate 'traceId'
   private final RestTemplate rest;
-
 
 
   // TODO cacheable
   public String retrieveBiographyForTeacher(long teacherId) {
     log.debug("Calling external web endpoint... (takes time)");
 //    String result = dummyCall(teacherId);
-    String result = callUsingFeignClient(teacherId);
+    String result = feignClient.getTeacherBio(teacherId);
 //    String result = callUsingRestTemplate(teacherId);
     log.debug("Got result");
     return result;
@@ -48,39 +45,13 @@ public class TeacherBioClient {
   }
 
   @SneakyThrows
-  public String callUsingFeignClient(long teacherId) {
-    return feignClient.registerSheep(teacherId);
-  }
-
-  @SneakyThrows
-  public String callUsingRestTemplate(long teacherId) {
-
-    // Auth#1 :) - no bearer
-//    String bearerToken = "joke";
-// TODO FIX migration
-    // Auth#2 - propagating MY clients' AccessToken
-//    KeycloakPrincipal<KeycloakSecurityContext> principal =
-//            (KeycloakPrincipal<KeycloakSecurityContext>)
-//            SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//    String accessToken = principal.getKeycloakSecurityContext().getTokenString();
-
-    // Auth#3 - manually creating a JWT token
-    //          String bearerToken = Jwts.builder()
-    //                .setSubject(SecurityContextHolder.getContext().getAuthentication().getName())
-    //                .claim("country", "Country")
-    //                .signWith(SignatureAlgorithm.HS512, jwtSecret)
-    //                .compact();
-//    log.info("Sending bearer: {}", accessToken);
-//    Map<String, List<String>> header = Map.of("Authorization", List.of("Bearer " + accessToken));
-
-
-    Map<String, List<String>> header = Map.of();
+  private String callUsingRestTemplate(long teacherId) {
+    Map<String, List<String>> headerMap = Map.of();
     ResponseEntity<String> response = rest.exchange(new RequestEntity<>(
-            CollectionUtils.toMultiValueMap(header),
-            HttpMethod.GET,
-            new URI(teacherBioUriBase + "/api/teachers/" + teacherId + "/bio")), String.class);
+        CollectionUtils.toMultiValueMap(headerMap),
+        HttpMethod.GET,
+        new URI(teacherBioUriBase + "/api/teachers/" + teacherId + "/bio")), String.class);
     return response.getBody();
   }
-
 }
 
--- spring-app/src/main/java/victor/training/spring/web/service/TeacherBioFeignClient.java
+++ spring-app/src/main/java/victor/training/spring/web/service/TeacherBioFeignClient.java
@@ -7,5 +7,5 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "teacherBioServices", url="${teacher.bio.uri.base}/api")
 interface TeacherBioFeignClient {
     @GetMapping("teachers/{teacherId}/bio")
-    String registerSheep(@PathVariable("teacherId") long teacherId);
+    String getTeacherBio(@PathVariable long teacherId);
 }
