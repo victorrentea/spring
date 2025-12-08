ff27448606f3a4418ef60edf276c80bbdb08cdb2
Victor Rentea <victorrentea@gmail.com>
2025-12-08 10:26:39 +0200

    daca numele punctului de injectie = numele beanului (numele clasei cu prima litera mica)
    cu @Qualifier

--- spring-start/src/main/java/victor/training/spring/first/Y.java
+++ spring-start/src/main/java/victor/training/spring/first/Y.java
@@ -1,18 +1,20 @@
 package victor.training.spring.first;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Y {
   @Autowired
-  private MailService mailServiceDummy; // polymorphic injection
+  @Qualifier("mailServiceImpl")
+  private MailService mailService; // polymorphic injection
   @Value("${props.gate}")
   private Integer gate;
 
   public int logic() {
-    mailServiceDummy.sendEmail("Go to gate " + gate);
+    mailService.sendEmail("Go to gate " + gate);
 
     return 1;
   }
