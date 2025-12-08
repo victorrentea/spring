ad306c5b63af6f05305167020eaaf178c197049c
Victor Rentea <victorrentea@gmail.com>
2025-12-08 09:57:01 +0200

    #asa da

--- spring-start/src/main/java/victor/training/spring/first/X.java
+++ spring-start/src/main/java/victor/training/spring/first/X.java
@@ -1,14 +1,22 @@
 package victor.training.spring.first;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-@Service
+@Slf4j
+@Service // clasa devine bean = obiect instantiat, config, manipulat de Spring
+@RequiredArgsConstructor // genereaza cod la javac
 public class X {
-  @Autowired
-  private Y y;
-
+// Dependency Injection = la startup, Spring cauta un bean de tip Y si il injecteaza aici
+//  @Autowired // #1 field injection folosita doar in teste
+//  private Y y;
+  private final Y y; // #2 constructor injection (recomandat + Lombok❤️)
+//  public X(Y y) {
+//    this.y = y;
+//  }
   public int logic() {
+    log.info("Ceva");
     return 1 + y.logic();
   }
 }
