40ee917976f4a7248a8c5bd6c151c80624dc5950
Victor Rentea <victorrentea@gmail.com>
2025-12-08 10:21:55 +0200

    2 candidati la 1 punct de injectie = app nu porneste

--- spring-start/src/main/java/victor/training/spring/first/MailServiceDummy.java
+++ spring-start/src/main/java/victor/training/spring/first/MailServiceDummy.java
@@ -1,6 +1,8 @@
 package victor.training.spring.first;
 
-// TODO use for local dev only
+import org.springframework.stereotype.Component;
+
+@Component
 public class MailServiceDummy implements MailService {
   public void sendEmail(String subject) {
     System.out.println("MailServiceDummy: faking send of " + subject);
