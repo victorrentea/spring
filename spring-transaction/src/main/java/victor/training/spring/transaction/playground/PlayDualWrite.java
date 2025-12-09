c5138185bc74b2edd2dead88bf5c8a743d570bdc
Victor Rentea <victorrentea@gmail.com>
2025-12-09 14:42:35 +0200

    TOT nu esti 100% robust.

--- spring-transaction/src/main/java/victor/training/spring/transaction/playground/PlayDualWrite.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/playground/PlayDualWrite.java
@@ -3,7 +3,6 @@ package victor.training.spring.transaction.playground;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
@@ -22,22 +21,28 @@ public class PlayDualWrite {
         repo.save(new MyEntity("E22"));
         repo.flush();
         //send(new MyMessage("M")); // ❌ => ex => rollback
-        applicationEventPublisher.publishEvent(new MyMessage("M"));
+        applicationEventPublisher.publishEvent(new ProcessPayment("M"));
     }
     // ruleaza dupa COMMITul tx din care s-a facut .publishEvent
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void afterCommit(MyMessage message) {
-        send(message);
+    public void afterCommit(ProcessPayment message) {
+        try {
+            send(message); // timeout dpa 100m
+        } catch (Exception e) {
+            // k8s kill/redeploy/OOME💥
+            // COMPENSEZI cu o a 2a tranzactie care da pa dos pa prima comisa deja
+            // "DILESTI CE INSERASHI"
+        }
     }
 // INSERT + @NotNull @Size
 // INSERT
 // COMMIT ❌ FK/PK/UK💥
-// SEND KAFKA/RABBIT/PUB-SUB ✅ nu prea pica=> il faci ultimul
+// SEND KAFKA/RABBIT/PUB-SUB ✅ nu *prea* pica=> il faci ultimul
 
-    public record MyMessage(String content) {
+    public record ProcessPayment(String content) {
     }
 
-    private void send(MyMessage message) {
+    private void send(ProcessPayment message) {
         log.info("kafkaTemplate.send(" + message + ") pretend");
         log.info("restTemplate.post(" + message + ") pretend API CALL");
 //    if (Math.random() < .5) throw new RuntimeException("BOOM"); // may fail
