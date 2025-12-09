a0eb3175c68319df49a6e8bb75cdf6146c8f9ad4
Victor Rentea <victorrentea@gmail.com>
2025-12-09 10:36:13 +0200

    JPA write behind: dureri la debug

--- spring-transaction/src/main/java/victor/training/spring/transaction/playground/PlayTransactions.java
+++ spring-transaction/src/main/java/victor/training/spring/transaction/playground/PlayTransactions.java
@@ -1,12 +1,11 @@
 package victor.training.spring.transaction.playground;
 
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import jakarta.persistence.EntityManager;
 
 import javax.sql.DataSource;
 import java.io.IOException;
@@ -15,28 +14,28 @@ import java.io.IOException;
 @Service
 @RequiredArgsConstructor
 public class PlayTransactions {
-  private final DataSource dataSource; // since 1998
-  private final JdbcTemplate jdbcTemplate; // since 2001
-  private final EntityManager entityManager; // since 2006
-//    mybatis
-  private final MyEntityRepo repo; // = Spring Data JPA, since 2011
-  private final OtherClass other;
+    private final DataSource dataSource; // since 1998
+    private final JdbcTemplate jdbcTemplate; // since 2001
+    private final EntityManager entityManager; // since 2006
+    //    mybatis
+    private final MyEntityRepo repo; // = Spring Data JPA, since 2011
+    private final OtherClass other;
 
-  @Transactional//#1(rollbackFor = Exception.class)❌
-  public void play() throws IOException {
-      // p6spy iti arata ? concret, conex, timpul query-ului, commit/rollback per conexiune
-    jdbcTemplate.update("insert into MY_ENTITY(id, name) values (100,?)","SQL");
-    if (true) throw new IOException("Boom"); // cauzeaza COMMIT in DB, spre stupoarea tuturor
-      // spring framework a furat developeri/proj de pe EJBullshit💥💥💥 J2EE in 2005< au copiat comportamentul @TransactionAttribute (EJB)
-      // :morala sa nu va prind cu throws <checked> din metode @Transactional ✅✅✅
-    repo.save(new MyEntity("JPA"));
-  }
+    @Transactional
+    public void play() throws IOException {
+        jdbcTemplate.update("insert into MY_ENTITY(id, name) values (100,?)", "SQL");
+        altaMetoda();
+        log.info("Ies din metoda"); // JPA "Write Behind" = INSERTul de la repo.save se face dupa iesire, inainte de COMMIT
+    }
+    private void altaMetoda() {
+        repo.save(new MyEntity("SQL")); // 💥💥💥💥💥
+    }
 }
 
 @Service
 @RequiredArgsConstructor
 class OtherClass {
-  private final MyEntityRepo repo;
+    private final MyEntityRepo repo;
 }
 
 // TODO
