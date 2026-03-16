e4f61c4101b6058532b467dabd634252a5902bfd
Victor Rentea <victorrentea@gmail.com>
2026-03-16 15:43:41 +0200

    quick demo of user-pass login

--- spring-app/src/main/java/victor/training/spring/security/config/GlobalCorsConfig.java
+++ spring-app/src/main/java/victor/training/spring/security/config/GlobalCorsConfig.java
@@ -1,22 +1,23 @@
 package victor.training.spring.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class GlobalCorsConfig {
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/api/**")
-//                        .allowedMethods("*")
-//                        .allowCredentials(true) // allows receiving session cookie (if using cookies)
-//                        .allowedOriginPatterns("http://localhost:8081") // eg NodeJS
-////					 .allowedOriginPatterns("http://*") // Too broad
-//                ;
-//                // also don't forget to add .cors() to spring security
-//            }
-//        };
-//    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedMethods("*")
+                        .allowCredentials(true) // allows receiving session cookie (if using cookies)
+                        .allowedOriginPatterns("http://localhost:4200") // eg or whatever port NodeJS runs on
+                ;
+            }
+        };
+    }
 }
--- spring-app/src/main/java/victor/training/spring/security/config/userpass/UserPassSecurity.java
+++ spring-app/src/main/java/victor/training/spring/security/config/userpass/UserPassSecurity.java
@@ -15,11 +15,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Slf4j
-@Profile("userpass")
-@Configuration
+import java.util.Map;
+
+@Slf4j // lombok
+@Profile("userpass")// not needed in your case
+@Configuration // allows @Bean
 @EnableWebSecurity // (debug = true) // see the filter chain in use
-@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true) // enables the use of @Secured
 public class UserPassSecurity {
   @PostConstruct
   public void hi() {
@@ -30,15 +32,20 @@ public class UserPassSecurity {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.csrf(csrf -> csrf.disable()); // OK since I never take <form> POSTs
 
-    // http.cors(Customizer.withDefaults()); // only if .js files come from a CDN (by default CORS requests get blocked)
+    // you need this to allow JS REST CALLS from .js loaded from node js eg localhost:4200
+     http.cors(Customizer.withDefaults()); // only if .js files come from a CDN (by default CORS requests get blocked)
+    // also see GlobalCorsConfig
 
     http.authorizeHttpRequests(authz -> authz
             .anyRequest().authenticated()
     );
 
-    http.formLogin(Customizer.withDefaults()) // display a login page
+    http.formLogin(Customizer.withDefaults()) // display a login page user+pass
             .userDetailsService(userDetailsService()); // distinguish vs Actuator user/pass
 
+    // this alllow you to call your apis using BasicAuth: Authorization: Basic base64(<user>:<pass>)
+    // curl http://localhost:8080/api/trainings -u user:user
+//    https://en.wikipedia.org/wiki/Basic_access_authentication
     http.httpBasic(Customizer.withDefaults()) // also accept Authorization: Basic ... request header
             .userDetailsService(userDetailsService()); // distinguish vs Actuator user/pass
 
@@ -47,13 +54,23 @@ public class UserPassSecurity {
 
   // *** Dummy users with plain text passwords - NEVER USE IN PRODUCTION
   @Bean
-  public UserDetailsService userDetailsService() {
-    UserDetails user = User.withDefaultPasswordEncoder()
-            .username("user").password("user").roles("USER").build();
+  public UserDetailsService userDetailsService(/*@Value("${user.to.pass}") Map<String, String > map*/) {
+    UserDetails user = User.builder()
+//            .username("user").password("user").roles("USER").build();
+            .username("user")
+        // for the demo https://bcrypt-generator.com/ generates for "user" the following string
+        // NEXT STEP: load a set of user/{bcrypt}pass from properties / DB
+            .password("{bcrypt}$2a$12$xidzKA7Elsd3Vo6tFtr5Oeeq7j.WbQulG2SHpVldFeRgkY4ty6f8u")
+            .roles("USER")
+        .build();
     UserDetails admin = User.withDefaultPasswordEncoder()
             .username("admin").password("admin").roles("ADMIN").build();
     UserDetails power = User.withDefaultPasswordEncoder()
             .username("power").password("power").roles("POWER").build();
+
+    //As an impressive add-on, it would be to load the coarse-grained role from the database,
+    // and after loading it, expand it to find fine-grained privileges in the same spirit that
+    // Picnic Co does. JdbcUserDetailsManagers
     return new InMemoryUserDetailsManager(user, admin, power);
   }
 
